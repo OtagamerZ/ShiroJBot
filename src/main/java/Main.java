85b15014efa21086189ae9a4bebd8103d90dbe37
KuuHaKu <yagogimenez1@hotmail.com>
2019-05-08 15:35:19 -0300

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.ReconnectedEvent;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -11,6 +15,20 @@ import java.time.format.DateTimeFormatter;
 public class Main extends ListenerAdapter {
     private JDA bot;
     private String prefix;
+    private User owner;
+    private TextChannel canalbv, canalav;
+
+    private void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    private void setCanalbv(TextChannel canalbv) {
+        this.canalbv = canalbv;
+    }
+
+    private void setCanalav(TextChannel canalav) {
+        this.canalav = canalav;
+    }
 
     public static void main(String[] args) throws LoginException {
         JDABuilder jda = new JDABuilder(AccountType.BOT);
@@ -25,6 +43,12 @@ public class Main extends ListenerAdapter {
         System.out.println("Estou pronta!");
         this.bot = event.getJDA();
         this.prefix = "!";
+        this.owner = bot.getUserById("350836145921327115");
+    }
+
+    @Override
+    public void onReconnect(ReconnectedEvent event) {
+        System.out.println("Voltei!");
     }
 
     @Override
@@ -38,11 +62,72 @@ public class Main extends ListenerAdapter {
         if (cmd[0].equals(prefix + "ping")) {
             message.getChannel().sendMessage("Pong! :ping_pong: " + bot.getPing() + " ms").queue();
         } else if (cmd[0].equals(prefix + "bug")) {
-            bot.getUserById("350836145921327115").openPrivateChannel().queue(channel -> channel.
+            owner.openPrivateChannel().queue(channel -> channel.
                     sendMessage(message.getMessage().getCreationTime().minusHours(3).format(DateTimeFormatter.ofPattern("dd/MM/yyyy (HH:mm)")) + " | " + message.getAuthor().getName() + "#" + message.getAuthor().getDiscriminator() + "(" +
                             message.getGuild().getName() + ")" + " enviou um bug: `" + String.join(" ", message.getMessage().getContentRaw().split(prefix + "bug ")).trim() + "`").queue());
         } else if (cmd[0].equals(prefix + "uptime")) {
-            message.getChannel().sendMessage("Hummm...acho que estou acordada a ");
+            message.getChannel().sendMessage("Hummm...acho que estou acordada a ").queue();
+        } else if (cmd[0].equals(prefix + "ajuda")) {
+            message.getAuthor().openPrivateChannel().queue(channel -> channel.sendMessage("__**Precisa de ajuda? Aqui estou eu!**__\n").queue());
+            message.getAuthor().openPrivateChannel().queue(channel -> channel.sendMessage(Embeds.helpEmbed(prefix)).queue());
+            message.getAuthor().openPrivateChannel().queue(channel -> channel.sendMessage("Precisa de mais ajuda? Fale com meu Nii-chan " + owner.getAsMention() + " ou venha para nosso servidor de suporte: https://discord.gg/HpuF3Vr").queue());
+        } else if (cmd[0].equals(prefix + "prefixo")) {
+            message.getChannel().sendMessage("Estou atualmente respondendo comandos que come\u00e7am com __**" + prefix + "**__").queue();
+        }
+
+        //OWNER--------------------------------------------------------------------------------->
+
+        if (message.getAuthor() == owner) {
+            if (cmd[0].equals(prefix + "kill")) {
+                message.getChannel().sendMessage("Sayonara, Nii-chan!").queue();
+                System.exit(0);
+            } else if (cmd[0].equals(prefix + "servers")) {
+                message.getChannel().sendMessage("Servidores que participo:\n" + Owner.getServers(bot)).queue();
+            } else if (cmd[0].equals(prefix + "broadcast")) {
+                Owner.broadcast(bot, String.join("", message.getMessage().getContentRaw().split(prefix + "broadcast")), message.getTextChannel());
+            }
+        }
+
+        ////ADMIN--------------------------------------------------------------------------------->
+
+        if (message.getMember().hasPermission(Permission.MANAGE_SERVER)) {
+            if (cmd[0].equals(prefix + "definir")) {
+                try {
+                    switch (cmd[1]) {
+                        case "canalbv":
+                            try {
+                                setCanalbv(message.getMessage().getMentionedChannels().get(0));
+                                message.getChannel().sendMessage("Canal de boas-vindas trocado para " + canalbv.getAsMention()).queue();
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                message.getChannel().sendMessage("E qual canal devo usar para mensagens de boas-vindas? N\u00falo n\u00e3o \u00e9 um canal v\u00e1lido!").queue();
+                            }
+                            break;
+                        case "canalav":
+                            try {
+                                setCanalav(message.getMessage().getMentionedChannels().get(0));
+                                message.getChannel().sendMessage("Canal de avisos trocado para " + canalav.getAsMention()).queue();
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                message.getChannel().sendMessage("E qual canal devo usar para mensagens de aviso? N\u00falo n\u00e3o \u00e9 um canal v\u00e1lido!").queue();
+                            }
+                            break;
+                        case "prefixo":
+                            try {
+                                setPrefix(cmd[2]);
+                                message.getChannel().sendMessage("Prefixo trocado para __**" + prefix + "**__").queue();
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                message.getChannel().sendMessage("Faltou me dizer o prefixo, bobo!").queue();
+                            }
+                            break;
+                        default:
+                            message.getChannel().sendMessage("N\u00e3o conhe\u00e7o esse comando, certeza que digitou corretamente?").queue();
+                            break;
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    message.getChannel().sendMessage("Voc\u00ea precisa me dizer o qu\u00ea devo definir").queue();
+                }
+            } else if (cmd[0].equals(prefix + "configs")) {
+                //PLACEHOLDER
+            }
         }
     }
 }
---
+++ src/main/java/Owner.java
@@ -0,0 +1,75 @@
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.entities.Guild;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Owner {
+    static String getServers(JDA bot) {
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> ids = new ArrayList<>();
+        ArrayList<String> guilds = new ArrayList<>();
+
+        bot.getGuilds().forEach(g -> {
+            names.add(g.getName());
+            ids.add(g.getId());
+        });
+
+        for (int i = 0; i < names.size(); i++) {
+            guilds.add(names.get(i) + " | " + ids.get(i));
+        }
+
+        if (guilds.isEmpty()) {
+            return "Nenhum servidor encontrado";
+        } else {
+            return String.join("\n", "```", guilds.toString().replace("[", "").replace("]", ""), "```");
+        }
+    }
+
+    private static ArrayList<String> getGuilds(JDA bot) throws NullPointerException {
+        ArrayList<String> names = new ArrayList<>();
+        ArrayList<String> guilds = new ArrayList<>();
+
+        bot.getGuilds().forEach(g -> names.add(g.getName()));
+
+        for (String name : names) {
+            guilds.add(name + " | ");
+        }
+
+        if (guilds.isEmpty()) {
+            return null;
+        } else {
+            return guilds;
+        }
+    }
+
+    static void broadcast(JDA bot, String message, TextChannel homeLog) {
+        List<Guild> guilds = bot.getGuilds();
+        List<String> status = getGuilds(bot);
+
+        for (int z = 0; z < guilds.size(); z++) {
+            try {
+                if (!guilds.get(z).getTextChannelsByName("avisos-shiro", true).isEmpty()) {
+                    guilds.get(z).getTextChannelsByName("avisos-shiro", true).get(0).sendMessage("Transmiss\u00e3o:```" + message + "```").queue();
+                } else {
+                    for (int i = 0; i < guilds.get(z).getChannels().size(); i++) {
+                        if (guilds.get(z).getTextChannels().get(i).canTalk()) {
+                            guilds.get(z).getTextChannels().get(i).sendMessage("Canal `avisos-shiro` n\u00e3o encontrado, enviando transmiss\u00e3o para primeiro canal dispon\u00eDvel:```" + message + "```").queue();
+                            break;
+                        }
+                    }
+                }
+                assert status != null;
+                status.set(z, status.get(z) + "SUCESSO\n");
+            } catch (Exception e) {
+                System.out.println("Erro: " + e);
+                assert status != null;
+                status.set(z, status.get(z) + "FALHA\n");
+            } finally {
+                assert status != null;
+                homeLog.sendMessage("Resultado da transmiss\u00e3o:```" + String.join("", status).replace("[", "").replace("]", "\n") + "```").queue();
+            }
+        }
+    }
+}
