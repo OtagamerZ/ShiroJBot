856b134eb335cdfe27ed61bc505496f41d14eb04
ygimenez <yagogimenez1@hotmail.com>
2019-05-10 23:02:59 -0300

    Adição de checagem de permissões de escrita e funções de listagem e saída de servidores

--- src/main/java/com/kuuhaku/controller/Anime.java
+++ src/main/java/com/kuuhaku/controller/Anime.java
@@ -1,29 +1,32 @@
-package com.kuuhaku.commands;
+package com.kuuhaku.controller;
 
 import org.apache.commons.io.IOUtils;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
-class Anime {
-    static String getData(String query) throws IOException {
-        String json = "{'query':'query " + query + "'}";
+public class Anime {
+    public static String getData(String query) throws IOException {
+        String json = "{\"query\":\"query" + query + "\"}";
         URL url = new URL("https://graphql.anilist.co");
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setConnectTimeout(5000);
         con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
         con.addRequestProperty("Accept", "application/json");
+        con.addRequestProperty("User-Agent", "OtagamerZ");
         con.setDoOutput(true);
         con.setDoInput(true);
         con.setRequestMethod("POST");
 
         OutputStream oStream = con.getOutputStream();
-        oStream.write(json.getBytes("UTF-8"));
+        oStream.write(json.getBytes(StandardCharsets.UTF_8));
         oStream.close();
+
+        OutputStreamWriter osw = new OutputStreamWriter(con.getOutputStream());
+        osw.write("client_credentials&Client_id=1987&Client_secret=oRIwidZip6e1mVHNFu5IA3gvfBwfCGdTxyC0tTGb");
+        osw.flush();
 
         InputStream iStream = new BufferedInputStream(con.getInputStream());
         String data = IOUtils.toString(iStream, "UTF-8");
