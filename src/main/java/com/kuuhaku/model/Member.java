f771230060a3c4010db8dcb5e7a4bbd6e7b13a65
Yago <yagogimenez1@hotmail.com>
2019-05-14 09:21:01 -0300

    Correção no algoritmo de xp

--- src/main/java/com/kuuhaku/model/Member.java
+++ src/main/java/com/kuuhaku/model/Member.java
@@ -2,14 +2,15 @@ package com.kuuhaku.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Arrays;
 
 @Entity
 public class Member {
     @Id
     private String id;
     private int level = 1, xp = 0;
-    private String[] warns = {};
-    private boolean[] badges = {false, false, false, false, false, false, false, false, false, false, false, false, false};
+    private String warns = "";
+    private String badges = "[false,false,false,false,false,false,false,false,false,false,false,false,false]";
 
     public Member(String id) {
         setId(id);
@@ -17,7 +18,7 @@ public class Member {
 
     public boolean addXp() {
         xp += 15;
-        if (xp >= level * 100) {
+        if (xp >= level * (100 * level)) {
             level++;
             return true;
         }
@@ -30,14 +31,14 @@ public class Member {
     }
 
     public void giveBadge(String index) {
-        if (Integer.parseInt(index) >= 0 && Integer.parseInt(index) < badges.length) {
-            this.badges[Integer.parseInt(index)] = true;
+        if (Integer.parseInt(index) >= 0 && Integer.parseInt(index) < getBadges().length) {
+            getBadges()[Integer.parseInt(index)] = true;
         }
     }
 
     public void removeBadge(String index) {
-        if (Integer.parseInt(index) >= 0 && Integer.parseInt(index) < badges.length) {
-            this.badges[Integer.parseInt(index)] = false;
+        if (Integer.parseInt(index) >= 0 && Integer.parseInt(index) < getBadges().length) {
+            getBadges()[Integer.parseInt(index)] = false;
         }
     }
 
@@ -58,19 +59,24 @@ public class Member {
     }
 
     public String[] getWarns() {
-        return warns;
+        return warns.replace("[", "").replace("]", "").split(",");
     }
 
     public void setWarns(String[] warns) {
-        this.warns = warns;
+        this.warns = Arrays.toString(warns);
     }
 
     public boolean[] getBadges() {
-        return badges;
+        String[] t = badges.replace("[", "").replace("]", "").split(",");
+        boolean[] tb = new boolean[t.length];
+        for (int i = 0; i < t.length; i++) {
+            tb[i] = Boolean.parseBoolean(t[0]);
+        }
+        return tb;
     }
 
     public void setBadges(boolean[] badges) {
-        this.badges = badges;
+        this.badges = Arrays.toString(badges);
     }
 
     private void setId(String id) {
