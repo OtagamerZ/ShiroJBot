3a54ad8590f0479502a5187230ab8bd400cb5259
ygimenez <yagogimenez1@hotmail.com>
2019-05-15 13:47:54 -0300

    Reações de imagem implementadas!

---
+++ src/main/java/com/kuuhaku/model/ReactionsList.java
@@ -0,0 +1,13 @@
+package com.kuuhaku.model;
+
+public class ReactionsList {
+    public static String[] hug() {
+        return new String[]{
+                "https://i.imgur.com/TCuWtPE.gif",
+                "https://i.imgur.com/yglgi2M.gif",
+                "https://i.imgur.com/QMoYvzS.gif",
+                "https://i.imgur.com/yO3F2kB.gif",
+                "https://i.imgur.com/uvIA7L8.gif"
+        };
+    }
+}
