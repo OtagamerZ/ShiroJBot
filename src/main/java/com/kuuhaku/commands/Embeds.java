fb251cb7c1289d15c17a7f1fa5c846aeca719811
Yago <yagogimenez1@hotmail.com>
2019-05-20 10:28:16 -0300

    Atualização de licença

--- src/main/java/com/kuuhaku/commands/Embeds.java
+++ src/main/java/com/kuuhaku/commands/Embeds.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2019 Yago Garcia Sanches Gimenez / KuuHaKu
+ *
+ * This file is part of Shiro J Bot.
+ *
+ *     Shiro J Bot is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Shiro J Bot is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Shiro J Bot.  If not, see https://www.gnu.org/licenses/
+ */
+
 package com.kuuhaku.commands;
 
 import com.kuuhaku.controller.Tradutor;
@@ -417,7 +436,7 @@ public class Embeds {
         BufferedImage image = ImageIO.read(con.getInputStream());
 
         StringBuilder answers = new StringBuilder();
-        for (int i = (-5 + 5 * index); i < 5 * index && i < ca.size(); i++) {
+        for (int i = (-10 + 10 * index); i < 10 * index && i < ca.size(); i++) {
             answers.append(ca.get(i).getId()).append(" | ").append("[").append(ca.get(i).getGatilho()).append("]: ").append(ca.get(i).getAnswer()).append("\n");
         }
 
