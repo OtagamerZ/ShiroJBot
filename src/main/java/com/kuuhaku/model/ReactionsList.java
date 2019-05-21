25b2c17b7c4b701d5ddec0fd67a7496001a480ff
ygimenez <yagogimenez1@hotmail.com>
2019-05-21 09:50:52 -0300

    Muita GIF nova

--- src/main/java/com/kuuhaku/model/ReactionsList.java
+++ src/main/java/com/kuuhaku/model/ReactionsList.java
@@ -30,8 +30,38 @@ public class ReactionsList {
                 "https://i.imgur.com/PcZqK9n.gif",
                 "https://i.imgur.com/VFLdqYA.gif",
                 "https://i.imgur.com/ADNi6x4.gif",
-                "https://i.imgur.com/aplG0T4.gif",
-                "https://i.imgur.com/H3J2Y4c.gif"
+                "https://i.imgur.com/H3J2Y4c.gif",
+                "https://i.imgur.com/FSZXuxH.gif",
+                "https://66.media.tumblr.com/18fdf4adcb5ad89f5469a91e860f80ba/tumblr_oltayyHynP1sy5k7wo1_400.gif",
+                "https://data.whicdn.com/images/171724525/original.gif",
+                "https://media.giphy.com/media/xJlOdEYy0r7ZS/giphy.gif",
+                "https://i.redd.it/eaz8jecdet511.gif",
+                "https://68.media.tumblr.com/28bb92375b809a23fc2eb42c071111d2/tumblr_nh7ozxV8qt1sglevjo1_500.gif",
+                "https://i.pinimg.com/originals/8c/34/bb/8c34bb8020f19f82844565e36570cf82.gif",
+                "https://media1.tenor.com/images/95d2569868cd11815bc4fc0431d1d782/tenor.gif",
+                "https://data.whicdn.com/images/210806639/original.gif",
+                "https://i.imgur.com/nrdYNtL.gif",
+                "https://media1.giphy.com/media/od5H3PmEG5EVq/giphy.gif",
+                "http://img.downloadwap.com/screensavers/d/new/anime/bleach_-_chizuru_and_orihime-220295.gif",
+                "http://24.media.tumblr.com/tumblr_m82b7yXh951r5jtugo1_500.gif",
+                "https://i.pinimg.com/originals/f2/80/5f/f2805f274471676c96aff2bc9fbedd70.gif",
+                "https://i.imgur.com/r9aU2xv.gif",
+                "https://thumbs.gfycat.com/GratefulComplexGlassfrog-size_restricted.gif",
+                "https://media1.tenor.com/images/ce6b3a9fd4ad07f5aa84b914b6dd91d4/tenor.gif",
+                "https://media1.giphy.com/media/l2QDM9Jnim1YVILXa/source.gif",
+                "https://i.imgur.com/gSGeZJF.gif",
+                "https://media.giphy.com/media/TGqhg8CXipgzK8UEdr/giphy.gif",
+                "http://i.imgur.com/pME21N2.gif",
+                "https://data.whicdn.com/images/320420363/original.gif",
+                "https://i.imgur.com/wOmoeF8.gif",
+                "https://i.pinimg.com/originals/f2/e6/7f/f2e67f04b6a0ab30870011bd17190409.gif",
+                "https://media3.giphy.com/media/j5cZsIIV5icG4/giphy.gif",
+                "https://media1.tenor.com/images/13be52a4a4a26b0c9e479df6644d6de5/tenor.gif",
+                "https://media2.giphy.com/media/voNrxdxanEyUU/giphy.gif",
+                "https://i.gifer.com/27tM.gif",
+                "https://media1.giphy.com/media/VXP04aclCaUfe/giphy.gif",
+                "http://i.imgur.com/gmiJEbu.gif",
+                "http://multiversosnerd.com.br/wp-content/uploads/2016/10/Imagem-03.gif"
         };
     }
 
@@ -71,7 +101,8 @@ public class ReactionsList {
                 "https://i.imgur.com/ok1eDJv.gif",
                 "https://i.imgur.com/jiYtyNm.gif",
                 "https://i.imgur.com/HJ9k2iv.gif",
-                "https://i.imgur.com/EWXeWvd.gif"
+                "https://i.imgur.com/EWXeWvd.gif",
+                "https://strikninene.files.wordpress.com/2015/05/dfrag.gif"
         };
     }
 
@@ -91,7 +122,10 @@ public class ReactionsList {
                 "https://i.imgur.com/dExTK6D.gif",
                 "https://i.imgur.com/CSJxTiA.gif",
                 "https://i.imgur.com/mQKyrId.gif",
-                "https://i.imgur.com/wdsZ4zo.gif"
+                "https://i.imgur.com/wdsZ4zo.gif",
+                "https://media0.giphy.com/media/z8VoEIL6GdUAM/giphy.gif",
+                "https://media.giphy.com/media/mpcFKjluEmlk4/giphy.gif",
+                "https://media.giphy.com/media/FWQh683IrEJ2w/giphy.gif"
         };
     }
 
@@ -121,7 +155,11 @@ public class ReactionsList {
                 "https://i.imgur.com/kzNZ3Bp.gif",
                 "https://i.imgur.com/viUAETo.gif",
                 "https://i.imgur.com/s3F40sO.gif",
-                "https://i.imgur.com/CU32GXP.gif"
+                "https://i.imgur.com/CU32GXP.gif",
+                "https://i.gifer.com/embedded/download/Dm8Z.gif",
+                "https://thumbs.gfycat.com/FragrantThoughtfulAmericancurl-small.gif",
+                "https://media1.giphy.com/media/Wy3xQIvod3GIU/giphy.gif",
+                "https://pa1.narvii.com/6662/68a6f789afee884b520dd4e84738bd41b34c6977_hq.gif"
         };
     }
 
@@ -131,7 +169,10 @@ public class ReactionsList {
                 "https://i.imgur.com/LjTdPcd.gif",
                 "https://i.imgur.com/95c9raG.gif",
                 "https://i.imgur.com/vIApR3I.gif",
-                "https://i.imgur.com/P0bl9pT.gif"
+                "https://i.imgur.com/P0bl9pT.gif",
+                "https://media3.giphy.com/media/CiMROUysDeuze/source.gif",
+                "https://data.whicdn.com/images/85031535/original.gif",
+                "https://i1.wp.com/i.imgur.com/KwuchrB.gif"
         };
     }
 
@@ -141,7 +182,25 @@ public class ReactionsList {
                 "https://d2w9rnfcy7mm78.cloudfront.net/2145256/original_cd9c01187cead45cd791beafa78f7e08.gif",
                 "https://media.giphy.com/media/euMGM3uD3NHva/giphy.gif",
                 "https://66.media.tumblr.com/9bc3b579d14b65d45c16f7f48ab08f15/tumblr_orprpaDbI21qzxv73o1_400.gif",
-                "https://media1.tenor.com/images/56350dfdcd3a5fa4fd66e9e87f9574bb/tenor.gif"
+                "https://media1.tenor.com/images/56350dfdcd3a5fa4fd66e9e87f9574bb/tenor.gif",
+                "https://j.gifs.com/zpnyQ8.gif",
+                "https://discordemoji.com/assets/emoji/1754_Chika_Dance.gif",
+                "https://thumbs.gfycat.com/HonestMildLamprey-size_restricted.gif"
+        };
+    }
+
+    public static String[] pat() {
+        return new String[]{
+                "https://i.imgur.com/aplG0T4.gif",
+                "https://media0.giphy.com/media/AOmJwSo5gikAE/giphy.gif",
+                "https://gifimage.net/wp-content/uploads/2017/07/head-pat-gif-15.gif",
+                "https://media0.giphy.com/media/QZwYfp3Uwqbuw/giphy.gif"
+        };
+    }
+
+    public static String[] bite() {
+        return  new String[]{
+                "https://i.imgur.com/udyNyrR.gif"
         };
     }
 }
