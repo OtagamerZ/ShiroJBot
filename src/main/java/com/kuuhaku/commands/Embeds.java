0caa53a5180b26fc70eb59de7771b359bce3959c
Yago <yagogimenez1@hotmail.com>
2019-05-22 09:29:49 -0300

    Modificações beyblade

--- src/main/java/com/kuuhaku/commands/Embeds.java
+++ src/main/java/com/kuuhaku/commands/Embeds.java
@@ -519,7 +519,7 @@ public class Embeds {
     public static void shopEmbed(MessageReceivedEvent message, Beyblade bb, String prefixo) {
         EmbedBuilder eb = new EmbedBuilder();
 
-        eb.setAuthor("Saldo de " + message.getAuthor().getName() + ": :diamond_shape_with_a_dot_inside: " + bb.getPoints() + " pontos de combate", message.getAuthor().getAvatarUrl());
+        eb.setAuthor("Saldo de " + message.getAuthor().getName() + ": " + bb.getPoints() + " pontos de combate", "https://i.imgur.com/H1KqxRc.png");
         eb.setColor(Color.decode(bb.getColor()));
         eb.setTitle("Loja de melhorias");
         eb.setThumbnail("https://i.pinimg.com/originals/d3/34/db/d334db51554b3c78fbb65d6cf5e0dcef.png");
