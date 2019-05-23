d7a9c71beced3d53bbc0f0cb56c622e0fbdc2492
ygimenez <yagogimenez1@hotmail.com>
2019-05-23 14:17:09 -0300

    Rework do perfil!

--- src/main/java/com/kuuhaku/model/Profile.java
+++ src/main/java/com/kuuhaku/model/Profile.java
@@ -17,7 +17,6 @@
 
 package com.kuuhaku.model;
 
-import com.kuuhaku.controller.Database;
 import net.coobird.thumbnailator.Thumbnails;
 
 import javax.imageio.ImageIO;
@@ -27,30 +26,70 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 
 public class Profile {
     public static InputStream makeProfile(Member m, net.dv8tion.jda.core.entities.Member u, Map<String, Tags> tags) throws IOException {
-        final BufferedImage avatar = Thumbnails.of(new URL(u.getUser().getAvatarUrl())).size(108, 108).asBufferedImage();
-        final BufferedImage card = ImageIO.read(new URL("https://i.imgur.com/VUJDwlq.png"));
+        URL url = new URL(u.getUser().getAvatarUrl());
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        final BufferedImage avatar = Thumbnails.of(con.getInputStream()).size(108, 108).asBufferedImage();
+
+        url = new URL("https://i.imgur.com/VUJDwlq.png");
+        con = (HttpURLConnection) url.openConnection();
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+        final BufferedImage card = ImageIO.read(con.getInputStream());
+
         BufferedImage defBg;
-        if (m.getBg() == null || m.getBg().isEmpty())
-            defBg = Thumbnails.of(new URL("https://i.imgur.com/lsSjE50.jpg")).size(512, 256).asBufferedImage();
-        else defBg = Thumbnails.of(new URL(m.getBg())).size(512, 256).asBufferedImage();
+        if (m.getBg() == null || m.getBg().isEmpty()) {
+            url = new URL("https://i.imgur.com/lsSjE50.jpg");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            defBg = Thumbnails.of(con.getInputStream()).size(512, 256).asBufferedImage();
+        } else {
+            url = new URL(m.getBg());
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            defBg = Thumbnails.of(con.getInputStream()).size(512, 256).asBufferedImage();
+        }
         BufferedImage dev;
         BufferedImage partner;
         BufferedImage toxic;
         if (tags.containsKey(u.getUser().getId())) {
-            dev = Thumbnails.of(new URL(tags.get(u.getUser().getId()).isStaff() ? "https://i.imgur.com/tIoRtFg.png" : "https://i.imgur.com/j8HUZpp.png")).size(26, 26).asBufferedImage();
-            partner = Thumbnails.of(new URL(tags.get(u.getUser().getId()).isPartner() ? "https://i.imgur.com/veKLeGW.png" : "https://i.imgur.com/wE4k5DG.png")).size(26, 26).asBufferedImage();
-            toxic = Thumbnails.of(new URL(tags.get(u.getUser().getId()).isToxic() ? "https://i.imgur.com/kJpl4Af.png" : "https://i.imgur.com/lvbHhih.png")).size(26, 26).asBufferedImage();
+            url = new URL(tags.get(u.getUser().getId()).isStaff() ? "https://i.imgur.com/tIoRtFg.png" : "https://i.imgur.com/j8HUZpp.png");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            dev = Thumbnails.of(con.getInputStream()).size(26, 26).asBufferedImage();
+
+            url = new URL(tags.get(u.getUser().getId()).isPartner() ? "https://i.imgur.com/veKLeGW.png" : "https://i.imgur.com/wE4k5DG.png");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            partner = Thumbnails.of(con.getInputStream()).size(26, 26).asBufferedImage();
+
+            url = new URL(tags.get(u.getUser().getId()).isToxic() ? "https://i.imgur.com/kJpl4Af.png" : "https://i.imgur.com/lvbHhih.png");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            toxic = Thumbnails.of(con.getInputStream()).size(26, 26).asBufferedImage();
         } else {
-            dev = Thumbnails.of(new URL("https://i.imgur.com/j8HUZpp.png")).size(26, 26).asBufferedImage();
-            partner = Thumbnails.of(new URL("https://i.imgur.com/wE4k5DG.png")).size(26, 26).asBufferedImage();
-            toxic = Thumbnails.of(new URL("https://i.imgur.com/lvbHhih.png")).size(26, 26).asBufferedImage();
+            url = new URL("https://i.imgur.com/j8HUZpp.png");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            dev = Thumbnails.of(con.getInputStream()).size(26, 26).asBufferedImage();
+
+            url = new URL("https://i.imgur.com/wE4k5DG.png");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            partner = Thumbnails.of(con.getInputStream()).size(26, 26).asBufferedImage();
+
+            url = new URL("https://i.imgur.com/lvbHhih.png");
+            con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent", "Mozilla/5.0");
+            toxic = Thumbnails.of(con.getInputStream()).size(26, 26).asBufferedImage();
         }
         BufferedImage bi = new BufferedImage(512, 256, BufferedImage.TYPE_INT_RGB);
+
         Graphics2D g2d = bi.createGraphics();
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2d.setColor(Color.BLUE);
@@ -67,7 +106,7 @@ public class Profile {
         g2d.setFont(new Font("LilyUPC", Font.PLAIN, 40));
         g2d.drawString("Tags ", 135 - g2d.getFontMetrics().stringWidth("Tags:"), 55);
         g2d.setFont(new Font("LilyUPC", Font.PLAIN, 40));
-        g2d.drawString("Level: " + m.getLevel() + " ("+m.getXp() * (int) Math.pow(m.getLevel(), 2)+"%)", 264, 246);
+        g2d.drawString("Level: " + m.getLevel() + " (" + m.getXp() * (int) Math.pow(m.getLevel(), 2) + "%)", 264, 246);
         g2d.drawString("Perfil de " + u.getEffectiveName(), 10, 33);
 
         g2d.dispose();
