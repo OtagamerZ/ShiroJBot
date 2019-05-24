15d449095545858055ab9214982e5f2652ab6acb
Yago <yagogimenez1@hotmail.com>
2019-05-24 09:37:36 -0300

    Correção de mensagens

--- src/main/java/com/kuuhaku/commands/Admin.java
+++ src/main/java/com/kuuhaku/commands/Admin.java
@@ -26,117 +26,119 @@ import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import org.json.JSONObject;
 
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class Admin {
     public static void config(String[] cmd, MessageReceivedEvent message, guildConfig gc) {
         try {
+            final Consumer<Void> insideCommas = tm -> message.getChannel().sendMessage("A mensagem deve estar entre aspas (\")").queue();
+            final Consumer<Void> plzMentionRole = tm -> message.getChannel().sendMessage("Por favor, mencione um cargo né!").queue();
+            final Consumer<Void> roleError = tm -> message.getChannel().sendMessage("Erro ao adicionar cargo.").queue();
             switch (cmd[1]) {
                 case "canalbv":
                     try {
                         gc.setCanalbv(message.getMessage().getMentionedChannels().get(0).getId());
-                        message.getChannel().sendMessage("Canal de boas-vindas trocado para " + message.getGuild().getTextChannelById(gc.getCanalbv()).getAsMention()).queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Canal de boas-vindas trocado para " + message.getGuild().getTextChannelById(gc.getCanalbv()).getAsMention()).queue());
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("E qual canal devo usar para mensagens de boas-vindas? Nulo não é um canal válido!").queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("E qual canal devo usar para mensagens de boas-vindas? Nulo não é um canal válido!").queue());
                     }
                     break;
                 case "canalav":
                     try {
                         gc.setCanalav(message.getMessage().getMentionedChannels().get(0).getId());
-                        message.getChannel().sendMessage("Canal de avisos trocado para " + message.getGuild().getTextChannelById(gc.getCanalav()).getAsMention()).queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Canal de avisos trocado para " + message.getGuild().getTextChannelById(gc.getCanalav()).getAsMention()).queue());
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("E qual canal devo usar para mensagens de aviso? Nulo não é um canal válido!").queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("E qual canal devo usar para mensagens de aviso? Nulo não é um canal válido!").queue());
                     }
                     break;
                 case "canaladeus":
                     try {
                         gc.setCanaladeus(message.getMessage().getMentionedChannels().get(0).getId());
-                        message.getChannel().sendMessage("Canal de adeus trocado para " + message.getGuild().getTextChannelById(gc.getCanaladeus()).getAsMention()).queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Canal de adeus trocado para " + message.getGuild().getTextChannelById(gc.getCanaladeus()).getAsMention()).queue());
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("E qual canal devo usar para mensagens de adeus? Nulo não é um canal válido!").queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("E qual canal devo usar para mensagens de adeus? Nulo não é um canal válido!").queue());
                     }
                     break;
                 case "canalsug":
                     try {
                         gc.setCanalsug(message.getMessage().getMentionedChannels().get(0).getId());
-                        message.getChannel().sendMessage("Canal de sugestões trocado para " + message.getGuild().getTextChannelById(gc.getCanalsug()).getAsMention()).queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Canal de sugestões trocado para " + message.getGuild().getTextChannelById(gc.getCanalsug()).getAsMention()).queue());
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("E qual canal devo ficar de olho para sugestões? Nulo não é um canal válido!").queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("E qual canal devo ficar de olho para sugestões? Nulo não é um canal válido!").queue());
                     }
                     break;
                 case "canallvl":
                     try {
                         gc.setCanallvl(message.getMessage().getMentionedChannels().get(0).getId());
-                        message.getChannel().sendMessage("Canal de notificações de level trocado para " + message.getGuild().getTextChannelById(gc.getCanallvl()).getAsMention()).queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Canal de notificações de level trocado para " + message.getGuild().getTextChannelById(gc.getCanallvl()).getAsMention()).queue());
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("E qual canal devo avisar sobre level ups? Nulo não é um canal válido!").queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("E qual canal devo avisar sobre level ups? Nulo não é um canal válido!").queue());
                     }
                     break;
                 case "prefixo":
                     try {
                         gc.setPrefix(cmd[2]);
-                        message.getChannel().sendMessage("Prefixo trocado para __**" + gc.getPrefix() + "**__").queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Prefixo trocado para __**" + gc.getPrefix() + "**__").queue());
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("Faltou me dizer o prefixo, bobo!").queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Faltou me dizer o prefixo, bobo!").queue());
                     }
                     break;
                 case "msgbv":
                     try {
                         if (cmd[2].contains("\"")) {
                             gc.setMsgBoasVindas(message.getMessage().getContentRaw().replace(gc.getPrefix() + "definir msgbv ", "").replace("\"", ""));
-                            message.getChannel().sendMessage("Agora irei dizer __**" + gc.getMsgBoasVindas() + "**__ para usuários que entrarem no servidor!").queue();
+                            message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Agora irei dizer __**" + gc.getMsgBoasVindas() + "**__ para usuários que entrarem no servidor!").queue());
                         } else {
-                            message.getChannel().sendMessage("A mensagem deve estar entre aspas (\")").queue();
+                            message.getChannel().sendTyping().queue(insideCommas);
                         }
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("Você não me disse que mensagem devo dizer quando alguém entrar!").queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Você não me disse que mensagem devo dizer quando alguém entrar!").queue());
                     }
                     break;
                 case "msgadeus":
                     try {
                         if (cmd[2].contains("\"")) {
                             gc.setMsgAdeus(message.getMessage().getContentRaw().replace(gc.getPrefix() + "definir msgadeus ", "").replace("\"", ""));
-                            message.getChannel().sendMessage("Agora irei dizer __**" + gc.getMsgAdeus() + "**__ para membros que deixarem o servidor!").queue();
+                            message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Agora irei dizer __**" + gc.getMsgAdeus() + "**__ para membros que deixarem o servidor!").queue());
                         } else {
-                            message.getChannel().sendMessage("A mensagem deve estar entre aspas (\")").queue();
+                            message.getChannel().sendTyping().queue(insideCommas);
                         }
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("Você não me disse que mensagem devo dizer quando alguém sair!").queue();
+                        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Você não me disse que mensagem devo dizer quando alguém sair!").queue());
                     }
                     break;
                 case "cargolvl":
                     try {
-                        int lvl = Integer.parseInt(cmd[2]);
                         if (message.getMessage().getMentionedRoles().size() != 0) {
                             Map<String, Object> cargos = gc.getCargoslvl();
                             cargos.put(cmd[2], message.getMessage().getMentionedRoles().get(0).getId());
                             gc.setCargoslvl(new JSONObject(cargos));
                         } else {
-                            message.getChannel().sendMessage("Por favor, mencione um cargo né!").queue();
+                            message.getChannel().sendTyping().queue(plzMentionRole);
                         }
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("Erro ao adicionar cargo.").queue();
+                        message.getChannel().sendTyping().queue(roleError);
                     }
                     break;
                 case "cargonovo":
                     try {
-                        int lvl = Integer.parseInt(cmd[2]);
                         if (message.getMessage().getMentionedRoles().size() != 0) {
                             Map<String, Object> cargos = gc.getCargoNew();
                             cargos.put(cmd[2], message.getMessage().getMentionedRoles().get(0).getId());
                             gc.setCargoNew(new JSONObject(cargos));
                         } else {
-                            message.getChannel().sendMessage("Por favor, mencione um cargo né!").queue();
+                            message.getChannel().sendTyping().queue(plzMentionRole);
                         }
                     } catch (ArrayIndexOutOfBoundsException e) {
-                        message.getChannel().sendMessage("Erro ao adicionar cargo.").queue();
+                        message.getChannel().sendTyping().queue(roleError);
                     }
                     break;
                 default:
-                    message.getChannel().sendMessage("Não conheço esse comando, certeza que digitou corretamente?").queue();
+                    message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Não conheço esse comando, certeza que digitou corretamente?").queue());
                     break;
             }
         } catch (ArrayIndexOutOfBoundsException e) {
-            message.getChannel().sendMessage("Você precisa me dizer o quê devo definir").queue();
+            message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Você precisa me dizer o quê devo definir").queue());
         }
     }
 
@@ -144,13 +146,13 @@ public class Admin {
         Member m = Database.getMemberById(message.getMessage().getMentionedUsers().get(0).getId() + message.getGuild().getId());
         m.addWarn(reason);
         Database.sendMember(m);
-        message.getChannel().sendMessage("O usuário " + message.getMessage().getMentionedUsers().get(0).getAsMention() + " teve um alerta registrado pelo seguinte motivo: `" + reason + "`").queue();
+        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("O usuário " + message.getMessage().getMentionedUsers().get(0).getAsMention() + " teve um alerta registrado pelo seguinte motivo: `" + reason + "`").queue());
     }
 
     public static void takeWarn(MessageReceivedEvent message) {
         Member m = Database.getMemberById(message.getMessage().getMentionedUsers().get(0).getId() + message.getGuild().getId());
         m.removeWarn(Integer.parseInt(message.getMessage().getContentRaw().split(" ")[2]));
         Database.sendMember(m);
-        message.getChannel().sendMessage("Foi retirado o alerta " + message.getMessage().getContentRaw().split(" ")[2] + " um alerta do usuário " + message.getMessage().getMentionedUsers().get(0).getAsMention()).queue();
+        message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage("Foi retirado o alerta " + message.getMessage().getContentRaw().split(" ")[2] + " um alerta do usuário " + message.getMessage().getMentionedUsers().get(0).getAsMention()).queue());
     }
 }
