669757b81b370882d98cc0c53516a6acb31ea537
VS <reydux95@gmail.com>
2019-05-28 00:03:27 +0200

    Ligeiro code cleaning em um dos comandos.

--- src/main/java/com/kuuhaku/command/commands/fun/BeijarCommand.java
+++ src/main/java/com/kuuhaku/command/commands/fun/BeijarCommand.java
@@ -24,63 +24,54 @@ public class BeijarCommand extends Command {
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
 
-		if (message.getMentionedUsers().size() == 1) {
-			if(!message.getMentionedUsers().get(0).getId().equals(author.getId())) {
-				try {
-					URL url = new URL(ReactionsList.kiss()[new Random().nextInt(ReactionsList.kiss().length)]);
-					HttpURLConnection con = (HttpURLConnection) Objects.requireNonNull(url).openConnection();
-					con.setRequestProperty("User-Agent", "Mozilla/5.0");
+		if (message.getMentionedUsers().size() == 0) { channel.sendMessage(":x: | Você tem que mencionar a pessoa que quer beijar!").queue(); return; }
+		if (message.getMentionedUsers().size() > 1) { channel.sendMessage(":x: | Você só pode beijar uma pessoa de cada vez! Você não tem duas bocas...").queue(); return; }
 
-					String msg = "";
+		if (message.getMentionedUsers().get(0).getId().equals(author.getId())) { channel.sendMessage(":x: | Você não pode se beijar a si mesmo... ou será que pode ?").queue(); return; }
 
-					if (message.getMentionedUsers().get(0).getId().equals(Main.getInfo().getSelfUser().getId())) {
-						if (author.getId().equals("321665807988031495")) {
-							switch (new Random().nextInt(3)) {
-								case 0:
-									msg = ("E-Ei!");
-									break;
-								case 1:
-									msg = ("N-Nii-chan?!");
-									break;
-								case 2:
-									msg = ("P-Pera!");
-									break;
-							}
-							channel.sendMessage(msg).addFile(con.getInputStream(), "kiss.gif").queue();
-						} else {
-							channel.sendMessage(msg).addFile(con.getInputStream(), "kiss.gif").queue();
+		try {
+			URL url = new URL(ReactionsList.kiss()[new Random().nextInt(ReactionsList.kiss().length)]);
+			HttpURLConnection con = (HttpURLConnection) Objects.requireNonNull(url).openConnection();
+			con.setRequestProperty("User-Agent", "Mozilla/5.0");
 
-							switch (new Random().nextInt(3)) {
-								case 0:
-									msg = "Não tão rápido!";
-									break;
-								case 1:
-									msg = "Só que não!";
-									break;
-								case 2:
-									msg = "Hoje não!";
-									break;
-							}
-							msg = (author.getAsMention() + " esquivou-se - " + msg);
-							channel.sendMessage(msg).addFile(con.getInputStream(), "nope.gif").queue();
-						}
-					} else {
-						if (!answer) { msg = (author.getAsMention() + " beijou " + message.getMentionedUsers().get(0).getAsMention() + msg); }
-						else { msg = (message.getMentionedUsers().get(0).getAsMention() + " também deu um beijo em " + author.getAsMention() + " - " + msg); }
-						channel.sendMessage(msg).addFile(con.getInputStream(), "kiss.gif").queue();
+			String msg = "";
+
+			if (message.getMentionedUsers().get(0).getId().equals(Main.getInfo().getSelfUser().getId())) {
+				if (author.getId().equals("321665807988031495")) {
+					switch (new Random().nextInt(3)) {
+						case 0:
+							msg = ("E-Ei!");
+							break;
+						case 1:
+							msg = ("N-Nii-chan?!");
+							break;
+						case 2:
+							msg = ("P-Pera!");
+							break;
 					}
-				} catch (IOException err) {
-					err.printStackTrace();
+					channel.sendMessage(msg).addFile(con.getInputStream(), "kiss.gif").queue();
+				} else {
+					channel.sendMessage(msg).addFile(con.getInputStream(), "kiss.gif").queue();
+					switch (new Random().nextInt(3)) {
+						case 0:
+							msg = "Não tão rápido!";
+							break;
+							case 1:
+								msg = "Só que não!";
+								break;
+							case 2:
+								msg = "Hoje não!";
+								break;
+					}
+					msg = (author.getAsMention() + " esquivou-se - " + msg);
+					channel.sendMessage(msg).addFile(con.getInputStream(), "nope.gif").queue();
 				}
 			} else {
-				channel.sendMessage(":x: | Você não pode se beijar a si mesmo...").queue();
+				if (!answer) { msg = (author.getAsMention() + " beijou " + message.getMentionedUsers().get(0).getAsMention() + msg); }
+				else { msg = (message.getMentionedUsers().get(0).getAsMention() + " também deu um beijo em " + author.getAsMention() + " - " + msg); }
+				channel.sendMessage(msg).addFile(con.getInputStream(), "kiss.gif").queue();
 			}
-		} else if (message.getMentionedUsers().size() > 1) {
-			channel.sendMessage(":x: | Você só pode beijar uma pessoa de cada vez! Você não tem duas bocas...").queue();
-		} else {
-			channel.sendMessage(":x: | Você tem que mencionar a pessoa que quer beijar!").queue();
-		}
-		
+		} catch (IOException err) { err.printStackTrace(); }
 	}
 
 }
