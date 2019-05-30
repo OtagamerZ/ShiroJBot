33c2c81b505758b83982e5cde6b95aa76571973b
Yago <yagogimenez1@hotmail.com>
2019-05-30 11:57:09 -0300

    Ranking Beyblade

---
+++ src/main/java/com/kuuhaku/command/commands/beyblade/RankCommand.java
@@ -0,0 +1,47 @@
+package com.kuuhaku.command.commands.beyblade;
+
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.controller.MySQL;
+import com.kuuhaku.model.Beyblade;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.events.Event;
+
+import java.awt.*;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class RankCommand extends Command {
+
+    public RankCommand() {
+        super("brank", new String[]{"branking", "btop10"}, "Mostra o ranking de Beyblades.", Category.BEYBLADE);
+    }
+
+    @Override
+    public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
+        channel.sendMessage(":hourglass: Buscando dados...").queue(m -> {
+            List<Beyblade> rank = MySQL.getBeybladeList();
+            assert rank != null;
+            rank.sort(Comparator.comparing(Beyblade::getKDA));
+            Collections.reverse(rank);
+            Beyblade champ = rank.get(0);
+            rank.remove(0);
+            EmbedBuilder eb = new EmbedBuilder();
+            StringBuilder sb = new StringBuilder();
+
+            eb.setTitle(":bar_chart: TOP 10 Beyblades");
+            eb.setThumbnail("https://www.pngkey.com/png/full/21-217733_free-png-trophy-png-images-transparent-winner-trophy.png");
+            eb.setColor(Color.decode(champ.getColor()));
+            for (int i = 0; i < rank.size() && i < 10; i++) {
+                sb.append(i + 2).append(" - ").append(rank.get(i).getName()).append(" (").append(Main.getInfo().getAPI().getUserById(rank.get(i).getId()).getName()).append(") | ").append(rank.get(i).getWins()).append("/").append(rank.get(i).getLoses()).append("\n");
+            }
+            eb.addField("1 - " + champ.getName() + " (" + Main.getInfo().getAPI().getUserById(champ.getId()).getName() + ") | " + champ.getWins() + "/" + champ.getLoses(), sb.toString(), false);
+
+            m.delete().queue();
+            message.getChannel().sendTyping().queue(tm -> message.getChannel().sendMessage(eb.build()).queue());
+        });
+    }
+}
