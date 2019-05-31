00836e71c0695d20152e72847a17cbc9a9cab64e
ygimenez <yagogimenez1@hotmail.com>
2019-05-31 00:17:52 -0300

    Comando Gamble

--- src/main/java/com/kuuhaku/managers/CommandManager.java
+++ src/main/java/com/kuuhaku/managers/CommandManager.java
@@ -88,6 +88,7 @@ public class CommandManager {
             add(new AlignmentCommand());
             add(new InfoCommand());
             add(new RankCommand());
+            add(new SlotsCommand());
         }};
     }
 
