3c1ac4baed450f87785ee03181f1d1d6f029c522
ygimenez <yagogimenez1@hotmail.com>
2019-06-10 14:07:18 -0300

    Mais info durante o duelo.

--- src/main/java/com/kuuhaku/utils/Helper.java
+++ src/main/java/com/kuuhaku/utils/Helper.java
@@ -469,7 +469,7 @@ public class Helper {
 	}
 
 	public static void spawnAd(MessageChannel channel) {
-		if (Helper.rng(1000) > 950) {
+		if (Helper.rng(1000) > 990) {
 			channel.sendMessage("Opa, está gostando de me utilizar em seu servidor? Caso sim, se puder votar me ajudaria **MUITO** a me tornar cada vez mais popular e ser chamada para mais servidores!\nhttps://discordbots.org/bot/572413282653306901").queue();
 		}
 	}
