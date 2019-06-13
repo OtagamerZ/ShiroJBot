28e89340fc8c0113f2b54968c8c595f0b9f72d6a
Yago <yagogimenez1@hotmail.com>
2019-06-13 10:46:34 -0300

    Modo relay completo.

---
+++ src/main/java/com/kuuhaku/command/commands/information/RelayCommand.java
@@ -0,0 +1,40 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ *     Shiro J Bot is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Shiro J Bot is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.information;
+
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.controller.SQLite;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.events.Event;
+
+import java.time.format.DateTimeFormatter;
+
+public class RelayCommand extends Command {
+
+    public RelayCommand() {
+        super("relay", new String[]{"relinfo", "relcon"}, "Mostra dados sobre a conexão relay.", Category.INFO);
+    }
+
+    @Override
+    public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
+        channel.sendMessage(Main.getRelay().getRelayInfo(SQLite.getGuildById(guild.getId()))).queue();
+    }
+}
