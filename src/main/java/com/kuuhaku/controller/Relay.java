e31d849fd8df1bfd026faa485a4041c2196246ee
ygimenez <yagogimenez1@hotmail.com>
2019-06-14 12:03:00 -0300

    Pequenas mudanças no relay.

--- src/main/java/com/kuuhaku/controller/Relay.java
+++ src/main/java/com/kuuhaku/controller/Relay.java
@@ -25,13 +25,21 @@ public class Relay extends SQLite {
 		updateRelays();
 
 		eb = new EmbedBuilder();
-		eb.setAuthor("(" + s.getName() + ") " + m.getEffectiveName() + " disse:", m.getUser().getAvatarUrl(), m.getUser().getAvatarUrl());
 		eb.setDescription(msg);
+		eb.setFooter(m.getUser().getId(), m.getUser().getAvatarUrl());
 		try {
 			eb.setColor(Helper.colorThief(m.getUser().getAvatarUrl()));
 		} catch (IOException e) {
 			eb.setColor(new Color(Helper.rng(255), Helper.rng(255), Helper.rng(255)));
 		}
+
+		if (m.getUser().getId().equals(Main.getInfo().getNiiChan()) || Main.getInfo().getDevelopers().contains(m.getUser().getId()))
+			eb.setAuthor("(" + s.getName() + ") <:Dev:589103373354270760>" + m.getEffectiveName() + " disse:", s.getIconUrl(), s.getIconUrl());
+		else if (SQLite.getTagById(m.getUser().getId()).isPartner())
+			eb.setAuthor("(" + s.getName() + ") <:Partner:589103374033485833>" + m.getEffectiveName() + " disse:", s.getIconUrl(), s.getIconUrl());
+		else if (SQLite.getTagById(m.getUser().getId()).isToxic())
+			eb.setAuthor("(" + s.getName() + ") <:Toxic:589103372926451713>" + m.getEffectiveName() + " disse:", s.getIconUrl(), s.getIconUrl());
+		else eb.setAuthor("(" + s.getName() + ") " + m.getEffectiveName() + " disse:", s.getIconUrl(), s.getIconUrl());
 
 		relays.forEach((k, r) -> {
 			if (!s.getId().equals(k) && m.getUser() != Main.getInfo().getSelfUser())
@@ -44,8 +52,7 @@ public class Relay extends SQLite {
 		EmbedBuilder eb = new EmbedBuilder();
 
 		eb.setTitle(":globe_with_meridians: Dados do relay");
-		eb.addField(":busts_in_silhouette: Clientes conectados: " + relays.size(), "", false);
-		eb.addField("Canal relay: " + (gc.getCanalRelay() == null ? "Não configurado" : Main.getInfo().getGuildByID(gc.getGuildID()).getTextChannelById(gc.getCanalRelay()).getName()), "", false);
+		eb.addField(":busts_in_silhouette: Clientes conectados: " + relays.size(), "Canal relay: " + (gc.getCanalRelay() == null ? "Não configurado" : Main.getInfo().getGuildByID(gc.getGuildID()).getTextChannelById(gc.getCanalRelay()).getAsMention()), false);
 		eb.setColor(new Color(Helper.rng(255), Helper.rng(255), Helper.rng(255)));
 
 		return eb.build();
