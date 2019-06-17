65fdf46528e38c48384f7b4992d64f116ed1f38d
ygimenez <yagogimenez1@hotmail.com>
2019-06-17 10:08:54 -0300

    Separação do Relay para a JibrilJBot

--- src/main/java/com/kuuhaku/events/guild/GuildEvents.java
+++ src/main/java/com/kuuhaku/events/guild/GuildEvents.java
@@ -17,11 +17,21 @@
 
 package com.kuuhaku.events.guild;
 
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.model.CustomAnswers;
 import com.kuuhaku.model.guildConfig;
 import com.kuuhaku.controller.SQLite;
+import com.kuuhaku.utils.Helper;
+import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.events.guild.GuildLeaveEvent;
+import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.persistence.NoResultException;
+import java.util.Objects;
 
 public class GuildEvents extends ListenerAdapter {
 
@@ -37,5 +47,123 @@ public class GuildEvents extends ListenerAdapter {
         gc.setGuildId(event.getGuild().getId());
         SQLite.removeGuildFromDB(gc);
         System.out.println("Acabei de sair do servidor \"" + event.getGuild().getName() + "\".");
+    }
+
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        if (Main.getInfo().isReady()) {
+            User author = event.getAuthor();
+            Member member = event.getMember();
+            Message message = event.getMessage();
+            MessageChannel channel = message.getChannel();
+            Guild guild = message.getGuild();
+            String rawMessage = message.getContentRaw();
+
+            String prefix = "";
+            if (!Main.getInfo().isDev()) {
+                try {
+                    prefix = SQLite.getGuildPrefix(guild.getId());
+                } catch (NoResultException | NullPointerException ignore) {
+                }
+            } else prefix = Main.getInfo().getDefaultPrefix();
+
+            if (rawMessage.startsWith(";") && author.getId().equals(Main.getInfo().getNiiChan())) {
+                try {
+                    message.delete().queue();
+                    channel.sendMessage(rawMessage.substring(1)).queue();
+                } catch (InsufficientPermissionException ignore) {
+                }
+            }
+
+            if (author.isBot() && !Main.getInfo().getSelfUser().getId().equals(author.getId())) return;
+
+		/*
+		if(event.getPrivateChannel()!=null) {
+			try {
+				Helper.sendPM(author, Helper.formatMessage(Messages.PM_CHANNEL, "help", author));
+			} catch (Exception e) {
+				DiscordHelper.sendAutoDeleteMessage(channel, YuiHelper.formatMessage(Messages.PM_CHANNEL, "help", author));
+			}
+			return;
+		}
+
+		if(message.getInvites().size()>0 && Helper.getPrivilegeLevel(member) == PrivilegeLevel.USER) {
+            message.delete().queue();
+            try {
+				Helper.sendPM(author, Messages.INVITE_SENT);
+            } catch (Exception e) {
+				Helper.sendPM(author, ":x: | ");
+            }
+            return;
+        }
+		*/
+
+            Helper.battle(event);
+
+            if (message.getContentDisplay().equals(Main.getInfo().getSelfUser().getAsMention())) {
+                channel.sendMessage("Para obter ajuda sobre como me utilizar use `" + prefix + "ajuda`.").queue();
+                return;
+            }
+
+            String rawMsgNoPrefix = rawMessage;
+            String commandName = "";
+            if (rawMessage.toLowerCase().contains(prefix)) {
+                rawMsgNoPrefix = rawMessage.substring(prefix.length()).trim();
+                commandName = rawMsgNoPrefix.split(" ")[0].trim();
+            }
+
+            try {
+                CustomAnswers ca = SQLite.getCAByTrigger(rawMessage, guild.getId());
+                if (!Objects.requireNonNull(ca).isMarkForDelete() && author != Main.getInfo().getSelfUser())
+                    Helper.typeMessage(channel, Objects.requireNonNull(ca).getAnswer());
+            } catch (NoResultException | NullPointerException ignore) {
+            }
+
+            boolean hasArgs = (rawMsgNoPrefix.split(" ").length > 1);
+            String[] args = new String[]{};
+            if (hasArgs) {
+                args = rawMsgNoPrefix.substring(commandName.length()).trim().split(" ");
+            }
+
+            boolean found = false;
+            for (Command command : Main.getCommandManager().getCommands()) {
+                if (command.getName().equalsIgnoreCase(commandName)) {
+                    found = true;
+                }
+                for (String alias : command.getAliases()) {
+                    if (alias.equalsIgnoreCase(commandName)) {
+                        found = true;
+                    }
+                }
+                if (command.getCategory().isEnabled()) {
+                    found = false;
+                }
+
+                if (found) {
+                    if (!Helper.hasPermission(member, command.getCategory().getPrivilegeLevel())) {
+                        channel.sendMessage(":x: | Você não tem permissão para executar este comando!").queue();
+                        Helper.spawnAd(channel);
+                        break;
+                    }
+                    command.execute(author, member, rawMsgNoPrefix, args, message, channel, guild, event, prefix);
+                    Helper.spawnAd(channel);
+                    break;
+                }
+            }
+
+            if (!found && !author.isBot()) {
+                try {
+                    com.kuuhaku.model.Member m = SQLite.getMemberById(member.getUser().getId() + member.getGuild().getId());
+                    boolean lvlUp = m.addXp();
+                    if (lvlUp && SQLite.getGuildById(guild.getId()).getLvlNotif()) {
+                        channel.sendMessage(member.getEffectiveName() + " subiu para o nível " + m.getLevel() + ". GGWP! :tada:").queue();
+                    }
+                    SQLite.saveMemberToDB(m);
+                } catch (NoResultException e) {
+                    SQLite.addMemberToDB(member);
+                } catch (InsufficientPermissionException ignore) {
+                }
+            }
+        }
     }
 }
