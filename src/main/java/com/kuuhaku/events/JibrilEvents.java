3a754dafa2737749c7c1e9d1feccdd7f482820f6
Yago <yagogimenez1@hotmail.com>
2019-06-17 13:19:30 -0300

    Separação do Relay para a JibrilJBot

--- src/main/java/com/kuuhaku/events/JibrilEvents.java
+++ src/main/java/com/kuuhaku/events/JibrilEvents.java
@@ -7,47 +7,50 @@ import com.kuuhaku.utils.Helper;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class JibrilEvents extends ListenerAdapter {
-	Thread relayThread;
+	private ExecutorService executor = Executors.newSingleThreadExecutor();
 
 	@Override
 	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
 		Runnable relay = () -> {
 			boolean done = false;
-			if (event.getChannel().getId().equals(SQLite.getGuildCanalRelay(event.getGuild().getId())) && !event.getAuthor().isBot()) {
-				while (!done) {
-					done = doRelay(event);
-					try {
-						Thread.sleep(100);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
+
+			while (!done) {
+				done = doRelay(event);
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 			}
 		};
 
-		if (relayThread == null) relayThread = new Thread(relay);
-		else if (!relayThread.isAlive()) relayThread.start();
-		else relayThread.run();
+		executor.execute(relay);
 	}
 
 	private boolean doRelay(GuildMessageReceivedEvent event) {
-		try {
-			if (RelayBlockList.check(event.getAuthor().getId())) {
-				event.getMessage().delete().queue();
-				event.getAuthor().openPrivateChannel().queue(c -> c.sendMessage(":x: | Você não pode mandar mensagens no chat global (bloqueado).").queue());
+		if (event.getChannel().getId().equals(SQLite.getGuildCanalRelay(event.getGuild().getId())) && !event.getAuthor().isBot()) {
+			try {
+				if (RelayBlockList.check(event.getAuthor().getId())) {
+					event.getMessage().delete().queue();
+					event.getAuthor().openPrivateChannel().queue(c -> c.sendMessage(":x: | Você não pode mandar mensagens no chat global (bloqueado).").queue());
+					return true;
+				}
+				String[] msg = event.getMessage().getContentRaw().split(" ");
+				for (int i = 0; i < msg.length; i++) {
+					if (Helper.findURL(msg[i])) msg[i] = "`LINK BLOQUEADO`";
+				}
+				if (String.join(" ", msg).length() < 2048)
+					Main.getRelay().relayMessage(String.join(" ", msg), event.getMember(), event.getGuild());
+				else event.getChannel().sendMessage(":x: | Mensagem muito longa! (Max. 2048 letras)").queue();
 				return true;
+			} catch (Exception e) {
+				return false;
 			}
-			String[] msg = event.getMessage().getContentRaw().split(" ");
-			for (int i = 0; i < msg.length; i++) {
-				if (Helper.findURL(msg[i])) msg[i] = "`LINK BLOQUEADO`";
-			}
-			if (String.join(" ", msg).length() < 2048)
-				Main.getRelay().relayMessage(String.join(" ", msg), event.getMember(), event.getGuild());
-			else event.getChannel().sendMessage(":x: | Mensagem muito longa! (Max. 2048 letras)").queue();
-			return true;
-		} catch (Exception e) {
-			return false;
 		}
+		return false;
 	}
 }
