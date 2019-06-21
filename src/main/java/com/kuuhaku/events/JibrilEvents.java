99d919eaa06b7ee32cea691073ffb7f3c78240fb
ygimenez <yagogimenez1@hotmail.com>
2019-06-20 21:44:42 -0300

    Anexo de imagens de usuários verificados no chat global

--- src/main/java/com/kuuhaku/events/JibrilEvents.java
+++ src/main/java/com/kuuhaku/events/JibrilEvents.java
@@ -1,6 +1,7 @@
 package com.kuuhaku.events;
 
 import com.kuuhaku.Main;
+import com.kuuhaku.controller.MySQL;
 import com.kuuhaku.model.RelayBlockList;
 import com.kuuhaku.utils.Helper;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
@@ -8,21 +9,30 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class JibrilEvents extends ListenerAdapter {
 
-	@Override
-	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-		if (Main.getRelay().getRelayMap().containsValue(event.getChannel().getId()) && !event.getAuthor().isBot()) {
-			if (RelayBlockList.check(event.getAuthor().getId())) {
-				event.getMessage().delete().queue();
-				event.getAuthor().openPrivateChannel().queue(c -> c.sendMessage(":x: | Você não pode mandar mensagens no chat global (bloqueado).").queue());
-				return;
-			}
-			String[] msg = event.getMessage().getContentRaw().split(" ");
-			for (int i = 0; i < msg.length; i++) {
-				if (Helper.findURL(msg[i])) msg[i] = "`LINK BLOQUEADO`";
-			}
-			if (String.join(" ", msg).length() < 2048)
-				Main.getRelay().relayMessage(String.join(" ", msg), event.getMember(), event.getGuild());
-			else event.getChannel().sendMessage(":x: | Mensagem muito longa! (Max. 2048 letras)").queue();
-		}
-	}
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        if (Main.getRelay().getRelayMap().containsValue(event.getChannel().getId()) && !event.getAuthor().isBot()) {
+            if (RelayBlockList.check(event.getAuthor().getId())) {
+                event.getMessage().delete().queue();
+                event.getAuthor().openPrivateChannel().queue(c -> c.sendMessage(":x: | Você não pode mandar mensagens no chat global (bloqueado).").queue());
+                return;
+            }
+            String[] msg = event.getMessage().getContentRaw().split(" ");
+            for (int i = 0; i < msg.length; i++) {
+                if (Helper.findURL(msg[i]) && !MySQL.getTagById(event.getAuthor().getId()).isVerified())
+                    msg[i] = "`LINK BLOQUEADO`";
+            }
+            if (String.join(" ", msg).length() < 2048) {
+                if (MySQL.getTagById(event.getAuthor().getId()).isVerified() && event.getMessage().getAttachments().size() > 0) {
+                    try {
+                        Main.getRelay().relayMessage(String.join(" ", msg), event.getMember(), event.getGuild(), event.getMessage().getAttachments().get(0).getUrl());
+                    } catch (Exception e) {
+                        Main.getRelay().relayMessage(String.join(" ", msg), event.getMember(), event.getGuild(), null);
+                    }
+                    return;
+                }
+                Main.getRelay().relayMessage(String.join(" ", msg), event.getMember(), event.getGuild(), null);
+            } else event.getChannel().sendMessage(":x: | Mensagem muito longa! (Max. 2048 letras)").queue();
+        }
+    }
 }
