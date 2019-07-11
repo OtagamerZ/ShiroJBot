9f9c2fc13be9184b8095bd13ad291a55af8c5e56
ygimenez <yagogimenez1@hotmail.com>
2019-07-11 10:14:37 -0300

    Listagem de emotes

--- src/main/java/com/kuuhaku/command/commands/misc/SayCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/SayCommand.java
@@ -2,6 +2,7 @@ package com.kuuhaku.command.commands.misc;
 
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
+import com.kuuhaku.utils.Helper;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.Event;
 
@@ -15,8 +16,8 @@ public class SayCommand extends Command {
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
 		
 		if(args.length == 0) { channel.sendMessage(":x: | Você precisa definir uma mensagem.").queue(); return; }
-        
-		channel.sendMessage(String.join(" ", args)).queue();
+
+		channel.sendMessage(Helper.makeEmoteFromMention(args)).queue();
 	}
 
 }
