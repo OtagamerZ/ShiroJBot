3d1371b8517c56e5fb14a9f8354f8e765d1fa5b8
Yago <yagogimenez1@hotmail.com>
2019-07-11 09:40:22 -0300

    Correção de NoLinkCommand. Listagem de emotes.

--- src/main/java/com/kuuhaku/command/commands/misc/EmoteListCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/EmoteListCommand.java
@@ -38,8 +38,9 @@ public class EmoteListCommand extends Command {
 				eb.setColor(new Color(Helper.rng(255), Helper.rng(255), Helper.rng(255)));
 				for (int i = -10 + (10 * page); i < ca.size() && i < (10 * page); i++) {
 					Emote e = Main.getInfo().getAPI().getEmotes().get(i);
-					eb.addField("Emote " + e.toString(), e.getAsMention() + " " + e.getAsMention() + " " + e.getAsMention(), false);
+					eb.addField("Emote " + e.getAsMention(), e.getAsMention() + " " + e.getAsMention() + " " + e.getAsMention(), false);
 				}
+				eb.setFooter("Página " + page + ". Mostrando " + (-10 + 10 * page) + " - " + (page * 10) + " resultados.", null);
 
 				channel.sendMessage(eb.build()).queue();
 			} catch (ArrayIndexOutOfBoundsException ex) {
