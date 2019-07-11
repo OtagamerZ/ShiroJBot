148045e38db7c837d419c2eef0c84c89e9c263dc
ygimenez <yagogimenez1@hotmail.com>
2019-07-11 10:34:18 -0300

    Listagem de emotes

--- src/main/java/com/kuuhaku/command/commands/misc/JibrilEmoteListCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/JibrilEmoteListCommand.java
@@ -3,6 +3,7 @@ package com.kuuhaku.command.commands.misc;
 import com.kuuhaku.Main;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
+import com.kuuhaku.controller.SQLite;
 import com.kuuhaku.utils.Helper;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.*;
@@ -37,6 +38,7 @@ public class JibrilEmoteListCommand extends Command {
 				Main.getJibril().getEmotes().forEach(e -> f.add(new MessageEmbed.Field("Emote " + e.getAsMention(), "Menção: " + e.getAsMention().replace("<", "`{").replace(">", "}`").replace(":", "&"), false)));
 				List<MessageEmbed.Field> subF = f.subList(-10 + (10 * page), 10 * page > f.size() ? f.size() - 1 : 10 * page);
 				subF.forEach(eb::addField);
+				eb.setAuthor("Para usar estes emotes, basta digitá-los no canal global, eles serão convertidos automaticamente");
 				eb.setFooter("Página " + page + ". Mostrando " + (-10 + 10 * page) + " - " + (10 * page > f.size() ? f.size() - 1 : 10 * page) + " resultados.", null);
 
 				channel.sendMessage(eb.build()).queue();
--- src/main/java/com/kuuhaku/command/commands/misc/ShiroEmoteListCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/ShiroEmoteListCommand.java
@@ -40,6 +40,7 @@ public class ShiroEmoteListCommand extends Command {
 				Main.getInfo().getAPI().getEmotes().forEach(e -> f.add(new MessageEmbed.Field("Emote " + e.getAsMention(), "Menção: " + e.getAsMention().replace("<", "`{").replace(">", "}`").replace(":", "&"), false)));
 				List<MessageEmbed.Field> subF = f.subList(-10 + (10 * page), 10 * page > f.size() ? f.size() - 1 : 10 * page);
 				subF.forEach(eb::addField);
+				eb.setAuthor("Para usar estes emotes, utilize o comando `" + SQLite.getGuildPrefix(guild.getId()) + "say MENÇÃO`");
 				eb.setFooter("Página " + page + ". Mostrando " + (-10 + 10 * page) + " - " + (10 * page > f.size() ? f.size() - 1 : 10 * page) + " resultados.", null);
 
 				channel.sendMessage(eb.build()).queue();
