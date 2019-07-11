23731e7b3852baa8c76e8a595d02e652054c13a7
Ygimenez <yagogimenez1@hotmail.com>
2019-07-11 11:25:09 -0300

    Listagem de emotes

--- src/main/java/com/kuuhaku/command/commands/misc/JibrilEmoteListCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/JibrilEmoteListCommand.java
@@ -13,6 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JibrilEmoteListCommand extends Command {
 
@@ -37,7 +38,7 @@ public class JibrilEmoteListCommand extends Command {
 				eb.setTitle("<a:SmugDance:598842924725305344> Emotes disponíveis para a Jibril:");
 				eb.setColor(new Color(Helper.rng(255), Helper.rng(255), Helper.rng(255)));
 
-				Main.getJibril().getEmotesByName(args[0], true).forEach(e -> f.add(new MessageEmbed.Field("Emote " + e.getAsMention(), "Menção: " + e.getAsMention().replace("<", "`{").replace(">", "}`").replace(":", "&"), false)));
+				Main.getJibril().getEmotes().stream().filter(e -> StringUtils.containsIgnoreCase(e.getName(), args[0])).collect(Collectors.toList()).forEach(e -> f.add(new MessageEmbed.Field("Emote " + e.getAsMention(), "Menção: " + e.getAsMention().replace("<", "`{").replace(">", "}`").replace(":", "&"), false)));
 				List<MessageEmbed.Field> subF = f.subList(-10 + (10 * page), 10 * page > f.size() ? f.size() - 1 : 10 * page);
 				subF.forEach(eb::addField);
 				eb.setAuthor("Para usar estes emotes, basta digitá-los no canal global, eles serão convertidos automaticamente");
@@ -45,7 +46,7 @@ public class JibrilEmoteListCommand extends Command {
 
 				channel.sendMessage(eb.build()).queue();
 			} catch (IndexOutOfBoundsException | IllegalArgumentException ex) {
-				channel.sendMessage(":x: | Página inválida, no total existem `" + Main.getJibril().getEmotesByName(args[0], true).size() / 10 + "` páginas de emotes.").queue();
+				channel.sendMessage(":x: | Página inválida, no total existem `" + (int) Main.getJibril().getEmotes().stream().filter(e -> StringUtils.containsIgnoreCase(e.getName(), args[0])).count() / 10 + "` páginas de emotes.").queue();
 			}
 		} else {
 			try {
