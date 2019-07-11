b90264c7a91195a487ae129472ccb4aa699af8f2
ygimenez <yagogimenez1@hotmail.com>
2019-07-11 11:20:08 -0300

    Listagem de emotes

--- src/main/java/com/kuuhaku/command/commands/misc/JibrilEmoteListCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/JibrilEmoteListCommand.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class JibrilEmoteListCommand extends Command {
 
 	public JibrilEmoteListCommand() {
-		super("jemotes", "<pagina/nome>", "Mostra a lista de emotes disponíveis para uso através da Jibril.", Category.PARTNER);
+		super("jemotes", "<nome> <página>", "Mostra a lista de emotes disponíveis para uso através da Jibril.", Category.PARTNER);
 	}
 
 	@Override
@@ -25,7 +25,28 @@ public class JibrilEmoteListCommand extends Command {
 		if (args.length == 0) {
 			channel.sendMessage(":x: | Você precisa definir uma página de emotes.").queue();
 		} else if (!StringUtils.isNumeric(args[0])) {
+			if (args.length == 1) {
+				channel.sendMessage(":x: | Você precisa definir uma página de emotes logo após o nome.").queue();
+				return;
+			}
+			try {
+				List<MessageEmbed.Field> f = new ArrayList<>();
+				int page = Integer.parseInt(args[1]);
+				EmbedBuilder eb = new EmbedBuilder();
 
+				eb.setTitle("<a:SmugDance:598842924725305344> Emotes disponíveis para a Jibril:");
+				eb.setColor(new Color(Helper.rng(255), Helper.rng(255), Helper.rng(255)));
+
+				Main.getJibril().getEmotesByName(args[0], true).forEach(e -> f.add(new MessageEmbed.Field("Emote " + e.getAsMention(), "Menção: " + e.getAsMention().replace("<", "`{").replace(">", "}`").replace(":", "&"), false)));
+				List<MessageEmbed.Field> subF = f.subList(-10 + (10 * page), 10 * page > f.size() ? f.size() - 1 : 10 * page);
+				subF.forEach(eb::addField);
+				eb.setAuthor("Para usar estes emotes, basta digitá-los no canal global, eles serão convertidos automaticamente");
+				eb.setFooter("Página " + page + ". Mostrando " + (-10 + 10 * page) + " - " + (10 * page > f.size() ? f.size() - 1 : 10 * page) + " resultados.", null);
+
+				channel.sendMessage(eb.build()).queue();
+			} catch (IndexOutOfBoundsException | IllegalArgumentException ex) {
+				channel.sendMessage(":x: | Página inválida, no total existem `" + Main.getJibril().getEmotesByName(args[0], true).size() / 10 + "` páginas de emotes.").queue();
+			}
 		} else {
 			try {
 				List<MessageEmbed.Field> f = new ArrayList<>();
--- src/main/java/com/kuuhaku/command/commands/misc/LocalEmoteListCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/LocalEmoteListCommand.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class LocalEmoteListCommand extends Command {
 
 	public LocalEmoteListCommand() {
-		super("emotes", "Mostra a lista de emotes disponíveis no servidor em que o comando foi executado.", Category.MISC);
+		super("emotes", "<nome> <página>", "Mostra a lista de emotes disponíveis no servidor em que o comando foi executado.", Category.MISC);
 	}
 
 	@Override
@@ -25,7 +25,28 @@ public class LocalEmoteListCommand extends Command {
 		if (args.length == 0) {
 			channel.sendMessage(":x: | Você precisa definir uma página de emotes.").queue();
 		} else if (!StringUtils.isNumeric(args[0])) {
+			if (args.length == 1) {
+				channel.sendMessage(":x: | Você precisa definir uma página de emotes logo após o nome.").queue();
+				return;
+			}
+			try {
+				List<MessageEmbed.Field> f = new ArrayList<>();
+				int page = Integer.parseInt(args[1]);
+				EmbedBuilder eb = new EmbedBuilder();
 
+				eb.setTitle("<a:SmugDance:598842924725305344> Emotes disponíveis neste servidor:");
+				eb.setColor(new Color(Helper.rng(255), Helper.rng(255), Helper.rng(255)));
+
+				guild.getEmotesByName(args[0], true).forEach(e -> f.add(new MessageEmbed.Field("Emote " + e.getAsMention(), "Menção: " + e.getAsMention().replace("<", "`{").replace(">", "}`").replace(":", "&"), false)));
+				List<MessageEmbed.Field> subF = f.subList(-10 + (10 * page), 10 * page > f.size() ? f.size() - 1 : 10 * page);
+				subF.forEach(eb::addField);
+				eb.setAuthor("Para usar estes emotes, utilize o comando \"" + SQLite.getGuildPrefix(guild.getId()) + "say MENÇÃO\"");
+				eb.setFooter("Página " + page + ". Mostrando " + (-10 + 10 * page) + " - " + (10 * page > f.size() ? f.size() - 1 : 10 * page) + " resultados.", null);
+
+				channel.sendMessage(eb.build()).queue();
+			} catch (IndexOutOfBoundsException | IllegalArgumentException ex) {
+				channel.sendMessage(":x: | Página inválida, no total existem `" + guild.getEmotesByName(args[0], true).size() / 10 + "` páginas de emotes.").queue();
+			}
 		} else {
 			try {
 				List<MessageEmbed.Field> f = new ArrayList<>();
@@ -38,7 +59,7 @@ public class LocalEmoteListCommand extends Command {
 				guild.getEmotes().forEach(e -> f.add(new MessageEmbed.Field("Emote " + e.getAsMention(), "Menção: " + e.getAsMention().replace("<", "`{").replace(">", "}`").replace(":", "&"), false)));
 				List<MessageEmbed.Field> subF = f.subList(-10 + (10 * page), 10 * page > f.size() ? f.size() - 1 : 10 * page);
 				subF.forEach(eb::addField);
-				eb.setAuthor("Para usar estes emotes, utilize o comando `" + SQLite.getGuildPrefix(guild.getId()) + "say MENÇÃO`");
+				eb.setAuthor("Para usar estes emotes, utilize o comando \"" + SQLite.getGuildPrefix(guild.getId()) + "say MENÇÃO\"");
 				eb.setFooter("Página " + page + ". Mostrando " + (-10 + 10 * page) + " - " + (10 * page > f.size() ? f.size() - 1 : 10 * page) + " resultados.", null);
 
 				channel.sendMessage(eb.build()).queue();
