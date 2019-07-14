257d05aa64ad62697ab63facee8808ccab056333
Ygimenez <yagogimenez1@hotmail.com>
2019-07-14 00:29:01 -0300

    Correção de reações

--- src/main/java/com/kuuhaku/command/commands/misc/PingCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/PingCommand.java
@@ -3,11 +3,9 @@ package com.kuuhaku.command.commands.misc;
 import com.kuuhaku.Main;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
-import com.kuuhaku.controller.Anime;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.Event;
-
-import java.io.IOException;
 
 public class PingCommand extends Command {
 	
@@ -15,7 +13,8 @@ public class PingCommand extends Command {
 
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
-		channel.sendMessage("Pong! :ping_pong: ").queue(msg -> msg.editMessage( msg.getContentRaw() + Main.getInfo().getPing() + " ms!").queue());
+        if (guild.getSelfMember().hasPermission(Permission.MESSAGE_WRITE))
+            channel.sendMessage("Pong! :ping_pong: ").queue(msg -> msg.editMessage(msg.getContentRaw() + Main.getInfo().getPing() + " ms!").queue());
 	}
 
 }
