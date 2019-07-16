c041c9e9b9b2ebf577b0724667855eafdeaacfa0
ygimenez <yagogimenez1@hotmail.com>
2019-07-16 14:04:12 -0300

    Criado comando de compilação em tempo real

---
+++ src/main/java/com/kuuhaku/utils/BannedVars.java
@@ -0,0 +1,17 @@
+package com.kuuhaku.utils;
+
+public class BannedVars {
+	public static String[] vars = new String[]{
+		"getJDA()",
+		"getAPI()",
+		"getJDA()",
+		"getManager()",
+		"getSelfUser()",
+		"getSelfMember()",
+		"delete()",
+		"Main.",
+		"getInfo()",
+		"while (true)",
+		"while (false)"
+	};
+}
