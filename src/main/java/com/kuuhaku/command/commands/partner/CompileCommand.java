ed0564b5c176dbcff82fe0a047e5bd93c98d497e
Yago <yagogimenez1@hotmail.com>
2019-07-17 09:00:49 -0300

    Melhoria no comando compile

--- src/main/java/com/kuuhaku/command/commands/partner/CompileCommand.java
+++ src/main/java/com/kuuhaku/command/commands/partner/CompileCommand.java
@@ -16,11 +16,15 @@ public class CompileCommand extends Command {
 
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
-		String code = String.join(" ", args);
 		channel.sendMessage("<a:Loading:598500653215645697> | Compilando...").queue(m -> {
 			try {
+				String code = String.join(" ", args);
 				if (!code.contains("out")) throw new Exception("Código sem retorno.");
-				else if (Arrays.stream(BannedVars.vars).parallel().anyMatch(code::contains)) throw new Exception("Código com métodos proibidos.");
+				else if (code.contains("```") && !code.contains("```java")) {
+					throw new Exception("Bloco de código com começo incorreto");
+				} else if (Arrays.stream(BannedVars.vars).parallel().anyMatch(code::contains))
+					throw new Exception("Código com métodos proibidos.");
+				code = code.replace("```java", "").replace("```", "");
 				Interpreter i = new Interpreter();
 				i.set("msg", message);
 				i.eval(code);
@@ -29,7 +33,7 @@ public class CompileCommand extends Command {
 						m.getChannel().sendMessage("<a:Loading:598500653215645697> | Executando...").queue(d ->
 								d.editMessage("-> " + out.toString()).queue()));
 			} catch (Exception e) {
-				m.editMessage(":x: | Erro ao compilar: " + e.toString()).queue();
+				m.editMessage(":x: | Erro ao compilar: ```" + e.toString().replace("`", "´") + "```").queue();
 			}
 		});
 	}
