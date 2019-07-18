41aaf0b6800094032976c29f7816109a422c52cd
Yago <yagogimenez1@hotmail.com>
2019-07-18 10:06:47 -0300

    Correção de atribuição de cargos por level

--- src/main/java/com/kuuhaku/events/guild/GuildEvents.java
+++ src/main/java/com/kuuhaku/events/guild/GuildEvents.java
@@ -232,16 +232,15 @@ public class GuildEvents extends ListenerAdapter {
 									guild.getController().removeSingleRoleFromMember(member, guild.getRoleById((String) v2)).queue();
 							});
 							if (SQLite.getGuildById(guild.getId()).getLvlNotif() && !member.getRoles().contains(guild.getRoleById((String) v))) {
-								guild.getController().addSingleRoleToMember(member, guild.getRoleById((String) v)).queue();
-								try {
-									if (member.getRoles().contains(guild.getRoleById((String) v))) {
+								guild.getController().addSingleRoleToMember(member, guild.getRoleById((String) v)).queue(m -> {
+									try {
 										if (finalLvlChannel != null) {
 											finalLvlChannel.sendMessage(":tada: " + author.getAsMention() + " ganhou o cargo " + guild.getRoleById((String) v).getAsMention() + " por alcançar o nível " + k).queue();
 										} else
 											channel.sendMessage(":tada: " + author.getAsMention() + " ganhou o cargo " + guild.getRoleById((String) v).getAsMention() + " por alcançar o nível " + k).queue();
+									} catch (InsufficientPermissionException ignore) {
 									}
-								} catch (InsufficientPermissionException ignore) {
-								}
+								});
 							}
 						}
 					});
