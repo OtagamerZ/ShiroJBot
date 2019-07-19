0a3e05fb82762795da5aca8433c1c9cd44eb6d9b
Yago <yagogimenez1@hotmail.com>
2019-07-19 12:38:06 -0300

    Log

--- src/main/java/com/kuuhaku/command/commands/moderation/MakeLogCommand.java
+++ src/main/java/com/kuuhaku/command/commands/moderation/MakeLogCommand.java
@@ -11,24 +11,23 @@ import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
 public class MakeLogCommand extends Command {
 
 	public MakeLogCommand() {
-		super("log", new String[]{"makelog"}, "Cria um canal de log para as ações da Shiro.", Category.MODERACAO);
+		super("logchannel", new String[]{"makelog"}, "Cria um canal de log para as ações da Shiro.", Category.MODERACAO);
 	}
 
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
 		try {
-			guildConfig gc = SQLite.getGuildById(guild.getId());
 
-			if (gc.getLogChannel() != null && !gc.getLogChannel().isEmpty()) {
-				try {
-					guild.getTextChannelById(gc.getLogChannel()).delete().queue();
-				} catch (Exception ignore) {
-				}
-				guild.getController().createTextChannel("shiro-log").queue(c -> {
-					gc.setLogChannel(c.getId());
-					channel.sendMessage("Canal de log criado com sucesso em " + ((TextChannel) c).getAsMention()).queue();
-				});
+			guildConfig gc = SQLite.getGuildById(guild.getId());
+			try {
+				guild.getTextChannelById(gc.getLogChannel()).delete().queue();
+			} catch (Exception ignore) {
 			}
+
+			guild.getController().createTextChannel("shiro-log").queue(c -> {
+				gc.setLogChannel(c.getId());
+				channel.sendMessage("Canal de log criado com sucesso em " + ((TextChannel) c).getAsMention()).queue();
+			});
 
 			SQLite.updateGuildChannels(gc);
 		} catch (InsufficientPermissionException e) {
