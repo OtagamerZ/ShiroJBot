a74087a157b6a6bb633b168da0db18b139a4a8e1
Ygimenez <yagogimenez1@hotmail.com>
2019-07-22 11:44:33 -0300

    Correções gerais

--- src/main/java/com/kuuhaku/model/Member.java
+++ src/main/java/com/kuuhaku/model/Member.java
@@ -38,6 +38,7 @@ public class Member {
 	private String bio = "";
 	@Column(columnDefinition = "String default \"\"")
 	private String waifu = "";
+	@Column(columnDefinition = "boolean default false")
 	private boolean markForDelete;
 
 	public Member() {
