cc1f12e10bc6a848d47458b32cebc048efce10ca
Yago <yagogimenez1@hotmail.com>
2019-07-31 08:48:14 -0300

    Correção na captura de músicas

--- src/main/java/com/kuuhaku/command/Category.java
+++ src/main/java/com/kuuhaku/command/Category.java
@@ -29,7 +29,7 @@ public enum Category {
 	MISC("Diversos", "Comandos diversos.", PrivilegeLevel.USER),
 	INFO("Informação", "Comandos de informação", PrivilegeLevel.USER),
 	BEYBLADE("Beyblade", "Comandos de Beyblade.", PrivilegeLevel.USER),
-	MUSICA("Música", "Comandos de música.", PrivilegeLevel.USER)
+	MUSICA("Música", "Comandos de música.", PrivilegeLevel.DJ)
 	;
 
 	private final String name;
