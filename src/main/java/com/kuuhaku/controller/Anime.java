87da9c3a30ac0c3f7b4d38571f7c166239e96ef5
Ygimenez <yagogimenez1@hotmail.com>
2019-08-01 14:23:12 -0300

    Troca de API do DreamAnimes

--- src/main/java/com/kuuhaku/controller/Anime.java
+++ src/main/java/com/kuuhaku/controller/Anime.java
@@ -68,12 +68,13 @@ public class Anime {
     }
 
     public static JSONObject getDAData(String name) throws IOException {
-        URL url = new URL("https://shiro-api.herokuapp.com/apis/da?name=" + name);
+        URL url = new URL("https://www.dreamanimes.com.br/api/anime-info/" + name);
         HttpURLConnection con = (HttpURLConnection) url.openConnection();
         con.setRequestMethod("GET");
         con.setRequestProperty("Accept", "application/json");
         con.addRequestProperty("Accept-Charset", "UTF-8");
         con.addRequestProperty("User-Agent", "Mozilla/5.0");
+        con.addRequestProperty("Authorization", System.getenv("DA_TOKEN"));
 
         BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
 
@@ -86,6 +87,6 @@ public class Anime {
         con.disconnect();
 
         Helper.log(Tradutor.class, LogLevel.DEBUG, resposta.toString());
-        return new JSONObject(resposta.toString());
+        return new JSONObject(resposta.toString()).getJSONObject("anime");
     }
 }
