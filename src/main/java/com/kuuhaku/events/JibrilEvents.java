a223cc20fbb34622f103cdb30931754652674850
Yago <yagogimenez1@hotmail.com>
2019-08-05 09:14:40 -0300

    Moderação do relay melhorada

--- src/main/java/com/kuuhaku/events/JibrilEvents.java
+++ src/main/java/com/kuuhaku/events/JibrilEvents.java
@@ -5,9 +5,11 @@ import com.kuuhaku.controller.MySQL;
 import com.kuuhaku.model.RelayBlockList;
 import com.kuuhaku.utils.Helper;
 import com.kuuhaku.utils.LogLevel;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.core.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.ErrorResponseException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.imageio.ImageIO;
@@ -41,11 +43,6 @@ public class JibrilEvents extends ListenerAdapter {
 				event.getMessage().delete().queue();
 				event.getAuthor().openPrivateChannel().queue(c -> c.sendMessage(":x: | Você não pode mandar mensagens no chat global (bloqueado).").queue());
 				return;
-			} else if (event.getMessage().getContentRaw().length() < 5) {
-				event.getAuthor().openPrivateChannel().queue(c -> c.getMessageById(c.getLatestMessageId()).queue(m -> {
-					String s = ":warning: | Cuidado, mensagens de SPAM podem fazer com que você seja bloqueado do chat global (desconsiderar caso não tenha sido SPAM)!\nEsta mensagem não aparecerá novamente enquanto for a última mensagem.";
-					if (!m.getContentRaw().equals(s)) c.sendMessage(s).queue();
-				}));
 			}
 			String[] msg = event.getMessage().getContentRaw().split(" ");
 			for (int i = 0; i < msg.length; i++) {
@@ -58,6 +55,11 @@ public class JibrilEvents extends ListenerAdapter {
 			}
 			if (String.join(" ", msg).length() < 2000) {
 				try {
+					event.getAuthor().openPrivateChannel().queue(c -> c.getMessageById(c.getLatestMessageId()).queue(m -> {
+						String s = ":warning: | Cuidado, mensagens de SPAM podem fazer com que você seja bloqueado do chat global (isto é só um aviso!\nEsta mensagem não aparecerá novamente enquanto for a última mensagem.";
+						if (!m.getContentRaw().equals(s)) c.sendMessage(s).queue();
+					}));
+
 					if (MySQL.getTagById(event.getAuthor().getId()).isVerified() && event.getMessage().getAttachments().size() > 0) {
 						try {
 							ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -71,8 +73,16 @@ public class JibrilEvents extends ListenerAdapter {
 					Main.getRelay().relayMessage(event.getMessage(), String.join(" ", msg), event.getMember(), event.getGuild(), null);
 				} catch (NoResultException e) {
 					Main.getRelay().relayMessage(event.getMessage(), String.join(" ", msg), event.getMember(), event.getGuild(), null);
+				} catch (ErrorResponseException ex) {
+					if (ex.getErrorCode() == 50007) {
+						event.getChannel().sendMessage(":x: | " + event.getAuthor().getAsMention() + ", você está com a opção \"Permitir mensagens diretas de membros do servidor.\" desligada, você não poderá mandar mensagens no chat global enquanto ela estiver desligada.\nPara habilitá-la, vá em `Configurações de privacidade -> Mensagens diretas` no menu do canto superior esquerdo, ao lado do nome do servidor.").queue();
+						if (event.getGuild().getSelfMember().hasPermission(Permission.MESSAGE_MANAGE)) event.getMessage().delete().queue();
+					}
 				}
-			} else event.getChannel().sendMessage(":x: | Mensagem muito longa! (Max. 2000 caractéres)").queue();
+			} else {
+				event.getChannel().sendMessage(":x: | Mensagem muito longa! (Max. 2000 caractéres)").queue();
+				if (event.getGuild().getSelfMember().hasPermission(Permission.MESSAGE_MANAGE)) event.getMessage().delete().queue();
+			}
 		}
 	}
 }
