75baaf238610cf3286089897c6eed91bcc87f440
Yago <yagogimenez1@hotmail.com>
2019-08-13 11:51:42 -0300

    Bloqueio de imagens no chat global. Checagem de parceiros.

--- src/main/java/com/kuuhaku/events/cron/PartnerCheckEvent.java
+++ src/main/java/com/kuuhaku/events/cron/PartnerCheckEvent.java
@@ -10,15 +10,21 @@ import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 
+import javax.persistence.NoResultException;
+
 public class PartnerCheckEvent implements Job {
 	public static JobDetail check;
 
 	@Override
 	public void execute(JobExecutionContext context) {
 		Main.getJibril().getGuilds().forEach(g -> {
-			if (!MySQL.getTagById(g.getOwnerId()).isPartner()) {
-				g.leave().queue();
-				Helper.log(this.getClass(), LogLevel.INFO, "Saí do servidor " + g.getName() + " por não estar na lista de parceiros.");
+			try {
+				if (!MySQL.getTagById(g.getOwnerId()).isPartner()) {
+					g.leave().queue();
+					Helper.log(this.getClass(), LogLevel.INFO, "Saí do servidor " + g.getName() + " por " + g.getOwner().getUser().getAsTag() + " não estar na lista de parceiros.");
+				}
+			} catch (NoResultException e) {
+				Helper.log(this.getClass(), LogLevel.WARN, "Erro ao sair de " + g.getName() + ", gerenciado por " + g.getOwner().getUser().getAsTag() + "(" + g.getOwnerId() + ").");
 			}
 		});
 	}
