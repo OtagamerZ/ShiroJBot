9722bbd5e1045b3940cef6a5968b1315a32722c5
Yago <yagogimenez1@hotmail.com>
2019-08-20 00:37:18 -0300

    Correção no relay

--- src/main/java/com/kuuhaku/controller/Relay.java
+++ src/main/java/com/kuuhaku/controller/Relay.java
@@ -39,10 +39,7 @@ public class Relay extends SQLite {
 	private WebhookMessage getMessage(String msg, Member m, Guild s) {
 		WebhookMessageBuilder wmb = new WebhookMessageBuilder();
 
-		String filtered = Arrays.stream(msg.split(" ")).map(w -> w =
-				w.contains("<") && w.contains(">") && w.contains(":") ? ":question:" : w
-		).collect(Collectors.joining(" "));
-		wmb.setContent(filtered);
+		wmb.setContent(msg);
 		wmb.setAvatarUrl(RelayBlockList.checkThumb(m.getUser().getId()) ? "https://i.pinimg.com/originals/46/15/87/461587d51087bfdf8906149d356f972f.jpg" : m.getUser().getAvatarUrl());
 		wmb.setUsername("(" + s.getName() + ") " + m.getEffectiveName());
 		return wmb.build();
