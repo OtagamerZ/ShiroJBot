910d103517959e7f01b78801d97f28b5a5735774
Yago <yagogimenez1@hotmail.com>
2019-08-19 23:36:25 -0300

    Melhorias no controller relay. Correção na tag tóxico

--- src/main/java/com/kuuhaku/events/cron/UnblockEvent.java
+++ src/main/java/com/kuuhaku/events/cron/UnblockEvent.java
@@ -15,6 +15,10 @@ public class UnblockEvent implements Job {
 	public void execute(JobExecutionContext context) {
 		RelayBlockList.getBlockedIDs().forEach(id -> {
 			if (!id.isEmpty()) {
+				Main.getInfo().getDevelopers().forEach(d -> Main.getJibril().getUserById(d).openPrivateChannel().queue(c -> {
+					String msg = Main.getInfo().getUserByID(id) + " foi desbloqueado no chat global.";
+					c.sendMessage(msg).queue();
+				}));
 				Main.getJibril().getUserById(id).openPrivateChannel().queue(c -> c.sendMessage(":stopwatch: | O tempo do seu bloqueio acabou, você está liberado para conversar no chat global novamente.\n\nReincidências podem fazer com que seja bloqueado permanentemente do chat global.").queue());
 			}
 			unblock(id);
