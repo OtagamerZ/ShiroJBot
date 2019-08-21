897fc7efc0db5e99d6c2f800f00a0c7464ea13f3
Ygimenez <yagogimenez1@hotmail.com>
2019-08-21 10:00:14 -0300

    Desenvolvimento do módulo Exceed

--- src/main/java/com/kuuhaku/model/Profile.java
+++ src/main/java/com/kuuhaku/model/Profile.java
@@ -314,4 +314,13 @@ public class Profile {
 			}
 		}
 	}
+
+	public static void drawRotate(Graphics2D g2d, double x, double y, int angle, String text)
+	{
+		g2d.translate((float)x,(float)y);
+		g2d.rotate(Math.toRadians(angle));
+		g2d.drawString(text,0,0);
+		g2d.rotate(-Math.toRadians(angle));
+		g2d.translate(-(float)x,-(float)y);
+	}
 }
