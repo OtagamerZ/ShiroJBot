d05aad0af3c10853a1d88a16317002d1b8c83f3f
ygimenez <yagogimenez1@hotmail.com>
2019-08-21 10:32:23 -0300

    Desenvolvimento do módulo Exceed

--- src/main/java/com/kuuhaku/command/commands/misc/PingCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/PingCommand.java
@@ -15,7 +15,7 @@ public class PingCommand extends Command {
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
         if (guild.getSelfMember().hasPermission(Permission.MESSAGE_WRITE))
-            channel.sendMessage("Pong! :ping_pong: " + MySQL.findWinner().getName()).queue(msg -> msg.editMessage(msg.getContentRaw() + Main.getInfo().getPing() + " ms!").queue());
+            channel.sendMessage("Pong! :ping_pong: ").queue(msg -> msg.editMessage(msg.getContentRaw() + Main.getInfo().getPing() + " ms!").queue());
 	}
 
 }
