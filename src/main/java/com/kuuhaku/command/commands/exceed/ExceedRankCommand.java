ca2825b90184deb704d29ffb2f8add168c8d6524
Ygimenez <yagogimenez1@hotmail.com>
2019-08-21 10:09:54 -0300

    Desenvolvimento do módulo Exceed

--- src/main/java/com/kuuhaku/command/commands/exceed/ExceedRankCommand.java
+++ src/main/java/com/kuuhaku/command/commands/exceed/ExceedRankCommand.java
@@ -67,8 +67,7 @@ public class ExceedRankCommand extends Command {
 
 				for (int i = 0; i < 6; i++) {
 					int h = (int) (10 + ((exceeds.get(i).getExp() * 100 / total) * 90 / 100)) * 358 / 100;
-					Profile.drawRotate(g2d, 186 + (113 * i), 577 - h, 45, names.get(i));
-					Profile.printCenteredString(names.get(i), 68, 152 + (113 * i), 577 - h, g2d);
+					Profile.drawRotate(g2d, 186 + (113 * i), 577 - h, -45, names.get(i));
 					g2d.setClip(new Rectangle2D.Float(152 + (113 * i), 230 + (358 - h), 68, h));
 					g2d.drawImage(bars.get(i), null, 152 + (113 * i), 230);
 					g2d.setClip(null);
