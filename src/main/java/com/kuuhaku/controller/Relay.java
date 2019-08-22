25acdd3b582eca3e36cbfea7b4c003b82ac880f5
Yago <yagogimenez1@hotmail.com>
2019-08-22 09:34:28 -0300

    Pequenas melhorias no relay

--- src/main/java/com/kuuhaku/controller/Relay.java
+++ src/main/java/com/kuuhaku/controller/Relay.java
@@ -44,7 +44,7 @@ public class Relay extends SQLite {
 		String filtered = Arrays.stream(msg.split(" ")).map(w -> w =
 				(w.contains("<") && w.contains(">") && w.contains(":")) ? ":question:" : w
 		).collect(Collectors.joining(" "));
-		wmb.setContent("||" + m.getUser().getId() + "||\n" + filtered);
+		wmb.setContent(filtered + "\n||" + m.getUser().getId() + "||");
 		wmb.setAvatarUrl(RelayBlockList.checkThumb(m.getUser().getId()) ? "https://i.pinimg.com/originals/46/15/87/461587d51087bfdf8906149d356f972f.jpg" : m.getUser().getAvatarUrl());
 		wmb.setUsername("(" + s.getName() + ") " + (exceed.isEmpty() ? "" : "[" + exceed + "] ") + m.getEffectiveName());
 		return wmb.build();
