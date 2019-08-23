42d43a1cbfa24604b9f86b12e3f4e2af501bf9a6
Yago <yagogimenez1@hotmail.com>
2019-08-23 13:51:58 -0300

    Correção do permablock

--- src/main/java/com/kuuhaku/events/cron/UnblockEvent.java
+++ src/main/java/com/kuuhaku/events/cron/UnblockEvent.java
@@ -1,12 +1,9 @@
 package com.kuuhaku.events.cron;
 
-import com.kuuhaku.Main;
 import com.kuuhaku.model.RelayBlockList;
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
-
-import static com.kuuhaku.model.RelayBlockList.unblock;
 
 public class UnblockEvent implements Job {
 	public static JobDetail unblock;
@@ -14,15 +11,6 @@ public class UnblockEvent implements Job {
 	@Override
 	public void execute(JobExecutionContext context) {
 		RelayBlockList.clearBlockedThumbs();
-		RelayBlockList.getBlockedIDs().forEach(id -> {
-			if (!id.isEmpty()) {
-				Main.getInfo().getDevelopers().forEach(d -> Main.getJibril().getUserById(d).openPrivateChannel().queue(c -> {
-					String msg = Main.getInfo().getUserByID(id) + " foi desbloqueado no chat global.";
-					c.sendMessage(msg).queue();
-				}));
-				Main.getJibril().getUserById(id).openPrivateChannel().queue(c -> c.sendMessage(":stopwatch: | O tempo do seu bloqueio acabou, você está liberado para conversar no chat global novamente.\n\nReincidências podem fazer com que seja bloqueado permanentemente do chat global.").queue());
-			}
-			unblock(id);
-		});
+		RelayBlockList.refresh();
 	}
 }
