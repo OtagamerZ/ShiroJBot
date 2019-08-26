77fb69059f4e75f0d97364ef918e70dade404980
ygimenez <yagogimenez1@hotmail.com>
2019-08-26 11:15:23 -0300

    Comando ID corrigido

--- src/main/java/com/kuuhaku/utils/BannedVars.java
+++ src/main/java/com/kuuhaku/utils/BannedVars.java
@@ -3,14 +3,14 @@ package com.kuuhaku.utils;
 public class BannedVars {
 	public static final String[] vars = new String[]{
 			"getJDA()",
-			//"getAPI()",
+			"getAPI()",
 			"getManager()",
 			"getController()",
 			"getSelfUser()",
 			"getSelfMember()",
 			"delete()",
-			//"Main.",
-			//"getInfo()",
+			"Main.",
+			"getInfo()",
 			"while (true)",
 			"while (false)",
 			"import",
