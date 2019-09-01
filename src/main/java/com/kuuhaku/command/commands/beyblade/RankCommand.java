593370650e9e116e59e193f2c27be556e349eaad
Ygimenez <yagogimenez1@hotmail.com>
2019-09-01 01:16:58 -0300

    Pequenas correções

--- src/main/java/com/kuuhaku/command/commands/beyblade/RankCommand.java
+++ src/main/java/com/kuuhaku/command/commands/beyblade/RankCommand.java
@@ -23,25 +23,29 @@ public class RankCommand extends Command {
     @Override
     public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
         channel.sendMessage("<a:Loading:598500653215645697> Buscando dados...").queue(m -> {
-            List<Beyblade> rank = MySQL.getBeybladeList();
-            assert rank != null;
-            rank.sort(Comparator.comparing(Beyblade::getKDA));
-            Collections.reverse(rank);
-            Beyblade champ = rank.get(0);
-            rank.remove(0);
-            EmbedBuilder eb = new EmbedBuilder();
-            StringBuilder sb = new StringBuilder();
+			try {
+				List<Beyblade> rank = MySQL.getBeybladeList();
+				assert rank != null;
+				rank.sort(Comparator.comparing(Beyblade::getKDA));
+				Collections.reverse(rank);
+				Beyblade champ = rank.get(0);
+				rank.remove(0);
+				EmbedBuilder eb = new EmbedBuilder();
+				StringBuilder sb = new StringBuilder();
 
-            eb.setTitle(":bar_chart: TOP 10 Beyblades");
-            eb.setThumbnail("https://www.pngkey.com/png/full/21-217733_free-png-trophy-png-images-transparent-winner-trophy.png");
-            eb.setColor(Color.decode(champ.getColor()));
-            for (int i = 0; i < rank.size() && i < 10; i++) {
-                sb.append(i + 2).append(" - ").append(rank.get(i).getName()).append(" (").append(Main.getInfo().getUserByID(rank.get(i).getId()).getName()).append(") | ").append(rank.get(i).getWins()).append("/").append(rank.get(i).getLoses()).append("\n");
-            }
-            eb.addField("1 - " + champ.getName() + " (" + Main.getInfo().getUserByID(champ.getId()).getName() + ") | " + champ.getWins() + "/" + champ.getLoses(), sb.toString(), false);
+				eb.setTitle(":bar_chart: TOP 10 Beyblades");
+				eb.setThumbnail("https://www.pngkey.com/png/full/21-217733_free-png-trophy-png-images-transparent-winner-trophy.png");
+				eb.setColor(Color.decode(champ.getColor()));
+				for (int i = 0; i < rank.size() && i < 10; i++) {
+					sb.append(i + 2).append(" - ").append(rank.get(i).getName()).append(" (").append(Main.getInfo().getUserByID(rank.get(i).getId()).getName()).append(") | ").append(rank.get(i).getWins()).append("/").append(rank.get(i).getLoses()).append("\n");
+				}
+				eb.addField("1 - " + champ.getName() + " (" + Main.getInfo().getUserByID(champ.getId()).getName() + ") | " + champ.getWins() + "/" + champ.getLoses(), sb.toString(), false);
 
-            m.delete().queue();
-            channel.sendMessage(eb.build()).queue();
+				m.delete().queue();
+				channel.sendMessage(eb.build()).queue();
+			} catch (Exception e) {
+				m.editMessage(":x: | Erro ao gerar placares. Este módulo está depreciado, um novo virá para substituí-lo, aguarde.").queue();
+			}
         });
     }
 }
