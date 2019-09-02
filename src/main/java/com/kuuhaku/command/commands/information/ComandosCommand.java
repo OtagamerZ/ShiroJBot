6be771c8d1bf58ed2e892d30b5892b728c04f980
Yago <yagogimenez1@hotmail.com>
2019-09-02 11:55:13 -0300

    Pequenas correções

--- src/main/java/com/kuuhaku/command/commands/information/ComandosCommand.java
+++ src/main/java/com/kuuhaku/command/commands/information/ComandosCommand.java
@@ -15,82 +15,100 @@ import java.util.Map;
 public class ComandosCommand extends Command {
 
 	public ComandosCommand() {
-		super("comandos", new String[] {"cmds", "cmd", "comando", "ajuda", "help"}, "Fornece uma lista de todos os comandos disponiveis no bot.", Category.INFO);
+		super("comandos", new String[]{"cmds", "cmd", "comando", "ajuda", "help"}, "Fornece uma lista de todos os comandos disponiveis no bot.", Category.INFO);
 	}
 
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
 
-			EmbedBuilder eb = new EmbedBuilder();
-			eb.setColor(Color.PINK);
-			eb.setFooter(Main.getInfo().getFullName(), null);
-			eb.setThumbnail("https://cdn.pixabay.com/photo/2012/04/14/16/26/question-34499_960_720.png");
+		EmbedBuilder eb = new EmbedBuilder();
+		eb.setTitle("**Lista de Comandos**");
+		eb.setDescription("Clique nas categorias abaixo para ver os comandos de cada uma.");
+		for (Category cat : Category.values()) {
+			eb.addField(cat.getEMOTE() + " | " + cat.getName(), Helper.VOID, true);
+		}
+		eb.setColor(Color.PINK);
+		eb.setFooter(Main.getInfo().getFullName(), null);
+		eb.setThumbnail("https://cdn.pixabay.com/photo/2012/04/14/16/26/question-34499_960_720.png");
 
-			if(args.length == 0) {
-				Map<String, MessageEmbed> pages = new HashMap<>();
 
-				for(Category cat : Category.values()) {
-					EmbedBuilder ceb = new EmbedBuilder();
-					eb.setTitle("**Lista de Comandos**");
+		if (args.length == 0) {
+			Map<String, MessageEmbed> pages = new HashMap<>();
 
-					eb.setDescription("Prefixo: `" + prefix + "`\n"
-							+ Category.values().length + " categorias encontradas!" + "\n"
-							+ Main.getCommandManager().getCommands().size() + " comandos encontrados!"
-							+ "\n" + Helper.VOID);
+			for (Category cat : Category.values()) {
+				EmbedBuilder ceb = new EmbedBuilder();
+				ceb.setColor(Color.PINK);
+				ceb.setFooter(Main.getInfo().getFullName(), null);
+				ceb.setThumbnail("https://cdn.pixabay.com/photo/2012/04/14/16/26/question-34499_960_720.png");
 
-					if(cat.isEnabled())
-						continue;
-					if(cat.getCmds().size()==0) {
-						ceb.addField(cat.getName(), cat.getDescription() + "\n*Ainda não existem comandos nesta categoria.*", false);
-						continue;
-					}
+				ceb.setDescription("Prefixo: `" + prefix + "`\n"
+						+ Category.values().length + " categorias encontradas!" + "\n"
+						+ Main.getCommandManager().getCommands().size() + " comandos encontrados!"
+						+ "\n" + Helper.VOID);
 
-					StringBuilder cmds = new StringBuilder();
-
-					for(Command cmd : cat.getCmds()) { cmds.append("`").append(cmd.getName()).append("`  "); }
-
-					ceb.addField(cat.getName(), cat.getDescription() + "\n" + cmds.toString().trim(), false);
-					eb.addField(Helper.VOID, "Para informações sobre um comando em especifico digite `" + prefix + "cmds [comando]`.", false);
-					pages.put(cat.getEMOTE(), ceb.build());
+				if (cat.isEnabled())
+					continue;
+				if (cat.getCmds().size() == 0) {
+					ceb.addField(cat.getName(), cat.getDescription() + "\n*Ainda não existem comandos nesta categoria.*", false);
+					continue;
 				}
 
-				channel.sendMessage(eb.build()).queue(s -> Helper.categorize(s, pages));
-				return;
+				StringBuilder cmds = new StringBuilder();
+
+				for (Command cmd : cat.getCmds()) {
+					cmds.append("`").append(cmd.getName()).append("`  ");
+				}
+
+				ceb.addField(cat.getName(), cat.getDescription() + "\n" + cmds.toString().trim(), false);
+				ceb.addField(Helper.VOID, "Para informações sobre um comando em especifico digite `" + prefix + "cmds [comando]`.", false);
+				pages.put(cat.getEMOTE(), ceb.build());
 			}
 
-			String cmdName = args[0];
+			channel.sendMessage(eb.build()).queue(s -> Helper.categorize(s, pages));
+			return;
+		}
 
-			Command cmd = null;
+		String cmdName = args[0];
 
-			for(Command cmmd : Main.getCommandManager().getCommands()) {
-				boolean found = false;
-				if(cmmd.getName().equalsIgnoreCase(cmdName)) { found = true; }
+		Command cmd = null;
 
-				for(String alias : cmmd.getAliases()) { if(alias.equalsIgnoreCase(cmdName)) { found = true; } }
+		for (Command cmmd : Main.getCommandManager().getCommands()) {
+			boolean found = false;
+			if (cmmd.getName().equalsIgnoreCase(cmdName)) {
+				found = true;
+			}
 
-				if(found) {
-					cmd = cmmd;
-					break;
+			for (String alias : cmmd.getAliases()) {
+				if (alias.equalsIgnoreCase(cmdName)) {
+					found = true;
 				}
 			}
 
-			if(cmd==null) {
-				channel.sendMessage(":x: | Esse comando não foi encontrado!").queue();
-				return;
+			if (found) {
+				cmd = cmmd;
+				break;
 			}
+		}
 
-			eb.setTitle(cmd.getName() + (cmd.getUsage()!=null ? " " + cmd.getUsage() : ""));
+		if (cmd == null) {
+			channel.sendMessage(":x: | Esse comando não foi encontrado!").queue();
+			return;
+		}
 
-			StringBuilder aliases = new StringBuilder("**Aliases**: ");
+		eb.setTitle(cmd.getName() + (cmd.getUsage() != null ? " " + cmd.getUsage() : ""));
 
-			for(String al : cmd.getAliases()) { aliases.append("`").append(al).append("` "); }
+		StringBuilder aliases = new StringBuilder("**Aliases**: ");
 
-			eb.setDescription(cmd.getDescription() + "\n"
-					+ (cmd.getAliases().length!=0 ? aliases.toString().trim() + "\n" : "")
-					+ "**Categoria**: " + cmd.getCategory().getName()
-					+ "\n");
+		for (String al : cmd.getAliases()) {
+			aliases.append("`").append(al).append("` ");
+		}
 
-			channel.sendMessage(eb.build()).queue();
-		
+		eb.setDescription(cmd.getDescription() + "\n"
+				+ (cmd.getAliases().length != 0 ? aliases.toString().trim() + "\n" : "")
+				+ "**Categoria**: " + cmd.getCategory().getName()
+				+ "\n");
+
+		channel.sendMessage(eb.build()).queue();
+
 	}
 }
