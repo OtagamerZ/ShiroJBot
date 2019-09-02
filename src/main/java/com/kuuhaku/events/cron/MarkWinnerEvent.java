6e3685258b8f819952f0f2fef715387f97ecd1b3
Ygimenez <yagogimenez1@hotmail.com>
2019-09-02 10:16:35 -0300

    Pequenas correções

--- src/main/java/com/kuuhaku/events/cron/MarkWinnerEvent.java
+++ src/main/java/com/kuuhaku/events/cron/MarkWinnerEvent.java
@@ -23,7 +23,6 @@ public class MarkWinnerEvent implements Job {
 				Main.getInfo().getUserByID(em.getMid()).openPrivateChannel().queue(c -> {
 					try {
 						c.sendMessage("O seu exceed foi campeão neste mês, parabéns!\n" +
-								"Foi adicionado 5 essências no cofre do seu exceed, o lider poderá utilizá-las para adquirir melhorias e bônus para os membros.\n" +
 								"Todos da " + ex + " ganharão experiência em dobro durante 1 semana.").queue();
 					} catch (Exception ignore) {
 					}
