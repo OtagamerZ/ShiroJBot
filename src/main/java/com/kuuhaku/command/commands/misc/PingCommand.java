ecc747c1d466336585affb88c9d13bd50b23df65
Yago <yagogimenez1@hotmail.com>
2019-09-02 10:26:40 -0300

    Pequenas correções

--- src/main/java/com/kuuhaku/command/commands/misc/PingCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/PingCommand.java
@@ -17,14 +17,5 @@ public class PingCommand extends Command {
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
         if (guild.getSelfMember().hasPermission(Permission.MESSAGE_WRITE))
             channel.sendMessage("Pong! :ping_pong: ").queue(msg -> msg.editMessage(msg.getContentRaw() + Main.getInfo().getPing() + " ms!").queue());
-		String ex = MySQL.getWinner();
-		MySQL.getExceedMembers(ExceedEnums.getByName(ex)).forEach(em ->
-				Main.getInfo().getUserByID(em.getMid()).openPrivateChannel().queue(c -> {
-					try {
-						c.sendMessage("O seu exceed foi campeão neste mês, parabéns!\n" +
-								"Todos da " + ex + " ganharão experiência em dobro durante 1 semana.").queue();
-					} catch (Exception ignore) {
-					}
-				}));
 	}
 }
