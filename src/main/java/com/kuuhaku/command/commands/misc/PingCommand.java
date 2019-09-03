496f6114b351c36de8ef62793d2fb06741e75f1b
ygimenez <yagogimenez1@hotmail.com>
2019-09-03 14:08:39 -0300

    Pequenas correções

--- src/main/java/com/kuuhaku/command/commands/misc/PingCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/PingCommand.java
@@ -3,19 +3,19 @@ package com.kuuhaku.command.commands.misc;
 import com.kuuhaku.Main;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
-import com.kuuhaku.controller.MySQL;
-import com.kuuhaku.utils.ExceedEnums;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.Event;
 
 public class PingCommand extends Command {
-	
-	public PingCommand() { super("ping", "Ping", Category.MISC); }
+
+	public PingCommand() {
+		super("ping", "Ping", Category.MISC);
+	}
 
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
-        if (guild.getSelfMember().hasPermission(Permission.MESSAGE_WRITE))
-            channel.sendMessage("Pong! :ping_pong: ").queue(msg -> msg.editMessage(msg.getContentRaw() + Main.getInfo().getPing() + " ms!").queue());
+		if (guild.getSelfMember().hasPermission(Permission.MESSAGE_WRITE))
+			channel.sendMessage("Pong! :ping_pong: ").queue(msg -> msg.editMessage(msg.getContentRaw() + Main.getInfo().getPing() + " ms!\nUso de CPU: " + Main.getInfo().getCPULoad() + "%").queue());
 	}
 }
