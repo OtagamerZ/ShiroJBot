1831f654ebf0d7f91db9b161894414fee326c440
Yago <yagogimenez1@hotmail.com>
2019-09-04 10:34:37 -0300

    Correção do compilador

--- src/main/java/com/kuuhaku/command/commands/misc/CompileCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/CompileCommand.java
@@ -44,9 +44,7 @@ public class CompileCommand extends Command {
 
 				@Override
 				public Object get() {
-					System.out.println("Executando");
 					Main.getInfo().getPool().submit(() -> {
-						System.out.println("Adicionado à fila");
 						final long start = System.currentTimeMillis();
 						try {
 							String code = String.join(" ", args);
@@ -75,6 +73,30 @@ public class CompileCommand extends Command {
 
 				@Override
 				public Object get(long timeout, @NotNull TimeUnit unit) {
+					Main.getInfo().getPool().submit(() -> {
+						final long start = System.currentTimeMillis();
+						try {
+							String code = String.join(" ", args);
+							if (!code.contains("out")) throw new IllegalArgumentException("Código sem retorno.");
+							else if (code.contains("```") && !code.contains("```java")) {
+								throw new IllegalArgumentException("Bloco de código com começo incorreto");
+							} else if (Arrays.stream(BannedVars.vars).parallel().anyMatch(code::contains))
+								throw new IllegalAccessException("Código com métodos proibidos.");
+							code = code.replace("```java", "").replace("```", "");
+							Interpreter i = new Interpreter();
+							i.set("msg", message);
+							i.set("code", String.join(" ", args));
+							i.eval(code);
+							Object out = i.get("out");
+							m.editMessage("<:Verified:591425071772467211> | Compilado com sucesso!").queue(n ->
+									m.getChannel().sendMessage("<a:Loading:598500653215645697> | Executando...").queue(d ->
+											d.editMessage("-> " + out.toString()).queue()));
+							message.delete().queue();
+							channel.sendMessage("<:Verified:591425071772467211> | Tempo de execução: " + (System.currentTimeMillis() - start) + " ms").queue();
+						} catch (Exception e) {
+							m.editMessage(":x: | Erro ao compilar: ```" + e.toString().replace("`", "´") + "```").queue();
+						}
+					});
 					return null;
 				}
 			};
