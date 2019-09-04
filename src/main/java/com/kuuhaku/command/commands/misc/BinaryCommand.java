a7e6f2ee4804d165260ca8244ce4a3a59b35e9f1
Yago <yagogimenez1@hotmail.com>
2019-09-04 14:22:21 -0300

    Adição do reinicializador

---
+++ src/main/java/com/kuuhaku/command/commands/misc/BinaryCommand.java
@@ -0,0 +1,35 @@
+package com.kuuhaku.command.commands.misc;
+
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.Event;
+
+public class BinaryCommand extends Command {
+
+	public BinaryCommand() {
+		super("bin", "Transforma uma frase ou texto em binário", Category.MISC);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
+		String text = String.join(" ", args);
+		try {
+			byte[] bytes = text.getBytes();
+			StringBuilder binary = new StringBuilder();
+			for (byte b : bytes)
+			{
+				int val = b;
+				for (int i = 0; i < 8; i++)
+				{
+					binary.append((val & 128) == 0 ? 0 : 1);
+					val <<= 1;
+				}
+				binary.append(' ');
+			}
+			channel.sendMessage(":1234: " + binary.toString()).queue();
+		} catch (IllegalStateException e) {
+			channel.sendMessage(":x: | Mensagem muito grande (Max: 2000 | Lembre-se que cada caractére vale por 8, incluindo espaços).").queue();
+		}
+	}
+}
--- src/main/java/com/kuuhaku/command/commands/misc/CompileCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/CompileCommand.java
@@ -24,37 +24,41 @@ public class CompileCommand extends Command {
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
 		channel.sendMessage("<a:Loading:598500653215645697> | Compilando...").queue(m -> {
-			Future<?> execute = Main.getInfo().getPool().submit(() -> {
-						final long start = System.currentTimeMillis();
-						try {
-							String code = String.join(" ", args);
-							if (!code.contains("out")) throw new IllegalArgumentException("Código sem retorno.");
-							else if (code.contains("```") && !code.contains("```java")) {
-								throw new IllegalArgumentException("Bloco de código com começo incorreto");
-							} else if (Arrays.stream(BannedVars.vars).parallel().anyMatch(code::contains))
-								throw new IllegalAccessException("Código com métodos proibidos.");
-							code = code.replace("```java", "").replace("```", "");
-							Interpreter i = new Interpreter();
-							i.set("msg", message);
-							i.set("code", String.join(" ", args));
-							i.eval(code);
-							Object out = i.get("out");
-							m.getChannel().sendMessage("<a:Loading:598500653215645697> | Executando...").queue(d ->
-									d.editMessage("-> " + out.toString()).queue());
-							message.delete().queue();
-							m.editMessage("<:Verified:591425071772467211> | Tempo de execução: " + (System.currentTimeMillis() - start) + " ms").queue();
-						} catch (Exception e) {
-							m.editMessage(":x: | Erro ao compilar: ```" + e.toString().replace("`", "´") + "```").queue();
-						}
-						return null;
-					});
 			try {
-				execute.get(10, TimeUnit.SECONDS);
-			} catch (InterruptedException | ExecutionException e) {
-				Helper.log(this.getClass(), LogLevel.ERROR, e + " | " + e.getStackTrace()[0]);
-			} catch (TimeoutException e) {
-				execute.cancel(true);
-				m.editMessage(":x: | Tempo limite de execução esgotado.").queue();
+				Future<?> execute = Main.getInfo().getPool().submit(() -> {
+					final long start = System.currentTimeMillis();
+					try {
+						String code = String.join(" ", args);
+						if (!code.contains("out")) throw new IllegalArgumentException("Código sem retorno.");
+						else if (code.contains("```") && !code.contains("```java")) {
+							throw new IllegalArgumentException("Bloco de código com começo incorreto");
+						} else if (Arrays.stream(BannedVars.vars).parallel().anyMatch(code::contains))
+							throw new IllegalAccessException("Código com métodos proibidos.");
+						code = code.replace("```java", "").replace("```", "");
+						Interpreter i = new Interpreter();
+						i.set("msg", message);
+						i.set("code", String.join(" ", args));
+						i.eval(code);
+						Object out = i.get("out");
+						m.getChannel().sendMessage("<a:Loading:598500653215645697> | Executando...").queue(d ->
+								d.editMessage("-> " + out.toString()).queue());
+						message.delete().queue();
+						m.editMessage("<:Verified:591425071772467211> | Tempo de execução: " + (System.currentTimeMillis() - start) + " ms").queue();
+					} catch (Exception e) {
+						m.editMessage(":x: | Erro ao compilar: ```" + e.toString().replace("`", "´") + "```").queue();
+					}
+					return null;
+				});
+				try {
+					execute.get(10, TimeUnit.SECONDS);
+				} catch (InterruptedException | ExecutionException e) {
+					Helper.log(this.getClass(), LogLevel.ERROR, e + " | " + e.getStackTrace()[0]);
+				} catch (TimeoutException e) {
+					execute.cancel(true);
+					m.editMessage(":x: | Tempo limite de execução esgotado.").queue();
+				}
+			} catch (Exception e) {
+				m.editMessage(":x: | Erro ao compilar: ```" + e.toString().replace("`", "´") + "```").queue();
 			}
 		});
 	}
