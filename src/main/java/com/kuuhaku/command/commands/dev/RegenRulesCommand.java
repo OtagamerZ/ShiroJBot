b33b8852fa39ee79783e7090330516a125ab81b6
Yago <yagogimenez1@hotmail.com>
2019-09-10 09:15:55 -0300

    Dois novos comandos (checagem de perms e regenerar regras)

---
+++ src/main/java/com/kuuhaku/command/commands/dev/RegenRulesCommand.java
@@ -0,0 +1,46 @@
+package com.kuuhaku.command.commands.dev;
+
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.utils.Helper;
+import com.kuuhaku.utils.LogLevel;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.Event;
+
+import java.io.IOException;
+
+public class RegenRulesCommand extends Command {
+
+	public RegenRulesCommand() {
+		super("rrules", new String[]{"makerules"}, "Regenera as regras do servidor de suporte.", Category.DEVS);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
+		message.delete().queue(s -> {
+			try {
+				channel.sendFile(Helper.getImage("https://i.imgur.com/JQ3LvGK.png"), "title.png").queue();
+				channel.sendFile(Helper.getImage("https://i.imgur.com/9dfpeel.png"), "welcome.png").queue();
+				channel.sendMessage("Seja bem-vindo(a) ao meu servidor oficial de suporte, qualquer duvida que tenha sobre como me utilizar será esclarecida por um de nossos membros, fique à vontade e lembre-se de sempre relatar quando achar algo suspeito").queue();
+				channel.sendFile(Helper.getImage("https://i.imgur.com/aCYUW1G.png"), "rules.png").queue();
+				channel.sendMessage("**1 - É proibido qualquer ato de SPAM, flood ou bullying**\n" +
+						"Por razões obvias, tais coisas poluem o servidor e prejudicam a comunidade e é considerado um ato grave para medidas de punição.\n" +
+						"Infratores serão punidos de acordo com seus atos.\n\n" +
+						"**2 - Proibido postar qualquer conteúdo pornográfico ou racista**\n" +
+						"Ninguem gosta de racismo, e este servidor não é um lugar para coisas NSFW, então mantenha-os no seu HD. (Incluí usar avatares disso)\n\n" +
+						"**3 - Atenha-se ao tópico**\n" +
+						"Existem vários canais para diversos assuntos, não há necessidade de entupir tudo em um canal só.\n\n" +
+						"**4 - Não peça cargos, xp ou coisas do tipo**\n" +
+						"Eu sou a única que gerencia isso, e meu Nii-chan não irá favorecer ninguem!\n" +
+						"Pronto pessoal, isso é tudo!\n" +
+						"Divirta-se e, caso tenha lido as regras você pode utilizar o comando s!arespostaé RESPOSTA para completar a seguinte frase e ganhar um emblema único:\n" +
+						"Infratores serão `_______ __ ______ ___ ____ ____`\n\n" +
+						"É proibido compartilhar qual é a resposta, se não não teria graça!").queue();
+				channel.sendFile(Helper.getImage("https://i.imgur.com/U9lTSWD.png"), "partners.png").queue();
+			} catch (IOException e) {
+				Helper.log(this.getClass(), LogLevel.ERROR, e + " | " + e.getStackTrace()[0]);
+			}
+		});
+	}
+}
