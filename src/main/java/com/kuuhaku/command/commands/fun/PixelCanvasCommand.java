9bcbb7ec73b04f1bc4cc0f72611bd20d2c7ce8ef
Yago <yagogimenez1@hotmail.com>
2019-09-11 12:21:53 -0300

    Correção do canvas

--- src/main/java/com/kuuhaku/command/commands/fun/PixelCanvasCommand.java
+++ src/main/java/com/kuuhaku/command/commands/fun/PixelCanvasCommand.java
@@ -29,7 +29,7 @@ public class PixelCanvasCommand extends Command {
 			if (opts.length == 1) {
 				channel.sendMessage(":x: | É preciso especificar a coordenada e a cor neste formato: `X;Y;#cor`.\nPara ver um chunk, digite apenas as coordenadas X e Y.").queue();
 				return;
-			} else if (Integer.parseInt(opts[0]) > 513 || Integer.parseInt(opts[1]) > 513 || Integer.parseInt(opts[0]) < -512 || Integer.parseInt(opts[1]) < -512) {
+			} else if (Integer.parseInt(opts[0]) > 512 || Integer.parseInt(opts[1]) > 513 || Integer.parseInt(opts[0]) < -513 || Integer.parseInt(opts[1]) < -512) {
 				channel.sendMessage(":x: | As coordenadas devem estar dentro da grade de 1024px x 1024px.").queue();
 				return;
 			}
@@ -42,7 +42,7 @@ public class PixelCanvasCommand extends Command {
 			int[] coords = new int[]{Integer.parseInt(opts[0]), Integer.parseInt(opts[1])};
 
 			if (opts.length == 2) {
-				if (coords[0] < 3 && coords[0] > -2 && coords[1] < 3 && coords[1] > -2) {
+				if (coords[0] < 2 && coords[0] > -3 && coords[1] < 3 && coords[1] > -2) {
 					Main.getInfo().getCanvas().viewChunk(message.getTextChannel(), coords).queue();
 					return;
 				} else {
