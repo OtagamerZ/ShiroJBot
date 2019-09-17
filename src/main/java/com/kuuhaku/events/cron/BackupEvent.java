b9413a2c7df2bf9e999388c8c766a809a3e3ad5f
Yago <yagogimenez1@hotmail.com>
2019-09-17 13:56:12 -0300

    Implementação do Tet

--- src/main/java/com/kuuhaku/events/cron/BackupEvent.java
+++ src/main/java/com/kuuhaku/events/cron/BackupEvent.java
@@ -6,6 +6,7 @@ import com.kuuhaku.controller.SQLite;
 import com.kuuhaku.model.DataDump;
 import com.kuuhaku.utils.Helper;
 import com.kuuhaku.utils.LogLevel;
+import net.dv8tion.jda.api.entities.Activity;
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
@@ -16,6 +17,7 @@ public class BackupEvent implements Job {
 	@Override
 	public void execute(JobExecutionContext context) {
 		Main.getInfo().getAPI().getPresence().setActivity(Main.getRandomActivity());
+		Main.getTet().getPresence().setActivity(Activity.playing(" em diversos mundos espalhados em " + Main.getTet().getGuilds().size() + " servidores!"));
 		MySQL.dumpData(new DataDump(SQLite.getCADump(), SQLite.getGuildDump()));
 		Helper.log(this.getClass(), LogLevel.INFO, "Respostas/Guilds salvos com sucesso!");
 		MySQL.dumpData(new DataDump(SQLite.getMemberDump()));
