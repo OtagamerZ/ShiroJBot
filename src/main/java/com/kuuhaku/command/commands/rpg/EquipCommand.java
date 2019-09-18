f657884ccc464395601ed33fb81cb8e794bc5749
Yago <yagogimenez1@hotmail.com>
2019-09-18 10:18:54 -0300

    Implementação do Tet

--- src/main/java/com/kuuhaku/command/commands/rpg/EquipCommand.java
+++ src/main/java/com/kuuhaku/command/commands/rpg/EquipCommand.java
@@ -18,7 +18,7 @@ public class EquipCommand extends Command {
 
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
-		if (Main.getInfo().getGames().get(guild.getId()).getMaster() != author) {
+		if (Main.getInfo().getGames().get(guild.getId()).getPlayers().containsKey(author.getId())) {
 			Equipped inv = Main.getInfo().getGames().get(guild.getId()).getPlayers().get(author.getId()).getCharacter().getInventory();
 			inv.equip(inv.getItem(String.join(" ", args)));
 			channel.sendMessage("Item " + String.join(" ", args) + " equipado.").queue();
