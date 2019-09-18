b45fe882df7982b3c199e338d1da46822d17c7ee
ygimenez <yagogimenez1@hotmail.com>
2019-09-18 09:30:23 -0300

    Implementação do Tet

---
+++ src/main/java/com/kuuhaku/command/commands/rpg/RemoveCommand.java
@@ -0,0 +1,69 @@
+package com.kuuhaku.command.commands.rpg;
+
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.handlers.games.RPG.Exceptions.UnknownItemException;
+import com.kuuhaku.handlers.games.RPG.Handlers.ItemRegisterHandler;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.Event;
+
+import java.util.Arrays;
+
+public class RemoveCommand extends Command {
+
+	public RemoveCommand() {
+		super("rremover", new String[]{"rremove"}, "Remove um registro", Category.BEYBLADE);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
+		if (Main.getInfo().getGames().get(guild.getId()).getMaster() == author) {
+			if (args.length < 2) {
+				channel.sendMessage(":x: | É necessário especificar o tipo de registro e o nome do registro").queue();
+				return;
+			}
+			try {
+				switch (args[0]) {
+					case "p":
+					case "player":
+						if (message.getMentionedUsers().size() == 0) {
+							channel.sendMessage(":x: | É necessário especificar o jogador por meio de menção").queue();
+							return;
+						}
+						Main.getInfo().getGames().get(guild.getId()).getPlayers().remove(message.getMentionedUsers().get(0).getId());
+						channel.sendMessage("Jogador excluído com sucesso.").queue();
+						break;
+					case "m":
+					case "monster":
+						Main.getInfo().getGames().get(guild.getId()).getMonsters().remove(String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
+						channel.sendMessage("Monstro excluído com sucesso.").queue();
+						break;
+					case "i":
+					case "item":
+						Main.getInfo().getGames().get(guild.getId()).getPlayers().forEach((k, v) -> {
+							try {
+								v.getCharacter().getInventory().unequip(Main.getInfo().getGames().get(guild.getId()).getItem(String.join(" ", Arrays.copyOfRange(args, 1, args.length))));
+								v.getCharacter().getInventory().removeItem(Main.getInfo().getGames().get(guild.getId()).getItem(String.join(" ", Arrays.copyOfRange(args, 1, args.length))));
+							} catch (UnknownItemException ignore) {
+							}
+						});
+						Main.getInfo().getGames().get(guild.getId()).getItems().remove(Main.getInfo().getGames().get(guild.getId()).getItem(String.join(" ", Arrays.copyOfRange(args, 1, args.length))));
+						channel.sendMessage("Item excluído com sucesso.").queue();
+						break;
+					case "w":
+					case "map":
+						Main.getInfo().getGames().get(guild.getId()).getMaps().remove(Integer.parseInt(args[1]));
+						channel.sendMessage("Mapa excluído com sucesso.").queue();
+						break;
+				}
+			} catch (ArrayIndexOutOfBoundsException e) {
+				channel.sendMessage(":x: | Índice maior que a lista.").queue();
+			} catch (UnknownItemException e) {
+				channel.sendMessage(":x: | Item inexistente").queue();
+			} catch (Exception e) {
+				channel.sendMessage(":x: | Cadastro inexistente.").queue();
+			}
+		}
+	}
+}
---
+++ src/main/java/com/kuuhaku/command/commands/rpg/StatusCommand.java
@@ -0,0 +1,21 @@
+package com.kuuhaku.command.commands.rpg;
+
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.Event;
+
+public class StatusCommand extends Command {
+
+	public StatusCommand() {
+		super("rperfil", new String[]{"rprofile", "rstatus"}, "Mostra a ficha do seu personagem", Category.BEYBLADE);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
+		if (Main.getInfo().getGames().get(guild.getId()).getMaster() != author) {
+			Main.getInfo().getGames().get(guild.getId()).getPlayers().get(author.getId()).getCharacter().openProfile(message.getTextChannel()).queue();
+		}
+	}
+}
