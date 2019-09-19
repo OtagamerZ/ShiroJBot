6592c6f92942062a34a7ff713c4b8829c2308849
ygimenez <yagogimenez1@hotmail.com>
2019-09-19 10:44:15 -0300

    Correções

--- src/main/java/com/kuuhaku/command/commands/misc/TheAnswerCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/TheAnswerCommand.java
@@ -9,28 +9,29 @@ import net.dv8tion.jda.api.events.Event;
 import javax.persistence.NoResultException;
 
 public class TheAnswerCommand extends Command {
-	
-	public TheAnswerCommand() { super("arespostaé", new String[]{"theansweris", "responder", "answer"}, "Leu as regras?", Category.MISC); }
+
+	public TheAnswerCommand() {
+		super("arespostaé", new String[]{"theansweris", "responder", "answer"}, "Leu as regras?", Category.MISC);
+	}
 
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
 		if (guild.getId().equals("421495229594730496")) {
-			if (!MySQL.getTagById(author.getId()).isReader()) {
-				message.delete().queue(s -> {
+			try {
+				if (MySQL.getTagById(author.getId()).isReader())
+					message.delete().queue(s -> channel.sendMessage(":x: | Você já descobriu a resposta, não precisa mais usar este comando.").queue());
+			} catch (NoResultException e) {
+				MySQL.addUserTagsToDB(author.getId());
+			} finally {
+				if (!MySQL.getTagById(author.getId()).isReader()) {
 					if (String.join(" ", args).replace(".", "").equalsIgnoreCase(System.getenv("SECRET"))) {
-						try {
-							MySQL.giveTagReader(author.getId());
-						} catch (NoResultException e) {
-							MySQL.addUserTagsToDB(author.getId());
-							MySQL.giveTagReader(author.getId());
-						}
+						MySQL.giveTagReader(author.getId());
 						channel.sendMessage("Obrigado por ler as regras!").queue();
 					} else {
 						channel.sendMessage(":x: | Resposta errada, leia as regras para achar a resposta.").queue();
 					}
-				});
-			} else
-				message.delete().queue(s -> channel.sendMessage(":x: | Você já descobriu a resposta, não precisa mais usar este comando.").queue());
+				}
+			}
 		} else channel.sendMessage(":x: | Este comando só pode ser usado no servidor OtagamerZ.").queue();
 	}
 }
