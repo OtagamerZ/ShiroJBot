b08871d5c145a7da31f2c1d4778af1cda855fd86
Ygimenez <yagogimenez1@hotmail.com>
2019-11-11 12:03:48 -0200

    Correções

---
+++ src/main/java/com/kuuhaku/command/commands/misc/LinkTesterCommand.java
@@ -0,0 +1,41 @@
+package com.kuuhaku.command.commands.misc;
+
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.utils.Helper;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.Event;
+
+public class LinkTesterCommand extends Command {
+
+	public LinkTesterCommand() {
+		super("link", new String[]{"try"}, "<link>", "Testa um link para ver se ele consegue burlar a detecção de links.", Category.MISC);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, Event event, String prefix) {
+		if (args.length == 0) {
+			channel.sendMessage(":x: | É necessário informar um link").queue();
+			return;
+		}
+
+		String link = rawCmd.replaceFirst(args[0], "");
+
+		String[] quotes = new String[]{
+				"Peguei esse!",
+				"Opa, hoje não!",
+				"Quase!",
+				"To ficando boa nisso!",
+				"Ha! Esse já conheço",
+				"Peguei no flagra!"
+		};
+
+		if (Helper.findURL(link)) {
+			channel.sendMessage(quotes[
+					(int) Helper.clamp(Math.round(quotes.length * Math.random()), 0, quotes.length - 1)
+					]).queue();
+		} else {
+			channel.sendMessage("Não detectei nenhum link nesta mensagem. Caso seja um link, por favor use o comando abaixo para informar meus desenvolvedores!\n`s!bug Link não detectado - " + link + "`").queue();
+		}
+	}
+}
