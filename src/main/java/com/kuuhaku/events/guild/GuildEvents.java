a099c5eb05255e1f1e22af2d1decac2406667857
ygimenez <yagogimenez1@hotmail.com>
2019-12-03 21:58:45 -0300

    Registro de log de uso de comandos implementado

--- src/main/java/com/kuuhaku/events/guild/GuildEvents.java
+++ src/main/java/com/kuuhaku/events/guild/GuildEvents.java
@@ -184,7 +184,7 @@ public class GuildEvents extends ListenerAdapter {
 				found = JDAEvents.isFound(commandName, found, command);
 
 				if (found) {
-					MySQL.saveLog(new Log().setGuild(guild.getName()).setUser(author.getAsTag()));
+					MySQL.saveLog(new Log().setGuild(guild.getName()).setUser(author.getAsTag()).setCommand(commandName));
 					Helper.logToChannel(author, true, command, "Um comando foi usado no canal " + ((TextChannel) channel).getAsMention(), guild);
 					if (JDAEvents.checkPermissions(event, author, member, message, channel, guild, prefix, rawMsgNoPrefix, args, command))
 						break;
