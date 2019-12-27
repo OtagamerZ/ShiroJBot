339ff25df68d499a591ce3dc3655d20d958c22a5
Ygimenez <yagogimenez1@hotmail.com>
2019-12-27 02:47:19 -0300

    Comando de broadcast

--- src/main/java/com/kuuhaku/events/cron/PartnerCheckEvent.java
+++ src/main/java/com/kuuhaku/events/cron/PartnerCheckEvent.java
@@ -3,6 +3,7 @@ package com.kuuhaku.events.cron;
 import com.kuuhaku.Main;
 import com.kuuhaku.controller.MySQL.TagDAO;
 import com.kuuhaku.utils.Helper;
+import net.dv8tion.jda.api.entities.Guild;
 import org.quartz.Job;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
@@ -15,27 +16,19 @@ public class PartnerCheckEvent implements Job {
 
 	@Override
 	public void execute(JobExecutionContext context) {
-		Main.getJibril().getGuilds().forEach(g -> {
-			try {
-				if (!TagDAO.getTagById(g.getOwnerId()).isPartner() && !Main.getInfo().getDevelopers().contains(g.getOwnerId())) {
-					g.leave().queue();
-					Helper.logger(this.getClass()).info("Saí do servidor " + g.getName() + " por " + Objects.requireNonNull(g.getOwner()).getUser().getAsTag() + " não estar na lista de parceiros.");
-				}
-			} catch (NoResultException e) {
+		Main.getJibril().getGuilds().forEach(PartnerCheckEvent::notif);
+		Main.getTet().getGuilds().forEach(PartnerCheckEvent::notif);
+	}
+
+	private static void notif(Guild g) {
+		try {
+			if (!TagDAO.getTagById(g.getOwnerId()).isPartner() && !Main.getInfo().getDevelopers().contains(g.getOwnerId())) {
 				g.leave().queue();
-				Helper.logger(this.getClass()).info("Saí do servidor " + g.getName() + " por " + Objects.requireNonNull(g.getOwner()).getUser().getAsTag() + " não possuir tags.");
+				Helper.logger(PartnerCheckEvent.class).info("Saí do servidor " + g.getName() + " por " + Objects.requireNonNull(g.getOwner()).getUser().getAsTag() + " não estar na lista de parceiros.");
 			}
-		});
-		Main.getTet().getGuilds().forEach(g -> {
-			try {
-				if (!TagDAO.getTagById(g.getOwnerId()).isPartner() && !Main.getInfo().getDevelopers().contains(g.getOwnerId())) {
-					g.leave().queue();
-					Helper.logger(this.getClass()).info("Saí do servidor " + g.getName() + " por " + Objects.requireNonNull(g.getOwner()).getUser().getAsTag() + " não estar na lista de parceiros.");
-				}
-			} catch (NoResultException e) {
-				g.leave().queue();
-				Helper.logger(this.getClass()).info("Saí do servidor " + g.getName() + " por " + Objects.requireNonNull(g.getOwner()).getUser().getAsTag() + " não possuir tags.");
-			}
-		});
+		} catch (NoResultException e) {
+			g.leave().queue();
+			Helper.logger(PartnerCheckEvent.class).info("Saí do servidor " + g.getName() + " por " + Objects.requireNonNull(g.getOwner()).getUser().getAsTag() + " não possuir tags.");
+		}
 	}
 }
