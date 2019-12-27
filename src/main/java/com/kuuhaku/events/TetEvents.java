f0359de4485aa75c055f89691019da65d6aebf05
KuuHaKu <yagogimenez1@hotmail.com>
2019-12-27 01:42:48 -0300

    Update TetEvents.java

--- src/main/java/com/kuuhaku/events/TetEvents.java
+++ src/main/java/com/kuuhaku/events/TetEvents.java
@@ -104,7 +104,7 @@ public class TetEvents extends ListenerAdapter {
 				commandName = rawMsgNoPrefix.split(" ")[0].trim();
 			}
 
-			if (message.getContentRaw().trim().equals(Main.getTet().getSelfUser().getAsMention())) {
+			if (message.getContentRaw().trim().equals("<@" + Main.getTet().getSelfUser().getId() + ">")) {
 				channel.sendMessage("Opa, eae jogador! Meus comandos são listados pela Shiro, digite `" + prefix + "help` e clique na categoria `RPG` para vê-los!").queue();
 				return;
 			}
