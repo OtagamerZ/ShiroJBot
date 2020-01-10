5144bbbcddeb174e3e2d86b956758eb4e6ff0fdd
Ygimenez <yagogimenez1@hotmail.com>
2020-01-10 11:38:08 -0300

    Adição de endpoints para requisições HTTP

--- src/main/java/com/kuuhaku/handlers/api/endpoint/ErrorHandler.java
+++ src/main/java/com/kuuhaku/handlers/api/endpoint/ErrorHandler.java
@@ -5,10 +5,17 @@ import com.kuuhaku.handlers.api.exception.InvalidTokenException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.persistence.NoResultException;
+
 @ControllerAdvice
 public class ErrorHandler {
 	@ExceptionHandler(InvalidTokenException.class)
 	public Exception invalidToken(Exception e) {
 		return new Exception(403, e.getCause(), e.getStacktrace());
+	}
+
+	@ExceptionHandler(NoResultException.class)
+	public Exception noResult(Exception e) {
+		return new Exception(500, e.getCause(), e.getStacktrace());
 	}
 }
