b9ad8bc8cc597405278f203fd45e87be31a06094
ygimenez <yagogimenez1@hotmail.com>
2020-01-10 13:26:57 -0300

    Adição de endpoints para requisições HTTP

--- src/main/java/com/kuuhaku/handlers/api/endpoint/ErrorHandler.java
+++ src/main/java/com/kuuhaku/handlers/api/endpoint/ErrorHandler.java
@@ -1,29 +1,24 @@
 package com.kuuhaku.handlers.api.endpoint;
 
 import com.kuuhaku.handlers.api.exception.Exception;
-import com.kuuhaku.utils.Helper;
+import com.kuuhaku.handlers.api.exception.InvalidTokenException;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @ControllerAdvice
 public class ErrorHandler implements ErrorController {
 	@RequestMapping("/error")
-	public Exception error(@RequestParam(value = "code", defaultValue = "500") String code, @RequestParam(value = "cause", defaultValue = "Erro interno") String cause) {
-		return new Exception(Integer.parseInt(code), cause);
+	public Exception error() {
+		return new Exception(500, "Erro interno", new String[]{});
 	}
 
-	@ExceptionHandler(java.lang.Exception.class)
-	public String collectData(Exception e) {
-		try {
-			return "forward:/error?code=" + e.getCode() + "&cause=" + URLEncoder.encode(e.getCause(), "UTF-8");
-		} catch (UnsupportedEncodingException ex) {
-			Helper.logger(this.getClass()).error(ex + " | " + ex.getStackTrace()[0]);
-			return null;
-		}
+	@ExceptionHandler(InvalidTokenException.class)
+	public Exception collectData(Exception e) {
+		return new Exception(403, e.getCause(), e.getStacktrace());
 	}
 
 	@Override
