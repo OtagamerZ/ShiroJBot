999ed73c56de5008cb20ed06628f7525c7a47879
Ygimenez <yagogimenez1@hotmail.com>
2020-01-10 13:26:57 -0300

    Adição de endpoints para requisições HTTP

--- src/main/java/com/kuuhaku/handlers/api/exception/Exception.java
+++ src/main/java/com/kuuhaku/handlers/api/exception/Exception.java
@@ -3,10 +3,12 @@ package com.kuuhaku.handlers.api.exception;
 public class Exception {
 	private final int code;
 	private final String cause;
+	private final Object[] stacktrace;
 
-	public Exception(int code, String cause) {
+	public Exception(int code, String cause, Object[] stacktrace) {
 		this.code = code;
 		this.cause = cause;
+		this.stacktrace = stacktrace;
 	}
 
 	public int getCode() {
@@ -15,5 +17,9 @@ public class Exception {
 
 	public String getCause() {
 		return cause;
+	}
+
+	public Object[] getStacktrace() {
+		return stacktrace;
 	}
 }
