327be6e2a669e3e653b3a2c0ded5ba729afb2b1c
Ygimenez <yagogimenez1@hotmail.com>
2020-01-16 15:49:28 -0300

    Pequenas otimizações

--- src/main/java/com/kuuhaku/handlers/api/endpoint/GuildRequest.java
+++ src/main/java/com/kuuhaku/handlers/api/endpoint/GuildRequest.java
@@ -1,13 +1,11 @@
 package com.kuuhaku.handlers.api.endpoint;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.kuuhaku.Main;
 import com.kuuhaku.controller.mysql.TokenDAO;
 import com.kuuhaku.controller.sqlite.GuildDAO;
 import com.kuuhaku.handlers.api.exception.InvalidTokenException;
 import com.kuuhaku.model.GuildConfig;
 import com.kuuhaku.utils.Helper;
-import net.dv8tion.jda.api.entities.Guild;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,11 +19,6 @@ public class GuildRequest {
 	@RequestMapping(value = "/guild/get", method = RequestMethod.GET)
 	public GuildConfig requestGuild(@RequestParam(value = "id") String id) {
 		return GuildDAO.getGuildById(id);
-	}
-
-	@RequestMapping(value = "/guild/get/jda", method = RequestMethod.GET)
-	public Guild requestJDAGuild(@RequestParam(value = "id") String id) {
-		return Main.getInfo().getGuildByID(id);
 	}
 
 	@RequestMapping(value = "/guild/update", method = RequestMethod.POST)
