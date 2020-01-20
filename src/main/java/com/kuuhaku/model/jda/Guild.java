fe29f2de8b3081044907d3d5a8777be339194ba3
ygimenez <yagogimenez1@hotmail.com>
2020-01-20 11:55:42 -0300

    Pequenas otimizações

--- src/main/java/com/kuuhaku/model/jda/Guild.java
+++ src/main/java/com/kuuhaku/model/jda/Guild.java
@@ -4,11 +4,13 @@ public class Guild {
 	private final String id;
 	private final String name;
 	private final String icon;
+	private final String owner;
 
-	public Guild(String id, String name, String icon) {
+	public Guild(String id, String name, String icon, String owner) {
 		this.id = id;
 		this.name = name;
 		this.icon = icon;
+		this.owner = owner;
 	}
 
 	public String getId() {
@@ -21,6 +23,10 @@ public class Guild {
 
 	public String getIcon() {
 		return icon;
+	}
+
+	public String getOwner() {
+		return owner;
 	}
 
 	@Override
