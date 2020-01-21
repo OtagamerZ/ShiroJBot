4d61376b697849473273dd381b6f895de68a98cd
Ygimenez <yagogimenez1@hotmail.com>
2020-01-21 17:27:37 -0300

    Pequenas otimizações

--- src/main/java/com/kuuhaku/handlers/api/websocket/WebSocketController.java
+++ src/main/java/com/kuuhaku/handlers/api/websocket/WebSocketController.java
@@ -3,7 +3,6 @@ package com.kuuhaku.handlers.api.websocket;
 import com.kuuhaku.utils.Helper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
@@ -19,7 +18,7 @@ public class WebSocketController {
 	}
 
 	@MessageMapping("/chat")
-	public void onMessage(@DestinationVariable String id,  Message message) {
+	public void onMessage(Message<String> message) {
 		Helper.logger(this.getClass()).info("Mensagem recebida: " + message);
 		this.template.convertAndSend("/topic/message", message);
 	}
