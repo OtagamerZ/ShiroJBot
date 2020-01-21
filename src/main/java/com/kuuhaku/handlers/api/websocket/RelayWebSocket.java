cf00509a0cd935ef2c71ba3eb23c4c87460e7c0c
Ygimenez <yagogimenez1@hotmail.com>
2020-01-21 10:09:16 -0300

    Pequenas otimizações

--- src/main/java/com/kuuhaku/handlers/api/websocket/RelayWebSocket.java
+++ src/main/java/com/kuuhaku/handlers/api/websocket/RelayWebSocket.java
@@ -1,35 +1,18 @@
 package com.kuuhaku.handlers.api.websocket;
 
 import com.kuuhaku.utils.Helper;
-import org.apache.logging.log4j.Logger;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
-import javax.websocket.*;
-import javax.websocket.server.ServerEndpoint;
-import java.io.IOException;
-
-@ServerEndpoint("/relay")
+@Controller
 public class RelayWebSocket {
 
-	private static final Logger LOGGER = Helper.logger(RelayWebSocket.class);
+	@MessageMapping("/relay")
+	@SendTo("/topic/relay")
+	public Message onMessage(Sender sender) {
+		Helper.logger(this.getClass()).info("sender");
 
-	@OnOpen
-	public void onOpen(Session session) {
-		LOGGER.info(String.format("WebSocket opened: %s", session.getId()));
-	}
-
-	@OnMessage
-	public void onMessage(String txt, Session session) throws IOException {
-		LOGGER.info(String.format("Message received: %s", txt));
-		session.getBasicRemote().sendText(txt.toUpperCase());
-	}
-
-	@OnClose
-	public void onClose(CloseReason reason, Session session) {
-		LOGGER.info(String.format("Closing a WebSocket (%s) due to %s", session.getId(), reason.getReasonPhrase()));
-	}
-
-	@OnError
-	public void onError(Session session, Throwable t) {
-		LOGGER.error(String.format("Error in WebSocket session %s%n", session == null ? "null" : session.getId()), t);
+		return new Message("Sua mensagem foi recebida, " + sender.getName());
 	}
 }
