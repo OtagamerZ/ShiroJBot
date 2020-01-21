0aeda83b71de5521569f31c2f497ade00bc6f678
ygimenez <yagogimenez1@hotmail.com>
2020-01-21 15:51:47 -0300

    Pequenas otimizações

--- src/main/java/com/kuuhaku/handlers/api/websocket/WebSocketConfig.java
+++ src/main/java/com/kuuhaku/handlers/api/websocket/WebSocketConfig.java
@@ -12,12 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/chat");
+		config.enableSimpleBroker("/topic");
 		config.setApplicationDestinationPrefixes("/app");
 	}
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
+		registry.addEndpoint("/chat").withSockJS();
 	}
 }
