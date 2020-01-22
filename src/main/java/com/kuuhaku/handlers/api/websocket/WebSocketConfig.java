c0cb401c69bc9a0e74ad7b1efdc4a1ab8e354415
ygimenez <yagogimenez1@hotmail.com>
2020-01-22 13:09:13 -0300

    Pequenas otimizações

--- src/main/java/com/kuuhaku/handlers/api/websocket/WebSocketConfig.java
+++ src/main/java/com/kuuhaku/handlers/api/websocket/WebSocketConfig.java
@@ -2,6 +2,9 @@ package com.kuuhaku.handlers.api.websocket;
 
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOServer;
+import com.kuuhaku.Main;
+import net.dv8tion.jda.api.entities.User;
+import org.json.JSONObject;
 
 public class WebSocketConfig {
 
@@ -13,7 +16,21 @@ public class WebSocketConfig {
 		config.setPort(8000);
 
 		socket = new SocketIOServer(config);
-		socket.addEventListener("chatevent", String.class, (client, data, ackSender) -> System.out.println(data));
+		socket.addEventListener("chatevent", String.class, (client, data, ackSender) -> {
+			JSONObject jo = new JSONObject(data);
+
+			User u = Main.getInfo().getUserByID(jo.getString("userID"));
+
+			System.out.println("Mensagem enviada por " + u.getName() + ": " + jo.getString("content"));
+
+			JSONObject out = new JSONObject();
+
+			out.put("name", u.getName());
+			out.put("avatar", u.getAvatarUrl());
+			out.put("content", jo.getString("content"));
+
+			socket.getBroadcastOperations().sendEvent("chat", out.toString());
+		});
 		socket.start();
 	}
 
