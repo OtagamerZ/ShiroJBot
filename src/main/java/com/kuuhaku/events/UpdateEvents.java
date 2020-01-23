ab51705c9ca8e919e3f9ed7c33af955e177fc1dd
ygimenez <yagogimenez1@hotmail.com>
2020-01-23 10:22:47 -0300

    Pequenas otimizações

--- src/main/java/com/kuuhaku/events/UpdateEvents.java
+++ src/main/java/com/kuuhaku/events/UpdateEvents.java
@@ -9,7 +9,6 @@ import net.dv8tion.jda.api.events.guild.update.GenericGuildUpdateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,12 +23,8 @@ public class UpdateEvents extends ListenerAdapter {
 
 	@Override
 	public void onGenericGuildMemberUpdate(@Nonnull GenericGuildMemberUpdateEvent event) {
-		List<String> ids = new ArrayList<String>(){{
-			add(event.getUser().getId());
-		}};
-
 		com.kuuhaku.model.jda.Member m = new com.kuuhaku.model.jda.Member(event.getUser().getId(), event.getGuild().getId(), event.getUser().getName(), event.getMember().getNickname(), event.getUser().getAvatarUrl());
 
-		Helper.notifyDashboard(ids, event.getUser().getId(), m);
+		Helper.notifyDashboard(event.getUser().getId(), event.getUser().getId(), m);
 	}
 }
