29e2590e4dff0804638eec1662472e14e698639b
Ygimenez <yagogimenez1@hotmail.com>
2020-01-28 14:23:00 -0300

    Melhorias na API

--- src/main/java/com/kuuhaku/command/commands/moderation/RoleChooserCommand.java
+++ src/main/java/com/kuuhaku/command/commands/moderation/RoleChooserCommand.java
@@ -59,7 +59,7 @@ public class RoleChooserCommand extends Command {
 		}
 
 		try {
-			Helper.addButton(args, message, channel, gc, message.getEmotes().size() > 0 ? message.getEmotes().get(0).getId() : args[1], false);
+			Helper.addButton(args, message, channel, gc, message.getEmotes().size() > 0 ? message.getEmotes().get(0).getId() : args[1]);
 
 			channel.sendMessage("Botão adicionado com sucesso!").queue(s -> Helper.refreshButtons(gc));
 		} catch (IllegalArgumentException e) {
