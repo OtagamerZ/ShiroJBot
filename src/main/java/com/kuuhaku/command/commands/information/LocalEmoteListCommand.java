a6637e18271fe10a987f9b9dc2d76d98af3a6bf1
ygimenez <yagogimenez1@hotmail.com>
2020-02-03 16:32:57 -0300

    Comando Use e correções gerais nos comandos de RPG

--- src/main/java/com/kuuhaku/command/commands/information/LocalEmoteListCommand.java
+++ src/main/java/com/kuuhaku/command/commands/information/LocalEmoteListCommand.java
@@ -47,15 +47,19 @@ public class LocalEmoteListCommand extends Command {
 
 		guild.getEmotes().stream().filter(e -> StringUtils.containsIgnoreCase(e.getAsMention(), args.length > 0 ? args[0] : "")).collect(Collectors.toList()).forEach(e -> f.add(new MessageEmbed.Field("Emote " + e.getAsMention(), "Menção: " + e.getAsMention().replace("<", "`{").replace(">", "}`").replace(":", "&"), false)));
 
-		for (int i = 0; i < Math.ceil(f.size() / 10f); i++) {
-			eb.clear();
-			List<MessageEmbed.Field> subF = f.subList(-10 + (10 * (i + 1)), Math.min(10 * (i + 1), f.size()));
-			subF.forEach(eb::addField);
+		try {
+			for (int i = 0; i < Math.ceil(f.size() / 10f); i++) {
+				eb.clear();
+				List<MessageEmbed.Field> subF = f.subList(-10 + (10 * (i + 1)), Math.min(10 * (i + 1), f.size()));
+				subF.forEach(eb::addField);
 
-			eb.setTitle("<a:SmugDance:598842924725305344> Emotes disponíveis neste servidor:");
-			Helper.finishEmbed(guild, pages, f, eb, i);
+				eb.setTitle("<a:SmugDance:598842924725305344> Emotes disponíveis neste servidor:");
+				Helper.finishEmbed(guild, pages, f, eb, i);
+			}
+
+			channel.sendMessage((MessageEmbed) pages.get(0).getContent()).queue(s -> Pages.paginate(Main.getInfo().getAPI(), s, pages, 60, TimeUnit.SECONDS));
+		} catch (IndexOutOfBoundsException e) {
+			channel.sendMessage(":x: | Nenhum emote encontrado com esse nome.").queue();
 		}
-
-		channel.sendMessage((MessageEmbed) pages.get(0).getContent()).queue(s -> Pages.paginate(Main.getInfo().getAPI(), s, pages, 60, TimeUnit.SECONDS));
 	}
 }
