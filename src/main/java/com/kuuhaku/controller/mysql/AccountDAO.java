f48b87ae8ed5ad5f8e4c8e10f97f24ef9431ec5d
ygimenez <yagogimenez1@hotmail.com>
2020-02-10 16:41:03 -0300

    Atualização de número de servidores na página da Shiro no Top.gg

---
+++ src/main/java/com/kuuhaku/controller/mysql/AccountDAO.java
@@ -0,0 +1,55 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.controller.mysql;
+
+import com.kuuhaku.model.persistent.Account;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+public class AccountDAO {
+
+	public static Account getAccount(String id) {
+		EntityManager em = Manager.getEntityManager();
+
+		Query q = em.createQuery("SELECT a FROM Account a WHERE userId LIKE :id", Account.class);
+		q.setParameter("id", id);
+
+		try {
+			return (Account) q.getSingleResult();
+		} catch (NoResultException e) {
+			Account acc = new Account();
+			acc.setUserId(id);
+			saveAccount(acc);
+			return acc;
+		} finally {
+			em.close();
+		}
+	}
+
+	public static void saveAccount(Account acc) {
+		EntityManager em = Manager.getEntityManager();
+
+		em.getTransaction().begin();
+		em.merge(acc);
+		em.getTransaction().commit();
+
+		em.close();
+	}
+}
---
+++ src/main/java/com/kuuhaku/handlers/api/endpoint/DiscordBotsListHandler.java
@@ -0,0 +1,60 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.handlers.api.endpoint;
+
+import com.kuuhaku.Main;
+import com.kuuhaku.controller.mysql.AccountDAO;
+import com.kuuhaku.controller.mysql.TokenDAO;
+import com.kuuhaku.handlers.api.exception.UnauthorizedException;
+import com.kuuhaku.model.persistent.Account;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+
+@RestController
+public class DiscordBotsListHandler {
+
+	@RequestMapping(value = "/webhook/dbl", consumes = "application/json", method = RequestMethod.POST)
+	public void handleVote(@RequestHeader(value = "Authorization") String token, @RequestBody JSONObject body) {
+		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
+
+		int credit = body.getBoolean("isWeekend") ? 250 : 125;
+
+		Account acc = AccountDAO.getAccount(body.getString("user"));
+
+		acc.addCredit(credit);
+
+		AccountDAO.saveAccount(acc);
+
+		try {
+			MessageChannel chn = Main.getInfo().getUserByID(body.getString("user")).openPrivateChannel().complete();
+
+			EmbedBuilder eb = new EmbedBuilder();
+
+			eb.setThumbnail("https://i.imgur.com/A0jXqpe.png");
+			eb.setTitle("Olá, obrigado por votar em mim!");
+			eb.setDescription("Como agradecimento, aqui estão " + credit + (body.getBoolean("isWeekend") ? " (bônus x2)" : "") + " créditos para serem utilizados nos módulos que utilizam o sistema de dinheiro.");
+			eb.setFooter("Seus créditos: " + acc.getBalance());
+			eb.setColor(Color.green);
+		} catch (RuntimeException ignore) {
+		}
+	}
+}
