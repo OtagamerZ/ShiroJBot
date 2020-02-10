5333704e9ba6c435f43d6fd5884feafe02547b09
Yago <yagogimenez1@hotmail.com>
2020-02-10 00:24:33 -0300

    Atualização de número de servidores na página da Shiro no Top.gg

---
+++ src/main/java/com/kuuhaku/events/cron/UpdateServerCountEvent.java
@@ -0,0 +1,32 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.events.cron;
+
+import com.kuuhaku.Main;
+import org.quartz.Job;
+import org.quartz.JobDetail;
+import org.quartz.JobExecutionContext;
+
+public class UpdateServerCountEvent implements Job {
+	public static JobDetail updateServerCount;
+
+	@Override
+	public void execute(JobExecutionContext context) {
+		Main.getInfo().getDblApi().setStats(Main.getInfo().getAPI().getGuilds().size());
+	}
+}
