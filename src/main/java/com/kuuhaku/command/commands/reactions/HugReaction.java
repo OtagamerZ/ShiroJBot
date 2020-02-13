3b4ff1e9e17dc013ba6e82e8964720c264909929
Yago <yagogimenez1@hotmail.com>
2020-02-12 22:53:53 -0300

    Comandos de Kawaigotchi implementados

--- src/main/java/com/kuuhaku/command/commands/reactions/HugReaction.java
+++ src/main/java/com/kuuhaku/command/commands/reactions/HugReaction.java
@@ -55,7 +55,7 @@ public class HugReaction extends Reaction {
 			if (!isAnswer())
 				Helper.sendReaction(getUrl("hug", (TextChannel) channel), channel, author.getAsMention() + " abraçou " + message.getMentionedUsers().get(0).getAsMention() + " - " + this.getReaction()[this.getReactionLength()], true);
 			else
-				Helper.sendReaction(getUrl("hug", (TextChannel) channel), channel, message.getMentionedUsers().get(1).getAsMention() + " devolveu o abraço de " + author.getAsMention() + " - " + this.getReaction()[this.getReactionLength()], false);
+				Helper.sendReaction(getUrl("hug", (TextChannel) channel), channel, message.getMentionedUsers().get(0).getAsMention() + " devolveu o abraço de " + author.getAsMention() + " - " + this.getReaction()[this.getReactionLength()], false);
 		} else {
 			Helper.typeMessage(channel, ":x: | Epa, você precisa mencionar alguém para abraçar!");
 		}
