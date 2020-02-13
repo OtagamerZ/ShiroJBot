f9cfc4b491c1c968153d5c583c44173f0aecfdc4
ygimenez <yagogimenez1@hotmail.com>
2020-02-13 15:58:56 -0300

    Correção nos comandos de reação

--- src/main/java/com/kuuhaku/controller/Anime.java
+++ src/main/java/com/kuuhaku/controller/Anime.java
@@ -79,6 +79,9 @@ public class Anime {
 
         String redir = Helper.getOr(con.getHeaderField("Location"), con.getHeaderField("location"));
 
+        System.out.println(redir);
+        System.out.println(IOUtils.toString(con.getInputStream(), StandardCharsets.UTF_8));
+
         if (redir != null) {
             return getDAData(redir.replace("/anime-info/", ""));
         }
