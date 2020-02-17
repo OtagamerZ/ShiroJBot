c9c8c6c08d1c94e42c4ea7692d0c01d598a1ca50
ygimenez <yagogimenez1@hotmail.com>
2020-02-17 11:44:42 -0300

    Mudanças na notificação do Kawaigotchi

---
+++ src/main/java/com/kuuhaku/command/commands/dev/SimpleWHMCommand.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.dev;
+
+import club.minnced.discord.webhook.WebhookClient;
+import club.minnced.discord.webhook.WebhookClientBuilder;
+import club.minnced.discord.webhook.send.WebhookMessageBuilder;
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.utils.Helper;
+import net.dv8tion.jda.api.entities.*;
+
+import java.util.concurrent.ExecutionException;
+
+public class SimpleWHMCommand extends Command {
+
+	public SimpleWHMCommand(String name, String description, Category category) {
+		super(name, description, category);
+	}
+
+	public SimpleWHMCommand(String name, String[] aliases, String description, Category category) {
+		super(name, aliases, description, category);
+	}
+
+	public SimpleWHMCommand(String name, String usage, String description, Category category) {
+		super(name, usage, description, category);
+	}
+
+	public SimpleWHMCommand(String name, String[] aliases, String usage, String description, Category category) {
+		super(name, aliases, usage, description, category);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+		Webhook wh = Helper.getOrCreateWebhook((TextChannel) channel, "Webhook Test", Main.getInfo().getAPI());
+
+		WebhookMessageBuilder wmb = new WebhookMessageBuilder();
+		wmb.setContent(String.join(" ", args));
+		wmb.setAvatarUrl(author.getAvatarUrl());
+		wmb.setUsername(author.getName());
+
+		assert wh != null;
+		WebhookClient wc = new WebhookClientBuilder(wh.getUrl()).build();
+		try {
+			wc.send(wmb.build()).get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+	}
+}
