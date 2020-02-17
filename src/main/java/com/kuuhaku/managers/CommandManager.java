da283ab634223a497696204d38f6758c08886986
Ygimenez <yagogimenez1@hotmail.com>
2020-02-17 11:46:02 -0300

    Mudanças na notificação do Kawaigotchi

--- src/main/java/com/kuuhaku/managers/CommandManager.java
+++ src/main/java/com/kuuhaku/managers/CommandManager.java
@@ -55,6 +55,7 @@ public class CommandManager {
 			add(new BroadcastCommand("transmitir", new String[]{"broadcast", "bc"}, "<tipo> <mensagem>", "Envia um aviso a todos os donos de servidor que possuem a Shiro, ou a todos o parceiros.", Category.DEVS));
 			add(new InviteCommand("convite", new String[]{"invite"}, "<ID do servidor>", "Obtém um convite de uso único do servidor informado.", Category.DEVS));
 			add(new UsageCommand("usos", new String[]{"uses", "usage"}, "Vê a quantidade de comandos usados por servidor.", Category.DEVS));
+			add(new SimpleWHMCommand("wh", "Envia uma simples mensagem via webhook.", Category.DEVS));
 
 			//SHERIFF
 			add(new BlockCommand("bloquear", new String[]{"block"}, "<tipo> <ID> <razão>", "Bloqueia alguém de usar o chat global.", Category.SHERIFFS));
