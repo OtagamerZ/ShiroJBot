69d16e70328a6344016bb31e0a0b6782cb03a580
ygimenez <yagogimenez1@hotmail.com>
2020-02-24 16:51:29 -0300

    Atualização do framework JDA

--- src/main/java/com/kuuhaku/command/commands/dev/RatingCommand.java
+++ src/main/java/com/kuuhaku/command/commands/dev/RatingCommand.java
@@ -63,21 +63,21 @@ public class RatingCommand extends Command {
 						c.sendFile(new File(Objects.requireNonNull(this.getClass().getClassLoader().getResource("feedback.png")).getPath()))
 								.flatMap(s -> c.sendMessage(eval(author)))
 								.queue(s -> {
-									s.delete().delay(5, TimeUnit.MINUTES).queue();
+									s.delete().queueAfter(5, TimeUnit.MINUTES);
 									c.sendMessage(questions()[0])
 											.queue(m -> {
 												addRates(author, m, (dev, i) -> dev.setInteraction(dev.getInteraction() == 0 ? i : (dev.getInteraction() + i) / 2f));
-												m.delete().delay(5, TimeUnit.MINUTES).queue();
+												m.delete().queueAfter(5, TimeUnit.MINUTES);
 											});
 									c.sendMessage(questions()[1])
 											.queue(m -> {
 												addRates(author, m, (dev, i) -> dev.setSolution(dev.getSolution() == 0 ? i : (dev.getSolution() + i) / 2f));
-												m.delete().delay(5, TimeUnit.MINUTES).queue();
+												m.delete().queueAfter(5, TimeUnit.MINUTES);
 											});
 									c.sendMessage(questions()[2])
 											.queue(m -> {
 												addRates(author, m, (dev, i) -> dev.setKnowledge(dev.getKnowledge() == 0 ? i : (dev.getKnowledge() + i) / 2f));
-												m.delete().delay(5, TimeUnit.MINUTES).queue();
+												m.delete().queueAfter(5, TimeUnit.MINUTES);
 											});
 								}),
 				ex -> channel.sendMessage(":x: | Não foi possível enviar a avaliação ao usuário. Razão: " + ex).queue()
