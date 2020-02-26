a81416765b1b96c03a47ad3af6e8d16e53044163
ygimenez <yagogimenez1@hotmail.com>
2020-02-26 09:24:32 -0300

    Correção na quebra de linha dos memes

--- src/main/java/com/kuuhaku/command/commands/dev/KillCommand.java
+++ src/main/java/com/kuuhaku/command/commands/dev/KillCommand.java
@@ -17,6 +17,7 @@
 
 package com.kuuhaku.command.commands.dev;
 
+import com.kuuhaku.Main;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
 import net.dv8tion.jda.api.entities.*;
@@ -41,6 +42,13 @@ public class KillCommand extends Command {
 
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+		if (author.getId().equals(Main.getInfo().getNiiChan())) {
+			channel.sendMessage("Sayonara, Nii-chan! <3").queue(m -> Main.kill = new String[]{channel.getId(), m.getId()});
+		} else {
+			channel.sendMessage("Iniciando o protocolo de encerramento...").queue(m -> Main.kill = new String[]{channel.getId(), m.getId()});
+		}
+
+		Main.shutdown();
 		System.exit(0);
 	}
 }
