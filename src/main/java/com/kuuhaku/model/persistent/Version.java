8ab0c71a72d7bf3db7bf7663b5137f5cb9e9b3e6
ygimenez <yagogimenez1@hotmail.com>
2020-03-04 15:12:13 -0300

    Continuação da implementação do i18n

---
+++ src/main/java/com/kuuhaku/model/persistent/Version.java
@@ -0,0 +1,46 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.model.persistent;
+
+import javax.persistence.*;
+
+@Entity
+public class Version {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int major;
+
+	@Column(columnDefinition = "INT NOT NULL DEFAULT 0")
+	private int minor = 0;
+
+	public Version(int major) {
+		this.major = major;
+	}
+
+	public Version() {
+	}
+
+	public int getMajor() {
+		return major;
+	}
+
+	public int getMinor() {
+		this.minor++;
+		return minor;
+	}
+}
