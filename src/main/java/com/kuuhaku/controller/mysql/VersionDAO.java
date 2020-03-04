4837083f4764c608a42007fd8372adc87ce09813
ygimenez <yagogimenez1@hotmail.com>
2020-03-04 15:12:13 -0300

    Continuação da implementação do i18n

---
+++ src/main/java/com/kuuhaku/controller/mysql/VersionDAO.java
@@ -0,0 +1,44 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.controller.mysql;
+
+import com.kuuhaku.model.persistent.Version;
+
+import javax.persistence.EntityManager;
+
+public class VersionDAO {
+	public static String getMinorVersion(int major) {
+		EntityManager em = Manager.getEntityManager();
+
+		Version v = em.find(Version.class, major);
+
+		try {
+			if (v == null) {
+				v = new Version(major);
+			}
+
+			return major + "." + v.getMinor();
+		} finally {
+			em.getTransaction().begin();
+			em.merge(v);
+			em.getTransaction().commit();
+
+			em.close();
+		}
+	}
+}
