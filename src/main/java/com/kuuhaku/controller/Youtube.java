b5a551603323fc1859a5f8b145601a64d97e1f70
ygimenez <yagogimenez1@hotmail.com>
2020-03-05 13:21:28 -0300

    Continuação da implementação do i18n

--- src/main/java/com/kuuhaku/controller/Youtube.java
+++ src/main/java/com/kuuhaku/controller/Youtube.java
@@ -61,7 +61,7 @@ public class Youtube {
 
 		JSONObject resposta = new JSONObject(IOUtils.toString(con.getInputStream(), StandardCharsets.UTF_8));
 
-		Helper.logger(Tradutor.class).info(resposta);
+		Helper.logger(Tradutor.class).debug(resposta);
 		return resposta.getJSONArray("items");
 	}
 
