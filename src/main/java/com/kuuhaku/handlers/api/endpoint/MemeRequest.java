c405dbd4a71a67611f845372a0f5b15f4bd6a543
ygimenez <yagogimenez1@hotmail.com>
2020-03-06 15:16:42 -0300

    Continuação da implementação do i18n

--- src/main/java/com/kuuhaku/handlers/api/endpoint/MemeRequest.java
+++ src/main/java/com/kuuhaku/handlers/api/endpoint/MemeRequest.java
@@ -37,10 +37,10 @@ public class MemeRequest {
 	@RequestMapping(value = "/meme/twobuttons", method = RequestMethod.POST)
 	public @ResponseBody
 	byte[] getTwoButtonsMeme(@RequestHeader(value = "token") String token,
-							 @RequestHeader(value = "field-A") String fieldA,
-							 @RequestHeader(value = "field-B") String fieldB) {
+							 @RequestHeader(value = "field-a") String fieldA,
+							 @RequestHeader(value = "field-b") String fieldB) {
 		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
-		else if (fieldA.isEmpty() || fieldB.isEmpty()) {
+		else if (Helper.isEmpty(fieldA, fieldB)) {
 			throw new NotEnoughArgsException();
 		}
 
@@ -62,6 +62,190 @@ public class MemeRequest {
 			g2d.dispose();
 
 			ImageIO.write(bi, "png", baos);
+			return baos.toByteArray();
+		} catch (IOException e) {
+			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+			throw new InternalErrorException();
+		}
+	}
+
+	@RequestMapping(value = "/meme/expandingbrain", method = RequestMethod.POST)
+	public @ResponseBody
+	byte[] getExpandingBrainMeme(@RequestHeader(value = "token") String token,
+								 @RequestHeader(value = "field-a") String fieldA,
+								 @RequestHeader(value = "field-b") String fieldB,
+								 @RequestHeader(value = "field-c") String fieldC,
+								 @RequestHeader(value = "field-d") String fieldD) {
+		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
+		else if (Helper.isEmpty(fieldA, fieldB, fieldC, fieldD)) {
+			throw new NotEnoughArgsException();
+		}
+
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			BufferedImage bi = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("Expanding-Brain.png")));
+			Graphics2D g2d = bi.createGraphics();
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+			g2d.setColor(Color.BLACK);
+			g2d.setFont(new Font("Arial", Font.BOLD, 40));
+			Profile.drawStringMultiLineNO(g2d, fieldA, 390, 20, 40);
+			Profile.drawStringMultiLineNO(g2d, fieldB, 390, 20, 340);
+			Profile.drawStringMultiLineNO(g2d, fieldC, 390, 20, 650);
+			Profile.drawStringMultiLineNO(g2d, fieldD, 390, 20, 930);
+
+			g2d.dispose();
+
+			ImageIO.write(bi, "png", baos);
+			return baos.toByteArray();
+		} catch (IOException e) {
+			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+			throw new InternalErrorException();
+		}
+	}
+
+	@RequestMapping(value = "/meme/drake", method = RequestMethod.POST)
+	public @ResponseBody
+	byte[] getDrakeMeme(@RequestHeader(value = "token") String token,
+						@RequestHeader(value = "field-a") String fieldA,
+						@RequestHeader(value = "field-b") String fieldB) {
+		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
+		else if (Helper.isEmpty(fieldA, fieldB)) {
+			throw new NotEnoughArgsException();
+		}
+
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			BufferedImage bi = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("drake.jpg")));
+			Graphics2D g2d = bi.createGraphics();
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+			g2d.setColor(Color.BLACK);
+			g2d.setFont(new Font("Arial", Font.BOLD, 40));
+			Profile.drawStringMultiLineNO(g2d, fieldA, 313, 362, 55);
+			Profile.drawStringMultiLineNO(g2d, fieldB, 313, 362, 337);
+
+			g2d.dispose();
+
+			ImageIO.write(bi, "png", baos);
+			return baos.toByteArray();
+		} catch (IOException e) {
+			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+			throw new InternalErrorException();
+		}
+	}
+
+	@RequestMapping(value = "/meme/sadreality", method = RequestMethod.POST)
+	public @ResponseBody
+	byte[] getSadRealityMeme(@RequestHeader(value = "token") String token,
+							 @RequestHeader(value = "field-a") String fieldA) {
+		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
+		else if (Helper.isEmpty(fieldA)) {
+			throw new NotEnoughArgsException();
+		}
+
+		try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+			BufferedImage bi = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("sadbuttrue.png")));
+			Graphics2D g2d = bi.createGraphics();
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+			g2d.setColor(Color.BLACK);
+			g2d.setFont(new Font("Arial", Font.BOLD, 20));
+			if (g2d.getFontMetrics().stringWidth(fieldA) > 270) {
+				Profile.drawStringMultiLineNO(g2d, fieldA, 263, 75, 554);
+			} else {
+				Profile.printCenteredString(fieldA, 270, 63, 554, g2d);
+			}
+
+			g2d.dispose();
+
+			ImageIO.write(bi, "png", baos);
+			return baos.toByteArray();
+		} catch (IOException e) {
+			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+			throw new InternalErrorException();
+		}
+	}
+
+	@RequestMapping(value = "/meme/stonks", method = RequestMethod.POST)
+	public @ResponseBody
+	byte[] getStonksMeme(@RequestHeader(value = "token") String token,
+						 @RequestHeader(value = "field-a") String fieldA) {
+		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
+		else if (Helper.isEmpty(fieldA)) {
+			throw new NotEnoughArgsException();
+		}
+
+		try {
+			BufferedImage bi = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("stonks.jpg")));
+
+			ByteArrayOutputStream baos = Helper.renderMeme(fieldA, bi);
+
+			baos.close();
+			return baos.toByteArray();
+		} catch (IOException e) {
+			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+			throw new InternalErrorException();
+		}
+	}
+
+	@RequestMapping(value = "/meme/stinks", method = RequestMethod.POST)
+	public @ResponseBody
+	byte[] getStinksMeme(@RequestHeader(value = "token") String token,
+						 @RequestHeader(value = "field-a") String fieldA) {
+		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
+		else if (Helper.isEmpty(fieldA)) {
+			throw new NotEnoughArgsException();
+		}
+
+		try {
+			BufferedImage bi = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("stinks.jpg")));
+
+			ByteArrayOutputStream baos = Helper.renderMeme(fieldA, bi);
+
+			baos.close();
+			return baos.toByteArray();
+		} catch (IOException e) {
+			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+			throw new InternalErrorException();
+		}
+	}
+
+	@RequestMapping(value = "/meme/spiderman", method = RequestMethod.POST)
+	public @ResponseBody
+	byte[] getSpidermanMeme(@RequestHeader(value = "token") String token,
+							@RequestHeader(value = "field-a") String fieldA) {
+		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
+		else if (Helper.isEmpty(fieldA)) {
+			throw new NotEnoughArgsException();
+		}
+
+		try {
+			BufferedImage bi = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("miranha.jpg")));
+
+			ByteArrayOutputStream baos = Helper.renderMeme(fieldA, bi);
+
+			baos.close();
+			return baos.toByteArray();
+		} catch (IOException e) {
+			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+			throw new InternalErrorException();
+		}
+	}
+
+	@RequestMapping(value = "/meme/tomcruise", method = RequestMethod.POST)
+	public @ResponseBody
+	byte[] getTomCruiseMeme(@RequestHeader(value = "token") String token,
+							@RequestHeader(value = "field-a") String fieldA) {
+		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
+		else if (Helper.isEmpty(fieldA)) {
+			throw new NotEnoughArgsException();
+		}
+
+		try {
+			BufferedImage bi = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("tomcruise.jpg")));
+
+			ByteArrayOutputStream baos = Helper.renderMeme(fieldA, bi);
+
+			baos.close();
 			return baos.toByteArray();
 		} catch (IOException e) {
 			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
