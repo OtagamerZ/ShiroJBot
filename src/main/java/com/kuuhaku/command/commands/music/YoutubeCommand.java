9381ec1cfefe9fa03c9c832c71b30b49acb97312
Ygimenez <yagogimenez1@hotmail.com>
2020-03-06 10:57:06 -0300

    Continuação da implementação do i18n

--- src/main/java/com/kuuhaku/command/commands/music/YoutubeCommand.java
+++ src/main/java/com/kuuhaku/command/commands/music/YoutubeCommand.java
@@ -18,6 +18,8 @@
 package com.kuuhaku.command.commands.music;
 
 import com.github.ygimenez.method.Pages;
+import com.github.ygimenez.model.Page;
+import com.github.ygimenez.type.PageType;
 import com.kuuhaku.Main;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
@@ -30,63 +32,76 @@ import net.dv8tion.jda.api.entities.*;
 import org.jetbrains.annotations.NonNls;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class YoutubeCommand extends Command {
 
-	public YoutubeCommand(String name, String description, Category category) {
-		super(name, description, category);
-	}
+    public YoutubeCommand(String name, String description, Category category) {
+        super(name, description, category);
+    }
 
-	public YoutubeCommand(String name, String[] aliases, String description, Category category) {
-		super(name, aliases, description, category);
-	}
+    public YoutubeCommand(String name, String[] aliases, String description, Category category) {
+        super(name, aliases, description, category);
+    }
 
-	public YoutubeCommand(String name, String usage, String description, Category category) {
-		super(name, usage, description, category);
-	}
+    public YoutubeCommand(String name, String usage, String description, Category category) {
+        super(name, usage, description, category);
+    }
 
-	public YoutubeCommand(@NonNls String name, @NonNls String[] aliases, String usage, String description, Category category) {
-		super(name, aliases, usage, description, category);
-	}
+    public YoutubeCommand(@NonNls String name, @NonNls String[] aliases, String usage, String description, Category category) {
+        super(name, aliases, usage, description, category);
+    }
 
-	@Override
-	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
-		if (args.length < 1) {
-			channel.sendMessage(":x: | Você precisa digitar um nome para pesquisar.").queue();
-			return;
-		}
-		channel.sendMessage("<a:Loading:598500653215645697> Buscando video...").queue(m -> {
-			try {
-				YoutubeVideo v = Youtube.getSingleData(String.join(" ", args));
-				EmbedBuilder eb = new EmbedBuilder();
+    @Override
+    public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+        if (args.length < 1) {
+            channel.sendMessage(":x: | Você precisa digitar um nome para pesquisar.").queue();
+            return;
+        }
+        channel.sendMessage("<a:Loading:598500653215645697> Buscando videos...").queue(m -> {
+            try {
+                List<YoutubeVideo> videos = Youtube.getData(String.join(" ", args));
+                EmbedBuilder eb = new EmbedBuilder();
 
-				m.editMessage(":mag: Resultados da busca").queue(s -> {
-					try {
-						eb.setTitle(v.getTitle(), v.getUrl());
-						eb.setDescription(v.getDesc());
-						eb.setImage(v.getThumb());
-						eb.setColor(Helper.colorThief(v.getThumb()));
-						eb.setFooter("Link: " + v.getUrl(), v.getUrl());
-						channel.sendMessage(eb.build()).queue(msg -> {
-							if (Objects.requireNonNull(member.getVoiceState()).inVoiceChannel()) {
-								Pages.buttonize(Main.getInfo().getAPI(), msg, Collections.singletonMap(Helper.ACCEPT, (mb, ms) -> {
-									Music.loadAndPlay(member, (TextChannel) channel, msg.getEmbeds().get(0).getFooter().getIconUrl());
-									message.delete().queue(null, Helper::doNothing);
-								}), true, 60, TimeUnit.SECONDS);
-							}
-						});
-					} catch (IOException e) {
-						m.editMessage(":x: | Nenhum vídeo encontrado.").queue();
-						Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
-					}
-				});
-			} catch (IOException e) {
-				m.editMessage(":x: | Erro ao buscar vídeos, meus developers já foram notificados.").queue();
-				Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
-			}
-		});
-	}
+                m.editMessage(":mag: Resultados da busca").queue(s -> {
+                    try {
+                        if (videos.stream().findFirst().isPresent()) {
+                            List<Page> pages = new ArrayList<>();
+
+                            for (YoutubeVideo v : videos) {
+                                eb.clear();
+                                eb.setAuthor("Para ouvir essa música, conecte-se à um canal de voz e clique no botão ✅");
+                                eb.setTitle(v.getTitle(), v.getUrl());
+                                eb.setDescription(v.getDesc());
+                                eb.setThumbnail(v.getThumb());
+                                eb.setColor(Helper.colorThief(v.getThumb()));
+                                eb.setFooter("Link: " + v.getUrl(), null);
+                                pages.add(new Page(PageType.EMBED, eb.build()));
+                            }
+
+                            channel.sendMessage((MessageEmbed) pages.get(0).getContent()).queue(msg -> {
+                                Pages.paginate(Main.getInfo().getAPI(), msg, pages, 60, TimeUnit.SECONDS);
+                                if (Objects.requireNonNull(member.getVoiceState()).inVoiceChannel()) {
+                                    Pages.buttonize(Main.getInfo().getAPI(), msg, Collections.singletonMap(Helper.ACCEPT, (mb, ms) -> {
+                                        Music.loadAndPlay(member, (TextChannel) channel, msg.getEmbeds().get(0).getFooter().getIconUrl());
+                                        message.delete().queue(null, Helper::doNothing);
+                                    }), true, 60, TimeUnit.SECONDS);
+                                }
+                            });
+                        } else m.editMessage(":x: | Nenhum vídeo encontrado").queue();
+                    } catch (IOException e) {
+                        m.editMessage(":x: | Erro ao buscar vídeos, meus developers já foram notificados.").queue();
+                        Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+                    }
+                });
+            } catch (IOException e) {
+                m.editMessage(":x: | Erro ao buscar vídeos, meus developers já foram notificados.").queue();
+                Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+            }
+        });
+    }
 }
