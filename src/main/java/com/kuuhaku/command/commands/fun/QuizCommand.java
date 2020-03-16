a4a9bacaa2ba714d163cb58b385506d2ec49b737
ygimenez <yagogimenez1@hotmail.com>
2020-03-16 16:46:46 -0300

    Melhorias no backup de servidores

---
+++ src/main/java/com/kuuhaku/command/commands/fun/QuizCommand.java
@@ -0,0 +1,102 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.fun;
+
+import com.github.ygimenez.method.Pages;
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.controller.mysql.AccountDAO;
+import com.kuuhaku.controller.mysql.QuizDAO;
+import com.kuuhaku.model.persistent.Account;
+import com.kuuhaku.model.persistent.Quiz;
+import com.kuuhaku.utils.Helper;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.*;
+import org.jetbrains.annotations.NonNls;
+
+import java.awt.*;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.kuuhaku.model.persistent.Quiz.OPTS;
+
+public class QuizCommand extends Command {
+
+	public QuizCommand(String name, String description, Category category, boolean requiresMM) {
+		super(name, description, category, requiresMM);
+	}
+
+	public QuizCommand(@NonNls String name, @NonNls String[] aliases, String description, Category category, boolean requiresMM) {
+		super(name, aliases, description, category, requiresMM);
+	}
+
+	public QuizCommand(String name, String usage, String description, Category category, boolean requiresMM) {
+		super(name, usage, description, category, requiresMM);
+	}
+
+	public QuizCommand(String name, String[] aliases, String usage, String description, Category category, boolean requiresMM) {
+		super(name, aliases, usage, description, category, requiresMM);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+		Quiz q = QuizDAO.getRandomQuiz();
+		Account acc = AccountDAO.getAccount(author.getId());
+		HashMap<String, String> opts = IntStream.range(0, q.getOptions().length()).boxed().collect(Collectors.toMap(k -> OPTS[k], v -> String.valueOf(q.getOptions().get(v)), (k, v) -> k, LinkedHashMap::new));
+
+
+		EmbedBuilder eb = new EmbedBuilder();
+		eb.setThumbnail("https://lh3.googleusercontent.com/proxy/ZvixvksWEH9fKXQXNtDTQYMRNxvRQDCrCDmMiC2g5tkotFwRPcSp9L8c4doZAcR31p5n5sXYmSSyNnQltoPOuRAUPh6fQtyf_PoeDLIUFJINbX0");
+		eb.setTitle("Hora do quiz!");
+		eb.setDescription(q.getQuestion());
+		eb.setColor(Color.yellow);
+		opts.forEach((k, v) -> eb.addField(k + " | " + v, Helper.VOID, false));
+
+		channel.sendMessage(eb.build()).queue(s -> {
+			HashMap<String, BiConsumer<Member, Message>> buttons = new LinkedHashMap<>();
+
+			for (int i = 0; i < OPTS.length; i++) {
+				int finalI = i;
+				buttons.put(OPTS[i], (mb, ms) -> {
+					acc.addCredit(q.getPrize());
+					eb.setThumbnail("https://lh3.googleusercontent.com/proxy/ZvixvksWEH9fKXQXNtDTQYMRNxvRQDCrCDmMiC2g5tkotFwRPcSp9L8c4doZAcR31p5n5sXYmSSyNnQltoPOuRAUPh6fQtyf_PoeDLIUFJINbX0");
+					if (mb.getId().equals(author.getId())) {
+						if (finalI == q.getCorrect()) {
+							eb.setTitle("Resposta correta!");
+							eb.setDescription("Seu prêmio é de " + q.getPrize() + " créditos!");
+							eb.setColor(Color.green);
+						} else {
+							eb.setTitle("Resposta incorreta.");
+							eb.setDescription("Você errou. Tente novamente!");
+							eb.setColor(Color.red);
+						}
+					}
+
+					s.editMessage(eb.build()).queue();
+				});
+			}
+
+			Pages.buttonize(Main.getInfo().getAPI(), s, buttons, false, 60, TimeUnit.SECONDS);
+		});
+	}
+}
---
+++ src/main/java/com/kuuhaku/controller/mysql/QuizDAO.java
@@ -0,0 +1,49 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.controller.mysql;
+
+import com.kuuhaku.model.persistent.Quiz;
+import com.kuuhaku.utils.Helper;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+public class QuizDAO {
+	public static void saveQuiz(Quiz q) {
+		EntityManager em = Manager.getEntityManager();
+
+		em.getTransaction().begin();
+		em.merge(q);
+		em.getTransaction().commit();
+
+		em.close();
+	}
+
+	@SuppressWarnings("unchecked")
+	public static Quiz getRandomQuiz() {
+		EntityManager em = Manager.getEntityManager();
+
+		Query q = em.createQuery("SELECT q FROM Quiz q", Quiz.class);
+		List<Quiz> quizzes = (List<Quiz>) q.getResultList();
+
+		em.close();
+
+		return quizzes.get(Helper.rng(quizzes.size()));
+	}
+}
