f2d9d076b9322bbd53c8918a73215c0b20e73c26
Ygimenez <yagogimenez1@hotmail.com>
2020-03-25 00:06:23 -0300

    Correções na busca por playlists

--- src/main/java/com/kuuhaku/model/common/YoutubeVideo.java
+++ src/main/java/com/kuuhaku/model/common/YoutubeVideo.java
@@ -18,37 +18,39 @@
 package com.kuuhaku.model.common;
 
 public class YoutubeVideo {
-	private final String id, title, desc, thumb, channel;
+    private final String id, title, desc, thumb, channel;
+    private final boolean playlist;
 
-	public YoutubeVideo(String id, String title, String desc, String thumb, String channel) {
-		this.id = id;
-		this.title = title;
-		this.desc = desc;
-		this.thumb = thumb;
-		this.channel = channel;
-	}
+    public YoutubeVideo(String id, String title, String desc, String thumb, String channel, boolean playlist) {
+        this.id = id;
+        this.title = title;
+        this.desc = desc;
+        this.thumb = thumb;
+        this.channel = channel;
+        this.playlist = playlist;
+    }
 
-	public String getUrl() {
-		return "https://www.youtube.com/watch?v=" + id;
-	}
+    public String getUrl() {
+        return playlist ? "https://www.youtube.com/playlist?list=" + id : "https://www.youtube.com/watch?v=" + id;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public String getDesc() {
-		return desc;
-	}
+    public String getDesc() {
+        return desc;
+    }
 
-	public String getThumb() {
-		return thumb;
-	}
+    public String getThumb() {
+        return thumb;
+    }
 
-	public String getChannel() {
-		return channel;
-	}
+    public String getChannel() {
+        return channel;
+    }
 }
