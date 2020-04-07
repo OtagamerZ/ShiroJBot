f17094a3c5faaa2f34a4d0be66269639252bd763
Sunny <carlosminha1@outlook.com>
2020-04-07 11:55:30 -0300

    Novo comando de meme

---
+++ src/main/java/com/kuuhaku/command/commands/fun/PatheticCommand.java
@@ -0,0 +1,69 @@
+/*
+ * This file is part of Shiro J Bot.
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.fun;
+
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.utils.Helper;
+import net.dv8tion.jda.api.entities.*;
+import org.jetbrains.annotations.NonNls;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+public class PatheticCommand extends Command {
+
+    public PatheticCommand(String name, String description, Category category, boolean requiresMM) {
+        super(name, description, category, requiresMM);
+    }
+
+    public PatheticCommand(String name, String[] aliases, String description, Category category, boolean requiresMM) {
+        super(name, aliases, description, category, requiresMM);
+    }
+
+    public PatheticCommand(String name, String usage, String description, Category category, boolean requiresMM) {
+        super(name, usage, description, category, requiresMM);
+    }
+
+    public PatheticCommand(@NonNls String name, @NonNls String[] aliases, String usage, String description, Category category, boolean requiresMM) {
+        super(name, aliases, usage, description, category, requiresMM);
+    }
+
+    @Override
+    public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+        if (args.length < 1) {
+            channel.sendMessage(":x: | Você tem que escrever a mensagem que deseja que apareca no meme.").queue();
+            return;
+        }
+
+        try {
+            String text = String.join(" ", args);
+            BufferedImage bi = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("pathetic.jpg")));
+
+            ByteArrayOutputStream baos = Helper.renderMeme(text, bi);
+
+            channel.sendMessage("Aqui está seu meme " + author.getAsMention() + "!").addFile(baos.toByteArray(), "pathetic.jpg").queue();
+            baos.close();
+        } catch (IOException e) {
+            Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+        }
+    }
+}
