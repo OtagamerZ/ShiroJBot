19ab1103d928e66abb6c8c2f0326d6086b73065d
ygimenez <yagogimenez1@hotmail.com>
2020-04-07 17:27:26 -0300

    O log agora é retornado usando a API do HasteBin

--- src/main/java/com/kuuhaku/command/commands/dev/LogCommand.java
+++ src/main/java/com/kuuhaku/command/commands/dev/LogCommand.java
@@ -20,9 +20,14 @@ package com.kuuhaku.command.commands.dev;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
 import net.dv8tion.jda.api.entities.*;
+import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NonNls;
+import org.json.JSONObject;
 
-import java.io.File;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class LogCommand extends Command {
 
@@ -46,10 +51,36 @@ public class LogCommand extends Command {
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
 		File log = new File("logs/stacktrace.log");
 		try {
-			if (log.exists()) channel.sendMessage("Aqui está!").addFile(log).queue();
+			HttpURLConnection hastebin = (HttpURLConnection) new URL("https://hastebin.com/documents").openConnection();
+			hastebin.setRequestMethod("POST");
+			hastebin.setRequestProperty("User-Agent", "Mozilla/5.0");
+			hastebin.setRequestProperty("Content-Type", "application/json; utf-8");
+			hastebin.setRequestProperty("Accept", "application/json");
+			hastebin.setDoOutput(true);
+			hastebin.connect();
+
+			try (OutputStream os = hastebin.getOutputStream()) {
+				//noinspection ImplicitDefaultCharsetUsage
+				os.write(IOUtils.toString(new FileReader(log)).getBytes(StandardCharsets.UTF_8));
+			}
+
+			String key;
+
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(hastebin.getInputStream(), StandardCharsets.UTF_8))) {
+				String response;
+				StringBuilder sb = new StringBuilder();
+
+				while ((response = br.readLine()) != null) {
+					sb.append(response.trim());
+				}
+
+				key = new JSONObject(sb.toString()).getString("key");
+			}
+
+			if (log.exists()) channel.sendMessage("Aqui está!\n" + "https://hastebin.com/" + key).queue();
 			else channel.sendMessage(":x: | Arquivo de log não encontrado.").queue();
-		} catch (Exception e) {
-			channel.sendMessage("Arquivo de log muito grande, por favor faça a leitura diretamente no VPS.").queue();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
