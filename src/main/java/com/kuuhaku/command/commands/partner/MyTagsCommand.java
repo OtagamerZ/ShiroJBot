0dfba6342e514a6988b1d5c32adb847e3dea8874
ygimenez <yagogimenez1@hotmail.com>
2020-04-09 15:28:58 -0300

    Melhorado o sistema de ratelimits

--- src/main/java/com/kuuhaku/command/commands/partner/MyTagsCommand.java
+++ src/main/java/com/kuuhaku/command/commands/partner/MyTagsCommand.java
@@ -74,9 +74,9 @@ public class MyTagsCommand extends Command {
             if (author.getId().equals(Main.getInfo().getNiiChan()) || Main.getInfo().getDevelopers().contains(author.getId()))
                 badges.append(TagIcons.getTag(TagIcons.DEV));
 
-            if (Main.getInfo().getSheriffs().contains(author.getId())) {
-                badges.append(TagIcons.getTag(TagIcons.SHERIFF));
-            }
+            if (Main.getInfo().getSupports().contains(author.getId())) {
+				badges.append(TagIcons.getTag(TagIcons.SHERIFF));
+			}
 
             if (Main.getInfo().getEditors().contains(author.getId()))
                 badges.append(TagIcons.getTag(TagIcons.EDITOR));
