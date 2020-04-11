a4d3454948169666e985771d709ecff2f340b473
Sunny <carlosminha1@outlook.com>
2020-04-11 13:59:51 -0300

    implementações no i8n

--- src/main/java/com/kuuhaku/command/commands/information/ColorTesterCommand.java
+++ src/main/java/com/kuuhaku/command/commands/information/ColorTesterCommand.java
@@ -54,9 +54,9 @@ public class ColorTesterCommand extends Command {
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
 		if (args.length < 1) {
-			channel.sendMessage(":x: | Você tem que especificar uma cor no seguinte formato: `#RRGGBB`").queue();
-			return;
-		}
+            channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_color-invalid-argument")).queue();
+            return;
+        }
 
 		try {
 			BufferedImage bi = new BufferedImage(128, 128, BufferedImage.TYPE_INT_RGB);
