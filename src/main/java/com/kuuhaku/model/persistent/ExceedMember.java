e5196e6b6bbe1cbaf685dfa53a9220c81511c90d
ygimenez <yagogimenez1@hotmail.com>
2020-04-27 14:50:42 -0300

    Correções básicas

---
+++ src/main/java/com/kuuhaku/model/persistent/ExceedMember.java
@@ -0,0 +1,70 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.model.persistent;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "exceedmember")
+public class ExceedMember {
+	@Id
+	@Column(columnDefinition = "VARCHAR(191)")
+	private String id;
+
+	@Column(columnDefinition = "VARCHAR(191)")
+	private String exceed;
+
+	@Column(columnDefinition = "BOOLEAN DEFAULT FALSE")
+	private boolean victorious = false;
+
+	public ExceedMember(String id, String ex) {
+		this.id = id;
+		this.exceed = ex;
+	}
+
+	public ExceedMember() {
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getExceed() {
+		return exceed;
+	}
+
+	public void setExceed(String exceed) {
+		this.exceed = exceed;
+	}
+
+	public boolean isVictorious() {
+		return victorious;
+	}
+
+	public void setVictorious(boolean victorious) {
+		this.victorious = victorious;
+	}
+}
