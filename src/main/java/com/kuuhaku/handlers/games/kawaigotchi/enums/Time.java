97d249d2b3e3b19fda543c7ebb4387c8b80f0282
ygimenez <yagogimenez1@hotmail.com>
2020-05-08 15:21:14 -0300

    Criação da API de autenticação para o dashboard

--- src/main/java/com/kuuhaku/handlers/games/kawaigotchi/enums/Time.java
+++ src/main/java/com/kuuhaku/handlers/games/kawaigotchi/enums/Time.java
@@ -27,13 +27,13 @@ import java.util.Objects;
 
 public enum Time {
 	DAY(new Image[]{
-			new ImageIcon(Objects.requireNonNull(Time.class.getClassLoader().getResource("sky_day.jpg"))).getImage(),
-			new ImageIcon(Objects.requireNonNull(Time.class.getClassLoader().getResource("gb_no_overlay.png"))).getImage()
+			new ImageIcon(Objects.requireNonNull(Time.class.getClassLoader().getResource("environment/sky_day.jpg"))).getImage(),
+			new ImageIcon(Objects.requireNonNull(Time.class.getClassLoader().getResource("environment/gb_no_overlay.png"))).getImage()
 	}, 9, 18),
 	//18 -> 22 lerp
 	NIGHT(new Image[]{
-			new ImageIcon(Objects.requireNonNull(Time.class.getClassLoader().getResource("sky_night.jpg"))).getImage(),
-			new ImageIcon(Objects.requireNonNull(Time.class.getClassLoader().getResource("gb_overlay.png"))).getImage()
+			new ImageIcon(Objects.requireNonNull(Time.class.getClassLoader().getResource("environment/sky_night.jpg"))).getImage(),
+			new ImageIcon(Objects.requireNonNull(Time.class.getClassLoader().getResource("environment/gb_overlay.png"))).getImage()
 	}, 22, 29);
 	//5 -> 9 lerp
 
