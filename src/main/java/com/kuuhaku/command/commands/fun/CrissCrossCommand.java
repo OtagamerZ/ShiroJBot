288e19cc2f62d6200464acd417eb69ea5aaf6ee8
ygimenez <yagogimenez1@hotmail.com>
2020-05-27 10:27:55 -0300

    Implementação do framework de jogos; Adição do jogo "jogo da velha";

---
+++ src/main/java/com/kuuhaku/command/commands/fun/CrissCrossCommand.java
@@ -0,0 +1,66 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.fun;
+
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.handlers.games.tabletop.entity.Tabletop;
+import com.kuuhaku.handlers.games.tabletop.games.CrissCross;
+import com.kuuhaku.utils.I18n;
+import com.kuuhaku.utils.ShiroInfo;
+import net.dv8tion.jda.api.entities.*;
+import org.jetbrains.annotations.NonNls;
+
+public class CrissCrossCommand extends Command {
+
+	public CrissCrossCommand(String name, String description, Category category, boolean requiresMM) {
+		super(name, description, category, requiresMM);
+	}
+
+	public CrissCrossCommand(String name, String[] aliases, String description, Category category, boolean requiresMM) {
+		super(name, aliases, description, category, requiresMM);
+	}
+
+	public CrissCrossCommand(String name, String usage, String description, Category category, boolean requiresMM) {
+		super(name, usage, description, category, requiresMM);
+	}
+
+	public CrissCrossCommand(@NonNls String name, @NonNls String[] aliases, String usage, String description, Category category, boolean requiresMM) {
+		super(name, aliases, usage, description, category, requiresMM);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+		if (message.getMentionedUsers().size() == 0) {
+			channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_no-user")).queue();
+			return;
+		}
+
+		String id = author.getId() + "." + message.getMentionedUsers().get(0).getId() + "." + guild.getId();
+
+		if (ShiroInfo.gameInProgress(author.getId())) {
+			channel.sendMessage(":x: | Você já está em um jogo neste servidor, por favor finalize-o antes de iniciar outro.").queue();
+			return;
+		}
+
+		Tabletop t = new CrissCross((TextChannel) channel, id, message.getMentionedUsers().get(0), author);
+		ShiroInfo.getGames().put(id, t);
+		t.execute();
+	}
+}
---
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/enums/Board.java
@@ -0,0 +1,91 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.handlers.games.tabletop.enums;
+
+import com.kuuhaku.handlers.games.tabletop.entity.Piece;
+import com.kuuhaku.handlers.games.tabletop.entity.Spot;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public enum Board {
+	SIZE_3X3(new Piece[3][3]),
+	SIZE_8X8(new Piece[8][8]);
+
+	private final Piece[][] board;
+
+	Board(Piece[][] board) {
+		this.board = board;
+	}
+
+	public Piece[][] getLayout() {
+		return board;
+	}
+
+	public Piece[] getColumn(int index) {
+		Piece[] column = new Piece[this.board.length];
+		for (int i = 0; i < this.board.length; i++) {
+			column[i] = this.board[i][index];
+		}
+		return column;
+	}
+
+	public Piece[] getRow(int index) {
+		return this.board[index];
+	}
+
+	public Piece[] getCrossSection(boolean toRight) {
+		Piece[] cross = new Piece[this.board.length];
+		if (toRight)
+			for (int i = 0; i < this.board.length; i++) {
+				cross[i] = this.board[i][i];
+			}
+		else
+			for (int i = 0; i < this.board.length; i++) {
+				cross[i] = this.board[i][(this.board.length - 1) - i];
+			}
+		return cross;
+	}
+
+	public boolean valid(Spot s) {
+		return (s.getX() < board.length && s.getX() >= 0) && (s.getY() < board.length && s.getY() >= 0);
+	}
+
+	public BufferedImage render() {
+		BufferedImage bi = new BufferedImage(64 * this.board.length, 64 * this.board.length, BufferedImage.TYPE_BYTE_GRAY);
+		Graphics2D g2d = bi.createGraphics();
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setFont(new Font(Font.MONOSPACED, Font.BOLD, 32));
+		for (int y = 0; y < this.board.length; y++) {
+			for (int x = 0; x < this.board.length; x++) {
+				g2d.setColor((y + x) % 2 == 0 ? Color.WHITE : Color.BLACK);
+				g2d.fillRect(64 * x, 64 * y, 64, 64);
+				g2d.setColor((y + x) % 2 == 0 ? Color.BLACK : Color.WHITE);
+				Piece p = this.board[y][x];
+				if (p != null) {
+					g2d.drawImage(p.getIcon().render(p.getOwner().isWhite()), 64 * x, 64 * y, null);
+				} else {
+					g2d.drawString(String.valueOf(Spot.getAlphabet().toUpperCase().charAt(x)) + (y + 1), 64 * x + 16, 64 * y + 42);
+				}
+			}
+		}
+		g2d.dispose();
+		return bi;
+	}
+}
