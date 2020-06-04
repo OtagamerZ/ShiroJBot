af6d60635646c44f4966e3c56f33b9a214622684
ygimenez <yagogimenez1@hotmail.com>
2020-06-04 15:44:15 -0300

    Implementação do ShiroCryptor

--- src/main/resources/shirocryptor/encrypt.py
+++ src/main/resources/shirocryptor/encrypt.py
@@ -4,36 +4,10 @@ import hashlib
 import os.path
 import sys
 
-def generate_key():
-    key = Fernet.generate_key()
-    open("key.sbk", "wb").write(key)
-    print("Nova chave de criptografia gerada - {0}".format(key.decode("UTF-8")))
-    return key
-
-def encrypt(target, key):
-    f = Fernet(key)
-    fb = open(target, "rb").read()
+def encrypt(t, k):
+    f = Fernet(k)
+    fb = t.encode("UTF-8")
     return f.encrypt(fb)
 
-
-if len(sys.argv) < 2:
-    print("É necessário informar o arquivo para ser criptografado")
-    exit()
-    
-target = sys.argv[1]
-key = ""
-if os.path.exists("key.sbk"):
-    key = open("key.sbk", "r").read()
-else:
-    key = generate_key()
-
-if not os.path.exists(target):
-    print("Arquivo não encontrado, verifique se digitou o nome corretamente")
-    exit()
-
-content = encrypt(target, key)
-
-open("{0}.sbd".format(target), "wb").write(content)
-open("{0}.hash".format(target), "w").write(hashlib.md5(content).hexdigest())
-
-print("Arquivo encriptado com sucesso, arquivo contendo a chave MD5 foi criada junto ao arquivo criptografado")
+encFile = encrypt(target, key).decode("UTF-8")
+hashFile = hashlib.md5(content).hexdigest()
