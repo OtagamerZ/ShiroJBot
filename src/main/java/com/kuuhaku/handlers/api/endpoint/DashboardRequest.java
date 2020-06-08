8506cb6b943818568f3fecb229161153120d69d8
Ygimenez <yagogimenez1@hotmail.com>
2020-06-08 11:30:51 -0300

    Implementação de BiContract para requisições do site

--- src/main/java/com/kuuhaku/handlers/api/endpoint/DashboardRequest.java
+++ src/main/java/com/kuuhaku/handlers/api/endpoint/DashboardRequest.java
@@ -19,35 +19,22 @@
 package com.kuuhaku.handlers.api.endpoint;
 
 import com.kuuhaku.Main;
-import com.kuuhaku.controller.postgresql.*;
-import com.kuuhaku.controller.sqlite.GuildDAO;
-import com.kuuhaku.controller.sqlite.MemberDAO;
-import com.kuuhaku.handlers.api.exception.Exception;
+import com.kuuhaku.controller.postgresql.TokenDAO;
 import com.kuuhaku.handlers.api.exception.UnauthorizedException;
-import com.kuuhaku.model.common.ExportableGuildConfig;
 import com.kuuhaku.model.common.Profile;
-import com.kuuhaku.model.persistent.*;
 import com.kuuhaku.utils.Helper;
-import com.kuuhaku.utils.PrivilegeLevel;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.User;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
 import javax.imageio.ImageIO;
 import javax.servlet.http.HttpServletResponse;
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.time.OffsetDateTime;
-import java.time.ZoneId;
-import java.util.List;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Base64;
+import java.util.Collections;
 
 @RestController
 public class DashboardRequest {
@@ -96,56 +83,6 @@ public class DashboardRequest {
 		}
 	}
 
-	@RequestMapping(value = "/api/validate", method = RequestMethod.POST)
-	public Exception requestCard(@RequestHeader(value = "token") String token) {
-		boolean valid = TokenDAO.validateToken(token);
-		if (!valid) return new Exception(403, "Unauthorized");
-
-		Token t = TokenDAO.getToken(token);
-		assert t != null;
-		User u = Main.getInfo().getUserByID(t.getUid());
-		User w = Member.getWaifu(u).isBlank() ? null : Main.getInfo().getUserByID(Member.getWaifu(u));
-		CoupleMultiplier cm = WaifuDAO.getMultiplier(u);
-
-		List<Member> profiles = MemberDAO.getMemberByMid(u.getId());
-		JSONObject user = new JSONObject() {{
-			put("waifu", w == null ? "" : w.getAsTag());
-			put("waifuMult", cm == null ? 1.25f : cm.getMult());
-			put("profiles", profiles);
-			put("exceed", new JSONObject(ExceedDAO.getExceedState(ExceedDAO.getExceed(u.getId()))));
-			put("credits", AccountDAO.getAccount(u.getId()).getBalance());
-			put("bonuses", Member.getBonuses(u));
-			put("badges", Tags.getUserBadges(u.getId()));
-		}};
-
-		List<Guild> g = u.getMutualGuilds();
-
-		JSONArray guilds = new JSONArray();
-		g.forEach(gd -> {
-			JSONObject guild = new JSONObject() {{
-				put("guildID", gd.getId());
-				put("name", gd.getName());
-				put("moderator", Helper.hasPermission(gd.getMember(u), PrivilegeLevel.MOD));
-				put("channels", gd.getTextChannels().stream().map(tc -> new JSONObject() {{
-					put("id", tc.getId());
-					put("name", tc.getName());
-				}}).collect(Collectors.toList()));
-				put("roles", gd.getRoles().stream().map(r -> new JSONObject() {{
-					put("id", r.getId());
-					put("name", r.getName());
-				}}).collect(Collectors.toList()));
-				put("configs", new ExportableGuildConfig(GuildDAO.getGuildById(gd.getId())).getGuildConfig());
-			}};
-
-			guilds.put(guild);
-		});
-
-		return new Exception(200, new JSONObject() {{
-			put("userData", user);
-			put("serverData", guilds);
-		}}.toString());
-	}
-
 	@RequestMapping(value = "/api/card", method = RequestMethod.POST)
 	public String requestCard(@RequestHeader(value = "id") String id, @RequestHeader(value = "guild") String guild) throws IOException {
 		if (TokenDAO.verifyToken(id) == null) throw new UnauthorizedException();
@@ -153,94 +90,6 @@ public class DashboardRequest {
 		net.dv8tion.jda.api.entities.Member mb = Main.getInfo().getGuildByID(guild).getMemberById(id);
 		assert mb != null;
 		return Base64.getEncoder().encodeToString(Profile.makeProfile(mb, mb.getGuild()).toByteArray());
-	}
-
-	@RequestMapping(value = "/api/update", method = RequestMethod.POST)
-	public void updateData(@RequestHeader(value = "token") String token, @RequestBody String payload) {
-		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
-		JSONObject cluster = new JSONObject(payload);
-
-		if (cluster.has("guildData")) {
-			JSONObject guild = cluster.getJSONObject("guildData");
-
-			GuildConfig gc = GuildDAO.getGuildById(guild.getString("guildID"));
-
-			JSONObject c = guild.getJSONObject("configs");
-
-			gc.setPrefix(c.getString("prefix"));
-
-			gc.setWarnTime(c.getInt("muteTime"));
-			gc.setPollTime(c.getInt("pollTime"));
-
-			if (!c.getJSONObject("muteRole").isEmpty()) gc.setCargoWarn(c.getJSONObject("muteRole").getString("id"));
-
-			gc.setMsgBoasVindas(c.getString("welcomeMessage"));
-			gc.setMsgAdeus(c.getString("goodbyeMessage"));
-
-			if (!c.getJSONObject("welcomeChannel").isEmpty())
-				gc.setCanalBV(c.getJSONObject("welcomeChannel").getString("id"));
-			if (!c.getJSONObject("goodbyeChannel").isEmpty())
-				gc.setCanalAdeus(c.getJSONObject("goodbyeChannel").getString("id"));
-			if (!c.getJSONObject("suggestionChannel").isEmpty())
-				gc.setCanalSUG(c.getJSONObject("suggestionChannel").getString("id"));
-			if (!c.getJSONObject("relayChannel").isEmpty())
-				gc.setCanalRelay(c.getJSONObject("relayChannel").getString("id"));
-			if (!c.getJSONObject("levelUpChannel").isEmpty())
-				gc.setCanalLvl(c.getJSONObject("levelUpChannel").getString("id"));
-
-			JSONObject lr = new JSONObject();
-			c.getJSONArray("levelRoles").forEach(o -> lr.put(((JSONObject) o).getString("level"), ((JSONObject) o).getString("id")));
-
-			gc.setCargosLvl(lr);
-
-			GuildDAO.updateGuildSettings(gc);
-		}
-
-		if (cluster.has("profileData")) {
-			JSONObject pd = cluster.getJSONObject("profileData");
-
-			JSONObject data = pd;
-			Member mb = MemberDAO.getMemberById(data.getString("id"));
-
-			mb.setBg(data.getString("bg"));
-			mb.setBio(data.getString("bio"));
-
-			MemberDAO.updateMemberConfigs(mb);
-		}
-	}
-
-	@RequestMapping(value = "/api/ticket", method = RequestMethod.POST)
-	public void sendTicket(@RequestHeader(value = "token") String token, @RequestBody String payload) {
-		if (!TokenDAO.validateToken(token)) throw new UnauthorizedException();
-		JSONObject data = new JSONObject(payload);
-
-		Token t = TokenDAO.getToken(token);
-
-		if (t == null) return;
-
-		int number = TicketDAO.openTicket(data.getString("message"), Main.getInfo().getUserByID(t.getUid()));
-
-		EmbedBuilder eb = new EmbedBuilder();
-
-		eb.setTitle("Feedback via site (Ticket Nº " + number + ")");
-		eb.addField("Enviador por:", t.getHolder(), true);
-		eb.addField("Enviado em:", Helper.dateformat.format(OffsetDateTime.now().atZoneSameInstant(ZoneId.of("GMT-3"))), true);
-		eb.addField("Assunto", data.getString("subject"), false);
-		eb.addField("Mensagem:", "```" + data.getString("message") + "```", false);
-		eb.setColor(Color.decode("#fefefe"));
-
-		Map<String, String> ids = new HashMap<>();
-
-		Main.getInfo().getDevelopers().forEach(dev -> Main.getInfo().getUserByID(dev).openPrivateChannel()
-				.flatMap(m -> m.sendMessage(eb.build()))
-				.flatMap(m -> {
-					ids.put(dev, m.getId());
-					return m.pin();
-				})
-				.complete()
-		);
-
-		TicketDAO.setIds(number, ids);
 	}
 
 	@RequestMapping(value = "/api/checkImage", method = RequestMethod.POST)
