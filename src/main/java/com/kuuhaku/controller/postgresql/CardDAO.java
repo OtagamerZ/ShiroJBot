98801adf5b844f68696bd721e3b6ce3b4d23c1f2
ygimenez <yagogimenez1@hotmail.com>
2020-06-19 11:27:50 -0300

    Transferência das cartas para o banco

---
+++ src/main/java/com/kuuhaku/controller/postgresql/CardDAO.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.controller.postgresql;
+
+import com.kuuhaku.model.persistent.Card;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+public class CardDAO {
+	@SuppressWarnings("unchecked")
+	public static List<Card> getCards() {
+		EntityManager em = Manager.getEntityManager();
+
+		Query q = em.createQuery("SELECT c FROM Card c", Card.class);
+		List<Card> c = (List<Card>) q.getResultList();
+
+		em.close();
+
+		return c;
+	}
+}
