868bdd8e85b0d855a46d92c5e4193a47017cefdc
ygimenez <yagogimenez1@hotmail.com>
2020-06-19 15:14:49 -0300

    Criação do módulo de drops

--- src/main/java/com/kuuhaku/command/commands/fun/SadButTrueCommand.java
+++ src/main/java/com/kuuhaku/command/commands/fun/SadButTrueCommand.java
@@ -63,6 +63,7 @@ public class SadButTrueCommand extends Command {
 		try {
 			BufferedImage bi = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("memes/sadbuttrue.png")));
 			Graphics2D g2d = bi.createGraphics();
+			g2d.setBackground(Color.black);
 			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
 			g2d.setColor(Color.BLACK);
