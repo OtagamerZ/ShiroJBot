e800f98fab1e36d4d0ad4d8e9d791605ef318e3e
ygimenez <yagogimenez1@hotmail.com>
2020-07-02 09:11:56 -0300

    Adição do comando para ver uma carta

--- src/main/java/com/kuuhaku/command/commands/misc/SeeCardCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/SeeCardCommand.java
@@ -30,6 +30,11 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.*;
 import org.jetbrains.annotations.NonNls;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
 public class SeeCardCommand extends Command {
 
 	public SeeCardCommand(String name, String description, Category category, boolean requiresMM) {
@@ -66,10 +71,23 @@ public class SeeCardCommand extends Command {
 
 		EmbedBuilder eb = new EmbedBuilder();
 
-		eb.setTitle(tc.getName() + " (" + tc.getRarity().toString() + ")");
-		eb.addField("Obtida:", kp.getCards().contains(card) ? "SIM" : "NÃO", false);
+		eb.setTitle(card.getName());
+		eb.addField("Obtida:", kp.getCards().contains(card) ? "Sim" : "Não", true);
+		eb.addField("Raridade:", tc.getRarity().toString(), true);
+		eb.addField("Tipo:", card.isFoil() ? "Cromada" : "Normal", true);
+		eb.addField("Anime:", tc.getAnime().toString(), true);
 		eb.setImage("attachment://kawaipon.jpg");
 
-		channel.sendMessage(eb.build()).addFile(Helper.getBytes())
+		try {
+			BufferedImage bi = (ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("kawaipon/missing.jpg"))));
+
+			if (kp.getCards().contains(card))
+				channel.sendMessage(eb.build()).addFile(Helper.getBytes(tc.drawCard(foil)), "kawaipon.jpg").queue();
+			else
+				channel.sendMessage(eb.build()).addFile(Helper.getBytes(bi), "kawaipon.jpg").queue();
+		} catch (IOException e) {
+			channel.sendMessage(":x: | Deu um pequeno erro aqui na hora de mostrar a carta, logo logo um dos meus desenvolvedores irá corrigi-lo!").queue();
+			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+		}
 	}
 }
