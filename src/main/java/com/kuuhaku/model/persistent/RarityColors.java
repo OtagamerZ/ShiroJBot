77604f1362cebd1784d3d91d27c82d8d9d65f88a
ygimenez <yagogimenez1@hotmail.com>
2020-07-03 15:54:58 -0300

    Troca para o uso de uma api para o banco de quizzes

--- src/main/java/com/kuuhaku/model/persistent/RarityColors.java
+++ src/main/java/com/kuuhaku/model/persistent/RarityColors.java
@@ -30,21 +30,21 @@ public class RarityColors {
 	@Enumerated(EnumType.STRING)
 	private KawaiponRarity rarity;
 
-	@Column(columnDefinition = "VARCHAR(191) NOT NULL DEFAULT 'FFFFFF'")
-	private String primary = "FFFFFF";
+	@Column(columnDefinition = "CHAR(6) NOT NULL DEFAULT 'FFFFFF'")
+	private String primaryColor = "FFFFFF";
 
-	@Column(columnDefinition = "VARCHAR(191) NOT NULL DEFAULT '000000'")
-	private String secondary = "000000";
+	@Column(columnDefinition = "CHAR(6) NOT NULL DEFAULT '000000'")
+	private String secondaryColor = "000000";
 
 	public KawaiponRarity getRarity() {
 		return rarity;
 	}
 
 	public Color getPrimary() {
-		return Color.decode("#" + primary);
+		return Color.decode("#" + primaryColor);
 	}
 
 	public Color getSecondary() {
-		return Color.decode("#" + secondary);
+		return Color.decode("#" + secondaryColor);
 	}
 }
