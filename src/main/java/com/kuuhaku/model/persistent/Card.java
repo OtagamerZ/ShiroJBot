cdf99b698ab3b3ccbc70ed77d764b7df03d1b057
ygimenez <yagogimenez1@hotmail.com>
2020-07-08 16:04:13 -0300

    Mudança de paleta no processamento das cartas cromadas

--- src/main/java/com/kuuhaku/model/persistent/Card.java
+++ src/main/java/com/kuuhaku/model/persistent/Card.java
@@ -101,7 +101,7 @@ public class Card {
 
 				g2d.dispose();
 
-				return cardCanvas;
+				return hueRotate(cardCanvas);
 			}
 		} catch (IOException | ExecutionException e) {
 			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
@@ -131,13 +131,25 @@ public class Card {
 			for (int y = 0; y < bi.getHeight(); y++) {
 				int rgb = bi.getRGB(x, y);
 				Color col = new Color(rgb);
-				float[] hsv = new float[3];
-				Color.RGBtoHSB(col.getRed(), col.getBlue(), col.getGreen(), hsv);
-
-				Color temp = Color.getHSBColor((hsv[0] + 0.5f) % 1, hsv[1], hsv[2]);
-				col = new Color(col.getRed(), temp.getGreen(), temp.getBlue());
-
+				col = new Color(col.getRed(), col.getBlue(), col.getGreen());
 				out.setRGB(x, y, col.getRGB());
+			}
+		}
+
+		return out;
+	}
+
+	private BufferedImage hueRotate(BufferedImage bi) {
+		BufferedImage out = new BufferedImage(bi.getWidth(), bi.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+		for (int x = 0; x < bi.getWidth(); x++) {
+			for (int y = 0; y < bi.getHeight(); y++) {
+				int rgb = bi.getRGB(x, y);
+				Color col = new Color(rgb);
+				float[] hsv = new float[3];
+				Color.RGBtoHSB(col.getRed(), col.getGreen(), col.getGreen(), hsv);
+				hsv[0] = (hsv[0] + 0.3f) % 1;
+				out.setRGB(x, y, Color.HSBtoRGB(hsv[0], hsv[1], hsv[2]));
 			}
 		}
 
