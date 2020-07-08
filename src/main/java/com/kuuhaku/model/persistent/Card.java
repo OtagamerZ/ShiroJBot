66bd293622c479967a08730fa4e11001dd90923f
ygimenez <yagogimenez1@hotmail.com>
2020-07-08 15:25:49 -0300

    Mudança de paleta no processamento das cartas cromadas

--- src/main/java/com/kuuhaku/model/persistent/Card.java
+++ src/main/java/com/kuuhaku/model/persistent/Card.java
@@ -131,7 +131,7 @@ public class Card {
 			for (int y = 0; y < bi.getHeight(); y++) {
 				int rgb = bi.getRGB(x, y);
 				Color col = new Color(rgb);
-				col = new Color(col.getRed(), (col.getRed() + col.getGreen()) / 2, 255 - (col.getGreen() + col.getBlue()) / 2);
+				col = new Color(col.getRed(), col.getGreen(), 255 - col.getBlue());
 				out.setRGB(x, y, col.getRGB());
 			}
 		}
