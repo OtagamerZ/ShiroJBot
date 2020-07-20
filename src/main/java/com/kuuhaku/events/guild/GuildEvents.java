f6b9763b3278979b0e3157992df22d64cecbca17
Ygimenez <yagogimenez1@hotmail.com>
2020-07-20 17:57:39 -0300

    Remoção de blocos síncronos do GuildEvents

--- src/main/java/com/kuuhaku/events/guild/GuildEvents.java
+++ src/main/java/com/kuuhaku/events/guild/GuildEvents.java
@@ -90,7 +90,7 @@ public class GuildEvents extends ListenerAdapter {
 							}
 						});
 						send.queue();
-						message.delete().complete();
+						message.delete().queue();
 					}
 				} catch (InsufficientPermissionException | ExecutionException | InterruptedException ignore) {
 				}
@@ -116,20 +116,22 @@ public class GuildEvents extends ListenerAdapter {
 
 			if (GuildDAO.getGuildById(guild.getId()).getNoSpamChannels().contains(channel.getId()) && author != Main.getInfo().getSelfUser()) {
 				if (GuildDAO.getGuildById(guild.getId()).isHardAntispam()) {
-					List<Message> h = channel.getHistory().retrievePast(20).complete();
-					h.removeIf(m -> ChronoUnit.MILLIS.between(m.getTimeCreated().toLocalDateTime(), OffsetDateTime.now().atZoneSameInstant(ZoneOffset.UTC)) > 5000 || m.getAuthor() != author);
+					channel.getHistory().retrievePast(20).queue(h -> {
+						h.removeIf(m -> ChronoUnit.MILLIS.between(m.getTimeCreated().toLocalDateTime(), OffsetDateTime.now().atZoneSameInstant(ZoneOffset.UTC)) > 5000 || m.getAuthor() != author);
 
-					countSpam(member, channel, guild, h);
+						countSpam(member, channel, guild, h);
+					});
 				} else {
-					List<Message> h = channel.getHistory().retrievePast(20).complete();
-					h.removeIf(m -> ChronoUnit.MILLIS.between(m.getTimeCreated().toLocalDateTime(), OffsetDateTime.now().atZoneSameInstant(ZoneOffset.UTC)) > 5000 || m.getAuthor() != author && StringUtils.containsIgnoreCase(m.getContentRaw(), rawMessage));
+					channel.getHistory().retrievePast(20).queue(h -> {
+						h.removeIf(m -> ChronoUnit.MILLIS.between(m.getTimeCreated().toLocalDateTime(), OffsetDateTime.now().atZoneSameInstant(ZoneOffset.UTC)) > 5000 || m.getAuthor() != author && StringUtils.containsIgnoreCase(m.getContentRaw(), rawMessage));
 
-					countSpam(member, channel, guild, h);
+						countSpam(member, channel, guild, h);
+					});
 				}
 			}
 
 			if (rawMessage.trim().equals("<@" + Main.getInfo().getSelfUser().getId() + ">") || rawMessage.trim().equals("<@!" + Main.getInfo().getSelfUser().getId() + ">")) {
-				channel.sendMessage("Quer saber como pode usar meus comandos? Digite `" + prefix + "ajuda` para ver todos eles ordenados por categoria!").complete();
+				channel.sendMessage("Quer saber como pode usar meus comandos? Digite `" + prefix + "ajuda` para ver todos eles ordenados por categoria!").queue();
 				return;
 			}
 
@@ -216,19 +218,31 @@ public class GuildEvents extends ListenerAdapter {
 					sortedLvls.keySet().stream().max(Integer::compare).ifPresent(i -> {
 						if (GuildDAO.getGuildById(guild.getId()).isLvlNotif() && !member.getRoles().contains(sortedLvls.get(i))) {
 							try {
-								guild.addRoleToMember(member, sortedLvls.get(i)).complete();
-								String content = author.getAsMention() + " ganhou o cargo " + sortedLvls.get(i).getAsMention() + "! :tada:";
-								if (finalLvlChannel != null) {
-									List<Message> m = finalLvlChannel.getHistory().retrievePast(5).complete();
-									if (m.stream().noneMatch(c -> c.getContentRaw().equals(content))) {
-										finalLvlChannel.sendMessage(content).complete();
+								guild.addRoleToMember(member, sortedLvls.get(i)).queue(s -> {
+									String content = author.getAsMention() + " ganhou o cargo " + sortedLvls.get(i).getAsMention() + "! :tada:";
+									if (finalLvlChannel != null) {
+										finalLvlChannel.getHistory().retrievePast(5).queue(m -> {
+											if (m.stream().noneMatch(c -> c.getContentRaw().equals(content))) {
+												finalLvlChannel.sendMessage(content).queue();
+											}
+										}, Helper::doNothing);
+									} else {
+										channel.getHistory().retrievePast(5).queue(m -> {
+											if (m.stream().noneMatch(c -> c.getContentRaw().equals(content))) {
+												channel.sendMessage(content).queue();
+											}
+										}, Helper::doNothing);
 									}
-								} else {
-									List<Message> m = channel.getHistory().retrievePast(5).complete();
-									if (m.stream().noneMatch(c -> c.getContentRaw().equals(content))) {
-										channel.sendMessage(content).complete();
+								}, ex -> {
+									try {
+										throw ex;
+									} catch (IllegalArgumentException e) {
+										Map<String, Object> cl = gc.getCargoslvl();
+										cl.remove(String.valueOf(i));
+										GuildDAO.updateGuildSettings(gc);
+									} catch (Throwable ignore) {
 									}
-								}
+								});
 							} catch (IllegalArgumentException e) {
 								Map<String, Object> cl = gc.getCargoslvl();
 								cl.remove(String.valueOf(i));
@@ -248,15 +262,15 @@ public class GuildEvents extends ListenerAdapter {
 								GuildDAO.updateGuildSettings(gc);
 							}
 						});
-						guild.modifyMemberRoles(member, null, list).complete();
+						guild.modifyMemberRoles(member, null, list).queue(null, Helper::doNothing);
 					});
 				} catch (InsufficientPermissionException ignore) {
 				}
 
 				try {
 					if (GuildDAO.getGuildById(guild.getId()).getNoLinkChannels().contains(channel.getId()) && Helper.findURL(rawMessage)) {
-						message.delete().reason("Mensagem possui um URL").complete();
-						channel.sendMessage(member.getAsMention() + ", é proibido postar links neste canal!").complete();
+						message.delete().reason("Mensagem possui um URL").queue();
+						channel.sendMessage(member.getAsMention() + ", é proibido postar links neste canal!").queue();
 					}
 
 					com.kuuhaku.model.persistent.Member m = MemberDAO.getMemberById(member.getUser().getId() + member.getGuild().getId());
@@ -267,10 +281,7 @@ public class GuildEvents extends ListenerAdapter {
 
 					boolean lvlUp = m.addXp(guild);
 					if (lvlUp && GuildDAO.getGuildById(guild.getId()).isLvlNotif()) {
-						if (lvlChannel != null) {
-							lvlChannel.sendMessage(author.getAsMention() + " subiu para o nível " + m.getLevel() + ". GGWP! :tada:").complete();
-						} else
-							channel.sendMessage(author.getAsMention() + " subiu para o nível " + m.getLevel() + ". GGWP! :tada:").complete();
+						Objects.requireNonNullElse(lvlChannel, channel).sendMessage(author.getAsMention() + " subiu para o nível " + m.getLevel() + ". GGWP! :tada:").queue();
 					}
 
 					MemberDAO.updateMemberConfigs(m);
