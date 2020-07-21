71929641d1f898bfaa5141a6bbd8d0cc272fe356
ygimenez <yagogimenez1@hotmail.com>
2020-07-21 13:55:14 -0300

    Filtragem de menções

--- src/main/java/com/kuuhaku/command/commands/misc/SayCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/SayCommand.java
@@ -65,7 +65,7 @@ public class SayCommand extends Command {
 
 		com.kuuhaku.model.persistent.Member m = MemberDAO.getMemberById(author.getId() + guild.getId());
 		MessageBuilder mb = new MessageBuilder();
-		mb.append(Helper.makeEmoteFromMention(args)).setAllowedMentions(Helper.mentionFilter);
+		mb.append(Helper.makeEmoteFromMention(args)).denyMentions(Message.MentionType.EVERYONE, Message.MentionType.HERE);
 
 		try {
 			Webhook wh = Helper.getOrCreateWebhook((TextChannel) channel, "Shiro", Main.getInfo().getAPI());
