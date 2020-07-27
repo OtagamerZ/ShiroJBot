8c196b546a6dd1d78aeeac758422881d953e1755
ygimenez <yagogimenez1@hotmail.com>
2020-07-27 14:11:55 -0300

    Comando de gerar ASCII agora também funciona com imagens

--- src/main/java/com/kuuhaku/command/commands/misc/AsciiCommand.java
+++ src/main/java/com/kuuhaku/command/commands/misc/AsciiCommand.java
@@ -20,6 +20,7 @@ package com.kuuhaku.command.commands.misc;
 
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
+import com.kuuhaku.utils.Helper;
 import com.kuuhaku.utils.I18n;
 import com.kuuhaku.utils.ShiroInfo;
 import net.dv8tion.jda.api.entities.*;
@@ -28,6 +29,10 @@ import okhttp3.Request;
 import okhttp3.Response;
 import org.jetbrains.annotations.NonNls;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.RescaleOp;
 import java.io.IOException;
 
 public class AsciiCommand extends Command {
@@ -51,7 +56,18 @@ public class AsciiCommand extends Command {
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
 		if (args.length == 0) {
-			channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_ascii-no-text")).queue();
+			if (message.getAttachments().size() == 0 || !message.getAttachments().get(0).isImage()) {
+				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_ascii-no-text-or-image")).queue();
+				return;
+			}
+
+			try {
+				channel.sendMessage(":warning: | O texto ASCII pode parecer deformado devido ao tamanho do seu ecrã!\n\n" + asciify(ImageIO.read(Helper.getImage(message.getAttachments().get(0).getUrl()))) + "").queue();
+			} catch (IOException e) {
+				Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_image-error")).queue();
+			}
+
 			return;
 		}
 
@@ -70,5 +86,56 @@ public class AsciiCommand extends Command {
 		} catch (IOException e) {
 			channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_api-error")).queue();
 		}
+	}
+
+	private String asciify(BufferedImage bi) {
+		final char base = '\u2800';
+
+		BufferedImage in = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2d = in.createGraphics();
+		g2d.drawImage(bi, 0, 0, 100, 100, null);
+		g2d.dispose();
+
+		RescaleOp ro = new RescaleOp(1, -50, null);
+		in = ro.filter(in, in);
+
+		int threshold = getGrayScale(in.getRGB(0, 0));
+		for (int y = 0; y < in.getHeight(); y++) {
+			for (int x = 0; x < in.getWidth(); x++) {
+				threshold = (threshold + getGrayScale(in.getRGB(x, y))) / 2;
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int y = 0; y < in.getHeight(); y += 4) {
+			for (int x = 0; x < in.getWidth(); x += 2) {
+				int bytes = 0;
+
+				if (getGrayScale(in.getRGB(x, y)) >= threshold) bytes += 1;
+				if (getGrayScale(in.getRGB(x, y + 1)) >= threshold) bytes += 2;
+				if (getGrayScale(in.getRGB(x, y + 2)) >= threshold) bytes += 4;
+
+				if (getGrayScale(in.getRGB(x + 1, y)) >= threshold) bytes += 8;
+				if (getGrayScale(in.getRGB(x + 1, y + 1)) >= threshold) bytes += 16;
+				if (getGrayScale(in.getRGB(x + 1, y + 2)) >= threshold) bytes += 32;
+
+				if (getGrayScale(in.getRGB(x, y + 3)) >= threshold) bytes += 64;
+				if (getGrayScale(in.getRGB(x + 1, y + 3)) >= threshold) bytes += 128;
+
+				if (bytes > 0) sb.append(Character.toString(base + bytes));
+				else sb.append("⡀");
+			}
+			sb.append('\n');
+		}
+
+		return sb.toString();
+	}
+
+	public int getGrayScale(int rgb) {
+		int r = (rgb >> 16) & 0xff;
+		int g = (rgb >> 8) & 0xff;
+		int b = (rgb) & 0xff;
+
+		return (int) (0.2126 * r + 0.7152 * g + 0.0722 * b);
 	}
 }
