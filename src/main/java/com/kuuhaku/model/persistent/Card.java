4b5b91f66f312af768b0db45270b0aabeb641594
ygimenez <yagogimenez1@hotmail.com>
2020-07-30 16:00:09 -0300

    Correções nas cartas cromadas e na cor padrão do perfil

--- src/main/java/com/kuuhaku/model/persistent/Card.java
+++ src/main/java/com/kuuhaku/model/persistent/Card.java
@@ -18,7 +18,6 @@
 
 package com.kuuhaku.model.persistent;
 
-import com.kuuhaku.controller.postgresql.DynamicParameterDAO;
 import com.kuuhaku.utils.AnimeName;
 import com.kuuhaku.utils.Helper;
 import com.kuuhaku.utils.KawaiponRarity;
@@ -133,10 +132,10 @@ public class Card {
 		for (int x = 0; x < bi.getWidth(); x++) {
 			for (int y = 0; y < bi.getHeight(); y++) {
 				int rgb = bi.getRGB(x, y);
-				Color col = new Color(rgb, true);
+				Color col = new Color(rgb);
 				col = new Color(col.getRed(), col.getBlue(), col.getGreen());
 				float[] hsv = ColorUtilities.RGBtoHSL(col);
-				hsv[0] = ((hsv[0] * 255 + Integer.parseInt(DynamicParameterDAO.getParam("FOIL_CARD_OFFSET").getValue())) % 255) / 255;
+				hsv[0] = ((hsv[0] * 255 + 30) % 255) / 255;
 
 				out.setRGB(x, y, ColorUtilities.HSLtoRGB(hsv[0], hsv[1], hsv[2]).getRGB());
 			}
