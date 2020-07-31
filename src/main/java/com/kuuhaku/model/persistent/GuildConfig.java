25d895f72f6d1f1581559a7a8528abc600d5279c
ygimenez <yagogimenez1@hotmail.com>
2020-07-31 11:52:40 -0300

    Adição de versionamento de objetos

--- src/main/java/com/kuuhaku/model/persistent/GuildConfig.java
+++ src/main/java/com/kuuhaku/model/persistent/GuildConfig.java
@@ -23,6 +23,8 @@ import com.kuuhaku.command.Category;
 import com.kuuhaku.utils.Helper;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
+import org.hibernate.annotations.OptimisticLockType;
+import org.hibernate.annotations.OptimisticLocking;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 
@@ -35,7 +37,7 @@ import java.util.Map;
 
 @Entity
 @Table(name = "guildconfig")
-//@OptimisticLocking(type = OptimisticLockType.VERSION)
+@OptimisticLocking(type = OptimisticLockType.VERSION)
 public class GuildConfig {
 	@Id
 	@Column(columnDefinition = "VARCHAR(191)")
