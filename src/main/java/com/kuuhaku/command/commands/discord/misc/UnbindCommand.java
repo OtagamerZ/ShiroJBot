11b4c39d1f5f78d0c5552db595396638a0f7ad03
ygimenez <yagogimenez1@hotmail.com>
2020-08-13 10:40:15 -0300

    Implementação de integração com a twitch

---
+++ src/main/java/com/kuuhaku/command/commands/discord/misc/UnbindCommand.java
@@ -0,0 +1,59 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.discord.misc;
+
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.controller.postgresql.AccountDAO;
+import com.kuuhaku.model.persistent.Account;
+import net.dv8tion.jda.api.entities.*;
+import org.jetbrains.annotations.NonNls;
+
+public class UnbindCommand extends Command {
+
+	public UnbindCommand(String name, String description, Category category, boolean requiresMM) {
+		super(name, description, category, requiresMM);
+	}
+
+	public UnbindCommand(String name, String[] aliases, String description, Category category, boolean requiresMM) {
+		super(name, aliases, description, category, requiresMM);
+	}
+
+	public UnbindCommand(String name, String usage, String description, Category category, boolean requiresMM) {
+		super(name, usage, description, category, requiresMM);
+	}
+
+	public UnbindCommand(@NonNls String name, @NonNls String[] aliases, String usage, String description, Category category, boolean requiresMM) {
+		super(name, aliases, usage, description, category, requiresMM);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+		Account acc = AccountDAO.getAccount(author.getId());
+
+		if (acc.getTwitchId().isBlank()) {
+			channel.sendMessage("❌ | Não há nenhuma conta da Twitch vinculada a este perfil.").queue();
+			return;
+		}
+
+		acc.setTwitchId("");
+		AccountDAO.saveAccount(acc);
+		channel.sendMessage("Conta desvinculada com sucesso").queue();
+	}
+}
