7a84f01a62fca99a7f46f4d117c683f682132d2d
ygimenez <yagogimenez1@hotmail.com>
2020-08-18 14:28:38 -0300

    Migração dos endpoints para o novo domínio

--- src/main/java/com/kuuhaku/handlers/api/endpoint/ErrorHandler.java
+++ src/main/java/com/kuuhaku/handlers/api/endpoint/ErrorHandler.java
@@ -18,68 +18,86 @@
 
 package com.kuuhaku.handlers.api.endpoint;
 
-import com.kuuhaku.handlers.api.exception.Exception;
-import com.kuuhaku.handlers.api.exception.*;
+import com.kuuhaku.handlers.api.exception.InvalidTokenException;
+import com.kuuhaku.handlers.api.exception.NotEnoughArgsException;
+import com.kuuhaku.handlers.api.exception.RatelimitException;
+import com.kuuhaku.handlers.api.exception.UnauthorizedException;
 import org.json.JSONException;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.MethodNotAllowedException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.NoResultException;
 
 @RestController
 @ControllerAdvice
 public class ErrorHandler implements ErrorController {
-	@ExceptionHandler(java.lang.Exception.class)
+	@ExceptionHandler({HttpMessageNotWritableException.class, ConversionNotSupportedException.class})
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-	public Exception internalError() {
-		return new Exception(500, "Internal server error");
+	public ResponseStatusException internalError() {
+		return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
 	}
 
-	@ExceptionHandler(MethodNotAllowedException.class)
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
 	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
-	public Exception methodNotAllowed() {
-		return new Exception(405, "Method not allowed");
+	public ResponseStatusException methodNotAllowed() {
+		return new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED, "Method not allowed");
 	}
 
 	@ExceptionHandler(InvalidTokenException.class)
 	@ResponseStatus(HttpStatus.UNAUTHORIZED)
-	public Exception invalidToken() {
-		return new Exception(403, "Invalid token");
+	public ResponseStatusException invalidToken() {
+		return new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token");
 	}
 
 	@ExceptionHandler(NoResultException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
-	public Exception noResult() {
-		return new Exception(404, "No entity found with that ID");
+	public ResponseStatusException noResult() {
+		return new ResponseStatusException(HttpStatus.NOT_FOUND, "No entity found with that ID");
 	}
 
 	@ExceptionHandler(UnauthorizedException.class)
 	@ResponseStatus(HttpStatus.UNAUTHORIZED)
-	public Exception unauthorized() {
-		return new Exception(403, "Access not authorized");
+	public ResponseStatusException unauthorized() {
+		return new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Access not authorized");
 	}
 
 	@ExceptionHandler(NotEnoughArgsException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public Exception notEnoughArgs() {
-		return new Exception(400, "Not enough arguments were given to this request");
+	public ResponseStatusException notEnoughArgs() {
+		return new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not enough arguments were given to this request");
 	}
 
-	@ExceptionHandler({IllegalArgumentException.class, NullPointerException.class, NumberFormatException.class, JSONException.class})
+	@ExceptionHandler({
+			IllegalArgumentException.class,
+			NullPointerException.class,
+			NumberFormatException.class,
+			JSONException.class,
+			MethodArgumentNotValidException.class,
+			MissingServletRequestParameterException.class,
+			MissingServletRequestPartException.class,
+			TypeMismatchException.class
+	})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public Exception wrongArgs() {
-		return new Exception(400, "Wrong arguments in the request");
+	public ResponseStatusException wrongArgs() {
+		return new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong arguments in the request");
 	}
 
 	@ExceptionHandler(RatelimitException.class)
 	@ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
-	public Exception ratelimited() {
-		return new Exception(429, "You are being ratelimited");
+	public ResponseStatusException ratelimited() {
+		return new ResponseStatusException(HttpStatus.TOO_MANY_REQUESTS, "You are being ratelimited");
 	}
 
 	@Override
