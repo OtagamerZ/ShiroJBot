178c6ede79effcc5b4776620a7faebada99bc7b7
ygimenez <yagogimenez1@hotmail.com>
2020-08-31 10:39:56 -0300

    Adicionada loteria de créditos; Correção no "valor muito alto";

---
+++ src/main/java/com/kuuhaku/command/commands/discord/misc/LotteryCommand.java
@@ -0,0 +1,86 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.discord.misc;
+
+import com.github.ygimenez.method.Pages;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.controller.postgresql.AccountDAO;
+import com.kuuhaku.controller.postgresql.LotteryDAO;
+import com.kuuhaku.model.persistent.Account;
+import com.kuuhaku.model.persistent.Lottery;
+import com.kuuhaku.utils.Helper;
+import com.kuuhaku.utils.I18n;
+import com.kuuhaku.utils.ShiroInfo;
+import net.dv8tion.jda.api.entities.*;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NonNls;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class LotteryCommand extends Command {
+
+	public LotteryCommand(String name, String description, Category category, boolean requiresMM) {
+		super(name, description, category, requiresMM);
+	}
+
+	public LotteryCommand(@NonNls String name, @NonNls String[] aliases, String description, Category category, boolean requiresMM) {
+		super(name, aliases, description, category, requiresMM);
+	}
+
+	public LotteryCommand(String name, String usage, String description, Category category, boolean requiresMM) {
+		super(name, usage, description, category, requiresMM);
+	}
+
+	public LotteryCommand(String name, String[] aliases, String usage, String description, Category category, boolean requiresMM) {
+		super(name, aliases, usage, description, category, requiresMM);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+		if (args.length < 1 || args[0].split(",").length != 6) {
+			channel.sendMessage("❌ | Você precisa informar 6 dezenas separadas por vírgula.").queue();
+			return;
+		}
+
+		Account acc = AccountDAO.getAccount(author.getId());
+
+		for (String dozen : args[0].split(",")) {
+			if (!StringUtils.isNumeric(dozen) || Helper.between(Integer.parseInt(dozen), 0, 60)) {
+				channel.sendMessage("❌ | As dezenas devem ser valores numéricos de 0 a 60.").queue();
+				return;
+			}
+		}
+
+		long cost = (long) (1000 * Math.pow(5, LotteryDAO.getLotteriesByUser(author.getId()).size()));
+		if (acc.getBalance() < cost) {
+			channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_insufficient-credits-user")).queue();
+			return;
+		}
+
+		channel.sendMessage("Você está prestes a comprar um bilhete de loteria com as dezenas `" + args[0].replace(",", " ") + "` por " + cost + " créditos, deseja confirmar?").queue(s ->
+				Pages.buttonize(s, Map.of(Helper.ACCEPT, (mb, ms) -> {
+					acc.removeCredit(cost, this.getClass());
+					AccountDAO.saveAccount(acc);
+					LotteryDAO.saveLottery(new Lottery(author.getId(), args[0]));
+				}), true, 1, TimeUnit.MINUTES)
+		);
+	}
+}
