2635030d14497828f9a9737831c9dfd390f9c96c
Ygimenez <yagogimenez1@hotmail.com>
2020-09-03 14:35:16 -0300

    Modificação nos dados relativos aos Exceeds

---
+++ src/main/java/com/kuuhaku/model/persistent/ExceedScore.java
@@ -0,0 +1,82 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.model.persistent;
+
+import com.kuuhaku.utils.ExceedEnum;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "exceedscore")
+public class ExceedScore {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Enumerated(EnumType.STRING)
+	private ExceedEnum exceed;
+
+	@Column(columnDefinition = "BIGINT NOT NULL DEFAULT 0")
+	private long points = 0;
+
+	@Column(columnDefinition = "DATE")
+	private LocalDate when = LocalDate.now();
+
+	public ExceedScore() {
+	}
+
+	public ExceedScore(ExceedEnum exceed, long points, LocalDate when) {
+		this.exceed = exceed;
+		this.points = points;
+		this.when = when;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public ExceedEnum getExceed() {
+		return exceed;
+	}
+
+	public void setExceed(ExceedEnum exceed) {
+		this.exceed = exceed;
+	}
+
+	public long getPoints() {
+		return points;
+	}
+
+	public void setPoints(long points) {
+		this.points = points;
+	}
+
+	public LocalDate getWhen() {
+		return when;
+	}
+
+	public void setWhen(LocalDate when) {
+		this.when = when;
+	}
+}
