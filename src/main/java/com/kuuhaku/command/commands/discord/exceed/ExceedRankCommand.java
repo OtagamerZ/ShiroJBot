db6ecf446d171765aa4b27d0f549a08d0af05997
ygimenez <yagogimenez1@hotmail.com>
2020-09-03 16:00:33 -0300

    Modificação nos dados relativos aos Exceeds

--- src/main/java/com/kuuhaku/command/commands/discord/exceed/ExceedRankCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/exceed/ExceedRankCommand.java
@@ -21,6 +21,7 @@ package com.kuuhaku.command.commands.discord.exceed;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
 import com.kuuhaku.controller.postgresql.ExceedDAO;
+import com.kuuhaku.model.common.Exceed;
 import com.kuuhaku.model.common.Profile;
 import com.kuuhaku.model.persistent.ExceedScore;
 import com.kuuhaku.utils.ExceedEnum;
@@ -29,9 +30,7 @@ import com.kuuhaku.utils.I18n;
 import com.kuuhaku.utils.ShiroInfo;
 import net.dv8tion.jda.api.entities.*;
 import org.jetbrains.annotations.NonNls;
-import org.knowm.xchart.BitmapEncoder;
-import org.knowm.xchart.XYChart;
-import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.*;
 import org.knowm.xchart.style.Styler;
 
 import javax.imageio.ImageIO;
@@ -71,52 +70,92 @@ public class ExceedRankCommand extends Command {
 			}
 
 			try {
-				List<List<ExceedScore>> exceeds = new ArrayList<>();
-				List<Color> colors = new ArrayList<>();
-				for (ExceedEnum ex : ExceedEnum.values()) {
-					exceeds.add(ExceedDAO.getExceedHistory(ex));
-					colors.add(ex.getPalette());
+				if (args.length == 0) {
+					List<List<ExceedScore>> exceeds = new ArrayList<>();
+					List<Color> colors = new ArrayList<>();
+					for (ExceedEnum ex : ExceedEnum.values()) {
+						exceeds.add(ExceedDAO.getExceedHistory(ex));
+						colors.add(ex.getPalette());
+					}
+
+					XYChart chart = new XYChartBuilder()
+							.width(800)
+							.height(600)
+							.title("Ranking dos Exceeds")
+							.xAxisTitle("Mês")
+							.yAxisTitle("Pontos (x1000)")
+							.build();
+
+
+					chart.getStyler()
+							.setPlotGridLinesColor(Color.decode("#404447"))
+							.setXAxisMin(1d)
+							.setXAxisMax(12d)
+							.setAxisTickLabelsColor(Color.WHITE)
+							.setAnnotationsFontColor(Color.WHITE)
+							.setChartFontColor(Color.WHITE)
+							.setLegendPosition(Styler.LegendPosition.InsideNE)
+							.setSeriesColors(colors.toArray(Color[]::new))
+							.setPlotBackgroundColor(Color.decode("#202225"))
+							.setChartBackgroundColor(Color.decode("#101114"))
+							.setLegendBackgroundColor(Color.decode("#101114"))
+							.setSeriesLines(Collections.nCopies(6, new BasicStroke(4, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND)).toArray(BasicStroke[]::new));
+
+					for (List<ExceedScore> ex : exceeds) {
+						ExceedEnum ee = ex.get(0).getExceed();
+						ex.add(new ExceedScore(ee, ExceedDAO.getExceed(ee).getExp(), LocalDate.now()));
+
+						chart.addSeries(ee.getName(),
+								ex.stream()
+										.map(e -> e.getTimestamp().getMonthValue())
+										.collect(Collectors.toList()),
+								ex.stream()
+										.map(e -> e.getPoints() / 1000)
+										.collect(Collectors.toList())
+						);
+					}
+
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					ImageIO.write(Profile.clipRoundEdges(BitmapEncoder.getBufferedImage(chart)), "png", baos);
+					channel.sendFile(baos.toByteArray(), "ranking.png").queue(s -> s.delete().queueAfter(1, TimeUnit.MINUTES));
+					m.delete().queue();
+				} else if (args[0].equalsIgnoreCase("atual")) {
+					List<Exceed> exceeds = new ArrayList<>();
+					List<Color> colors = new ArrayList<>();
+					for (ExceedEnum ex : ExceedEnum.values()) {
+						exceeds.add(ExceedDAO.getExceed(ex));
+						colors.add(ex.getPalette());
+					}
+
+					CategoryChart chart = new CategoryChartBuilder()
+							.width(800)
+							.height(600)
+							.title("Ranking dos Exceeds")
+							.yAxisTitle("Pontos (x1000)")
+							.build();
+
+					chart.getStyler()
+							.setAxisTickLabelsColor(Color.WHITE)
+							.setAnnotationsFontColor(Color.WHITE)
+							.setChartFontColor(Color.WHITE)
+							.setLegendPosition(Styler.LegendPosition.InsideNE)
+							.setSeriesColors(colors.toArray(Color[]::new))
+							.setPlotBackgroundColor(Color.decode("#202225"))
+							.setChartBackgroundColor(Color.decode("#101114"))
+							.setLegendBackgroundColor(Color.decode("#101114"));
+
+					for (Exceed ex : exceeds) {
+						chart.addSeries(ex.getExceed().getName(),
+								List.of("Pontos Totais"),
+								List.of(ex.getExp())
+						);
+					}
+
+					ByteArrayOutputStream baos = new ByteArrayOutputStream();
+					ImageIO.write(Profile.clipRoundEdges(BitmapEncoder.getBufferedImage(chart)), "png", baos);
+					channel.sendFile(baos.toByteArray(), "ranking.png").queue(s -> s.delete().queueAfter(1, TimeUnit.MINUTES));
+					m.delete().queue();
 				}
-
-				XYChart chart = new XYChartBuilder()
-						.width(800)
-						.height(600)
-						.title("Ranking dos Exceeds")
-						.build();
-
-
-				chart.getStyler()
-						.setPlotGridLinesColor(Color.decode("#404447"))
-						.setXAxisMin(1d)
-						.setXAxisMax(12d)
-						.setAxisTickLabelsColor(Color.WHITE)
-						.setAnnotationsFontColor(Color.WHITE)
-						.setChartFontColor(Color.WHITE)
-						.setLegendPosition(Styler.LegendPosition.InsideNE)
-						.setSeriesColors(colors.toArray(Color[]::new))
-						.setPlotBackgroundColor(Color.decode("#202225"))
-						.setChartBackgroundColor(Color.decode("#101114"))
-						.setLegendBackgroundColor(Color.decode("#101114"))
-						.setSeriesLines(Collections.nCopies(6, new BasicStroke(4, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND)).toArray(BasicStroke[]::new));
-
-				for (List<ExceedScore> ex : exceeds) {
-					ExceedEnum ee = ex.get(0).getExceed();
-					ex.add(new ExceedScore(ee, ExceedDAO.getExceed(ee).getExp(), LocalDate.now()));
-
-					chart.addSeries(ee.getName(),
-							ex.stream()
-									.map(e -> e.getTimestamp().getMonthValue())
-									.collect(Collectors.toList()),
-							ex.stream()
-									.map(ExceedScore::getPoints)
-									.collect(Collectors.toList())
-					);
-				}
-
-				ByteArrayOutputStream baos = new ByteArrayOutputStream();
-				ImageIO.write(Profile.clipRoundEdges(BitmapEncoder.getBufferedImage(chart)), "png", baos);
-				channel.sendFile(baos.toByteArray(), "ranking.png").queue(s -> s.delete().queueAfter(1, TimeUnit.MINUTES));
-				m.delete().queue();
 			} catch (Exception e) {
 				m.editMessage(ShiroInfo.getLocale(I18n.PT).getString("err_exceed-rank")).queue();
 				Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
