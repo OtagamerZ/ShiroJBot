58e54e1ddbf9a6852f5bfa54e3aa5438c32332fa
ygimenez <yagogimenez1@hotmail.com>
2020-09-17 10:36:52 -0300

    Correção no XP

--- src/main/java/com/kuuhaku/events/guild/GuildEvents.java
+++ src/main/java/com/kuuhaku/events/guild/GuildEvents.java
@@ -297,6 +297,8 @@ public class GuildEvents extends ListenerAdapter {
 					} catch (InsufficientPermissionException e) {
 						channel.sendMessage(author.getAsMention() + " subiu para o nível " + m.getLevel() + ". GGWP! :tada:").queue();
 					}
+
+					MemberDAO.updateMemberConfigs(m);
 				} catch (NoResultException e) {
 					MemberDAO.addMemberToDB(member);
 				} catch (ErrorResponseException | NullPointerException e) {
