2a9f2edbb700422932f29019e9ec7f4f608cafad
Ygimenez <yagogimenez1@hotmail.com>
2020-10-02 11:17:47 -0300

    Correções gerais

--- src/main/java/com/kuuhaku/command/commands/discord/exceed/ExceedRankCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/exceed/ExceedRankCommand.java
@@ -77,7 +77,7 @@ public class ExceedRankCommand extends Command {
 						e.add(new ExceedScore(ex, ExceedDAO.getExceed(ex).getExp(), LocalDate.now()));
 						e.sort(Comparator.comparing(ExceedScore::getTimestamp));
 						exceeds.add(e);
-						colors.add(ex.getPalette());
+						colors.add(new Color(ex.getPalette().getRed(), ex.getPalette().getGreen(), ex.getPalette().getBlue(), 200));
 					}
 
 					XYChart chart = new XYChartBuilder()
