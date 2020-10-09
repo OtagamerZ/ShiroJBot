801e96ea25c55eb54f47b3ac8ba1f99362067d33
ygimenez <yagogimenez1@hotmail.com>
2020-10-09 14:07:02 -0300

    Adição de compra/venda de equipamentos na loja de cartas

--- src/main/java/com/kuuhaku/model/persistent/EquipmentMarket.java
+++ src/main/java/com/kuuhaku/model/persistent/EquipmentMarket.java
@@ -30,7 +30,7 @@ import java.util.Date;
 @Table(name = "equipmentmarket")
 public class EquipmentMarket {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator = "cardmarket_id_seq")
 	private int id;
 
 	@Column(columnDefinition = "VARCHAR(191) NOT NULL")
