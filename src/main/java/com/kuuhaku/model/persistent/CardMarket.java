1c59a6fdd0d2347d256cf871f82bde25d6efefca
ygimenez <yagogimenez1@hotmail.com>
2020-10-09 15:48:00 -0300

    Comando See agora mostra cartas Shoukan

--- src/main/java/com/kuuhaku/model/persistent/CardMarket.java
+++ src/main/java/com/kuuhaku/model/persistent/CardMarket.java
@@ -28,8 +28,8 @@ import java.util.Date;
 @Table(name = "cardmarket")
 public class CardMarket {
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cardmarket_id_seq")
-	@SequenceGenerator(name = "cardmarket_id_seq", initialValue = 3263, allocationSize = 1, schema = "shiro")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "market_id_seq")
+	@SequenceGenerator(name = "market_id_seq", initialValue = 3263, allocationSize = 1, schema = "shiro")
 	private int id;
 
 	@Column(columnDefinition = "VARCHAR(191) NOT NULL")
