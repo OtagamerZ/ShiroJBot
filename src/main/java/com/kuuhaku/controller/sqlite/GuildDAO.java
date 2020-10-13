8a968c71e4b5064053b208508a97b3167e844105
ygimenez <yagogimenez1@hotmail.com>
2020-10-13 15:54:04 -0300

    Cargos de Exceed automaticos para todos! (que quiserem)

--- src/main/java/com/kuuhaku/controller/sqlite/GuildDAO.java
+++ src/main/java/com/kuuhaku/controller/sqlite/GuildDAO.java
@@ -91,6 +91,18 @@ public class GuildDAO {
 	}
 
 	@SuppressWarnings("unchecked")
+	public static List<GuildConfig> getAllGuildsWithExceedRoles() {
+		EntityManager em = Manager.getEntityManager();
+
+		Query gc = em.createQuery("SELECT g FROM GuildConfig g WHERE exceedRolesEnabled = TRUE", GuildConfig.class);
+		List<GuildConfig> gcs = (List<GuildConfig>) gc.getResultList();
+
+		em.close();
+
+		return gcs;
+	}
+
+	@SuppressWarnings("unchecked")
 	public static List<GuildConfig> getAllGuildsWithButtons() {
 		EntityManager em = Manager.getEntityManager();
 
