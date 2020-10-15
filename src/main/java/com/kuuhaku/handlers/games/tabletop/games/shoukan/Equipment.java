ee02c8d7cf0b5ba96d78f9f051f46d39fb5a08c9
ygimenez <yagogimenez1@hotmail.com>
2020-10-15 13:34:27 -0300

    Melhoria nas cartas-equipamento

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Equipment.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Equipment.java
@@ -23,12 +23,13 @@ import com.kuuhaku.handlers.games.tabletop.games.shoukan.interfaces.Drawable;
 import com.kuuhaku.model.common.Profile;
 import com.kuuhaku.model.persistent.Account;
 import com.kuuhaku.model.persistent.Card;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 
+import javax.imageio.ImageIO;
 import javax.persistence.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.Objects;
 
 @Entity
@@ -71,7 +72,7 @@ public class Equipment implements Drawable, Cloneable {
 			g2d.drawImage(acc.getFrame().getFrontEquipment(), 0, 0, null);
 			g2d.setFont(Profile.FONT.deriveFont(Font.PLAIN, 20));
 
-			Profile.drawOutlinedText("(" + StringUtils.repeat("×", tier) + ") " + card.getName(), 13, 32, g2d);
+			Profile.drawOutlinedText(card.getName(), 13, 30, g2d);
 
 			g2d.setColor(Color.red);
 			Profile.drawOutlinedText(String.valueOf(atk), 45, 316, g2d);
@@ -110,6 +111,13 @@ public class Equipment implements Drawable, Cloneable {
 
 		g2d.setColor(Color.green);
 		Profile.drawOutlinedText(String.valueOf(def), 178 - g2d.getFontMetrics().stringWidth(String.valueOf(def)), 316, g2d);
+
+		try {
+			BufferedImage star = ImageIO.read(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("shoukan/star.png")));
+			for (int i = 0; i < tier; i++)
+				g2d.drawImage(star, (bi.getWidth() / 2) - ((star.getWidth() * tier / 2) + star.getWidth() * i), 42, null);
+		} catch (IOException ignore) {
+		}
 
 		if (!available) {
 			g2d.setColor(new Color(0, 0, 0, 150));
