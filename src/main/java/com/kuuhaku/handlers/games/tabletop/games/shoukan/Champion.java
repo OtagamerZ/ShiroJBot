15bc57d686b4ebbcd34dbd87f586a2a509da83a8
KuuHaKu <yagogimenez1@hotmail.com>
2020-10-21 22:32:57 -0300

    Update Champion.java

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Champion.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Champion.java
@@ -264,6 +264,7 @@ public class Champion implements Drawable, Cloneable {
     public void getEffect(EffectParameters ep) {
         String imports = """
                 //%s
+		import com.kuuhaku.handlers.games.tabletop.games.shoukan.interfaces.Drawable;
                 import com.kuuhaku.handlers.games.tabletop.games.shoukan.enums.Phase;
                 import com.kuuhaku.handlers.games.tabletop.games.shoukan.enums.Race;
                 import com.kuuhaku.handlers.games.tabletop.games.shoukan.enums.Side;
