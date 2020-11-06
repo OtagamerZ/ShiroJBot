ed86c5a3f0625486161fc7940932650adf63f718
ygimenez <yagogimenez1@hotmail.com>
2020-11-06 11:23:18 -0300

    Adição de endpoint para o webhook do Patreon

---
+++ src/main/java/com/kuuhaku/handlers/api/endpoint/PatreonHandler.java
@@ -0,0 +1,35 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.handlers.api.endpoint;
+
+import com.kuuhaku.utils.Helper;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.StandardCharsets;
+
+@RestController
+public class PatreonHandler {
+
+	@RequestMapping(value = "/webhook/patreon", consumes = "application/json", produces = "application/json", method = RequestMethod.POST)
+	public void handleVote(@RequestHeader(value = "X-Patreon-Signature") String signature, @RequestBody String payload) {
+		Helper.logger(this.getClass()).info("Signature: " + signature);
+		Helper.logger(this.getClass()).info("Body MD5: " + Helper.hash(payload.getBytes(StandardCharsets.UTF_8), "MD5"));
+		Helper.logger(this.getClass()).info("Body: " + payload);
+	}
+}
