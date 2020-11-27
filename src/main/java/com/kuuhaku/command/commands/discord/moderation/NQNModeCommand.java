daf9b87b00c0c707cfae7c8c9cc90c141714f00b
ygimenez <yagogimenez1@hotmail.com>
2020-11-27 13:59:48 -0300

    Criação do modo NQN

---
+++ src/main/java/com/kuuhaku/command/commands/discord/moderation/NQNModeCommand.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.discord.moderation;
+
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Command;
+import com.kuuhaku.controller.postgresql.TagDAO;
+import com.kuuhaku.controller.sqlite.GuildDAO;
+import com.kuuhaku.model.enums.I18n;
+import com.kuuhaku.model.persistent.GuildConfig;
+import com.kuuhaku.utils.ShiroInfo;
+import net.dv8tion.jda.api.entities.*;
+import org.jetbrains.annotations.NonNls;
+
+public class NQNModeCommand extends Command {
+
+	public NQNModeCommand(String name, String description, Category category, boolean requiresMM) {
+		super(name, description, category, requiresMM);
+	}
+
+	public NQNModeCommand(@NonNls String name, @NonNls String[] aliases, String description, Category category, boolean requiresMM) {
+		super(name, aliases, description, category, requiresMM);
+	}
+
+	public NQNModeCommand(String name, String usage, String description, Category category, boolean requiresMM) {
+		super(name, usage, description, category, requiresMM);
+	}
+
+	public NQNModeCommand(@NonNls String name, @NonNls String[] aliases, String usage, String description, Category category, boolean requiresMM) {
+		super(name, aliases, usage, description, category, requiresMM);
+	}
+
+	@Override
+	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+		if (TagDAO.getTagById(guild.getOwnerId()).isToxic()) {
+			channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_toxic-owner")).queue();
+			return;
+		}
+
+		GuildConfig gc = GuildDAO.getGuildById(guild.getId());
+
+		if (gc.isNQNMode()) {
+			gc.toggleNQNMode();
+			channel.sendMessage("Modo NQN desativado.").queue();
+		} else {
+			gc.toggleNQNMode();
+			channel.sendMessage("Modo NQN ativado.").queue();
+		}
+
+		GuildDAO.updateGuildSettings(gc);
+	}
+}
