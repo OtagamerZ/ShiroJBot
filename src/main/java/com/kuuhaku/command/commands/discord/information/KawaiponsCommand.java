6d017b0448dba9391c77f526890fba761492f2ca
ygimenez <yagogimenez1@hotmail.com>
2020-11-28 17:59:36 -0300

    Correção menor

--- src/main/java/com/kuuhaku/command/commands/discord/information/KawaiponsCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/information/KawaiponsCommand.java
@@ -51,165 +51,168 @@ import java.util.stream.Collectors;
 
 public class KawaiponsCommand extends Command {
 
-	public KawaiponsCommand(String name, String description, com.kuuhaku.command.Category category, boolean requiresMM) {
-		super(name, description, category, requiresMM);
-	}
+    public KawaiponsCommand(String name, String description, com.kuuhaku.command.Category category, boolean requiresMM) {
+        super(name, description, category, requiresMM);
+    }
 
-	public KawaiponsCommand(@NonNls String name, @NonNls String[] aliases, String description, com.kuuhaku.command.Category category, boolean requiresMM) {
-		super(name, aliases, description, category, requiresMM);
-	}
+    public KawaiponsCommand(@NonNls String name, @NonNls String[] aliases, String description, com.kuuhaku.command.Category category, boolean requiresMM) {
+        super(name, aliases, description, category, requiresMM);
+    }
 
-	public KawaiponsCommand(String name, String usage, String description, com.kuuhaku.command.Category category, boolean requiresMM) {
-		super(name, usage, description, category, requiresMM);
-	}
+    public KawaiponsCommand(String name, String usage, String description, com.kuuhaku.command.Category category, boolean requiresMM) {
+        super(name, usage, description, category, requiresMM);
+    }
 
-	public KawaiponsCommand(@NonNls String name, @NonNls String[] aliases, String usage, String description, com.kuuhaku.command.Category category, boolean requiresMM) {
-		super(name, aliases, usage, description, category, requiresMM);
-	}
+    public KawaiponsCommand(@NonNls String name, @NonNls String[] aliases, String usage, String description, com.kuuhaku.command.Category category, boolean requiresMM) {
+        super(name, aliases, usage, description, category, requiresMM);
+    }
 
-	@Override
-	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
-		channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("str_generating-collection")).queue(m -> {
-			try {
-				Kawaipon kp = KawaiponDAO.getKawaipon(author.getId());
+    @Override
+    public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+        channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("str_generating-collection")).queue(m -> {
+            try {
+                Kawaipon kp = KawaiponDAO.getKawaipon(author.getId());
 
-				if (kp.getCards().size() == 0) {
-					m.editMessage("❌ | Você ainda não coletou nenhum Kawaipon.").queue();
-					return;
-				} else if (args.length == 0) {
-					Set<KawaiponCard> collection = new HashSet<>();
-					for (AnimeName anime : AnimeName.validValues()) {
-						if (CardDAO.totalCards(anime) == kp.getCards().stream().filter(k -> k.getCard().getAnime().equals(anime) && !k.isFoil()).count())
-							collection.add(new KawaiponCard(CardDAO.getUltimate(anime), false));
-					}
+                if (kp.getCards().size() == 0) {
+                    m.editMessage("❌ | Você ainda não coletou nenhum Kawaipon.").queue();
+                    return;
+                } else if (args.length == 0) {
+                    Set<KawaiponCard> collection = new HashSet<>();
+                    for (AnimeName anime : AnimeName.validValues()) {
+                        if (CardDAO.totalCards(anime) == kp.getCards().stream().filter(k -> k.getCard().getAnime().equals(anime) && !k.isFoil()).count())
+                            collection.add(new KawaiponCard(CardDAO.getUltimate(anime), false));
+                    }
 
-					KawaiponBook kb = new KawaiponBook(collection);
-					BufferedImage cards = kb.view(CardDAO.getCardsByRarity(KawaiponRarity.ULTIMATE), "Coleção Kawaipon", false);
+                    KawaiponBook kb = new KawaiponBook(collection);
+                    BufferedImage cards = kb.view(CardDAO.getCardsByRarity(KawaiponRarity.ULTIMATE), "Coleção Kawaipon", false);
 
-					EmbedBuilder eb = new ColorlessEmbedBuilder();
-					int count = collection.size();
-					int foil = (int) kp.getCards().stream().filter(KawaiponCard::isFoil).count();
-					int common = kp.getCards().size() - foil;
+                    EmbedBuilder eb = new ColorlessEmbedBuilder();
+                    int count = collection.size();
+                    int foil = (int) kp.getCards().stream().filter(KawaiponCard::isFoil).count();
+                    int common = kp.getCards().size() - foil;
 
-					eb.setTitle("\uD83C\uDFB4 | Kawaipons de " + author.getName());
-					eb.addField(":books: | Coleções completas:", count + " de " + AnimeName.validValues().length + " (" + Helper.prcntToInt(count, AnimeName.validValues().length) + "%)", true);
-					eb.addField(":red_envelope: | Total de cartas normais:", common + " de " + CardDAO.totalCards() + " (" + Helper.prcntToInt(common, CardDAO.totalCards()) + "%)", true);
-					eb.addField(":star2: | Total de cartas cromadas:", foil + " de " + CardDAO.totalCards() + " (" + Helper.prcntToInt(foil, CardDAO.totalCards()) + "%)", true);
-					eb.setImage("attachment://cards.png");
-					eb.setFooter("Total coletado (normais + cromadas): " + Helper.prcntToInt(kp.getCards().size(), CardDAO.totalCards() * 2) + "%");
+                    eb.setTitle("\uD83C\uDFB4 | Kawaipons de " + author.getName());
+                    eb.addField(":books: | Coleções completas:", count + " de " + AnimeName.validValues().length + " (" + Helper.prcntToInt(count, AnimeName.validValues().length) + "%)", true);
+                    eb.addField(":red_envelope: | Total de cartas normais:", common + " de " + CardDAO.totalCards() + " (" + Helper.prcntToInt(common, CardDAO.totalCards()) + "%)", true);
+                    eb.addField(":star2: | Total de cartas cromadas:", foil + " de " + CardDAO.totalCards() + " (" + Helper.prcntToInt(foil, CardDAO.totalCards()) + "%)", true);
+                    eb.setImage("attachment://cards.png");
+                    eb.setFooter("Total coletado (normais + cromadas): " + Helper.prcntToInt(kp.getCards().size(), CardDAO.totalCards() * 2) + "%");
 
-					m.delete().queue();
-					channel.sendMessage(eb.build()).addFile(Helper.getBytes(cards, "png"), "cards.png").queue();
-					return;
-				}
+                    m.delete().queue();
+                    channel.sendMessage(eb.build()).addFile(Helper.getBytes(cards, "png"), "cards.png").queue();
+                    return;
+                }
 
-				Category c = Category.getByName(args[0]);
-				KawaiponRarity rr = KawaiponRarity.getByName(args[0]);
+                Category c = Category.getByName(args[0]);
+                KawaiponRarity rr = KawaiponRarity.getByName(args[0]);
 
-				if (rr == null) {
-					if (c == null) {
-						if (args[0].equalsIgnoreCase("total")) {
-							boolean foil = args.length > 1 && args[1].equalsIgnoreCase("C");
-							Set<KawaiponCard> collection = kp.getCards();
-							Set<KawaiponCard> toRender = collection.stream().filter(k -> k.isFoil() == foil).collect(Collectors.toSet());
+                if (rr == null) {
+                    if (c == null) {
+                        if (args[0].equalsIgnoreCase("total")) {
+                            boolean foil = args.length > 1 && args[1].equalsIgnoreCase("C");
+                            Set<KawaiponCard> collection = kp.getCards();
+                            Set<KawaiponCard> toRender = collection.stream().filter(k -> k.isFoil() == foil).collect(Collectors.toSet());
 
-							KawaiponBook kb = new KawaiponBook(toRender);
-							BufferedImage cards = kb.view(CardDAO.getCards(), "Todas as cartas", foil);
+                            KawaiponBook kb = new KawaiponBook(toRender);
+                            BufferedImage cards = kb.view(CardDAO.getCards(), "Todas as cartas", foil);
 
-							send(author, channel, m, collection, cards, "Todas as cartas", CardDAO.totalCards());
-							return;
-						} else if (args[0].equalsIgnoreCase("elegiveis")) {
-							List<Champion> cardList = CardDAO.getAllChampions();
+                            send(author, channel, m, collection, cards, "Todas as cartas", CardDAO.totalCards());
+                            return;
+                        } else if (args[0].equalsIgnoreCase("elegiveis")) {
+                            List<Champion> cardList = CardDAO.getAllChampions();
 
-							KawaiponBook kb = new KawaiponBook();
-							BufferedImage cards = kb.view(cardList, AccountDAO.getAccount(author.getId()), "Cartas elegíveis");
+                            KawaiponBook kb = new KawaiponBook();
+                            BufferedImage cards = kb.view(cardList, AccountDAO.getAccount(author.getId()), "Cartas elegíveis");
 
-							send(author, channel, m, cards, "Cartas elegíveis", null);
-							return;
-						} else if (Arrays.stream(AnimeName.validValues()).noneMatch(a -> a.name().equals(args[0].toUpperCase()))) {
-							m.editMessage("❌ | Anime inválido ou ainda não adicionado, você não quis dizer `" + Helper.didYouMean(args[0], Arrays.stream(AnimeName.validValues()).map(AnimeName::name).toArray(String[]::new)) + "`? (colocar `_` no lugar de espaços)").queue();
-							return;
-						}
+                            send(author, channel, m, cards, "Cartas elegíveis", null);
+                            return;
+                        } else if (Arrays.stream(AnimeName.validValues()).noneMatch(a -> a.name().equals(args[0].toUpperCase()))) {
+                            m.editMessage("❌ | Anime inválido ou ainda não adicionado, você não quis dizer `" + Helper.didYouMean(args[0], Arrays.stream(AnimeName.validValues()).map(AnimeName::name).toArray(String[]::new)) + "`? (colocar `_` no lugar de espaços)").queue();
+                            return;
+                        }
 
-						boolean foil = args.length > 1 && args[1].equalsIgnoreCase("C");
-						AnimeName anime = AnimeName.valueOf(args[0].toUpperCase());
-						Set<KawaiponCard> collection = kp.getCards().stream().filter(k -> k.getCard().getAnime().equals(anime)).collect(Collectors.toSet());
-						Set<KawaiponCard> toRender = collection.stream().filter(k -> k.isFoil() == foil).collect(Collectors.toSet());
+                        boolean foil = args.length > 1 && args[1].equalsIgnoreCase("C");
+                        AnimeName anime = AnimeName.valueOf(args[0].toUpperCase());
+                        Set<KawaiponCard> collection = kp.getCards().stream().filter(k -> k.getCard().getAnime().equals(anime)).collect(Collectors.toSet());
+                        Set<KawaiponCard> toRender = collection.stream().filter(k -> k.isFoil() == foil).collect(Collectors.toSet());
 
-						KawaiponBook kb = new KawaiponBook(toRender);
-						BufferedImage cards = kb.view(CardDAO.getCardsByAnime(anime), anime.toString(), foil);
+                        KawaiponBook kb = new KawaiponBook(toRender);
+                        BufferedImage cards = kb.view(CardDAO.getCardsByAnime(anime), anime.toString(), foil);
 
-						send(author, channel, m, collection, cards, anime.toString(), CardDAO.totalCards(anime));
-						return;
-					}
+                        send(author, channel, m, collection, cards, anime.toString(), CardDAO.totalCards(anime));
+                        return;
+                    }
 
-					List<Champion> cardList = CardDAO.getChampions(c);
+                    List<Champion> cardList = CardDAO.getChampions(c);
 
-					KawaiponBook kb = new KawaiponBook();
-					BufferedImage cards = kb.view(cardList, AccountDAO.getAccount(author.getId()), c.getName());
+                    KawaiponBook kb = new KawaiponBook();
+                    BufferedImage cards = kb.view(cardList, AccountDAO.getAccount(author.getId()), c.getName());
 
-					send(author, channel, m, cards, c.getName(), c);
-				} else {
-					boolean foil = args.length > 1 && args[1].equalsIgnoreCase("C");
-					Set<KawaiponCard> collection = kp.getCards().stream().filter(k -> k.getCard().getRarity().equals(rr)).collect(Collectors.toSet());
-					Set<KawaiponCard> toRender = collection.stream().filter(k -> k.isFoil() == foil).collect(Collectors.toSet());
+                    send(author, channel, m, cards, c.getName(), c);
+                } else {
+                    boolean foil = args.length > 1 && args[1].equalsIgnoreCase("C");
+                    Set<KawaiponCard> collection = kp.getCards().stream().filter(k -> k.getCard().getRarity().equals(rr)).collect(Collectors.toSet());
+                    Set<KawaiponCard> toRender = collection.stream().filter(k -> k.isFoil() == foil).collect(Collectors.toSet());
 
-					KawaiponBook kb = new KawaiponBook(toRender);
-					BufferedImage cards = kb.view(CardDAO.getCardsByRarity(rr), rr.toString(), foil);
+                    KawaiponBook kb = new KawaiponBook(toRender);
+                    BufferedImage cards = kb.view(CardDAO.getCardsByRarity(rr), rr.toString(), foil);
 
 
-					send(author, channel, m, collection, cards, rr.toString(), CardDAO.totalCards(rr));
-				}
-			} catch (IOException | InterruptedException e) {
-				m.editMessage(ShiroInfo.getLocale(I18n.PT).getString("err_collection-generation-error")).queue();
-				Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
-			}
-		});
-	}
+                    send(author, channel, m, collection, cards, rr.toString(), CardDAO.totalCards(rr));
+                }
+            } catch (IOException | InterruptedException e) {
+                m.editMessage(ShiroInfo.getLocale(I18n.PT).getString("err_collection-generation-error")).queue();
+                Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+            }
+        });
+    }
 
-	private void send(User author, MessageChannel channel, Message m, Set<KawaiponCard> collection, BufferedImage cards, String s, long l) throws IOException {
-		String hash = Helper.hash((author.getId() + System.currentTimeMillis()).getBytes(StandardCharsets.UTF_8), "SHA-1");
-		File f = new File(Main.getInfo().getCollectionsFolder(), hash + ".jpg");
-		byte[] bytes = Helper.getBytes(Helper.removeAlpha(cards), "jpg", 0.5f);
-		//byte[] bytes = Helper.getBytes(Helper.removeAlpha(cards), "jpg");
-		try (FileOutputStream fos = new FileOutputStream(f)) {
-			fos.write(bytes);
-		}
+    private void send(User author, MessageChannel channel, Message m, Set<KawaiponCard> collection, BufferedImage cards, String s, long l) throws IOException {
+        String hash = Helper.hash((author.getId() + System.currentTimeMillis()).getBytes(StandardCharsets.UTF_8), "SHA-1");
+        File f = new File(Main.getInfo().getCollectionsFolder(), hash + ".jpg");
+        byte[] bytes = Helper.getBytes(Helper.removeAlpha(cards), "jpg", 0.5f);
+        //byte[] bytes = Helper.getBytes(Helper.removeAlpha(cards), "jpg");
+        try (FileOutputStream fos = new FileOutputStream(f)) {
+            fos.write(bytes);
+        }
 
-		Helper.keepMaximumNFiles(Main.getInfo().getCollectionsFolder(), 20);
+        Helper.keepMaximumNFiles(Main.getInfo().getCollectionsFolder(), 20);
 
-		EmbedBuilder eb = new ColorlessEmbedBuilder();
-		int foil = (int) collection.stream().filter(KawaiponCard::isFoil).count();
-		int common = collection.size() - foil;
+        EmbedBuilder eb = new ColorlessEmbedBuilder();
+        int foil = (int) collection.stream().filter(KawaiponCard::isFoil).count();
+        int common = collection.size() - foil;
 
-		eb.setTitle("\uD83C\uDFB4 | Kawaipons de " + author.getName() + " (" + s + ")");
-		eb.addField(":red_envelope: | Cartas normais:", common + " de " + l + " (" + Helper.prcntToInt(common, l) + "%)", true);
-		eb.addField(":star2: | Cartas cromadas:", foil + " de " + l + " (" + Helper.prcntToInt(foil, l) + "%)", true);
-		eb.setFooter("Total coletado (normais + cromadas): " + Helper.prcntToInt(collection.size(), l * 2) + "%");
-		eb.setImage("https://api." + System.getenv("SERVER_URL") + "/collection?id=" + hash);
-		m.delete().queue();
+        String url = "https://api." + System.getenv("SERVER_URL") + "/collection?id=" + hash;
+        eb.setTitle("\uD83C\uDFB4 | Kawaipons de " + author.getName() + " (" + s + ")")
+                .setDescription("[Clique para abrir a imagem no navegador](" + url + ")")
+                .addField(":red_envelope: | Cartas normais:", common + " de " + l + " (" + Helper.prcntToInt(common, l) + "%)", true)
+                .addField(":star2: | Cartas cromadas:", foil + " de " + l + " (" + Helper.prcntToInt(foil, l) + "%)", true)
+                .setFooter("Total coletado (normais + cromadas): " + Helper.prcntToInt(collection.size(), l * 2) + "%")
+                .setImage(url);
+        m.delete().queue();
 
-		channel.sendMessage(eb.build()).queue();
-	}
+        channel.sendMessage(eb.build()).queue();
+    }
 
-	private void send(User author, MessageChannel channel, Message m, BufferedImage cards, String s, Category c) throws IOException {
-		String hash = Helper.hash((author.getId() + System.currentTimeMillis()).getBytes(StandardCharsets.UTF_8), "SHA-1");
-		File f = new File(Main.getInfo().getCollectionsFolder(), hash + ".jpg");
-		byte[] bytes = Helper.getBytes(Helper.removeAlpha(cards), "jpg", 0.5f);
-		//byte[] bytes = Helper.getBytes(Helper.removeAlpha(cards), "jpg");
-		try (FileOutputStream fos = new FileOutputStream(f)) {
-			fos.write(bytes);
-		}
+    private void send(User author, MessageChannel channel, Message m, BufferedImage cards, String s, Category c) throws IOException {
+        String hash = Helper.hash((author.getId() + System.currentTimeMillis()).getBytes(StandardCharsets.UTF_8), "SHA-1");
+        File f = new File(Main.getInfo().getCollectionsFolder(), hash + ".jpg");
+        byte[] bytes = Helper.getBytes(Helper.removeAlpha(cards), "jpg", 0.5f);
+        //byte[] bytes = Helper.getBytes(Helper.removeAlpha(cards), "jpg");
+        try (FileOutputStream fos = new FileOutputStream(f)) {
+            fos.write(bytes);
+        }
 
-		Helper.keepMaximumNFiles(Main.getInfo().getCollectionsFolder(), 20);
+        Helper.keepMaximumNFiles(Main.getInfo().getCollectionsFolder(), 20);
 
-		EmbedBuilder eb = new ColorlessEmbedBuilder();
+        EmbedBuilder eb = new ColorlessEmbedBuilder();
 
-		eb.setTitle("\uD83C\uDFB4 | Cartas Senshi (" + s + ")")
-				.setDescription(c == null ? null : c.getDescription())
-				.setImage("https://api." + System.getenv("SERVER_URL") + "/collection?id=" + hash);
-		m.delete().queue();
+        String url = "https://api." + System.getenv("SERVER_URL") + "/collection?id=" + hash;
+        eb.setTitle("\uD83C\uDFB4 | Cartas Senshi (" + s + ")")
+                .setDescription((c == null ? "" : c.getDescription() + "\n\n") + "[Clique para abrir a imagem no navegador](" + url + ")")
+                .setImage(url);
+        m.delete().queue();
 
-		channel.sendMessage(eb.build()).queue();
-	}
+        channel.sendMessage(eb.build()).queue();
+    }
 }
