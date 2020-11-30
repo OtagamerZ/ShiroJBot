936d4c7b01aa55db428ca8a3605d6ca9d856e789
Ygimenez <yagogimenez1@hotmail.com>
2020-11-29 23:11:39 -0300

    Correção no cancelamento de certos comandos

--- src/main/java/com/kuuhaku/command/commands/discord/dev/SweepCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/dev/SweepCommand.java
@@ -37,113 +37,115 @@ import java.util.stream.Collectors;
 
 public class SweepCommand extends Command {
 
-	public SweepCommand(String name, String description, Category category, boolean requiresMM) {
-		super(name, description, category, requiresMM);
-	}
+    public SweepCommand(String name, String description, Category category, boolean requiresMM) {
+        super(name, description, category, requiresMM);
+    }
 
-	public SweepCommand(@NonNls String name, @NonNls String[] aliases, String description, Category category, boolean requiresMM) {
-		super(name, aliases, description, category, requiresMM);
-	}
+    public SweepCommand(@NonNls String name, @NonNls String[] aliases, String description, Category category, boolean requiresMM) {
+        super(name, aliases, description, category, requiresMM);
+    }
 
-	public SweepCommand(String name, String usage, String description, Category category, boolean requiresMM) {
-		super(name, usage, description, category, requiresMM);
-	}
+    public SweepCommand(String name, String usage, String description, Category category, boolean requiresMM) {
+        super(name, usage, description, category, requiresMM);
+    }
 
-	public SweepCommand(String name, String[] aliases, String usage, String description, Category category, boolean requiresMM) {
-		super(name, aliases, usage, description, category, requiresMM);
-	}
+    public SweepCommand(String name, String[] aliases, String usage, String description, Category category, boolean requiresMM) {
+        super(name, aliases, usage, description, category, requiresMM);
+    }
 
-	@Override
-	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
-		if (Main.getInfo().getConfirmationPending().getIfPresent(author.getId()) != null) {
-			channel.sendMessage("❌ | Você possui um comando com confirmação pendente, por favor resolva-o antes de usar este comando novamente.").queue();
-			return;
-		}
+    @Override
+    public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
+        if (Main.getInfo().getConfirmationPending().getIfPresent(author.getId()) != null) {
+            channel.sendMessage("❌ | Você possui um comando com confirmação pendente, por favor resolva-o antes de usar este comando novamente.").queue();
+            return;
+        }
 
-		channel.sendMessage("<a:loading:697879726630502401> | Comparando índices...").queue(s -> {
-			Set<GuildConfig> gds = new HashSet<>(GuildDAO.getAllGuilds());
-			Set<com.kuuhaku.model.persistent.Member> mbs = new HashSet<>(MemberDAO.getAllMembers());
+        channel.sendMessage("<a:loading:697879726630502401> | Comparando índices...").queue(s -> {
+            Set<GuildConfig> gds = new HashSet<>(GuildDAO.getAllGuilds());
+            Set<com.kuuhaku.model.persistent.Member> mbs = new HashSet<>(MemberDAO.getAllMembers());
 
-			Set<String> guildTrashBin = new HashSet<>();
-			Set<String> memberTrashBin = new HashSet<>();
+            Set<String> guildTrashBin = new HashSet<>();
+            Set<String> memberTrashBin = new HashSet<>();
 
-			s.editMessage("<a:loading:697879726630502401> | Comparando índices... (" + gds.size() + " guilds)").queue();
+            s.editMessage("<a:loading:697879726630502401> | Comparando índices... (" + gds.size() + " guilds)").queue();
 
-			gds.forEach(gd -> {
-				if (Main.getInfo().getGuildByID(gd.getGuildID()) == null) {
-					guildTrashBin.add(gd.getGuildID());
-					Helper.logger(this.getClass()).debug(gd.getName() + " is null, added to trash bin");
-				}
-			});
+            gds.forEach(gd -> {
+                if (Main.getInfo().getGuildByID(gd.getGuildID()) == null) {
+                    guildTrashBin.add(gd.getGuildID());
+                    Helper.logger(this.getClass()).debug(gd.getName() + " is null, added to trash bin");
+                }
+            });
 
-			s.editMessage("<a:loading:697879726630502401> | Comparando índices... (" + mbs.size() + " membros)").queue();
+            s.editMessage("<a:loading:697879726630502401> | Comparando índices... (" + mbs.size() + " membros)").queue();
 
-			Map<String, List<String>> members = new HashMap<>();
-			mbs.forEach(mb -> {
-				members.putIfAbsent(mb.getSid(), new ArrayList<>());
-				members.get(mb.getSid()).add(mb.getMid());
-			});
+            Map<String, List<String>> members = new HashMap<>();
+            mbs.forEach(mb -> {
+                members.putIfAbsent(mb.getSid(), new ArrayList<>());
+                members.get(mb.getSid()).add(mb.getMid());
+            });
 
-			Set<String> foundIds = new HashSet<>();
-			Map<String, Set<String>> missingIds = new HashMap<>();
-			for (Map.Entry<String, List<String>> e : members.entrySet()) {
-				if (guildTrashBin.contains(e.getKey())) {
-					missingIds.putIfAbsent(e.getKey(), new HashSet<>());
-					missingIds.get(e.getKey()).addAll(e.getValue());
-					continue;
-				}
+            Set<String> foundIds = new HashSet<>();
+            Map<String, Set<String>> missingIds = new HashMap<>();
+            for (Map.Entry<String, List<String>> e : members.entrySet()) {
+                if (guildTrashBin.contains(e.getKey())) {
+                    missingIds.putIfAbsent(e.getKey(), new HashSet<>());
+                    missingIds.get(e.getKey()).addAll(e.getValue());
+                    continue;
+                }
 
-				Guild g = Main.getInfo().getGuildByID(e.getKey());
-				if (g == null) {
-					guildTrashBin.add(e.getKey());
-					missingIds.putIfAbsent(e.getKey(), new HashSet<>());
-					missingIds.get(e.getKey()).addAll(e.getValue().stream().map(id -> id + e.getKey()).collect(Collectors.toList()));
-					Helper.logger(this.getClass()).debug("GID " + e.getKey() + " is null, added to trash bin");
-				} else {
-					List<Member> membrs = g.loadMembers().get();
-					foundIds.addAll(
-							membrs.stream()
-									.map(m -> m.getId() + e.getKey())
-									.collect(Collectors.toList())
-					);
-					Helper.logger(this.getClass()).debug(g.getName() + ": Loaded " + membrs.size() + " members");
-				}
-			}
+                Guild g = Main.getInfo().getGuildByID(e.getKey());
+                if (g == null) {
+                    guildTrashBin.add(e.getKey());
+                    missingIds.putIfAbsent(e.getKey(), new HashSet<>());
+                    missingIds.get(e.getKey()).addAll(e.getValue().stream().map(id -> id + e.getKey()).collect(Collectors.toList()));
+                    Helper.logger(this.getClass()).debug("GID " + e.getKey() + " is null, added to trash bin");
+                } else {
+                    List<Member> membrs = g.loadMembers().get();
+                    foundIds.addAll(
+                            membrs.stream()
+                                    .map(m -> m.getId() + e.getKey())
+                                    .collect(Collectors.toList())
+                    );
+                    Helper.logger(this.getClass()).debug(g.getName() + ": Loaded " + membrs.size() + " members");
+                }
+            }
 
-			mbs.stream()
-					.filter(mb -> !foundIds.contains(mb.getId()))
-					.forEach(mb -> {
-						missingIds.putIfAbsent(mb.getSid(), new HashSet<>());
-						missingIds.get(mb.getSid()).add(mb.getMid());
-					});
+            mbs.stream()
+                    .filter(mb -> !foundIds.contains(mb.getId()))
+                    .forEach(mb -> {
+                        missingIds.putIfAbsent(mb.getSid(), new HashSet<>());
+                        missingIds.get(mb.getSid()).add(mb.getMid());
+                    });
 
-			missingIds.forEach((k, v) -> {
-				for (String id : v) {
-					memberTrashBin.add(id + k);
-				}
-			});
+            missingIds.forEach((k, v) -> {
+                for (String id : v) {
+                    memberTrashBin.add(id + k);
+                }
+            });
 
-			if (guildTrashBin.size() + memberTrashBin.size() > 0) {
-				String hash = Helper.generateHash(guild, author);
-				ShiroInfo.getHashes().add(hash);
-				Main.getInfo().getConfirmationPending().put(author.getId(), true);
-				String gText = guildTrashBin.size() > 0 ? guildTrashBin.size() == 1 ? guildTrashBin.size() + " índice de servidor" : guildTrashBin.size() + " índices de servidores" : "";
-				String mText = memberTrashBin.size() > 0 ? memberTrashBin.size() == 1 ? memberTrashBin.size() + " membro" : memberTrashBin.size() + " membros" : "";
+            if (guildTrashBin.size() + memberTrashBin.size() > 0) {
+                String hash = Helper.generateHash(guild, author);
+                ShiroInfo.getHashes().add(hash);
+                Main.getInfo().getConfirmationPending().put(author.getId(), true);
+                String gText = guildTrashBin.size() > 0 ? guildTrashBin.size() == 1 ? guildTrashBin.size() + " índice de servidor" : guildTrashBin.size() + " índices de servidores" : "";
+                String mText = memberTrashBin.size() > 0 ? memberTrashBin.size() == 1 ? memberTrashBin.size() + " membro" : memberTrashBin.size() + " membros" : "";
 
-				s.editMessage(":warning: | " + (guildTrashBin.size() + memberTrashBin.size() > 1 ? "Foram encontrados " : "Foi encontrado ") + gText + (!gText.isBlank() && !mText.isBlank() ? " e " : "") + mText + (guildTrashBin.size() + memberTrashBin.size() > 1 ? " inexistentes" : " inexistente") + ", deseja executar a limpeza?").queue(m ->
-						Pages.buttonize(m, Map.of(Helper.ACCEPT, (mb, ms) -> {
-							if (!ShiroInfo.getHashes().remove(hash)) return;
-							Main.getInfo().getConfirmationPending().invalidate(author.getId());
-							Sweeper.sweep(guildTrashBin, memberTrashBin);
+                s.editMessage(":warning: | " + (guildTrashBin.size() + memberTrashBin.size() > 1 ? "Foram encontrados " : "Foi encontrado ") + gText + (!gText.isBlank() && !mText.isBlank() ? " e " : "") + mText + (guildTrashBin.size() + memberTrashBin.size() > 1 ? " inexistentes" : " inexistente") + ", deseja executar a limpeza?")
+                        .queue(m -> Pages.buttonize(m, Map.of(Helper.ACCEPT, (mb, ms) -> {
+                                    if (!ShiroInfo.getHashes().remove(hash)) return;
+                                    Main.getInfo().getConfirmationPending().invalidate(author.getId());
+                                    Sweeper.sweep(guildTrashBin, memberTrashBin);
 
-							m.delete().queue(null, Helper::doNothing);
-							channel.sendMessage(Helper.ACCEPT + " | Entradas limpas com sucesso!").queue();
-						}), true, 1, TimeUnit.MINUTES, (u) -> u.getId().equals(author.getId()), ms -> {
-							ShiroInfo.getHashes().remove(hash);
-							Main.getInfo().getConfirmationPending().invalidate(author.getId());
-						})
-				);
-			} else s.editMessage(Helper.ACCEPT + " | Não há entradas para serem limpas.").queue();
-		});
-	}
+                                    m.delete().queue(null, Helper::doNothing);
+                                    channel.sendMessage(Helper.ACCEPT + " | Entradas limpas com sucesso!").queue();
+                                }), true, 1, TimeUnit.MINUTES,
+                                u -> u.getId().equals(author.getId()),
+                                ms -> {
+                                    ShiroInfo.getHashes().remove(hash);
+                                    Main.getInfo().getConfirmationPending().invalidate(author.getId());
+                                })
+                        );
+            } else s.editMessage(Helper.ACCEPT + " | Não há entradas para serem limpas.").queue();
+        });
+    }
 }
