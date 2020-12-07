ae7d3db41bb788ed00ff5533ee0919fbc2332f78
ygimenez <yagogimenez1@hotmail.com>
2020-12-07 16:50:47 -0300

    Melhoria no logger do comando de busca por anime

--- src/main/java/com/kuuhaku/command/commands/discord/misc/AnimeCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/misc/AnimeCommand.java
@@ -64,70 +64,74 @@ public class AnimeCommand extends Command {
 		channel.sendMessage("<a:loading:697879726630502401> Buscando anime...").queue(m -> {
 			try {
 				String query = IOUtils.toString(Objects.requireNonNull(this.getClass().getClassLoader().getResourceAsStream("anilist.graphql")), StandardCharsets.UTF_8);
-
 				JSONObject data = AnimeRequest.getData(String.join(" ", args), query);
-				Anime anime = new Anime(data);
+				try {
+					Anime anime = new Anime(data);
 
-				EmbedBuilder eb = new EmbedBuilder();
-				boolean hentai = anime.getGenres().toLowerCase().contains("hentai");
-				if (hentai && !message.getTextChannel().isNSFW()) {
-					m.editMessage("Humm safadinho, eu não posso postar sobre Hentais neste canal!").queue();
-					return;
-				}
+					EmbedBuilder eb = new EmbedBuilder();
+					boolean hentai = anime.getGenres().toLowerCase().contains("hentai");
+					if (hentai && !message.getTextChannel().isNSFW()) {
+						m.editMessage("Humm safadinho, eu não posso postar sobre Hentais neste canal!").queue();
+						return;
+					}
 
-				JSONObject jo = hentai ? AnimeRequest.getMHData(anime.gettRomaji()) : AnimeRequest.getNAData(anime.gettRomaji());
+					JSONObject jo = hentai ? AnimeRequest.getMHData(anime.gettRomaji()) : AnimeRequest.getNAData(anime.gettRomaji());
 
-				String link;
-				if (jo.has("desc")) {
-					if (hentai) {
-						link = "[Mega Hentais](https://www.megahentais.com/?page_id=%s&ref=%s)".formatted(
-								jo.getInt("id"),
-								Helper.hash(System.getenv("MEGAHENTAIS_TOKEN").getBytes(StandardCharsets.UTF_8), "SHA-1")
-						);
+					String link;
+					if (jo.has("desc")) {
+						if (hentai) {
+							link = "[Mega Hentais](https://www.megahentais.com/?page_id=%s&ref=%s)".formatted(
+									jo.getInt("id"),
+									Helper.hash(System.getenv("MEGAHENTAIS_TOKEN").getBytes(StandardCharsets.UTF_8), "SHA-1")
+							);
+						} else {
+							link = "[Now Animes](https://www.nowanimes.com/?page_id=%s&ref=%s)".formatted(
+									jo.getInt("id"),
+									Helper.hash(System.getenv("NOWANIMES_TOKEN").getBytes(StandardCharsets.UTF_8), "SHA-1")
+							);
+						}
 					} else {
-						link = "[Now Animes](https://www.nowanimes.com/?page_id=%s&ref=%s)".formatted(
-								jo.getInt("id"),
-								Helper.hash(System.getenv("NOWANIMES_TOKEN").getBytes(StandardCharsets.UTF_8), "SHA-1")
+						link = "Link indisponível";
+					}
+
+					eb.setColor(anime.getcColor());
+					if (hentai) eb.setAuthor("Bem, aqui está um novo hentai para você assistir! ( ͡° ͜ʖ ͡°)\n");
+					else eb.setAuthor("Bem, aqui está um novo anime para você assistir!\n");
+					eb.setTitle(anime.gettRomaji() + (!anime.gettRomaji().equals(anime.gettEnglish()) ? " (" + anime.gettEnglish() + ")" : ""));
+
+					eb.setImage(anime.getcImage());
+					eb.addField("Estúdio:", anime.getStudio(), true);
+					eb.addField("Criado por:", anime.getCreator(), true);
+					eb.addField("Ano:", anime.getsDate(), true);
+					eb.addField("Estado:", anime.getStatus(), true);
+					eb.addField("Episódios:", anime.getDuration(), true);
+					if (anime.getNaeAiringAt() != null)
+						eb.addField("Próximo episódio:", anime.getNaeAiringAt(), true);
+					eb.addField("Nota:", anime.getScore() == -1 ? "Nenhuma" : Float.toString(anime.getScore() / 10), true);
+					eb.addField("Popularidade:", Integer.toString(anime.getPopularity()), true);
+					eb.addField("Assista em:", link, true);
+
+					if (!link.equalsIgnoreCase("Link indisponível")) {
+						eb.setDescription(jo.getString("desc"));
+					} else {
+						eb.setDescription(anime.getDescription()
+								.replace("<br>", "\n")
+								.replaceAll("(<i>|</i>)", "_")
+								.replaceAll("(<b>|</b>)", "**")
+								.replaceAll("(<u>|</u>)", "__")
+								.replaceAll("(<p>|</p>)", "")
 						);
 					}
-				} else {
-					link = "Link indisponível";
+					eb.addField("Gêneros:", anime.getGenres(), false);
+
+					m.delete().queue();
+					channel.sendMessage(eb.build()).queue();
+				} catch (JSONException e) {
+					m.editMessage(ShiroInfo.getLocale(I18n.PT).getString("err_anime-not-found")).queue();
+					Helper.logger(this.getClass()).warn(e + " | " + e.getStackTrace()[0]);
+					Helper.logger(this.getClass()).warn(data.toString());
 				}
-
-				eb.setColor(anime.getcColor());
-				if (hentai) eb.setAuthor("Bem, aqui está um novo hentai para você assistir! ( ͡° ͜ʖ ͡°)\n");
-				else eb.setAuthor("Bem, aqui está um novo anime para você assistir!\n");
-				eb.setTitle(anime.gettRomaji() + (!anime.gettRomaji().equals(anime.gettEnglish()) ? " (" + anime.gettEnglish() + ")" : ""));
-
-				eb.setImage(anime.getcImage());
-				eb.addField("Estúdio:", anime.getStudio(), true);
-				eb.addField("Criado por:", anime.getCreator(), true);
-				eb.addField("Ano:", anime.getsDate(), true);
-				eb.addField("Estado:", anime.getStatus(), true);
-				eb.addField("Episódios:", anime.getDuration(), true);
-				if (anime.getNaeAiringAt() != null)
-					eb.addField("Próximo episódio:", anime.getNaeAiringAt(), true);
-				eb.addField("Nota:", anime.getScore() == -1 ? "Nenhuma" : Float.toString(anime.getScore() / 10), true);
-				eb.addField("Popularidade:", Integer.toString(anime.getPopularity()), true);
-				eb.addField("Assista em:", link, true);
-
-				if (!link.equalsIgnoreCase("Link indisponível")) {
-					eb.setDescription(jo.getString("desc"));
-				} else {
-					eb.setDescription(anime.getDescription()
-							.replace("<br>", "\n")
-							.replaceAll("(<i>|</i>)", "_")
-							.replaceAll("(<b>|</b>)", "**")
-							.replaceAll("(<u>|</u>)", "__")
-							.replaceAll("(<p>|</p>)", "")
-					);
-				}
-				eb.addField("Gêneros:", anime.getGenres(), false);
-
-				m.delete().queue();
-				channel.sendMessage(eb.build()).queue();
-			} catch (IOException | JSONException e) {
-				m.editMessage(ShiroInfo.getLocale(I18n.PT).getString("err_anime-not-found")).queue();
+			} catch (IOException e) {
 				Helper.logger(this.getClass()).warn(e + " | " + e.getStackTrace()[0]);
 			}
 		});
