6068d4d59201d48354f91af959fa71ff8a8ed880
ygimenez <yagogimenez1@hotmail.com>
2020-12-09 09:35:53 -0300

    Adição do modo treino no Shoukan

--- src/main/java/com/kuuhaku/command/commands/discord/fun/ShoukanCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/fun/ShoukanCommand.java
@@ -61,7 +61,28 @@ public class ShoukanCommand extends Command {
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
 		boolean practice = (args.length > 0 && Helper.equalsAny(args[0], "practice", "treino"));
 
-		if (!practice) {
+		if (practice) {
+			JSONObject custom = Helper.findJson(rawCmd);
+			boolean daily = Helper.equalsAny(args[1], "daily", "diario");
+
+			if (!daily) {
+				Kawaipon kp = KawaiponDAO.getKawaipon(author.getId());
+				if (kp.getChampions().size() < 30) {
+					channel.sendMessage("❌ | É necessário ter ao menos 30 cartas no deck para poder jogar Shoukan.").queue();
+					return;
+				}
+			}
+
+			String id = author.getId() + "." + message.getMentionedUsers().get(0).getId() + "." + guild.getId();
+
+			if (Main.getInfo().gameInProgress(author.getId())) {
+				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_you-are-in-game")).queue();
+				return;
+			}
+
+			Game t = new Shoukan(Main.getInfo().getAPI(), (TextChannel) channel, 0, custom, daily, author, author);
+			t.start();
+		} else {
 			if (message.getMentionedUsers().size() == 0) {
 				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_no-user")).queue();
 				return;
@@ -72,64 +93,56 @@ public class ShoukanCommand extends Command {
 				channel.sendMessage("❌ | Este usuário possui um comando com confirmação pendente, por favor espere ele resolve-lo antes de usar este comando novamente.").queue();
 				return;
 			}
-		}
 
-		Account uacc = AccountDAO.getAccount(author.getId());
-		Account tacc = AccountDAO.getAccount(message.getMentionedUsers().get(0).getId());
-		JSONObject custom;
-		if (practice) custom = Helper.getOr(Helper.findJson(rawCmd), new JSONObject());
-		else custom = Helper.findJson(rawCmd);
+			Account uacc = AccountDAO.getAccount(author.getId());
+			Account tacc = AccountDAO.getAccount(message.getMentionedUsers().get(0).getId());
+			JSONObject custom = Helper.findJson(rawCmd);
 
-		int bet = 0;
-		if (args.length > 1 && StringUtils.isNumeric(args[1]) && custom == null && !practice) {
-			bet = Integer.parseInt(args[1]);
-			if (bet < 0) {
-				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_invalid-credit-amount")).queue();
+			int bet = 0;
+			if (args.length > 1 && StringUtils.isNumeric(args[1]) && custom == null) {
+				bet = Integer.parseInt(args[1]);
+				if (bet < 0) {
+					channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_invalid-credit-amount")).queue();
+					return;
+				} else if (uacc.getBalance() < bet) {
+					channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_insufficient-credits-user")).queue();
+					return;
+				} else if (tacc.getBalance() < bet) {
+					channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_insufficient-credits-target")).queue();
+					return;
+				}
+			}
+
+			boolean daily = (args.length > 1 && Helper.equalsAny(args[1], "daily", "diario")) || (args.length > 2 && Helper.equalsAny(args[2], "daily", "diario"));
+
+			if (!daily) {
+				Kawaipon kp = KawaiponDAO.getKawaipon(author.getId());
+				Kawaipon target = KawaiponDAO.getKawaipon(message.getMentionedUsers().get(0).getId());
+				if (kp.getChampions().size() < 30) {
+					channel.sendMessage("❌ | É necessário ter ao menos 30 cartas no deck para poder jogar Shoukan.").queue();
+					return;
+				} else if (target.getChampions().size() < 30) {
+					channel.sendMessage("❌ | " + message.getMentionedUsers().get(0).getAsMention() + " não possui cartas suficientes, é necessário ter ao menos 30 cartas para poder jogar Shoukan.").queue();
+					return;
+				}
+			}
+
+			String id = author.getId() + "." + message.getMentionedUsers().get(0).getId() + "." + guild.getId();
+
+			if (Main.getInfo().gameInProgress(author.getId())) {
+				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_you-are-in-game")).queue();
 				return;
-			} else if (uacc.getBalance() < bet) {
-				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_insufficient-credits-user")).queue();
+			} else if (Main.getInfo().gameInProgress(message.getMentionedUsers().get(0).getId())) {
+				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_user-in-game")).queue();
 				return;
-			} else if (tacc.getBalance() < bet) {
-				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_insufficient-credits-target")).queue();
+			} else if (message.getMentionedUsers().get(0).getId().equals(author.getId())) {
+				channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_cannot-play-with-yourself")).queue();
 				return;
 			}
-		}
 
-		boolean daily = (args.length > 1 && Helper.equalsAny(args[1], "daily", "diario")) || (args.length > 2 && Helper.equalsAny(args[2], "daily", "diario"));
-
-		if (!daily) {
-			Kawaipon kp = KawaiponDAO.getKawaipon(author.getId());
-			Kawaipon target = KawaiponDAO.getKawaipon(message.getMentionedUsers().get(0).getId());
-			if (kp.getChampions().size() < 30) {
-				channel.sendMessage("❌ | É necessário ter ao menos 30 cartas no deck para poder jogar Shoukan.").queue();
-				return;
-			} else if (target.getChampions().size() < 30) {
-				channel.sendMessage("❌ | " + message.getMentionedUsers().get(0).getAsMention() + " não possui cartas suficientes, é necessário ter ao menos 30 cartas para poder jogar Shoukan.").queue();
-				return;
-			}
-		}
-
-		String id = author.getId() + "." + message.getMentionedUsers().get(0).getId() + "." + guild.getId();
-
-		if (Main.getInfo().gameInProgress(author.getId())) {
-			channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_you-are-in-game")).queue();
-			return;
-		} else if (Main.getInfo().gameInProgress(message.getMentionedUsers().get(0).getId())) {
-			channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_user-in-game")).queue();
-			return;
-		} else if (message.getMentionedUsers().get(0).getId().equals(author.getId())) {
-			channel.sendMessage(ShiroInfo.getLocale(I18n.PT).getString("err_cannot-play-with-yourself")).queue();
-			return;
-		}
-
-		if (practice) {
-			Game t = new Shoukan(Main.getInfo().getAPI(), (TextChannel) channel, bet, custom, daily, author, author);
-			t.start();
-		} else {
 			String hash = Helper.generateHash(guild, author);
 			ShiroInfo.getHashes().add(hash);
 			Main.getInfo().getConfirmationPending().put(author.getId(), true);
-
 			Game t = new Shoukan(Main.getInfo().getAPI(), (TextChannel) channel, bet, custom, daily, author, message.getMentionedUsers().get(0));
 			channel.sendMessage(message.getMentionedUsers().get(0).getAsMention() + " você foi desafiado a uma partida de Shoukan, deseja aceitar?" + (daily ? " (desafio diário)" : "") + (custom != null ? " (contém regras personalizadas)" : bet != 0 ? " (aposta: " + bet + " créditos)" : ""))
 					.queue(s -> Pages.buttonize(s, Map.of(Helper.ACCEPT, (mb, ms) -> {
