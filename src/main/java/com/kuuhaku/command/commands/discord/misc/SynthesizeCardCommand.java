82cce21cdf1747de9b50a0d2ffac9e119b976c5f
ygimenez <yagogimenez1@hotmail.com>
2020-12-11 00:00:11 -0300

    Correção na síntese de equipamentos

--- src/main/java/com/kuuhaku/command/commands/discord/misc/SynthesizeCardCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/misc/SynthesizeCardCommand.java
@@ -198,7 +198,7 @@ public class SynthesizeCardCommand extends Command {
 										channel.sendMessage("❌ | Você já possui 18 equipamentos, as cartas usadas cartas foram convertidas em " + change + " créditos.").queue();
 									else if (kp.getEquipments().stream().filter(e::equals).count() == 3)
 										channel.sendMessage("❌ | Você já possui 3 cópias de **" + e.getCard().getName() + "**! (" + tier + "), as cartas usadas foram convertidas em " + change + " créditos.").queue();
-									else
+									else if (kp.getEquipments().stream().filter(eq -> eq.getTier() == 4).count() == 1 && e.getTier() == 4)
 										channel.sendMessage("❌ | Você já possui 1 equipamento tier 4, **" + e.getCard().getName() + "**! (" + tier + "), as cartas usadas foram convertidas em " + change + " créditos.").queue();
 
 									if (dp.getValue().isBlank())
