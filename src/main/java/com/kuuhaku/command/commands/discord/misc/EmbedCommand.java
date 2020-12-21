6d489541e1c4a7dbdcd7cb3a2dead4b2c5a6cd80
ygimenez <yagogimenez1@hotmail.com>
2020-12-21 19:49:13 -0300

    Adição de configuração de embeds de servidor

--- src/main/java/com/kuuhaku/command/commands/discord/misc/EmbedCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/misc/EmbedCommand.java
@@ -18,9 +18,14 @@
 
 package com.kuuhaku.command.commands.discord.misc;
 
+import com.github.ygimenez.method.Pages;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Command;
+import com.kuuhaku.controller.sqlite.GuildDAO;
 import com.kuuhaku.model.common.ColorlessEmbedBuilder;
+import com.kuuhaku.model.enums.PrivilegeLevel;
+import com.kuuhaku.model.persistent.GuildConfig;
+import com.kuuhaku.utils.Helper;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.*;
 import org.jetbrains.annotations.NonNls;
@@ -28,6 +33,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.awt.*;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class EmbedCommand extends Command {
 
@@ -50,6 +57,16 @@ public class EmbedCommand extends Command {
 	@Override
 	public void execute(User author, Member member, String rawCmd, String[] args, Message message, MessageChannel channel, Guild guild, String prefix) {
 		channel.sendMessage("<a:loading:697879726630502401> Construindo embed...").queue(m -> {
+			if (Helper.hasPermission(member, PrivilegeLevel.MOD) && args.length > 0 && Helper.equalsAny(args[0], "reset", "resetar")) {
+				GuildConfig gc = GuildDAO.getGuildById(guild.getId());
+				gc.setEmbedTemplate(null);
+				GuildDAO.updateGuildSettings(gc);
+
+				m.delete().queue(null, Helper::doNothing);
+				channel.sendMessage(":white_check_mark: | Embed de servidor limpo com sucesso!").queue();
+				return;
+			}
+
 			try {
 				JSONObject json = new JSONObject(String.join(" ", args));
 
@@ -60,6 +77,7 @@ public class EmbedCommand extends Command {
 				if (json.has("title")) eb.setTitle(json.getString("title"));
 				if (json.has("color")) eb.setColor(Color.decode(json.getString("color")));
 				if (json.has("thumbnail")) eb.setThumbnail(json.getString("thumbnail"));
+				if (json.has("image")) eb.setThumbnail(json.getString("image"));
 				if (json.has("body")) eb.setDescription(json.getString("body"));
 
 				if (json.has("fields")) json.getJSONArray("fields").forEach(j -> {
@@ -72,8 +90,24 @@ public class EmbedCommand extends Command {
 
 				if (json.has("footer")) eb.setFooter(json.getString("footer"), null);
 
-				m.delete().queue();
-				channel.sendMessage(eb.build()).queue();
+				m.delete().queue(null, Helper::doNothing);
+				if (Helper.hasPermission(member, PrivilegeLevel.MOD))
+					channel.sendMessage("Embed construído com sucesso, deseja configurá-lo para ser o formato das mensagens de boas-vindas/adeus?")
+							.embed(eb.build())
+							.queue(s ->
+									Pages.buttonize(s, Map.of(Helper.ACCEPT, (mb, ms) -> {
+												GuildConfig gc = GuildDAO.getGuildById(guild.getId());
+												gc.setEmbedTemplate(json);
+												GuildDAO.updateGuildSettings(gc);
+
+												s.delete().queue(null, Helper::doNothing);
+												channel.sendMessage(":white_check_mark: | Embed de servidor definido com sucesso!").queue();
+											}), true, 1, TimeUnit.MINUTES
+											, u -> u.getId().equals(author.getId())
+									)
+							);
+				else
+					channel.sendMessage(eb.build()).queue();
 			} catch (JSONException ex) {
 				m.editMessage("❌ | JSON em formato inválido, recomendo utilizar este site para checar se está tudo correto: https://jsonlint.com/.").queue();
 			} catch (Exception e) {
