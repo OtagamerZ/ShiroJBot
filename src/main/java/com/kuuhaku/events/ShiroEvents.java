9ab1cb1202ce551df04788ca1299d9628008c970
Ygimenez <yagogimenez1@hotmail.com>
2020-12-26 15:11:29 -0300

    Update ShiroEvents.java

--- src/main/java/com/kuuhaku/events/ShiroEvents.java
+++ src/main/java/com/kuuhaku/events/ShiroEvents.java
@@ -557,8 +557,7 @@ public class ShiroEvents extends ListenerAdapter {
 				Helper.logToChannel(author, false, null, "Um usuário foi bloqueado de entrar no servidor", guild);
 				guild.kick(member).queue();
 			}
-		} catch (Exception e) {
-			Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+		} catch (Exception ignore) {
 		}
 	}
 
