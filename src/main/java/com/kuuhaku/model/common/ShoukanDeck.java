a77bd32b22619b839ef8a572488a15dc3d5c3258
ygimenez <yagogimenez1@hotmail.com>
2021-01-06 22:26:18 -0300

    Update ShoukanDeck.java

--- src/main/java/com/kuuhaku/model/common/ShoukanDeck.java
+++ src/main/java/com/kuuhaku/model/common/ShoukanDeck.java
@@ -32,6 +32,7 @@ import java.io.IOException;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class ShoukanDeck {
 	private final Account acc;
