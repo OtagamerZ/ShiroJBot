41c5e54f28a6da8461dfce9a659f0fc427c3dbc9
ygimenez <yagogimenez1@hotmail.com>
2021-01-11 13:46:54 -0300

    Melhoria no sistema de avaliação de suportes

--- src/main/java/com/kuuhaku/model/persistent/Slots.java
+++ src/main/java/com/kuuhaku/model/persistent/Slots.java
@@ -42,8 +42,8 @@ public class Slots {
 	static {
 		GamblePool gp = new GamblePool();
 		String[] icon = {LEMON, WATERMELON, CHERRY, HEART, BELL, BAR, HORSESHOE, DIAMOND, JACKPOT};
-		for (String s : icon) {
-			gp.addGamble(new GamblePool.Gamble(s, 1));
+		for (int i = 0; i < icon.length; i++) {
+			gp.addGamble(new GamblePool.Gamble(icon[i], icon.length - i));
 		}
 		slots = gp.getPool();
 	}
