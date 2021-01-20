8f06920c51e4ea5a0264bdc9d6100b49082a0c6c
ygimenez <yagogimenez1@hotmail.com>
2021-01-20 11:51:11 -0300

    Melhorias no sistema de clãs

--- src/main/java/com/kuuhaku/model/enums/RankedTier.java
+++ src/main/java/com/kuuhaku/model/enums/RankedTier.java
@@ -18,6 +18,7 @@
 
 package com.kuuhaku.model.enums;
 
+import com.kuuhaku.utils.Helper;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.imageio.ImageIO;
@@ -28,15 +29,16 @@ import java.util.Objects;
 
 public enum RankedTier {
 	UNRANKED(0, "Sem ranking", 10),
-	APPRENTICE_IV(1, "Aprendiz IV", 3),
-	APPRENTICE_III(1, "Aprendiz III", 3),
-	APPRENTICE_II(1, "Aprendiz II", 3),
-	APPRENTICE_I(1, "Aprendiz I", 5),
 
-	INITIATE_IV(2, "Iniciado IV", 3),
-	INITIATE_III(2, "Iniciado III", 3),
-	INITIATE_II(2, "Iniciado II", 3),
-	INITIATE_I(2, "Iniciado I", 5),
+	INITIATE_IV(1, "Iniciado IV", 3),
+	INITIATE_III(1, "Iniciado III", 3),
+	INITIATE_II(1, "Iniciado II", 3),
+	INITIATE_I(1, "Iniciado I", 5),
+
+	APPRENTICE_IV(2, "Aprendiz IV", 3),
+	APPRENTICE_III(2, "Aprendiz III", 3),
+	APPRENTICE_II(2, "Aprendiz II", 3),
+	APPRENTICE_I(2, "Aprendiz I", 5),
 
 	DISCIPLE_IV(3, "Discípulo IV", 3),
 	DISCIPLE_III(3, "Discípulo III", 3),
@@ -85,52 +87,53 @@ public enum RankedTier {
 	}
 
 	public RankedTier getNext() {
-		return switch (this) {
-			case UNRANKED -> APPRENTICE_IV;
-			case APPRENTICE_IV -> APPRENTICE_III;
-			case APPRENTICE_III -> APPRENTICE_II;
-			case APPRENTICE_II -> APPRENTICE_I;
-			case APPRENTICE_I -> INITIATE_IV;
-			case INITIATE_IV -> INITIATE_III;
-			case INITIATE_III -> INITIATE_II;
-			case INITIATE_II -> INITIATE_I;
-			case INITIATE_I -> DISCIPLE_IV;
-			case DISCIPLE_IV -> DISCIPLE_III;
-			case DISCIPLE_III -> DISCIPLE_II;
-			case DISCIPLE_II -> DISCIPLE_I;
-			case DISCIPLE_I -> ADEPT_IV;
-			case ADEPT_IV -> ADEPT_III;
-			case ADEPT_III -> ADEPT_II;
-			case ADEPT_II -> ADEPT_I;
-			case ADEPT_I -> MASTER;
-			case MASTER -> ORACLE;
-			case ORACLE -> ARCHMAGE;
-			case ARCHMAGE -> null;
-		};
+		return Helper.getNext(this,
+				UNRANKED,
+				INITIATE_IV,
+				INITIATE_III,
+				INITIATE_II,
+				INITIATE_I,
+				APPRENTICE_IV,
+				APPRENTICE_III,
+				APPRENTICE_II,
+				APPRENTICE_I,
+				DISCIPLE_IV,
+				DISCIPLE_III,
+				DISCIPLE_II,
+				DISCIPLE_I,
+				ADEPT_IV,
+				ADEPT_III,
+				ADEPT_II,
+				ADEPT_I,
+				MASTER,
+				ORACLE,
+				ARCHMAGE
+		);
 	}
 
 	public RankedTier getPrevious() {
-		return switch (this) {
-			case UNRANKED, APPRENTICE_IV -> null;
-			case APPRENTICE_III -> APPRENTICE_IV;
-			case APPRENTICE_II -> APPRENTICE_III;
-			case APPRENTICE_I -> APPRENTICE_II;
-			case INITIATE_IV -> APPRENTICE_I;
-			case INITIATE_III -> INITIATE_IV;
-			case INITIATE_II -> INITIATE_III;
-			case INITIATE_I -> INITIATE_II;
-			case DISCIPLE_IV -> INITIATE_I;
-			case DISCIPLE_III -> DISCIPLE_IV;
-			case DISCIPLE_II -> DISCIPLE_III;
-			case DISCIPLE_I -> DISCIPLE_II;
-			case ADEPT_IV -> DISCIPLE_I;
-			case ADEPT_III -> ADEPT_IV;
-			case ADEPT_II -> ADEPT_III;
-			case ADEPT_I -> ADEPT_II;
-			case MASTER -> ADEPT_I;
-			case ORACLE -> MASTER;
-			case ARCHMAGE -> MASTER;
-		};
+		return Helper.getPrevious(this,
+				null,
+				INITIATE_IV,
+				INITIATE_III,
+				INITIATE_II,
+				INITIATE_I,
+				APPRENTICE_IV,
+				APPRENTICE_III,
+				APPRENTICE_II,
+				APPRENTICE_I,
+				DISCIPLE_IV,
+				DISCIPLE_III,
+				DISCIPLE_II,
+				DISCIPLE_I,
+				ADEPT_IV,
+				ADEPT_III,
+				ADEPT_II,
+				ADEPT_I,
+				MASTER,
+				ORACLE,
+				ARCHMAGE
+		);
 	}
 
 	public static String getTierName(int tier, boolean enumName) {
