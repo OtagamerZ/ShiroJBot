b89fc4d1bedd2619b9ea0a2478baa18711ed9a10
ygimenez <yagogimenez1@hotmail.com>
2021-01-20 17:16:16 -0300

    Criação de sistema de bolsa de valores

--- src/main/java/com/kuuhaku/controller/postgresql/EquipmentMarketDAO.java
+++ src/main/java/com/kuuhaku/controller/postgresql/EquipmentMarketDAO.java
@@ -90,6 +90,7 @@ public class EquipmentMarketDAO {
 				AND em.publishDate >= :date
 				AND em.buyer <> ''
 				AND em.buyer <> em.seller
+				AND em.price / AVG(em.price) BETWEEN -0.5 AND 0.5 
 				""");
 		q.setParameter("card", c);
 		q.setParameter("date", cal.getTime());
@@ -117,6 +118,7 @@ public class EquipmentMarketDAO {
 					AND em.publishDate >= :date
 					AND em.buyer <> ''
 					AND em.buyer <> em.seller
+					AND em.price / AVG(em.price) BETWEEN -0.5 AND 0.5 
 					""");
 			q.setParameter("card", c);
 			q.setParameter("date", cal.getTime());
@@ -130,6 +132,7 @@ public class EquipmentMarketDAO {
 					AND em.publishDate < :date
 					AND em.buyer <> ''
 					AND em.buyer <> em.seller
+					AND em.price / AVG(em.price) BETWEEN -0.5 AND 0.5 
 					""");
 			q.setParameter("card", c);
 			q.setParameter("date", cal.getTime());
