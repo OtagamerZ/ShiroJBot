a37d3596d9645ba1babbc3252976c50812579d02
Ygimenez <yagogimenez1@hotmail.com>
2021-01-29 16:51:05 -0300

    Adição de filas ranqueadas (Solo/Duo)

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Shoukan.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Shoukan.java
@@ -144,8 +144,7 @@ public class Shoukan extends GlobalGame {
 	}
 
 	public Shoukan(ShardManager handler, GameChannel channel, int bet, JSONObject custom, boolean daily, boolean ranked, User... players) {
-		super(handler, new Board(BoardSize.S_NONE, bet, Arrays.stream(players).map(User::getId).toArray(String[]::new)), channel, custom, ranked, players.length == 4 ? RankedQueue.DUO : RankedQueue.SOLO);
-		System.out.println(Arrays.toString(players));
+		super(handler, new Board(BoardSize.S_NONE, bet, Arrays.stream(players).map(User::getId).toArray(String[]::new)), channel, ranked, custom, players.length == 4 ? RankedQueue.DUO : RankedQueue.SOLO);
 		this.channel = channel;
 		this.daily = daily;
 		this.team = players.length == 4;
