cf851093796d431fb26c4cc3ba8c4967c1d90be9
ygimenez <yagogimenez1@hotmail.com>
2021-02-01 17:31:11 -0300

    Rework no CommandManager

--- src/main/java/com/kuuhaku/command/commands/discord/misc/LearnToSearchCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/misc/LearnToSearchCommand.java
@@ -20,32 +20,21 @@ package com.kuuhaku.command.commands.discord.misc;
 
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Executable;
+import com.kuuhaku.model.annotations.Command;
 import com.kuuhaku.model.common.ColorlessEmbedBuilder;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.*;
-import org.jetbrains.annotations.NonNls;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 
-
+@Command(
+		name = "netcat",
+		aliases = {"search", "lts", "aap"},
+		usage = "req_http",
+		category = Category.INFO
+)
 public class LearnToSearchCommand implements Executable {
-
-	public LearnToSearchCommand(String name, String description, Category category, boolean requiresMM) {
-		super(name, description, category, requiresMM);
-	}
-
-	public LearnToSearchCommand(String name, String[] aliases, String description, Category category, boolean requiresMM) {
-		super(name, aliases, description, category, requiresMM);
-	}
-
-	public LearnToSearchCommand(@NonNls String name, String usage, String description, Category category, boolean requiresMM) {
-		super(name, usage, description, category, requiresMM);
-	}
-
-	public LearnToSearchCommand(String name, String[] aliases, String usage, String description, Category category, boolean requiresMM) {
-		super(name, aliases, usage, description, category, requiresMM);
-	}
 
 	@Override
 	public void execute(User author, Member member, String command, String argsAsText, String[] args, Message message, TextChannel channel, Guild guild, String prefix) {
