1f1cf941114a5b311348520a2be36cae705a648e
Ygimenez <yagogimenez1@hotmail.com>
2021-02-06 15:32:47 -0300

    Correção no encerramento das partidas

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Shoukan.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Shoukan.java
@@ -1666,10 +1666,10 @@ public class Shoukan extends GlobalGame {
 		for (Map.Entry<Side, Hand> entry : getHands().entrySet()) {
 			Hand h = entry.getValue();
 			if (!finished.get()) {
-				Hand op = getHands().get(next);
+				Hand op = getHands().get(h.getSide() == Side.TOP ? Side.BOTTOM : Side.TOP);
 				if (h.getHp() == 0) {
 					if (getCustom() == null) {
-						getHistory().setWinner(next);
+						getHistory().setWinner(op.getSide());
 						getBoard().awardWinner(this, daily, op.getUser().getId());
 					} else close();
 					finished.set(true);
