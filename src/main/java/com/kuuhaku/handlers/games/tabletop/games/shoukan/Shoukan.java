9ce69be858a8ffda5c467bc4ad0cb7d7954bb9f7
ygimenez <yagogimenez1@hotmail.com>
2021-02-08 17:31:06 -0300

    Correções menores

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Shoukan.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Shoukan.java
@@ -2265,8 +2265,6 @@ public class Shoukan extends GlobalGame {
 					pg.compute(DailyTask.RACE_TASK, (k, v) -> v == null ? summons : v + summons);
 					acc.setDailyProgress(pg);
 					AccountDAO.saveAccount(acc);
-
-					System.out.println(acc.getDailyProgress());
 				}
 			}
 		}
