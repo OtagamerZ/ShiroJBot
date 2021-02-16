1d482c208e9b8bc7d77be9f6274c0e1bb45964be
ygimenez <yagogimenez1@hotmail.com>
2021-02-16 13:02:23 -0300

    Revisão geral no código

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Field.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Field.java
@@ -46,10 +46,10 @@ public class Field implements Drawable, Cloneable {
 	private Card card;
 
 	@Column(columnDefinition = "VARCHAR(191) NOT NULL DEFAULT '{}'")
-	private final String modifiers = "{}";
+	private String modifiers = "{}";
 
 	@Column(columnDefinition = "BOOLEAN NOT NULL DEFAULT FALSE")
-	private final boolean effectOnly = false;
+	private boolean effectOnly = false;
 
 	private transient Account acc = null;
 	private transient Clan clan = null;
@@ -161,6 +161,18 @@ public class Field implements Drawable, Cloneable {
 
 	public JSONObject getModifiers() {
 		return new JSONObject(modifiers);
+	}
+
+	public void setModifiers(String modifiers) {
+		this.modifiers = modifiers;
+	}
+
+	public boolean isEffectOnly() {
+		return effectOnly;
+	}
+
+	public void setEffectOnly(boolean effectOnly) {
+		this.effectOnly = effectOnly;
 	}
 
 	@Override
