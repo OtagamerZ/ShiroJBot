65d50816a0cf97d5b34d34de3d10319a5ff46910
ygimenez <yagogimenez1@hotmail.com>
2021-02-23 13:44:28 -0300

    Adição de tokens de suporte; Remoção do salário mensal e contagem de tickets;

--- src/main/java/com/kuuhaku/command/commands/discord/misc/ThanksCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/misc/ThanksCommand.java
@@ -40,8 +40,10 @@ public class ThanksCommand implements Executable {
 
 	@Override
 	public void execute(User author, Member member, String command, String argsAsText, String[] args, Message message, TextChannel channel, Guild guild, String prefix) {
+		Account acc = AccountDAO.getAccount(author.getId());
+
 		if (message.getMentionedUsers().size() < 1) {
-			channel.sendMessage("❌ | Você precisa mencionar um suporte para agradecer.").queue();
+			channel.sendMessage("Você ainda possui " + acc.getThanksTokens() + " de 4 tokens de suporte.").queue();
 			return;
 		} else if (ShiroInfo.getStaff().contains(author.getId())) {
 			channel.sendMessage("❌ | Membros da equipe da Shiro não podem dar tokens de suporte.").queue();
@@ -52,7 +54,6 @@ public class ThanksCommand implements Executable {
 		}
 
 		LocalDateTime time = LocalDateTime.now();
-		Account acc = AccountDAO.getAccount(author.getId());
 		if (acc.getThanksTokens() <= 0) {
 			int rem = time.getHour() % 6;
 			channel.sendMessage("❌ | Você não possui tokens de suporte restantes, você irá receber 1 em " + (rem == 1 ? rem + " hora" : rem + "horas") + ".").queue();
