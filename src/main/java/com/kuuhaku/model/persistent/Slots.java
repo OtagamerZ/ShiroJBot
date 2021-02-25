469502c6e0b4d7f696a8765e219ab7c2a9054ecc
ygimenez <yagogimenez1@hotmail.com>
2021-02-25 10:38:26 -0300

    Adição de opção de usar cartas cromadas no Shoukan

--- src/main/java/com/kuuhaku/model/persistent/Slots.java
+++ src/main/java/com/kuuhaku/model/persistent/Slots.java
@@ -42,8 +42,8 @@ public class Slots {
 	static {
 		GamblePool gp = new GamblePool();
 		String[] icon = {LEMON, WATERMELON, CHERRY, HEART, BELL, BAR, HORSESHOE, DIAMOND, JACKPOT};
-		for (int i = 0; i < icon.length; i++) {
-			gp.addGamble(new GamblePool.Gamble(icon[i], i == 6 ? 1 : 3));
+		for (String s : icon) {
+			gp.addGamble(new GamblePool.Gamble(s, 3));
 		}
 		slots = gp.getPool();
 	}
