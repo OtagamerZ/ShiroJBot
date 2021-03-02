09a20906b8a6ec9bc9b33c1cea6cb27a9420658b
Ygimenez <yagogimenez1@hotmail.com>
2021-03-02 14:17:18 -0300

    Agora é possível adicionar emotes usando nome + arquivo

--- src/main/java/com/kuuhaku/command/commands/discord/moderation/RemoveEmoteCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/moderation/RemoveEmoteCommand.java
@@ -35,7 +35,7 @@ import java.util.stream.Collectors;
 @Command(
 		name = "removeremote",
 		aliases = {"removeremoji", "remote", "remoji"},
-		usage = "req_emotes",
+		usage = "req_emotes-name-image",
 		category = Category.MODERATION
 )
 @Requires({Permission.MANAGE_EMOTES})
@@ -43,23 +43,36 @@ public class RemoveEmoteCommand implements Executable {
 
 	@Override
 	public void execute(User author, Member member, String command, String argsAsText, String[] args, Message message, TextChannel channel, Guild guild, String prefix) {
-		if (message.getEmotes().isEmpty()) {
+		if (message.getEmotes().isEmpty() && args.length < 1) {
 			channel.sendMessage("❌ | Você precisa informar ao menos 1 emote para remover.").queue();
 			return;
 		}
 
-		List<Emote> filteredList = message.getEmotes().stream().filter(e -> guild.getEmoteById(e.getId()) != null).collect(Collectors.toList());
-		List<AuditableRestAction<Void>> acts = new ArrayList<>();
-		int removed = 0;
-		for (Emote emote : filteredList) {
-			acts.add(emote.delete());
-			removed++;
-		}
+		if (message.getEmotes().isEmpty()) {
+			Emote e = guild.getEmotesByName(args[0], true).stream().findFirst().orElse(null);
 
-		int finalRemoved = removed;
-		RestAction.allOf(acts)
-				.mapToResult()
-				.flatMap(s -> channel.sendMessage("✅ | " + finalRemoved + " emotes removidos com sucesso!"))
-				.queue(null, Helper::doNothing);
+			if (e == null) {
+				channel.sendMessage("❌ | Emote inválido.").queue();
+				return;
+			}
+
+			e.delete()
+					.flatMap(s -> channel.sendMessage("✅ | Emote removido com sucesso!"))
+					.queue(null, Helper::doNothing);
+		} else {
+			List<Emote> filteredList = message.getEmotes().stream().filter(e -> guild.getEmoteById(e.getId()) != null).collect(Collectors.toList());
+			List<AuditableRestAction<Void>> acts = new ArrayList<>();
+			int removed = 0;
+			for (Emote emote : filteredList) {
+				acts.add(emote.delete());
+				removed++;
+			}
+
+			int finalRemoved = removed;
+			RestAction.allOf(acts)
+					.mapToResult()
+					.flatMap(s -> channel.sendMessage("✅ | " + finalRemoved + " emotes removidos com sucesso!"))
+					.queue(null, Helper::doNothing);
+		}
 	}
 }
