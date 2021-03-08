ac0c2423bac967b85235f8c04fd14193ef3a8450
ygimenez <yagogimenez1@hotmail.com>
2021-03-08 15:19:30 -0300

    Melhoria em alguns efeitos de raça

--- src/main/java/com/kuuhaku/controller/postgresql/AccountDAO.java
+++ src/main/java/com/kuuhaku/controller/postgresql/AccountDAO.java
@@ -138,22 +138,4 @@ public class AccountDAO {
 			em.close();
 		}
 	}
-
-	public static void addSupportTokens() {
-		EntityManager em = Manager.getEntityManager();
-
-		Query q = em.createQuery("SELECT a FROM Account a ORDER BY a.balance DESC", Account.class);
-		q.setLockMode(LockModeType.PESSIMISTIC_WRITE);
-
-		em.getTransaction().begin();
-		em.createQuery("""
-				UPDATE Account a 
-				SET a.thanksTokens = CASE 
-					WHEN (a.thanksTokens > 3) THEN 3
-					ELSE a.thanksTokens
-				END + 1
-				""").executeUpdate();
-		em.getTransaction().commit();
-		em.close();
-	}
 }
