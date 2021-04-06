5c58be38e73376cbdb840f04d1d31e4104dbbf2c
ygimenez <yagogimenez1@hotmail.com>
2021-04-06 15:37:23 -0300

    Correção no alinhamento dos icones de combo

--- src/main/java/com/kuuhaku/model/persistent/Slots.java
+++ src/main/java/com/kuuhaku/model/persistent/Slots.java
@@ -37,7 +37,7 @@ public class Slots {
 		GamblePool gp = new GamblePool();
 		Slot[] icon = {LEMON, WATERMELON, CHERRY, HEART, BELL, BAR, HORSESHOE, DIAMOND, JACKPOT};
 		for (Slot s : icon) {
-			gp.addGamble(new GamblePool.Gamble(s, 1));
+			gp.addGamble(new GamblePool.Gamble(s, s.equals(JACKPOT) ? 1 : 3));
 		}
 		slots = gp.getPool();
 	}
