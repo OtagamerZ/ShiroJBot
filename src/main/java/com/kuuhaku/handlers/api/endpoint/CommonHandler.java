cc22d222bdfc90c1168ad371aa4de2a0d5cbf286
ygimenez <yagogimenez1@hotmail.com>
2021-04-13 13:17:19 -0300

    Melhoria na API de cartas

--- src/main/java/com/kuuhaku/handlers/api/endpoint/CommonHandler.java
+++ src/main/java/com/kuuhaku/handlers/api/endpoint/CommonHandler.java
@@ -28,64 +28,120 @@ import org.springframework.web.bind.annotation.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @RestController
 public class CommonHandler {
-    @RequestMapping(value = "/collection", method = RequestMethod.GET)
-    public @ResponseBody
-    HttpEntity<byte[]> serveCollectionImage(@RequestParam(value = "id") String id) throws IOException {
-        File f = new File(Main.getInfo().getCollectionsFolder(), id + ".jpg");
-        if (!f.exists()) throw new FileNotFoundException();
-        byte[] bytes = FileUtils.readFileToByteArray(f);
+	@RequestMapping(value = "/collection", method = RequestMethod.GET)
+	public @ResponseBody
+	HttpEntity<byte[]> serveCollectionImage(@RequestParam(value = "id") String id) throws IOException {
+		File f = new File(Main.getInfo().getCollectionsFolder(), id + ".jpg");
+		if (!f.exists()) throw new FileNotFoundException();
+		byte[] bytes = FileUtils.readFileToByteArray(f);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG);
-        headers.setContentLength(bytes.length);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		headers.setContentLength(bytes.length);
 
-        return new HttpEntity<>(bytes, headers);
-    }
+		return new HttpEntity<>(bytes, headers);
+	}
 
-    @RequestMapping(value = "/card", method = RequestMethod.GET)
-    public @ResponseBody
-    HttpEntity<byte[]> serveCardImage(@RequestParam(value = "name") String name, @RequestParam(value = "anime") String anime) throws IOException {
-        File f = new File(System.getenv("CARDS_PATH") + anime, name + ".png");
-        if (!f.exists()) throw new FileNotFoundException();
-        byte[] bytes = FileUtils.readFileToByteArray(f);
+	@RequestMapping(value = "/card", method = RequestMethod.GET)
+	public @ResponseBody
+	HttpEntity<byte[]> serveCardImage(@RequestParam(value = "name", defaultValue = "") String name, @RequestParam(value = "anime", defaultValue = "") String anime) throws IOException {
+		try {
+			URL pageUrl = this.getClass().getClassLoader().getResource("template.html");
+			if (pageUrl == null) throw new IllegalArgumentException();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_PNG);
-        headers.setContentLength(bytes.length);
+			String page = Files.readString(Path.of(pageUrl.toURI()), StandardCharsets.UTF_8);
+			String item = "<li><a href=\"%s\">%s</a></li>\n";
 
-        return new HttpEntity<>(bytes, headers);
-    }
+			if (anime.isBlank()) {
+				File f = new File(System.getenv("CARDS_PATH"));
+				if (!f.exists()) throw new FileNotFoundException();
 
-    @RequestMapping(value = "/image", method = RequestMethod.GET)
-    public @ResponseBody
-    HttpEntity<byte[]> serveImage(@RequestParam(value = "id") String id) throws IOException {
-        File f = new File(Main.getInfo().getTemporaryFolder(), id + ".jpg");
-        if (!f.exists()) throw new FileNotFoundException();
-        byte[] bytes = FileUtils.readFileToByteArray(f);
+				StringBuilder sb = new StringBuilder();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_JPEG);
-        headers.setContentLength(bytes.length);
+				String[] available = f.list();
+				if (available == null) available = new String[0];
+				for (String s : available) {
+					sb.append(item.formatted("?anime=" + s, s));
+				}
 
-        return new HttpEntity<>(bytes, headers);
-    }
+				byte[] bytes = page.formatted(sb.toString()).getBytes(StandardCharsets.UTF_8);
+				HttpHeaders headers = new HttpHeaders();
+				headers.setContentType(MediaType.TEXT_HTML);
+				headers.setContentLength(bytes.length);
 
-    @RequestMapping(value = "/readme", method = RequestMethod.GET)
-    public @ResponseBody
-    HttpEntity<String> serveReadme() throws IOException {
-        File f = new File("README.md");
-        if (!f.exists()) throw new FileNotFoundException();
+				return new HttpEntity<>(bytes, headers);
+			} else if (name.isBlank()) {
+				File f = new File(System.getenv("CARDS_PATH") + anime);
+				if (!f.exists()) throw new FileNotFoundException();
 
-        String readme = FileUtils.readFileToString(f, StandardCharsets.UTF_8);
+				StringBuilder sb = new StringBuilder();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(new MediaType(MediaType.TEXT_PLAIN, StandardCharsets.UTF_8));
-        headers.setContentLength(readme.length());
+				String[] available = f.list();
+				if (available == null) available = new String[0];
+				for (String s : available) {
+					sb.append(item.formatted("&name=" + s, s));
+				}
 
-        return new HttpEntity<>(readme, headers);
-    }
+				byte[] bytes = page.formatted(sb.toString()).getBytes(StandardCharsets.UTF_8);
+				HttpHeaders headers = new HttpHeaders();
+				headers.setContentType(MediaType.TEXT_HTML);
+				headers.setContentLength(bytes.length);
+
+				return new HttpEntity<>(bytes, headers);
+			} else {
+				File f = new File(System.getenv("CARDS_PATH") + anime, name + ".png");
+				if (!f.exists()) throw new FileNotFoundException();
+				byte[] bytes = FileUtils.readFileToByteArray(f);
+
+				HttpHeaders headers = new HttpHeaders();
+				headers.setContentType(MediaType.IMAGE_PNG);
+				headers.setContentLength(bytes.length);
+
+				return new HttpEntity<>(bytes, headers);
+			}
+		} catch (URISyntaxException e) {
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.IMAGE_PNG);
+			headers.setContentLength(new byte[0].length);
+
+			return new HttpEntity<>(new byte[0], headers);
+		}
+	}
+
+	@RequestMapping(value = "/image", method = RequestMethod.GET)
+	public @ResponseBody
+	HttpEntity<byte[]> serveImage(@RequestParam(value = "id") String id) throws IOException {
+		File f = new File(Main.getInfo().getTemporaryFolder(), id + ".jpg");
+		if (!f.exists()) throw new FileNotFoundException();
+		byte[] bytes = FileUtils.readFileToByteArray(f);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		headers.setContentLength(bytes.length);
+
+		return new HttpEntity<>(bytes, headers);
+	}
+
+	@RequestMapping(value = "/readme", method = RequestMethod.GET)
+	public @ResponseBody
+	HttpEntity<String> serveReadme() throws IOException {
+		File f = new File("README.md");
+		if (!f.exists()) throw new FileNotFoundException();
+
+		String readme = FileUtils.readFileToString(f, StandardCharsets.UTF_8);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(new MediaType(MediaType.TEXT_PLAIN, StandardCharsets.UTF_8));
+		headers.setContentLength(readme.length());
+
+		return new HttpEntity<>(readme, headers);
+	}
 }
