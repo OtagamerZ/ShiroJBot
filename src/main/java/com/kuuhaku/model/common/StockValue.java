e18a452a4ef4672036ad44628d6b7fee625c389c
Ygimenez <yagogimenez1@hotmail.com>
2021-04-21 00:47:52 -0300

    Nova tag: Investidor

--- src/main/java/com/kuuhaku/model/common/StockValue.java
+++ src/main/java/com/kuuhaku/model/common/StockValue.java
@@ -33,7 +33,7 @@ public class StockValue {
 
 		GeometricMean gm = new GeometricMean();
 		this.value = (int) Math.round(gm.evaluate(now));
-		this.growth = Helper.mirroredFloor((1d - Helper.prcnt(gm.evaluate(before), gm.evaluate(now))) * -100000) / 1000d;
+		this.growth = Helper.mirroredFloor(Helper.prcnt(gm.evaluate(now), gm.evaluate(before)) * 100000) / 1000d - 100;
 	}
 
 	public String getId() {
