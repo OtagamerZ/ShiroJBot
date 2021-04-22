a412f07312f48336821ae9ffa433aba2448581f2
ygimenez <yagogimenez1@hotmail.com>
2021-04-22 17:58:56 -0300

    Remoção completa do s!settings

--- src/main/java/com/kuuhaku/command/commands/discord/moderation/RelayChannelCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/moderation/RelayChannelCommand.java
@@ -42,7 +42,7 @@ public class RelayChannelCommand implements Executable {
 			if (chn == null)
 				channel.sendMessage("Ainda não foi definido um canal global.").queue();
 			else
-				channel.sendMessage("O canal global atual do servidor é " + chn + ".").queue();
+				channel.sendMessage("O canal global atual do servidor é " + chn.getAsMention() + ".").queue();
 			return;
 		}
 
