b1a362f94a1ef4b2c07d70e6fd85a53a36ff4467
ygimenez <yagogimenez1@hotmail.com>
2021-04-28 13:43:31 -0300

    Correções menores

--- src/main/java/com/kuuhaku/controller/postgresql/CardMarketDAO.java
+++ src/main/java/com/kuuhaku/controller/postgresql/CardMarketDAO.java
@@ -264,7 +264,7 @@ public class CardMarketDAO {
 		if (!params[0].isBlank()) q.setParameter("name", "%" + name + "%");
 		if (!params[1].isBlank()) q.setParameter("min", min);
 		if (!params[2].isBlank()) q.setParameter("max", max);
-		if (!params[3].isBlank()) q.setParameter("rarity", rarity);
+		if (!params[3].isBlank()) q.setParameter("rarity", KawaiponRarity.getByFragment(rarity));
 		if (!params[4].isBlank()) q.setParameter("anime", "%" + anime + "%");
 		if (!params[5].isBlank()) q.setParameter("foil", foil);
 		if (!params[6].isBlank()) q.setParameter("seller", seller);
