eac0d1ee7185cb1c4643e900b4c21d2e0d754528
ygimenez <yagogimenez1@hotmail.com>
2021-05-05 13:23:40 -0300

    Correções gerais

---
+++ src/main/java/com/kuuhaku/model/persistent/guild/VoiceRole.java
@@ -0,0 +1,74 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.model.persistent.guild;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "voicerole")
+public class VoiceRole {
+	@Id
+	private String id;
+
+	@Column(columnDefinition = "INT NOT NULL")
+	private long time;
+
+	public VoiceRole() {
+	}
+
+	public VoiceRole(String id, int time) {
+		this.id = id;
+		this.time = time;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public long getTime() {
+		return time;
+	}
+
+	public void setTime(long level) {
+		this.time = level;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		else if (o == null || (getClass() != o.getClass() && o.getClass() != String.class)) return false;
+		else if (o instanceof String) return id.equalsIgnoreCase((String) o);
+
+		VoiceRole levelRole = (VoiceRole) o;
+		return time == levelRole.time && Objects.equals(id, levelRole.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, time);
+	}
+}
