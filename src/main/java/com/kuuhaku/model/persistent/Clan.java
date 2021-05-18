87cc6bb1d3ae7bf5be3b2b59f49aa0a8e9a427fd
ygimenez <yagogimenez1@hotmail.com>
2021-05-18 10:49:39 -0300

    Correções menores

--- src/main/java/com/kuuhaku/model/persistent/Clan.java
+++ src/main/java/com/kuuhaku/model/persistent/Clan.java
@@ -26,9 +26,10 @@ import net.dv8tion.jda.api.entities.User;
 import org.apache.commons.lang3.tuple.Pair;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import javax.persistence.*;
-import javax.transaction.Transactional;
 import java.awt.image.BufferedImage;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -36,7 +37,6 @@ import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "clan")
-@Transactional
 public class Clan {
 	@Id
 	@Column(columnDefinition = "CHAR(64) NOT NULL")
@@ -158,48 +158,48 @@ public class Clan {
 		ClanHierarchy ch = members.get(id);
 		ClanHierarchy next = Helper.getNext(ch, ClanHierarchy.MEMBER, ClanHierarchy.CAPTAIN, ClanHierarchy.SUBLEADER);
 		members.put(id, Helper.getOr(next, ch));
-		transactions.add(u.getAsTag() + " promoveu o membro com ID " + id + ".");
+		getTransactions().add(u.getAsTag() + " promoveu o membro com ID " + id + ".");
 	}
 
 	public void promote(User tgt, User u) {
 		ClanHierarchy ch = members.get(tgt.getId());
 		ClanHierarchy next = Helper.getNext(ch, ClanHierarchy.MEMBER, ClanHierarchy.CAPTAIN, ClanHierarchy.SUBLEADER);
 		members.put(tgt.getId(), Helper.getOr(next, ch));
-		transactions.add(u.getAsTag() + " promoveu o membro " + tgt.getAsTag() + ".");
+		getTransactions().add(u.getAsTag() + " promoveu o membro " + tgt.getAsTag() + ".");
 	}
 
 	public void demote(String id, User u) {
 		ClanHierarchy ch = members.get(id);
 		ClanHierarchy previous = Helper.getPrevious(ch, ClanHierarchy.MEMBER, ClanHierarchy.CAPTAIN, ClanHierarchy.SUBLEADER);
 		members.put(id, Helper.getOr(previous, ch));
-		transactions.add(u.getAsTag() + " rebaixou o membro com ID " + id + ".");
+		getTransactions().add(u.getAsTag() + " rebaixou o membro com ID " + id + ".");
 	}
 
 	public void demote(User tgt, User u) {
 		ClanHierarchy ch = members.get(tgt.getId());
 		ClanHierarchy previous = Helper.getPrevious(ch, ClanHierarchy.MEMBER, ClanHierarchy.CAPTAIN, ClanHierarchy.SUBLEADER);
 		members.put(tgt.getId(), Helper.getOr(previous, ch));
-		transactions.add(u.getAsTag() + " rebaixou o membro " + tgt.getAsTag() + ".");
+		getTransactions().add(u.getAsTag() + " rebaixou o membro " + tgt.getAsTag() + ".");
 	}
 
 	public void kick(String id, User u) {
 		members.remove(id);
-		transactions.add(u.getAsTag() + " expulsou o membro com ID " + id + ".");
+		getTransactions().add(u.getAsTag() + " expulsou o membro com ID " + id + ".");
 	}
 
 	public void kick(User tgt, User u) {
 		members.remove(tgt.getId());
-		transactions.add(u.getAsTag() + " expulsou o membro " + tgt.getAsTag() + ".");
+		getTransactions().add(u.getAsTag() + " expulsou o membro " + tgt.getAsTag() + ".");
 	}
 
 	public void invite(String id, User u) {
 		members.put(id, ClanHierarchy.MEMBER);
-		transactions.add(u.getAsTag() + " adicionou o membro com ID " + id + ".");
+		getTransactions().add(u.getAsTag() + " adicionou o membro com ID " + id + ".");
 	}
 
 	public void invite(User tgt, User u) {
 		members.put(tgt.getId(), ClanHierarchy.MEMBER);
-		transactions.add(u.getAsTag() + " adicionou o membro " + tgt.getAsTag() + ".");
+		getTransactions().add(u.getAsTag() + " adicionou o membro " + tgt.getAsTag() + ".");
 	}
 
 	public void leave(String id) {
@@ -230,18 +230,18 @@ public class Clan {
 
 	public void deposit(long amount, User u) {
 		this.vault += amount;
-		transactions.add(u.getAsTag() + " depositou " + Helper.separate(amount) + " créditos.");
+		getTransactions().add(u.getAsTag() + " depositou " + Helper.separate(amount) + " créditos.");
 	}
 
 	public void withdraw(long amount, User u) {
 		this.vault -= amount;
-		transactions.add(u.getAsTag() + " sacou " + Helper.separate(amount) + " créditos.");
+		getTransactions().add(u.getAsTag() + " sacou " + Helper.separate(amount) + " créditos.");
 	}
 
 	public void upgrade(User u) {
 		this.vault -= tier.getCost();
 		this.tier = this.tier.getNext();
-		transactions.add(u.getAsTag() + " evoluiu o tier do clã por " + Helper.separate(tier) + " créditos.");
+		getTransactions().add(u.getAsTag() + " evoluiu o tier do clã por " + Helper.separate(tier) + " créditos.");
 	}
 
 	public Deck getDeck() {
@@ -252,8 +252,13 @@ public class Clan {
 		this.clanDeck = clanDeck;
 	}
 
-	public List<String> getTransactions() {
+	@Query("SELECT t FROM Clan c JOIN FETCH c.transactions t WHERE c.hash = :hash")
+	public List<String> getTransactions(@Param("hash") String hash) {
 		return transactions;
+	}
+
+	public List<String> getTransactions() {
+		return getTransactions(hash);
 	}
 
 	public void setTransactions(List<String> transactions) {
