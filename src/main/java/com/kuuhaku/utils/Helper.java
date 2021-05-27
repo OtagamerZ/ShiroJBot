947e3720a984c9b78a32d785ef186cc877f996c6
Ygimenez <yagogimenez1@hotmail.com>
2021-05-27 16:56:18 -0300

    Remoção da biblioteca Gson; Melhoria no manipulador de GIFs;

--- src/main/java/com/kuuhaku/utils/Helper.java
+++ src/main/java/com/kuuhaku/utils/Helper.java
@@ -2718,7 +2718,11 @@ public class Helper {
 							gim.getHeight(),
 							meta.getLeftPosition(),
 							meta.getTopPosition(),
-							meta.getDelay()
+							meta.getDelay() < 50 ?
+									i > 0 ?
+											Math.max(50, out.get(i - 1).getDelay())
+											: 50
+									: meta.getDelay()
 					)
 			);
 		}
