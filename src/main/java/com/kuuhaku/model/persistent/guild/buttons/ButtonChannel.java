9be0322fef0a48a54c24b8c1f23af568c2cb7af5
ygimenez <yagogimenez1@hotmail.com>
2021-05-30 13:10:29 -0300

    Correções menores

---
+++ src/main/java/com/kuuhaku/model/persistent/guild/buttons/ButtonChannel.java
@@ -0,0 +1,61 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.model.persistent.guild.buttons;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "buttonchannel")
+public class ButtonChannel {
+	@Id
+	@Column(columnDefinition = "VARCHAR(191) NOT NULL")
+	private String id;
+
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+	@JoinColumn(name = "channel_id")
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Set<ButtonMessage> messages = new HashSet<>();
+
+	public String getId() {
+		return id;
+	}
+
+	public Set<ButtonMessage> getMessages() {
+		return messages;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		ButtonChannel that = (ButtonChannel) o;
+		return Objects.equals(id, that.id);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+}
