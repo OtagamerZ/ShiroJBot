d4a0b7a858e7c772e7b25f08ade3be5e5dfdd25b
ygimenez <yagogimenez1@hotmail.com>
2021-05-30 13:10:29 -0300

    Correções menores

---
+++ src/main/java/com/kuuhaku/model/persistent/guild/buttons/Button.java
@@ -0,0 +1,71 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.model.persistent.guild.buttons;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Role;
+import org.jsoup.internal.StringUtil;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "buttonmessage")
+public class Button {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(columnDefinition = "VARCHAR(191) NOT NULL")
+	private String role;
+
+	@Column(columnDefinition = "VARCHAR(191) NOT NULL")
+	private String emote;
+
+	public Button() {
+	}
+
+	public Button(String role, String emote) {
+		this.role = role;
+		this.emote = emote;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public Role getRole(Guild g) {
+		return g.getRoleById(role);
+	}
+
+	public void setRole(Role role) {
+		this.role = role.getId();
+	}
+
+	public String getEmote() {
+		return emote;
+	}
+
+	public void setEmote(String emote) {
+		this.emote = emote;
+	}
+
+	public boolean isEmote() {
+		return StringUtil.isNumeric(emote);
+	}
+}
