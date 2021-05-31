e42e12fe63e5f149bbb5dad27d0f8628d284f970
ygimenez <yagogimenez1@hotmail.com>
2021-05-31 14:22:04 -0300

    Nomes de clãs agora podem ser alterados; Adicionado aluguel para os clãs;

--- src/main/java/com/kuuhaku/model/persistent/Trophy.java
+++ src/main/java/com/kuuhaku/model/persistent/Trophy.java
@@ -34,6 +34,7 @@ public class Trophy {
 
 	@Enumerated(value = EnumType.STRING)
 	@ElementCollection(fetch = FetchType.EAGER)
+	@JoinColumn(name = "trophy_uid")
 	@OnDelete(action = OnDeleteAction.CASCADE)
 	private Set<TrophyType> trophies = EnumSet.noneOf(TrophyType.class);
 
