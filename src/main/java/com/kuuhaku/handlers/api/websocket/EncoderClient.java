ca93b15f9ecdb7cdc921804b882d09b7021009e3
ygimenez <yagogimenez1@hotmail.com>
2021-06-05 00:11:58 -0300

    Criação de client da API de encoding da Shiro

--- src/main/java/com/kuuhaku/handlers/api/websocket/EncoderClient.java
+++ src/main/java/com/kuuhaku/handlers/api/websocket/EncoderClient.java
@@ -42,13 +42,6 @@ import java.util.concurrent.TimeUnit;
 public class EncoderClient extends Endpoint {
 	private static final ExecutorService exec = Executors.newSingleThreadExecutor();
 	private static final TempCache<String, CompletableFuture<String>> completed = new TempCache<>(10, TimeUnit.MINUTES);
-	private final MessageHandler handler = (MessageHandler.Whole<String>) message -> {
-		JSONObject res = new JSONObject(message);
-
-		if (res.getInt("code") == HttpStatus.OK.value()) {
-			completed.get(res.getString("hash")).complete(res.getString("url"));
-		}
-	};
 	private Session session = null;
 
 	public EncoderClient(String url) throws URISyntaxException, DeploymentException, IOException {
@@ -69,7 +62,13 @@ public class EncoderClient extends Endpoint {
 	@Override
 	public void onOpen(Session session, EndpointConfig config) {
 		this.session = session;
-		this.session.addMessageHandler(handler);
+		this.session.addMessageHandler((MessageHandler.Whole<String>) message -> {
+			JSONObject res = new JSONObject(message);
+
+			if (res.getInt("code") == HttpStatus.OK.value()) {
+				completed.get(res.getString("hash")).complete(res.getString("url"));
+			}
+		});
 		Helper.logger(this.getClass()).debug("Conectado ao webSocket \"encoder\" com sucesso");
 	}
 
