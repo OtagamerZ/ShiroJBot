90f9f41fdbaeb690969a4a80663525c7b911e146
ygimenez <yagogimenez1@hotmail.com>
2021-06-08 21:19:47 -0300

    Correção no indicador de turno

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/enums/FrameColor.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/enums/FrameColor.java
@@ -52,7 +52,7 @@ public enum FrameColor {
 			case GREEN -> new Color(139, 232, 116);
 			case YELLOW -> new Color(232, 222, 116);
 			case RED -> new Color(232, 116, 116);
-			case GREY -> new Color(232, 232, 232);
+			case GREY -> new Color(190, 190, 190);
 		};
 	}
 
