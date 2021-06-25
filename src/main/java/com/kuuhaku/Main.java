86d68556f9621a0a2d86ec1e5540157b149b0f77
ygimenez <yagogimenez1@hotmail.com>
2021-06-25 13:18:08 -0300

    Updated copyright notice

--- src/main/java/com/kuuhaku/Main.java
+++ src/main/java/com/kuuhaku/Main.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of Shiro J Bot.
- * Copyright (C) 2020  Yago Gimenez (KuuHaKu)
+ * Copyright (C) 2021  Yago Gimenez (KuuHaKu)
  *
  * Shiro J Bot is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -73,7 +73,7 @@ public class Main implements Thread.UncaughtExceptionHandler {
 		ImageIO.setUseCache(false);
 
 		Helper.logger(Main.class).info("""
-				Shiro J. Bot  Copyright (C) 2020 Yago Gimenez (KuuHaKu)
+				Shiro J. Bot  Copyright (C) 2021 Yago Gimenez (KuuHaKu)
 				This program comes with ABSOLUTELY NO WARRANTY
 				This is free software, and you are welcome to redistribute it under certain conditions
 				See license for more information regarding redistribution conditions
