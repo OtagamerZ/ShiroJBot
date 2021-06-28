4ce0590e1f2c222271a6baa6c6b5ab69b6e5bf47
ygimenez <yagogimenez1@hotmail.com>
2021-06-28 16:42:22 -0300

    Correções menores

--- src/main/java/com/kuuhaku/command/commands/discord/dev/CompileCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/dev/CompileCommand.java
@@ -18,15 +18,16 @@
 
 package com.kuuhaku.command.commands.discord.dev;
 
-import bsh.Interpreter;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Executable;
 import com.kuuhaku.model.annotations.Command;
 import com.kuuhaku.model.annotations.Requires;
 import com.kuuhaku.utils.Helper;
 import com.kuuhaku.utils.ShiroInfo;
+import groovy.util.Eval;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
+import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -43,31 +44,41 @@ public class CompileCommand implements Executable {
 	@Override
 	public void execute(User author, Member member, String command, String argsAsText, String[] args, Message message, TextChannel channel, Guild guild, String prefix) {
 		channel.sendMessage("<a:loading:697879726630502401> | Compilando...").queue(m -> {
-			try {
-				Future<?> execute = ShiroInfo.getCompilationPool().submit(() -> {
-					final long start = System.currentTimeMillis();
-					try {
-						String code = argsAsText.replace("```java", "").replace("```", "");
-						Interpreter i = new Interpreter();
-						i.set("msg", message);
-						i.eval(code);
-						Object out = i.get("out");
-						m.getChannel().sendMessage("<a:loading:697879726630502401> | Executando...").queue(d ->
-								d.editMessage("-> " + out).queue());
-						message.delete().queue();
-						m.editMessage("✅ | Tempo de execução: " + (System.currentTimeMillis() - start) + " ms").queue();
-					} catch (Exception e) {
-						m.editMessage("❌ | Erro ao compilar: ```" + e.toString().replace("`", "´") + "```").queue();
-					}
-					return null;
-				});
+			Future<Pair<String, Long>> execute = ShiroInfo.getCompilationPool().submit(() -> {
+				final long start = System.currentTimeMillis();
+
 				try {
-					execute.get();
-				} catch (InterruptedException | ExecutionException e) {
-					Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
+					String code = argsAsText.replace("```java", "").replace("```", "");
+
+					return Pair.of(String.valueOf(Eval.x(message, code)), System.currentTimeMillis() - start);
+				} catch (Exception e) {
+					return Pair.of(e.toString().replace("`", "´"), -1L);
 				}
-			} catch (Exception e) {
-				m.editMessage("❌ | Erro ao compilar: ```" + e.toString().replace("`", "´") + "```").queue();
+			});
+
+			try {
+				Pair<String, Long> out = execute.get();
+				if (out.getRight() > -1) {
+					m.editMessage("""
+							```diff
+							+ Tempo de execução: %s ms
+							--------------------------------
+							Out -> %s       
+							```
+							""".formatted(out.getLeft(), out.getRight())
+					).queue();
+				} else {
+					m.editMessage("""
+							```diff
+							- Erro ao compilar
+							--------------------------------
+							Out -> %s
+							```
+							""".formatted(out.getLeft())
+					).queue();
+				}
+			} catch (ExecutionException | InterruptedException e) {
+				Helper.logger(this.getClass()).error(e + " | " + e.getStackTrace()[0]);
 			}
 		});
 	}
