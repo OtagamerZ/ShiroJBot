5a49c9ea2b9f83fd06e1548f2b758b8a7860b627
ygimenez <yagogimenez1@hotmail.com>
2021-08-04 17:35:12 -0300

    Criado jogo Shiritori

---
+++ src/main/java/com/kuuhaku/command/commands/discord/fun/ShiritoriCommand.java
@@ -0,0 +1,155 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2021  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.discord.fun;
+
+import com.github.ygimenez.method.Pages;
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Executable;
+import com.kuuhaku.controller.postgresql.AccountDAO;
+import com.kuuhaku.handlers.games.normal.framework.Game;
+import com.kuuhaku.handlers.games.normal.games.Shiritori;
+import com.kuuhaku.model.annotations.Command;
+import com.kuuhaku.model.annotations.Requires;
+import com.kuuhaku.model.enums.I18n;
+import com.kuuhaku.model.persistent.Account;
+import com.kuuhaku.utils.Helper;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+@Command(
+		name = "shiritori",
+		usage = "req_bet-mentions",
+		category = Category.FUN
+)
+@Requires({
+		Permission.MESSAGE_MANAGE,
+		Permission.MESSAGE_ADD_REACTION,
+		Permission.MESSAGE_ATTACH_FILES
+})
+public class ShiritoriCommand implements Executable {
+
+	@Override
+	public void execute(User author, Member member, String argsAsText, String[] args, Message message, TextChannel channel, Guild guild, String prefix) {
+		if (message.getMentionedUsers().isEmpty()) {
+			channel.sendMessage(I18n.getString("err_no-user")).queue();
+			return;
+		} else if (Main.getInfo().getConfirmationPending().get(author.getId()) != null) {
+			channel.sendMessage("❌ | Você possui um comando com confirmação pendente, por favor resolva-o antes de usar este comando novamente.").queue();
+			return;
+		}
+
+		for (User u : message.getMentionedUsers()) {
+			if (Main.getInfo().getConfirmationPending().get(u.getId()) != null) {
+				channel.sendMessage("❌ | " + u.getAsMention() + " possui um comando com confirmação pendente, por favor espere ele resolve-lo antes de usar este comando novamente.").queue();
+				return;
+			} else if (Main.getInfo().gameInProgress(u.getId())) {
+				channel.sendMessage(I18n.getString("err_mention-in-game", u.getAsMention())).queue();
+				return;
+			} else if (u.getId().equals(author.getId())) {
+				channel.sendMessage(I18n.getString("err_cannot-play-with-yourself")).queue();
+				return;
+			}
+		}
+
+		Account acc = AccountDAO.getAccount(author.getId());
+
+		int bet = 0;
+		if (args.length > 1 && StringUtils.isNumeric(args[0])) {
+			bet = Integer.parseInt(args[0]);
+			if (bet < 0) {
+				channel.sendMessage(I18n.getString("err_invalid-credit-amount")).queue();
+				return;
+			} else if (acc.getBalance() < bet) {
+				channel.sendMessage(I18n.getString("err_insufficient-credits-user")).queue();
+				return;
+			}
+
+			for (User u : message.getMentionedUsers()) {
+				Account a = AccountDAO.getAccount(u.getId());
+				if (a.getBalance() < bet) {
+					channel.sendMessage(I18n.getString("err_insufficient-credits-mention", u.getAsMention())).queue();
+					return;
+				}
+			}
+		}
+
+		String id = author.getId() + "." + message.getMentionedUsers().stream().map(User::getId).collect(Collectors.joining(".")) + "." + guild.getId();
+
+		if (Main.getInfo().gameInProgress(author.getId())) {
+			channel.sendMessage(I18n.getString("err_you-are-in-game")).queue();
+			return;
+		}
+
+		List<User> players = new ArrayList<>() {{
+			add(author);
+			addAll(message.getMentionedUsers());
+		}};
+
+		Set<String> accepted = new HashSet<>() {{
+			add(author.getId());
+		}};
+
+		Game t = new Shiritori(Main.getShiroShards(), channel, bet, players.toArray(User[]::new));
+		String msg;
+		if (players.size() > 2)
+			msg = message.getMentionedUsers().stream().map(User::getAsMention).map(s -> s + ", ").collect(Collectors.collectingAndThen(Collectors.toList(), Helper.properlyJoin())) + ", vocês foram desafiados a uma partida de Shiritori, desejam aceitar?" + (bet != 0 ? " (aposta: " + Helper.separate(bet) + " créditos)" : "");
+		else
+			msg = message.getMentionedUsers().get(0).getAsMention() + " você foi desafiado a uma partida de Shiritori, deseja aceitar?" + (bet != 0 ? " (aposta: " + Helper.separate(bet) + " créditos)" : "");
+
+		for (User player : players) {
+			Main.getInfo().getConfirmationPending().put(player.getId(), true);
+		}
+		channel.sendMessage(msg).queue(s -> Pages.buttonize(s, Map.of(Helper.ACCEPT, (mb, ms) -> {
+					if (players.contains(mb.getUser())) {
+						if (Main.getInfo().gameInProgress(mb.getId())) {
+							channel.sendMessage(I18n.getString("err_you-are-in-game")).queue();
+							return;
+						} else if (Main.getInfo().gameInProgress(author.getId())) {
+							channel.sendMessage(I18n.getString("err_user-in-game")).queue();
+							return;
+						}
+
+						if (!accepted.contains(mb.getId())) {
+							channel.sendMessage(mb.getAsMention() + " aceitou a partida.").queue();
+							accepted.add(mb.getId());
+						}
+
+						if (accepted.size() == players.size()) {
+							Main.getInfo().getConfirmationPending().remove(author.getId());
+							//Main.getInfo().getGames().put(id, t);
+							s.delete().queue(null, Helper::doNothing);
+							t.start();
+						}
+					}
+				}), true, 1, TimeUnit.MINUTES,
+				u -> players.parallelStream().map(User::getId).anyMatch(i -> i.equals(u.getId())),
+				ms -> {
+					for (User player : players) {
+						Main.getInfo().getConfirmationPending().remove(player.getId());
+					}
+				}
+		));
+	}
+}
---
+++ src/main/java/com/kuuhaku/handlers/games/normal/games/Shiritori.java
@@ -0,0 +1,199 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2021  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.handlers.games.normal.games;
+
+import com.github.ygimenez.method.Pages;
+import com.github.ygimenez.model.ThrowingBiConsumer;
+import com.kuuhaku.events.SimpleMessageListener;
+import com.kuuhaku.handlers.games.normal.framework.Game;
+import com.kuuhaku.handlers.games.normal.framework.Table;
+import com.kuuhaku.utils.Helper;
+import com.kuuhaku.utils.ShiroInfo;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
+
+public class Shiritori extends Game {
+	private final TextChannel channel;
+	private final SimpleMessageListener listener = new SimpleMessageListener() {
+		@Override
+		public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+			if (canInteract(event)) play(event);
+		}
+	};
+	private final Set<String> available = new HashSet<>();
+	private final Set<String> used = new HashSet<>();
+	private Message message = null;
+	private String word;
+
+	public Shiritori(ShardManager handler, TextChannel channel, int bet, User... players) {
+		super(handler, new Table(bet, Arrays.stream(players).map(User::getId).toArray(String[]::new)), channel);
+		this.channel = channel;
+		setTime(60);
+
+		setActions(
+				s -> close(),
+				s -> {
+					getTable().leaveGame();
+					resetTimer();
+					if (getTable().getInGamePlayers().size() == 1) {
+						User u = getCurrent();
+						getTable().awardWinner(this, u.getId());
+						close();
+						channel.sendMessage(getCurrent().getAsMention() + " é o último jogador na mesa, temos um vencedor!! (" + getRound() + " turnos)")
+								.queue(msg -> {
+									if (this.message != null) this.message.delete().queue(null, Helper::doNothing);
+								});
+					}
+				}
+		);
+
+		try {
+			URL url = this.getClass().getClassLoader().getResource("shiritori/ptBR_dict.txt");
+			assert url != null;
+			File f = new File(url.toURI());
+			available.addAll(Files.readAllLines(f.toPath()));
+		} catch (URISyntaxException | IOException ignore) {
+		}
+	}
+
+	@Override
+	public void start() {
+		channel.sendMessage(getCurrent().getAsMention() + " você começa, digite uma palavra para começar!")
+				.queue(s -> {
+					this.message = s;
+					ShiroInfo.getShiroEvents().addHandler(channel.getGuild(), listener);
+					Pages.buttonize(s, getButtons(), false, 3, TimeUnit.MINUTES, us -> us.getId().equals(getCurrent().getId()));
+				});
+	}
+
+	@Override
+	public boolean canInteract(GuildMessageReceivedEvent evt) {
+		Predicate<GuildMessageReceivedEvent> condition = e -> e.getChannel().getId().equals(channel.getId());
+
+		return condition
+				.and(e -> e.getAuthor().getId().equals(getCurrent().getId()))
+				.and(e -> isOpen())
+				.test(evt);
+	}
+
+	@Override
+	public synchronized void play(GuildMessageReceivedEvent evt) {
+		Message message = evt.getMessage();
+		String command = StringUtils.stripAccents(message.getContentRaw().toLowerCase(Locale.ROOT));
+
+		if (word != null && !command.substring(0, 2).equals(word.substring(word.length() - 2))) {
+			channel.sendMessage("❌ | Palavra não permitida. Só são permitidas palavras onde as duas primeiras letras sejam as mesmas que as 2 últimas letras da palavra anterior (ex: maca**co** -> **co**lmeia).").queue();
+		} else if (used.contains(command)) {
+			channel.sendMessage(getCurrent().getAsMention() + " escreveu uma palavra já usada, ESTÁ FORA!").queue(null, Helper::doNothing);
+			getTable().leaveGame();
+			resetTimer();
+
+			if (getTable().getInGamePlayers().size() == 1) {
+				getTable().awardWinner(this, getCurrent().getId());
+				close();
+				channel.sendMessage(getCurrent().getAsMention() + " é o último jogador na mesa, temos um vencedor!! (" + getRound() + " turnos)")
+						.queue(msg -> {
+							if (this.message != null) this.message.delete().queue(null, Helper::doNothing);
+						});
+			} else {
+				channel.sendMessage(getCurrent().getAsMention() + " agora é sua vez (palavra atual: " + getHighlightedWord() + ").")
+						.queue(s -> {
+							if (this.message != null) this.message.delete().queue(null, Helper::doNothing);
+							this.message = s;
+							Pages.buttonize(s, getButtons(), false, 3, TimeUnit.MINUTES, us -> us.getId().equals(getCurrent().getId()));
+						});
+			}
+		} else if (available.contains(command)) {
+			available.remove(command);
+			used.add(command);
+			word = command;
+
+			channel.sendMessage(getCurrent().getAsMention() + " agora é sua vez (palavra atual: " + getHighlightedWord() + ").")
+					.queue(s -> {
+						if (this.message != null) this.message.delete().queue(null, Helper::doNothing);
+						this.message = s;
+						Pages.buttonize(s, getButtons(), false, 3, TimeUnit.MINUTES, us -> us.getId().equals(getCurrent().getId()));
+					});
+		} else {
+			channel.sendMessage("❌ | Palavra inválida, veja se escreveu-a corretamente.").queue();
+		}
+
+		if (getRound() > 50) {
+			channel.sendMessage(":warning: | ALERTA: Morte-súbita ativada, o tempo para W.O. foi reduzido para 30 segundos!").queue();
+			setTime(30);
+		}
+	}
+
+	public TextChannel getChannel() {
+		return channel;
+	}
+
+	@Override
+	public Map<String, ThrowingBiConsumer<Member, Message>> getButtons() {
+		Map<String, ThrowingBiConsumer<Member, Message>> buttons = new LinkedHashMap<>();
+		buttons.put("\uD83C\uDFF3️", (mb, ms) -> {
+			channel.sendMessage(getCurrent().getAsMention() + " desistiu!").queue(null, Helper::doNothing);
+			getTable().leaveGame();
+			resetTimer();
+
+			if (getTable().getInGamePlayers().size() == 1) {
+				getTable().awardWinner(this, getCurrent().getId());
+				close();
+				channel.sendMessage(getCurrent().getAsMention() + " é o último jogador na mesa, temos um vencedor!! (" + getRound() + " turnos)")
+						.queue(msg -> {
+							if (this.message != null) this.message.delete().queue(null, Helper::doNothing);
+						});
+			} else {
+				channel.sendMessage(getCurrent().getAsMention() + " agora é sua vez (palavra atual: " + getHighlightedWord() + ").")
+						.queue(s -> {
+							if (this.message != null) this.message.delete().queue(null, Helper::doNothing);
+							this.message = s;
+							Pages.buttonize(s, getButtons(), false, 3, TimeUnit.MINUTES, us -> us.getId().equals(getCurrent().getId()));
+						});
+			}
+		});
+
+		return buttons;
+	}
+
+	private String getHighlightedWord() {
+		return word.substring(0, word.length() - 2) + "**" + word.substring(word.length() - 2) + "**";
+	}
+
+	@Override
+	public void close() {
+		listener.close();
+		super.close();
+	}
+}
