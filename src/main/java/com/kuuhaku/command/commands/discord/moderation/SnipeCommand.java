79b06e8f9149037c68d1d2a1378164c128434990
ygimenez <yagogimenez1@hotmail.com>
2021-08-11 15:35:54 -0300

    Correções menores

---
+++ src/main/java/com/kuuhaku/command/commands/discord/moderation/SnipeCommand.java
@@ -0,0 +1,75 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2021  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.commands.discord.moderation;
+
+import com.github.ygimenez.method.Pages;
+import com.github.ygimenez.model.Page;
+import com.kuuhaku.Main;
+import com.kuuhaku.command.Category;
+import com.kuuhaku.command.Executable;
+import com.kuuhaku.model.annotations.Command;
+import com.kuuhaku.model.annotations.Requires;
+import com.kuuhaku.model.common.ColorlessEmbedBuilder;
+import com.kuuhaku.utils.Helper;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
+import net.jodah.expiringmap.ExpiringMap;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+@Command(
+		name = "snipe",
+		aliases = {"undelete", "desdeletar"},
+		category = Category.INFO
+)
+@Requires({Permission.MESSAGE_HISTORY})
+public class SnipeCommand implements Executable {
+
+	@Override
+	public void execute(User author, Member member, String argsAsText, String[] args, Message message, TextChannel channel, Guild guild, String prefix) {
+		ExpiringMap<String, Message> c = Main.getInfo().retrieveCache(guild);
+
+		List<List<Message>> chunks = Helper.chunkify(c.values().stream().filter(m -> m.getChannel().getId().equals(channel.getId())).toList(), 10);
+		if (chunks.isEmpty()) {
+			channel.sendMessage("❌ | Não há nenhuma mensagem deletada recentemente neste canal.").queue();
+			return;
+		}
+
+		List<Page> pages = new ArrayList<>();
+		EmbedBuilder eb = new ColorlessEmbedBuilder()
+				.setTitle(":detective: | Mensagens deletadas recentemente");
+		for (List<Message> chunk : chunks) {
+			eb.clearFields();
+
+			for (Message msg : chunk) {
+				eb.addField("(" + Helper.TIMESTAMP.formatted(msg.getTimeCreated().toEpochSecond()) + ") " + msg.getAuthor().getAsTag() + " disse:", StringUtils.abbreviate(msg.getContentRaw(), 100), false);
+			}
+
+			pages.add(new Page(eb.build()));
+		}
+
+		channel.sendMessageEmbeds((MessageEmbed) pages.get(0).getContent()).queue(s ->
+				Pages.paginate(s, pages, 1, TimeUnit.MINUTES, u -> u.getId().equals(author.getId()))
+		);
+	}
+}
