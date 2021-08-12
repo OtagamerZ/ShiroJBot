cbb37428fb8a56b4690337a1198bc913aefe04ab
ygimenez <yagogimenez1@hotmail.com>
2021-08-12 13:11:31 -0300

    Melhoria na customização das mensagens personalizadas

--- src/main/java/com/kuuhaku/controller/postgresql/CustomAnswerDAO.java
+++ src/main/java/com/kuuhaku/controller/postgresql/CustomAnswerDAO.java
@@ -39,8 +39,8 @@ public class CustomAnswerDAO {
 					 , c.answer
 					 , c.anywhere
 					 , c.chance
-					 , array_to_string(array_agg(cu.users), ',') AS users
-					 , array_to_string(array_agg(cc.channels), ',') AS channels
+					 , string_agg(cu.users, ',') AS users
+					 , string_agg(cc.channels, ',') AS channels
 				FROM CustomAnswer c
 						 LEFT JOIN customanswer_users cu ON c.id = cu.customanswer_id
 						 LEFT JOIN customanswer_channels cc ON c.id = cc.customanswer_id
