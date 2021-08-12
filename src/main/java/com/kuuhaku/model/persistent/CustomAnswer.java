0f08002b4469203c53b8e56e82049593c6f7a20b
ygimenez <yagogimenez1@hotmail.com>
2021-08-12 12:02:49 -0300

    Melhoria na customização das mensagens personalizadas

--- src/main/java/com/kuuhaku/model/persistent/CustomAnswer.java
+++ src/main/java/com/kuuhaku/model/persistent/CustomAnswer.java
@@ -20,7 +20,10 @@ package com.kuuhaku.model.persistent;
 
 import javax.persistence.*;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Entity
 @Table(name = "customanswer")
@@ -52,13 +55,15 @@ public class CustomAnswer {
     @JoinColumn(name = "customanswer_id")
     private Set<String> channels = new HashSet<>();
 
-    public CustomAnswer(int id, String guildId, String trigger, String answer, boolean anywhere, int chance) {
+    public CustomAnswer(int id, String guildId, String trigger, String answer, boolean anywhere, int chance, String[] users, String[] channels) {
         this.id = id;
         this.guildId = guildId;
         this.trigger = trigger;
         this.answer = answer;
         this.anywhere = anywhere;
         this.chance = chance;
+        this.users = Stream.of(users).filter(Objects::nonNull).collect(Collectors.toSet());
+        this.channels = Stream.of(channels).filter(Objects::nonNull).collect(Collectors.toSet());
     }
 
     public CustomAnswer(String guildId, String trigger, String answer) {
