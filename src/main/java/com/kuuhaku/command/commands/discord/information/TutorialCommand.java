a613017f6f3e6f3395d79b42b6b6516a23dc0ee8
ygimenez <yagogimenez1@hotmail.com>
2021-08-25 16:24:20 -0300

    Correções menores

--- src/main/java/com/kuuhaku/command/commands/discord/information/TutorialCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/information/TutorialCommand.java
@@ -174,7 +174,7 @@ public class TutorialCommand implements Executable {
 
 			{
 				next.set(new CompletableFuture<>());
-				msg = channel.sendMessageEmbeds(sixthStep()).complete();
+				msg = channel.sendMessageEmbeds(sixthStep(prefix)).complete();
 				Pages.buttonize(
 						msg,
 						Map.of("▶️", (mb, ms) -> next.get().complete(true)),
@@ -246,7 +246,7 @@ public class TutorialCommand implements Executable {
 
 			{
 				next.set(new CompletableFuture<>());
-				msg = channel.sendMessageEmbeds(tenthStep()).complete();
+				msg = channel.sendMessageEmbeds(tenthStep(prefix)).complete();
 				Pages.buttonize(
 						msg,
 						Map.of("▶️", (mb, ms) -> next.get().complete(true)),
@@ -264,7 +264,7 @@ public class TutorialCommand implements Executable {
 
 			{
 				next.set(new CompletableFuture<>());
-				msg = channel.sendMessageEmbeds(finalStep()).complete();
+				msg = channel.sendMessageEmbeds(finalStep(prefix)).complete();
 				Pages.buttonize(
 						msg,
 						Map.of("✅", (mb, ms) -> next.get().complete(true)),
@@ -304,7 +304,7 @@ public class TutorialCommand implements Executable {
 		return new ColorlessEmbedBuilder()
 				.setDescription("""
 						Ok, vamos começar falando sobre os **créditos** (CR).
-												
+
 						Crédito é a principal moeda usada em meus módulos, eles são necessários para várias coisas como coletar cartas, apostar em jogos, comprar melhorias, comprar itens na loja, etc.
 						Existem muitas formas de conseguir créditos, as mais populares são:
 						- Votando em mim
@@ -312,7 +312,7 @@ public class TutorialCommand implements Executable {
 						- Abrindo drops
 						- Vendendo cartas
 						- Jogando minigames
-												
+
 						Para continuar, digite `%satm`.
 						""".formatted(prefix))
 				.build();
@@ -322,12 +322,12 @@ public class TutorialCommand implements Executable {
 		return new ColorlessEmbedBuilder()
 				.setDescription("""
 						Muito bem!
-												
+
 						Com este comando você pode ver quantos créditos você possui, além dos créditos voláteis, dívida, gemas acumuladas e a data do seu último voto.
 						- **Créditos voláteis:** São iguais aos créditos normais, mas só podem ser usados para transações comigo além de perder 25% a cada hora.
 						- **Dívidas:** Caso você faça um empréstimo de créditos (ou seja pego abusando de algum comando) você receberá um valor de dívida, fazendo com que você não receba mais créditos até terminar de pagá-la.
 						- **Gemas:** A moeda VIP para quem vota frequentemente em mim, com ela você consegue comprar melhorias exóticas na loja exclusiva.
-												
+
 						Para continuar, clique em ▶️.
 						""")
 				.build();
@@ -337,10 +337,10 @@ public class TutorialCommand implements Executable {
 		return new ColorlessEmbedBuilder()
 				.setDescription("""
 						Olha só!
-												
+
 						Essa é uma carta kawaipon, elas aparecem conforme o movimento nos chats do servidor e podem ser coletadas para adicionar à sua coleção.
 						Para coletá-las você precisa de créditos (conforme escrito abaixo da imagem), mas como estamos em um tutorial esta carta sairá de graça.
-												
+
 						Pegue-a, rápido!
 						Para continuar, digite `%scoletar`.
 						""".formatted(prefix))
@@ -351,25 +351,25 @@ public class TutorialCommand implements Executable {
 		return new ColorlessEmbedBuilder()
 				.setDescription("""
 						Ótimo, agora vamos ver quantas cartas faltam!
-												
+
 						Para continuar, digite `%skps no_game_no_life`.
 						""".formatted(prefix))
 				.build();
 	}
 
-	private MessageEmbed sixthStep() {
+	private MessageEmbed sixthStep(String prefix) {
 		return new ColorlessEmbedBuilder()
 				.setDescription("""
 						Nossa, faltam muitas ainda não? Mas não se preocupe, com o tempo você irá completar todas as coleções, pode confiar!
-												
+
 						Existem 2 tipos de cartas:
 						- **Normais:** Cartas que podem ser convertidas para o Shoukan, além de serem usadas como material para sintetizar equipamentos evogear.
 						- **Cromadas:** Cartas que possuem uma paleta alternativa, podendo também serem usadas como material para sintetizar campos.
-												
-						Se estiver com dificuldades para ver a imagem ou quiser ver a lista em formato de texto, basta usar o comando `restante` e o nome do anime, igual você fez antes.
-												
+
+						Se estiver com dificuldades para ver a imagem ou quiser ver a lista em formato de texto, basta usar o comando `%srestante` e o nome do anime, igual você fez antes.
+
 						Para continuar, clique em ▶️.
-						""")
+						""".formatted(prefix))
 				.build();
 	}
 
@@ -378,12 +378,16 @@ public class TutorialCommand implements Executable {
 				.setDescription("""
 						Sobre o Shoukan, recomendo ler [este guia](https://www.reddit.com/r/ShiroJBot/comments/jkbjtd/shoukan_o_duelo_entre_invocadores/) quando tiver um tempo, ele foi escrito por meu Nii-chan e contém informações muito úteis sobre como jogar!
 						Você sabia? Jogadores que leem o guia têm 99%% de chance de não passar vergonha na primeira partida!
-												
-						Para poder jogar, você vai precisar de um deck, que existem 3 tipos.
-						OBS: Para ver seu deck use o comando `deck`.
-												
+
+						Para poder jogar, você vai precisar montar um deck:
+						- Para adicionar cartas, use `%sconverter` e o nome da carta (apenas as que você possui na coleção).
+						- Para remover cartas, use `%sreverter` e o nome da carta (apenas se você já não possuir uma na coleção).
+						OBS: Para ver seu deck use o comando `%sdeck`.
+
+						Aliás, a carta que você acabou de coletar (a Queen) é elegível, então você pode adicioná-la em seu deck.
+
 						Para continuar, digite `%skps elegiveis`.
-						""".formatted(prefix))
+						""".formatted(prefix, prefix, prefix, prefix))
 				.build();
 	}
 
@@ -391,15 +395,15 @@ public class TutorialCommand implements Executable {
 		return new ColorlessEmbedBuilder()
 				.setDescription("""
 						**Senshi**
-												
+
 						A sua linha de frente, campeões que irão lutar até o fim para lhe trazer a vitória.
 						Cada um possui seus próprios atributos, efeitos, raças e classes, permitindo uma ampla customização do seu deck.
-												
+
 						Certos campeões também possuem fusões - versões muito mais poderosas que podem alterar o rumo de uma partida.
-												
+
 						Se quiser (e eu recomendo) ler mais sobre as raças e classes, basta usar este mesmo comando mas informando a raça/classe que deseja ver.
 						Adicionalmente, usar várias cartas de uma mesma raça garantem efeitos únicos e poderosos durante o jogo, então planeje bem sua estratégia!
-												
+
 						Para continuar, digite `%skps evogear`.
 						""".formatted(prefix))
 				.build();
@@ -409,57 +413,54 @@ public class TutorialCommand implements Executable {
 		return new ColorlessEmbedBuilder()
 				.setDescription("""
 						**Evogear**
-												
+
 						Equipamentos e feitiços para adicionar um "tempero" às suas cartas.
 						Eles são obtidos através de sínteses ou drops e são essenciais em qualquer deck, seja ele de um guerreiro ou de um mago.
-												
+
 						Você possui 24 espaços para adicionar evogears, mas eles consomem espaços de acordo com o tier:
 						- **Tier 1:** Evogears geralmente baratos e de atributos inferiores, mas consomem apenas 1 espaço do deck permitindo usar uma grande quantidade deles.
 						- **Tier 2:** Evogears de atributos medianos e efeitos variados, digamos que "um meio termo" entre os tiers 1 e 3.
 						- **Tier 3:** Evogears poderosos e efeitos capazes de virar o jogo ou garantir a vitoria. Apesar de fortes deve-se usá-los com moderação pois ocupam 3 espaços do deck.
 						- **Tier 4 (mítico):** Evogears extremamente impactantes que criam oportunidades de vitória instantânea se usados corretamente. Um deck pode possuir apenas 1 evogear mítico e são banidos logo após o uso, então a escolha deve levar em consideração o seu deck como um todo.
-												
-						Para continuar, digite `%ssee forest s`.
+
+						Para obter um evogear você precisa sintetizar 3 cartas normais usando o comando `%ssintetizar`, aumentando a chance de conseguir tiers maiores dependendo da raridade das cartas usadas.
+
+						Para continuar, digite `%skps campo`.
+						""".formatted(prefix, prefix))
+				.build();
+	}
+
+	private MessageEmbed tenthStep(String prefix) {
+		return new ColorlessEmbedBuilder()
+				.setDescription("""
+						**Campo**
+
+						Campos são modificadores globais que afetam cartas de determinadas raças, tanto positivamente quanto negativamente.
+						Geralmente são usados campos que sinergizam com seu deck, mas também podem ser usados como medida anti-deck contra o oponente.
+
+						A raça da carta pode ser descoberta olhando no canto esquerdo superior, logo antes do nome.
+						Para obter um campo você precisa sintetizar 3 cartas cromadas usando o comando `%ssintetizar`.
+
+						Para continuar, clique em ▶️.
 						""".formatted(prefix))
 				.build();
 	}
 
-	private MessageEmbed tenthStep() {
-		return new ColorlessEmbedBuilder()
-				.setDescription("""
-						**Campo**
-												
-						Campos são modificadores globais que afetam cartas de determinadas raças, tanto positivamente quanto negativamente.
-						Geralmente são usados campos que sinergizam com seu deck, mas também podem ser usados como medida anti-deck contra o oponente.
-												
-						Neste campo por exemplo, os efeitos são:
-						- **Elfo:** +25% atributos
-						- **Betial:** +25% atributos
-						- **Místico:** +33% atributos
-						- **Criatura:** +33% atributos
-												
-						A raça da carta pode ser descoberta olhando no canto esquerdo superior, logo antes do nome.
-												
-						Para continuar, clique em ▶️.
-						""")
-				.build();
-	}
-
-	private MessageEmbed finalStep() {
+	private MessageEmbed finalStep(String prefix) {
 		return new ColorlessEmbedBuilder()
 				.setDescription("""
 						Isso é tudo, acha que está pronto para começar sua jornada?
 						Para aprofundar ainda mais nas mecânicas do Shoukan recomendo encontrar um jogador disposto a fazer um contrato de aprendiz, ele dará à ambos uma recompensa após completá-lo.
-						Para fazer um, use o comando `tutor` e mencione o usuário disposto a treinar você para tornar-se um mestre invocador!
-												
+						Para fazer um, use o comando `%stutor` e mencione o usuário disposto a treinar você para tornar-se um mestre invocador!
+
 						**Mas espere, é perigoso ir sozinho!**
 						Aqui, como recompensa por completar o tutorial e para lhe ajudar a começar sua jornada, vou te dar **25.000** créditos de iniciante.
 						Eles são iguais aos créditos voláteis mas não expiram com o tempo.
-												
+
 						Boa sorte jogador, esperarei seu sucesso nos campos de invocação!
-												
+
 						Para terminar, clique em ✅.
-						""")
+						""".formatted(prefix))
 				.setImage("https://c.tenor.com/kDAUCWniovoAAAAC/no-game-no-life-thumbs-up.gif")
 				.build();
 	}
