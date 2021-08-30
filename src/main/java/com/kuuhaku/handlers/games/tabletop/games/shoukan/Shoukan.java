42d71193666c652b32f025b77f094c5a05b08e40
Ygimenez <yagogimenez1@hotmail.com>
2021-08-30 09:53:07 -0300

    Correções menores

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Shoukan.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Shoukan.java
@@ -2384,10 +2384,8 @@ public class Shoukan extends GlobalGame {
 			Iterator<PersistentEffect> i = persistentEffects.iterator();
 			for (PersistentEffect e = i.next(); i.hasNext(); e = i.next()) {
 				if (e.getTarget() == null || e.getTarget() == to) {
-					if (Helper.equalsAny(trigger, BEFORE_TURN, AFTER_TURN) && e.getTurns() > 0) {
-						if (Helper.containsAny(e.getTriggers(), BEFORE_TURN, AFTER_TURN) || trigger == AFTER_TURN)
-							e.decreaseTurn();
-					}
+					if (trigger == AFTER_TURN && e.getTurns() > 0)
+						e.decreaseTurn();
 
 					if (e.getTriggers().contains(trigger))
 						e.activate(to, index);
