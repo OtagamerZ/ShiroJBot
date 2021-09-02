46daeb9dc0219dfdab096abf7139f4bf776cd241
ygimenez <yagogimenez1@hotmail.com>
2021-09-02 10:48:44 -0300

    Correções menores

--- src/main/java/com/kuuhaku/model/persistent/tournament/Tournament.java
+++ src/main/java/com/kuuhaku/model/persistent/tournament/Tournament.java
@@ -64,7 +64,7 @@ public class Tournament {
 	@JoinColumn(name = "tournament")
 	private List<Participant> participants = new ArrayList<>();
 
-	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
+	@ManyToOne(fetch = FetchType.EAGER)
 	private Bracket bracket;
 
 	@Column(columnDefinition = "BOOLEAN NOT NULL DEFAULT FALSE")
