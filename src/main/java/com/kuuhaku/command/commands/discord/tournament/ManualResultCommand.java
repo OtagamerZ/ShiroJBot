27524eb6bffe85c0d3f6211788c8d69c64afe8e1
ygimenez <yagogimenez1@hotmail.com>
2021-09-04 15:36:55 -0300

    Correções menores

--- src/main/java/com/kuuhaku/command/commands/discord/tournament/ManualResultCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/tournament/ManualResultCommand.java
@@ -24,6 +24,7 @@ import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Executable;
 import com.kuuhaku.controller.postgresql.TournamentDAO;
 import com.kuuhaku.model.annotations.Command;
+import com.kuuhaku.model.persistent.tournament.Participant;
 import com.kuuhaku.model.persistent.tournament.Phase;
 import com.kuuhaku.model.persistent.tournament.Tournament;
 import com.kuuhaku.utils.Helper;
@@ -31,6 +32,7 @@ import net.dv8tion.jda.api.entities.*;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -58,26 +60,50 @@ public class ManualResultCommand implements Executable {
 
 		try {
 			Tournament t = TournamentDAO.getTournament(Integer.parseInt(args[0]));
-			Phase p = t.getPhase(Integer.parseInt(args[1]));
 
-			int index = Integer.parseInt(args[2]);
-			Pair<String, String> match = p.getMatch(index);
-			User winner = Main.getInfo().getUserByID(p.getParticipants().get(index));
-			String matchName = Arrays.stream(new String[]{match.getLeft(), match.getRight()})
-					.map(Helper::getUsername)
-					.collect(Collectors.joining(" VS "));
+			int phase = Integer.parseInt(args[1]);
+			if (phase == t.getBracket().getPhases().size()) {
+				int index = Integer.parseInt(args[2]);
+				List<Participant> match = t.getTPMatch();
+				User winner = Main.getInfo().getUserByID(match.get(index).getUid());
+				String matchName = match.stream()
+						.map(Participant::getUid)
+						.map(Helper::getUsername)
+						.collect(Collectors.joining(" VS "));
 
-			channel.sendMessage("Você está prestes a definir `" + winner.getName() + "` como vencedor da partida `" + matchName + "`, deseja confirmar?").queue(
-					s -> Pages.buttonize(s, Map.of(Helper.ACCEPT, (mb, ms) -> {
-								t.setResult(p.getPhase(), index);
-								TournamentDAO.save(t);
+				channel.sendMessage("Você está prestes a definir `" + winner.getName() + "` como vencedor da partida `" + matchName + "`, deseja confirmar?").queue(
+						s -> Pages.buttonize(s, Map.of(Helper.ACCEPT, (mb, ms) -> {
+									t.setTPResult(index);
+									TournamentDAO.save(t);
 
-								s.delete().queue(null, Helper::doNothing);
-								channel.sendMessage("✅ | Resultado registrado com sucesso!").queue();
-							}), true, 1, TimeUnit.MINUTES
-							, u -> u.getId().equals(author.getId())
-					), Helper::doNothing
-			);
+									s.delete().queue(null, Helper::doNothing);
+									channel.sendMessage("✅ | Resultado registrado com sucesso!").queue();
+								}), true, 1, TimeUnit.MINUTES
+								, u -> u.getId().equals(author.getId())
+						), Helper::doNothing
+				);
+			} else {
+				Phase p = t.getPhase(phase);
+
+				int index = Integer.parseInt(args[2]);
+				Pair<String, String> match = p.getMatch(index);
+				User winner = Main.getInfo().getUserByID(p.getParticipants().get(index));
+				String matchName = Arrays.stream(new String[]{match.getLeft(), match.getRight()})
+						.map(Helper::getUsername)
+						.collect(Collectors.joining(" VS "));
+
+				channel.sendMessage("Você está prestes a definir `" + winner.getName() + "` como vencedor da partida `" + matchName + "`, deseja confirmar?").queue(
+						s -> Pages.buttonize(s, Map.of(Helper.ACCEPT, (mb, ms) -> {
+									t.setResult(phase, index);
+									TournamentDAO.save(t);
+
+									s.delete().queue(null, Helper::doNothing);
+									channel.sendMessage("✅ | Resultado registrado com sucesso!").queue();
+								}), true, 1, TimeUnit.MINUTES
+								, u -> u.getId().equals(author.getId())
+						), Helper::doNothing
+				);
+			}
 		} catch (NumberFormatException e) {
 			channel.sendMessage("❌ | ID inválido.").queue();
 		}
