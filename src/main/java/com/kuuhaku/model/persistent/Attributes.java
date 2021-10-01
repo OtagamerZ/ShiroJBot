872b2771a062996a69fa8373fbba87da99d99daf
Ygimenez <yagogimenez1@hotmail.com>
2021-10-01 19:46:20 -0300

    Correções menores

--- src/main/java/com/kuuhaku/model/persistent/Attributes.java
+++ src/main/java/com/kuuhaku/model/persistent/Attributes.java
@@ -117,11 +117,11 @@ public class Attributes {
 	}
 
 	public int calcAtk() {
-		return (int) Helper.roundTrunc(200 + 2800 * (1 - Math.exp(-0.02 * str + -0.005 * agi)), 25);
+		return (int) Helper.roundTrunc(100 + 3000 * (1 - Math.exp(-0.02 * str + -0.005 * agi)), 25);
 	}
 
 	public int calcDef() {
-		return (int) Helper.roundTrunc(250 + 2250 * (1 - Math.exp(-0.03 * res + -0.0075 * agi)), 25);
+		return (int) Helper.roundTrunc(100 + 2500 * (1 - Math.exp(-0.03 * res + -0.0075 * agi)), 25);
 	}
 
 	public int calcDodge() {
