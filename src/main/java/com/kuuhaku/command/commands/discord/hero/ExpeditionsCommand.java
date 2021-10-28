d2224daa797aeb3c31e9185eebbb2744ad2ded45
ygimenez <yagogimenez1@hotmail.com>
2021-10-28 15:40:51 -0300

    Correções menores

--- src/main/java/com/kuuhaku/command/commands/discord/hero/ExpeditionsCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/hero/ExpeditionsCommand.java
@@ -23,16 +23,14 @@ import com.github.ygimenez.model.ThrowingBiConsumer;
 import com.kuuhaku.Main;
 import com.kuuhaku.command.Category;
 import com.kuuhaku.command.Executable;
-import com.kuuhaku.controller.postgresql.CardDAO;
+import com.kuuhaku.controller.postgresql.ExpeditionDAO;
 import com.kuuhaku.controller.postgresql.KawaiponDAO;
-import com.kuuhaku.controller.postgresql.MatchMakingRatingDAO;
-import com.kuuhaku.handlers.games.tabletop.games.shoukan.Champion;
 import com.kuuhaku.handlers.games.tabletop.games.shoukan.Hero;
-import com.kuuhaku.handlers.games.tabletop.games.shoukan.enums.Perk;
 import com.kuuhaku.model.annotations.Command;
 import com.kuuhaku.model.annotations.Requires;
 import com.kuuhaku.model.common.ColorlessEmbedBuilder;
-import com.kuuhaku.model.persistent.MatchMakingRating;
+import com.kuuhaku.model.enums.Reward;
+import com.kuuhaku.model.persistent.Expedition;
 import com.kuuhaku.utils.Helper;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
@@ -45,8 +43,8 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Command(
-		name = "lojaefeitos",
-		aliases = {"effectshop", "lojae", "shope"},
+		name = "expedicoes",
+		aliases = {"expeditions"},
 		category = Category.MISC
 )
 @Requires({
@@ -54,7 +52,7 @@ import java.util.concurrent.TimeUnit;
 		Permission.MESSAGE_EMBED_LINKS,
 		Permission.MESSAGE_ADD_REACTION
 })
-public class EffectShopCommand implements Executable {
+public class ExpeditionsCommand implements Executable {
 
 	@Override
 	public void execute(User author, Member member, String argsAsText, String[] args, Message message, TextChannel channel, Guild guild, String prefix) {
@@ -68,27 +66,43 @@ public class EffectShopCommand implements Executable {
 			return;
 		}
 
-		MatchMakingRating mmr = MatchMakingRatingDAO.getMMR(author.getId());
-
-		int max = mmr.getTier().getTier() + 1;
-		boolean manaless = h.getPerks().contains(Perk.MANALESS);
-
-		List<Champion> masters = CardDAO.getAllChampionsWithEffect(!manaless && mmr.getTier().getTier() >= 5, Math.min(manaless ? 4 : max, max));
 		Calendar cal = Calendar.getInstance();
-		List<Champion> pool = Helper.getRandomN(masters, 5, 1, author.getIdLong() + h.getId() + cal.get(Calendar.DAY_OF_YEAR) + cal.get(Calendar.YEAR));
+		List<Expedition> pool = Helper.getRandomN(ExpeditionDAO.getExpeditions(), 3, 1, author.getIdLong() + h.getId() + cal.get(Calendar.WEEK_OF_YEAR) + cal.get(Calendar.YEAR));
 		Map<String, ThrowingBiConsumer<Member, Message>> buttons = new LinkedHashMap<>();
 		for (int i = 0; i < pool.size(); i++) {
-			Champion c = pool.get(i);
-			int cost = c.getMana() + (c.isFusion() ? 5 : 0);
+			Expedition e = pool.get(i);
+
+			EmbedBuilder eb = new ColorlessEmbedBuilder()
+					.setTitle("Possíveis espólios");
+
+			for (Map.Entry<String, Object> entry : e.getRewards().entrySet()) {
+				Reward rew = Reward.valueOf(entry.getKey());
+				int val = (int) entry.getValue();
+
+				eb.addField(rew.toString(),
+						switch (rew) {
+							case XP, CREDIT, GEM -> "Até " + Helper.separate(val);
+							case EQUIPMENT -> val + "% de chance";
+						}, true);
+			}
+
 			buttons.put(Helper.getFancyNumber(i + 1), (mb, ms) -> {
+				if (h.getEnergy() < 1) {
+					channel.sendMessage("❌ | Seu herói está cansado (sem energia suficiente).").queue();
+					return;
+				} else if (h.getHp() < h.getMaxHp() / 4) {
+					channel.sendMessage("❌ | Seu herói está muito ferido para ir em uma expedição (HP muito baixo).").queue();
+					return;
+				}
+
 				Main.getInfo().getConfirmationPending().put(h.getUid(), true);
-				channel.sendMessage(h.getName() + " será treinado por " + c.getName() + ", deseja confirmar?")
+				channel.sendMessage(h.getName() + " irá em uma expedição para " + e + " por " + Helper.toStringDuration(e.getTime()) + ", deseja confirmar?")
 						.queue(s -> Pages.buttonize(s, Map.of(Helper.ACCEPT, (mem, msg) -> {
-									h.setReferenceChampion(c.getId());
+									h.setExpedition(e);
 									KawaiponDAO.saveHero(h);
 
 									Main.getInfo().getConfirmationPending().remove(author.getId());
-									s.delete().flatMap(d -> channel.sendMessage("✅ | Treinado com sucesso!")).queue();
+									s.delete().flatMap(d -> channel.sendMessage("✅ | Herói enviado com sucesso!")).queue();
 								}), true, 1, TimeUnit.MINUTES,
 								u -> u.getId().equals(h.getUid()),
 								m -> Main.getInfo().getConfirmationPending().remove(author.getId())
@@ -97,20 +111,25 @@ public class EffectShopCommand implements Executable {
 		}
 
 
-		channel.sendMessageEmbeds(getEmbed(pool)).queue(s ->
+		channel.sendMessageEmbeds(getEmbed(h, pool)).queue(s ->
 				Pages.buttonize(s, buttons, true, 1, TimeUnit.MINUTES, u -> u.getId().equals(author.getId()))
 		);
 	}
 
-	private MessageEmbed getEmbed(List<Champion> pool) {
+	private MessageEmbed getEmbed(Hero h, List<Expedition> pool) {
 		EmbedBuilder eb = new ColorlessEmbedBuilder()
 				.setTitle("Mestres disponíveis")
 				.setDescription("Seu tier no Shoukan ranqueado afetará quais mestres estão dispostos a treinar seu herói. Rotaciona a cada dia.");
 
 		for (int i = 0; i < pool.size(); i++) {
-			Champion c = pool.get(i);
-			int cost = (c.getMana() + (c.isFusion() ? 5 : 0)) / 2;
-			eb.addField(Helper.getFancyNumber(i + 1) + " :droplet: " + cost + " | Mestre: " + c.getName(), c.getDescription(), false);
+			Expedition e = pool.get(i);
+			eb.addField(Helper.getFancyNumber(i + 1) + " | " + e
+					, "Dificuldade: %s (Sucesso: %s%%)\nDuração: %s".formatted(
+							e.getDifficulty(),
+							e.getSuccessChance(h),
+							Helper.toStringDuration(e.getTime())
+					), false
+			);
 		}
 
 		return eb.build();
