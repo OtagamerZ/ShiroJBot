07caa0e56868b0a43295130b327bba538248d1a5
Ygimenez <yagogimenez1@hotmail.com>
2021-10-29 17:05:15 -0300

    Correções menores

--- src/main/java/com/kuuhaku/command/commands/discord/hero/ExpeditionsCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/hero/ExpeditionsCommand.java
@@ -62,7 +62,7 @@ public class ExpeditionsCommand implements Executable {
 		if (h == null) {
 			channel.sendMessage("❌ | Você não possui ou não selecionou um herói.").queue();
 			return;
-		} else if (!h.hasArrived()) {
+		} else if (h.isInExpedition()) {
 			channel.sendMessage("❌ | Este herói está em uma expedição.").queue();
 			return;
 		}
