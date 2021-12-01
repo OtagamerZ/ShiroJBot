88071adb612d29dcf1c6ff8932338e04786c6968
ygimenez <yagogimenez1@hotmail.com>
2021-12-01 10:37:07 -0300

    Correções menores

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Champion.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Champion.java
@@ -709,9 +709,9 @@ public class Champion implements Drawable, Cloneable {
 
 	public boolean isDuelling() {
 		if (nemesis != null && nemesis.getIndex() == -1) {
-			nemesis = null;
 			if (index != -1)
 				onDuelEnd.accept(game.getSideById(acc.getUid()), game);
+			nemesis = null;
 			onDuelEnd = null;
 		}
 
