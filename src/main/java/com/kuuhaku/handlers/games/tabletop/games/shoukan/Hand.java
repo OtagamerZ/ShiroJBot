a146422240c731073860d1bb4f34bfff069a47cc
ygimenez <yagogimenez1@hotmail.com>
2021-12-10 14:25:02 -0300

    Adicionado deck de iniciante

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Hand.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Hand.java
@@ -116,7 +116,8 @@ public class Hand {
 						.map(Drawable::copy)
 						.collect(Collectors.toList())
 		);
-		if (hero != null && hero.getHp() > 0 && !hero.isUnavailable()) deque.add(hero.toChampion());
+		if (hero != null && hero.getHp() > 0 && hero.getQuest() == null)
+			deque.add(hero.toChampion());
 
 		int baseHp;
 		int baseManaPerTurn;
