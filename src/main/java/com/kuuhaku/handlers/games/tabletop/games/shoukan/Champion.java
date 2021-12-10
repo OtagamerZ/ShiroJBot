58f6490dd651433018cccc59d4c5babf5d051afc
ygimenez <yagogimenez1@hotmail.com>
2021-12-10 14:23:25 -0300

    Adicionado deck de iniciante

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Champion.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Champion.java
@@ -171,7 +171,7 @@ public class Champion implements Drawable, Cloneable {
 			g2d.setFont(Fonts.HAMMERSMITH_ONE.deriveFont(Font.PLAIN, 11));
 			Profile.drawStringMultiLineNO(g2d, c.getDescription(), 205, 9, 293);
 
-			if (isStunned() || isSleeping() || (hero != null && hero.getQuest() != null)) {
+			if (isStunned() || isSleeping() || (hero != null && hero.isUnavailable())) {
 				available = false;
 			}
 
