92ead8247af17629d2d18f928dfd05a0d9afc368
ygimenez <yagogimenez1@hotmail.com>
2022-01-11 18:59:36 -0300

    Correções menores

--- src/main/java/com/kuuhaku/model/records/SoloLobby.java
+++ src/main/java/com/kuuhaku/model/records/SoloLobby.java
@@ -4,10 +4,9 @@ import com.kuuhaku.model.persistent.MatchMakingRating;
 import net.dv8tion.jda.api.entities.TextChannel;
 
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 
-public record SoloLobby(MatchMakingRating mmr, TextChannel channel, AtomicInteger threshold, AtomicBoolean unlocked) {
+public record SoloLobby(MatchMakingRating mmr, TextChannel channel, AtomicBoolean unlocked) {
 	public SoloLobby(MatchMakingRating mmr, TextChannel channel) {
-		this(mmr, channel, new AtomicInteger(), new AtomicBoolean());
+		this(mmr, channel, new AtomicBoolean());
 	}
 }
