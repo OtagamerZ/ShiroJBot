092facc91c9784343805d550a82bb2e394c94595
ygimenez <yagogimenez1@hotmail.com>
2022-01-12 16:44:24 -0300

    Correções menores.

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/SlotColumn.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/SlotColumn.java
@@ -56,6 +56,9 @@ public class SlotColumn implements Cloneable {
 	}
 
 	public void setBottom(Equipment bottom) {
+		if (this.bottom != null) {
+			this.bottom.setIndex(-2);
+		}
 		if (bottom != null) {
 			bottom.setIndex(index);
 			bottom.bind(hand);
