7ab41b09bacefc49cac2c951790bae322969b670
ygimenez <yagogimenez1@hotmail.com>
2022-01-17 13:37:57 -0300

    Correções menores

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Hero.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/Hero.java
@@ -214,7 +214,7 @@ public class Hero implements Cloneable {
     }
 
     public int getXpToNext() {
-        return (int) Helper.getFibonacci(getLevel() + 1);
+        return (int) Helper.getFibonacci(getLevel() + 1) * 5;
     }
 
     public int getBonusPoints() {
