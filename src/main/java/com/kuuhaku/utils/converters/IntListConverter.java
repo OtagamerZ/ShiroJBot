3b84a22c422212bea52c26a5bcfee66f524ab80b
ygimenez <yagogimenez1@hotmail.com>
2022-02-14 10:42:08 -0300

    Correções menores

--- src/main/java/com/kuuhaku/utils/converters/IntListConverter.java
+++ src/main/java/com/kuuhaku/utils/converters/IntListConverter.java
@@ -5,6 +5,6 @@ import javax.persistence.Converter;
 @Converter
 public class IntListConverter extends ListConverter<Integer> {
     public IntListConverter() {
-        super(Integer.class, o -> ((Number) o).intValue());
+        super(Integer.class, o -> o == null ? null : ((Number) o).intValue());
     }
 }
