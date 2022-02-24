20fc1947e9fe28819dc005e98c99f15d5bc95967
ygimenez <yagogimenez1@hotmail.com>
2022-02-24 16:41:40 -0300

    Correções menores

--- src/main/java/com/kuuhaku/handlers/api/Application.java
+++ src/main/java/com/kuuhaku/handlers/api/Application.java
@@ -20,7 +20,11 @@ package com.kuuhaku.handlers.api;
 
 import com.kuuhaku.utils.Helper;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -34,5 +38,16 @@ public class Application implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**").allowedMethods("*");
+	}
+
+	@Override
+	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+		configurer.setDefaultTimeout(-1);
+		configurer.setTaskExecutor(asyncTaskExecutor());
+	}
+
+	@Bean
+	public AsyncTaskExecutor asyncTaskExecutor() {
+		return new SimpleAsyncTaskExecutor("async");
 	}
 }
