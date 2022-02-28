2bd74440f7ef585bd1781d532e8ad198d2daec96
ygimenez <yagogimenez1@hotmail.com>
2022-02-28 14:10:43 -0300

    Correções menores

--- src/main/java/com/kuuhaku/command/commands/discord/fun/FaceoffCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/fun/FaceoffCommand.java
@@ -38,7 +38,7 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Consumer;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Command(
 		name = "confronto",
@@ -66,11 +66,49 @@ public class FaceoffCommand implements Executable {
 			int max = Helper.rng(700 - (level * 100));
 			int time = min + max;
 			AtomicLong start = new AtomicLong(0);
+			AtomicBoolean win = new AtomicBoolean();
+			AtomicReference<ScheduledFuture<?>> timeout = new AtomicReference<>();
+
+			ShiroInfo.getShiroEvents().addHandler(guild, new SimpleMessageListener() {
+				@Override
+				public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
+					if (win.get()) {
+						if (!isClosed()) close();
+						return;
+					}
+
+					if (!event.getAuthor().getId().equals(author.getId()) || !event.getChannel().getId().equals(channel.getId()))
+						return;
+
+					if (start.get() == 0) {
+						channel.sendMessage("Você atirou antes da hora! Você perdeu.").queue();
+						close();
+						return;
+					}
+
+					String value = event.getMessage().getContentRaw();
+					if (value.equalsIgnoreCase("bang")) {
+						win.set(true);
+						close();
+						timeout.get().cancel(true);
+						timeout.set(null);
+
+						long react = Helper.clamp(System.currentTimeMillis() - start.get(), min, time);
+						int prize = (int) Math.round(100f * (level + 1) + (min * Helper.rng(250f * (level + 1)) / react));
+						channel.sendMessage("Você ganhou com um tempo de reação de **" + react + " ms**. Seu prêmio é de **" + prize + " CR**!").queue();
+
+						Account acc = AccountDAO.getAccount(author.getId());
+						acc.addCredit(prize, this.getClass());
+						AccountDAO.saveAccount(acc);
+
+						LeaderboardsDAO.submit(author, FaceoffCommand.class, (int) react);
+					}
+				}
+			});
 
 			RestAction<Message> rst = channel.sendMessage("Prepare-se, o duelo começará em 3 segundos (digite `bang` quando eu disser fogo)!")
 					.delay(3, TimeUnit.SECONDS)
 					.flatMap(s -> s.editMessage("Em suas marcas..."));
-
 
 			rst = rst.delay(Helper.rng(500, 3000), TimeUnit.MILLISECONDS);
 
@@ -86,50 +124,13 @@ public class FaceoffCommand implements Executable {
 
 			rst.queue(t -> {
 				start.set(System.currentTimeMillis());
-				ShiroInfo.getShiroEvents().addHandler(guild, new SimpleMessageListener() {
-					private final Consumer<Void> success = s -> close();
-					private final AtomicBoolean win = new AtomicBoolean();
-					private ScheduledFuture<?> timeout = Main.getInfo().getScheduler().schedule(() -> {
-						if (!win.get()) {
-							win.set(true);
-							success.accept(null);
-							channel.sendMessage(":gun: | BANG! Você perdeu.").complete();
-								}
-							}, time, TimeUnit.MILLISECONDS
-					);
-
-					@Override
-					public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
-						if (!event.getAuthor().getId().equals(author.getId()) || !event.getChannel().getId().equals(channel.getId()))
-							return;
-
-						String value = event.getMessage().getContentRaw();
-						if (!value.equalsIgnoreCase("bang")) {
-							channel.sendMessage("Você errou o gatilho.").queue();
-							success.accept(null);
-							timeout.cancel(true);
-							timeout = null;
-							return;
-						}
-
-						if (!win.get()) {
-							win.set(true);
-							success.accept(null);
-							timeout.cancel(true);
-							timeout = null;
-
-							long react = Helper.clamp(System.currentTimeMillis() - start.get(), min, time);
-							int prize = (int) Math.round(100f * (level + 1) + (min * Helper.rng(250f * (level + 1)) / react));
-							channel.sendMessage("Você ganhou com um tempo de reação de **" + react + " ms**. Seu prêmio é de **" + prize + " CR**!").queue();
-
-							Account acc = AccountDAO.getAccount(author.getId());
-							acc.addCredit(prize, this.getClass());
-							AccountDAO.saveAccount(acc);
-
-							LeaderboardsDAO.submit(author, FaceoffCommand.class, (int) react);
-						}
-					}
-				});
+				timeout.set(Main.getInfo().getScheduler().schedule(() -> {
+							if (!win.get()) {
+								win.set(true);
+								channel.sendMessage(":gun: | BANG! Você perdeu.").complete();
+							}
+						}, time, TimeUnit.MILLISECONDS
+				));
 			});
 		} catch (NumberFormatException e) {
 			channel.sendMessage("❌ | Dificuldade inválida, ela deve ser um valor entre 0 (fácil) e 3 (difícil).").queue();
