d02ee283e41689f90e7ba2591a7eafa3e2a96eff
ygimenez <yagogimenez1@hotmail.com>
2022-03-04 09:22:04 -0300

    Correções menores

--- src/main/java/com/kuuhaku/utils/ShiroInfo.java
+++ src/main/java/com/kuuhaku/utils/ShiroInfo.java
@@ -102,8 +102,7 @@ public class ShiroInfo {
 	);
 	private static final Map<String, SupportTier> supports = Map.of(
 			"656542716108472340", SupportTier.NORMAL, //Lazuli
-			"553244700258336825", SupportTier.SENIOR, //The Caos
-			"860121300110671902", SupportTier.NORMAL, //Hidekin
+			"553244700258336825", SupportTier.SENIOR, //Caos
 			"435229114132201493", SupportTier.NORMAL, //Megu
 			"774526344708620298", SupportTier.NORMAL  //Mask
 	);
