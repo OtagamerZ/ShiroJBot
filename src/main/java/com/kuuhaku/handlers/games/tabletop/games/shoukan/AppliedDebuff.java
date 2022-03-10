df021e2bb8a63e95ebf0cd8febb8c6a69df17619
ygimenez <yagogimenez1@hotmail.com>
2022-03-10 10:37:58 -0300

    Correções menores

--- src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/AppliedDebuff.java
+++ src/main/java/com/kuuhaku/handlers/games/tabletop/games/shoukan/AppliedDebuff.java
@@ -1,5 +1,7 @@
 package com.kuuhaku.handlers.games.tabletop.games.shoukan;
 
+import com.kuuhaku.controller.postgresql.DebuffDAO;
+
 import javax.persistence.*;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -10,8 +12,8 @@ import java.util.Objects;
 @Table(name = "applieddebuff")
 public class AppliedDebuff {
 	@Id
-	@ManyToOne(fetch = FetchType.EAGER)
-	private Debuff debuff;
+	@Column(columnDefinition = "VARCHAR(255) NOT NULL")
+	private String debuff;
 
 	@Column(columnDefinition = "TIMESTAMP")
 	private ZonedDateTime expiration = null;
@@ -20,16 +22,16 @@ public class AppliedDebuff {
 	}
 
 	public AppliedDebuff(Debuff debuff, long duration) {
-		this.debuff = debuff;
+		this.debuff = debuff.getId();
 		this.expiration = ZonedDateTime.now(ZoneId.of("GMT-3")).plus(duration, ChronoUnit.SECONDS);
 	}
 
 	public Debuff getDebuff() {
-		return debuff;
+		return DebuffDAO.getDebuff(debuff);
 	}
 
 	public void setDebuff(Debuff debuff) {
-		this.debuff = debuff;
+		this.debuff = debuff.getId();
 	}
 
 	public ZonedDateTime getExpiration() {
