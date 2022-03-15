af48bc274f38fa4171fb593c1faa85df111b1226
ygimenez <yagogimenez1@hotmail.com>
2022-03-15 15:51:26 -0300

    Correções menores

--- src/main/java/com/kuuhaku/command/commands/discord/tournament/DeclareWoCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/tournament/DeclareWoCommand.java
@@ -73,6 +73,7 @@ public class DeclareWoCommand implements Executable {
 								} catch (RuntimeException ignore) {
 								}
 							} else {
+								p.setWo(true);
 								t.setResult(p.getPhase(), p.getIndex() % 2 == 0 ? p.getIndex() + 1 : p.getIndex() - 1);
 							}
 
