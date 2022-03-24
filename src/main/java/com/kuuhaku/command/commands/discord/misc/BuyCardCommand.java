021e92c023d100754129ced8a0c9268a4bb7a26e
Ygimenez <yagogimenez1@hotmail.com>
2022-03-24 14:44:09 -0300

    Correções menores

--- src/main/java/com/kuuhaku/command/commands/discord/misc/BuyCardCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/misc/BuyCardCommand.java
@@ -289,15 +289,10 @@ public class BuyCardCommand implements Executable {
 
 				channel.sendMessage("✅ | Carta `" + name + "` comprada com sucesso!").queue();
 			} else {
+				Market finalM = m;
 				Main.getInfo().getConfirmationPending().put(author.getId(), true);
 				channel.sendMessage("Você está prestes a comprar a carta `" + name + "` por **" + Helper.separate(price) + " CR**, deseja confirmar?")
 						.queue(s -> Pages.buttonize(s, Map.of(Helper.parseEmoji(Helper.ACCEPT), wrapper -> {
-									Market finalM = MarketDAO.getCard(Integer.parseInt(args[0]));
-									if (finalM == null) {
-										channel.sendMessage("❌ | ID inválido ou a carta já foi comprada por alguém.").queue();
-										return;
-									}
-
 									Kawaipon kp = KawaiponDAO.getKawaipon(author.getId());
 									switch (finalM.getType()) {
 										case EVOGEAR -> {
@@ -328,8 +323,14 @@ public class BuyCardCommand implements Executable {
 									Main.getInfo().getConfirmationPending().remove(author.getId());
 									KawaiponDAO.saveKawaipon(kp);
 
-									finalM.setBuyer(author.getId());
-									MarketDAO.saveCard(finalM);
+									Market mkt = MarketDAO.getCard(Integer.parseInt(args[0]));
+									if (mkt == null) {
+										channel.sendMessage("❌ | ID inválido ou a carta já foi comprada por alguém.").queue();
+										return;
+									}
+
+									mkt.setBuyer(author.getId());
+									MarketDAO.saveCard(mkt);
 
 									seller.addCredit(price, this.getClass());
 									buyer.removeCredit(price, this.getClass());
