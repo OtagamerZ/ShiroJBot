aba661bc20a6f51a0e61549f0dd1e4480b0baa16
ygimenez <yagogimenez1@hotmail.com>
2022-03-31 13:28:29 -0300

    Alterações na busca de cartas.

--- src/main/java/com/kuuhaku/command/commands/discord/misc/CardStashCommand.java
+++ src/main/java/com/kuuhaku/command/commands/discord/misc/CardStashCommand.java
@@ -101,6 +101,8 @@ public class CardStashCommand implements Executable {
 								op.getDescription()
 						));
 					}
+
+					eb.setDescription(sb.toString());
 				}
 
 				for (Stash s : cards) {
