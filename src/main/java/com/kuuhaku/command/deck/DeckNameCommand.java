0cc62089e6e74bb4e2dca3fc7d1a5d7293913715
Ygimenez <yagogimenez1@hotmail.com>
2022-06-28 10:48:10 -0300

    Continuation

---
+++ src/main/java/com/kuuhaku/command/deck/DeckNameCommand.java
@@ -0,0 +1,56 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2022  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.deck;
+
+import com.kuuhaku.interfaces.Executable;
+import com.kuuhaku.interfaces.annotations.Command;
+import com.kuuhaku.interfaces.annotations.Requires;
+import com.kuuhaku.interfaces.annotations.Signature;
+import com.kuuhaku.model.enums.Category;
+import com.kuuhaku.model.enums.I18N;
+import com.kuuhaku.model.persistent.shoukan.Deck;
+import com.kuuhaku.model.records.EventData;
+import com.kuuhaku.model.records.MessageData;
+import com.kuuhaku.utils.json.JSONObject;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.Permission;
+
+@Command(
+		name = "deck",
+		subname = "name",
+		category = Category.INFO
+)
+@Signature("<name:word>")
+@Requires(Permission.MESSAGE_ATTACH_FILES)
+public class DeckNameCommand implements Executable {
+	@Override
+	public void execute(JDA bot, I18N locale, EventData data, MessageData.Guild event, JSONObject args) {
+		Deck d = data.profile().getAccount().getCurrentDeck();
+		if (d == null) {
+			event.channel().sendMessage(locale.get("error/no_deck")).queue();
+			return;
+		}
+
+		String name = args.getString("name").replace(" ", "_");
+		d.setName(name);
+		d.save();
+
+		event.channel().sendMessage(locale.get("success/deck_rename", d.getName())).queue();
+	}
+}
