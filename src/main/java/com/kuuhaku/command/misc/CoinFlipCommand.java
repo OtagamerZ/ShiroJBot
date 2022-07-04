c200ec5e7637dac93d6ca853bda87d917a2cc88c
Ygimenez <yagogimenez1@hotmail.com>
2022-07-04 09:54:49 -0300

    Continuation

---
+++ src/main/java/com/kuuhaku/command/misc/CoinFlipCommand.java
@@ -0,0 +1,41 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2022  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.misc;
+
+import com.kuuhaku.Constants;
+import com.kuuhaku.interfaces.Executable;
+import com.kuuhaku.interfaces.annotations.Command;
+import com.kuuhaku.model.enums.Category;
+import com.kuuhaku.model.enums.I18N;
+import com.kuuhaku.model.records.EventData;
+import com.kuuhaku.model.records.MessageData;
+import com.kuuhaku.util.json.JSONObject;
+import net.dv8tion.jda.api.JDA;
+
+@Command(
+		name = "calc",
+		category = Category.MISC
+)
+public class CoinFlipCommand implements Executable {
+	@Override
+	public void execute(JDA bot, I18N locale, EventData data, MessageData.Guild event, JSONObject args) {
+		boolean tails = Constants.DEFAULT_RNG.nextBoolean();
+		event.channel().sendMessage(locale.get("str/coinflip", tails ? locale.get("str/tails") : locale.get("str/heads"))).queue();
+	}
+}
