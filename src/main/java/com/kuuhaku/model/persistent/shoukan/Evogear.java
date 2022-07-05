f6d9dd80726282327692c6f441177252d549c053
Ygimenez <yagogimenez1@hotmail.com>
2022-07-05 13:40:28 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Evogear.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Evogear.java
@@ -291,8 +291,7 @@ public class Evogear extends DAO<Evogear> implements Drawable<Evogear>, EffectHo
 
 		g2d.setFont(new Font("Arial", Font.BOLD, 20));
 		g2d.setColor(deck.getFrame().getPrimaryColor());
-		String name = StringUtils.abbreviate(card.getName(), MAX_NAME_LENGTH);
-		Graph.drawOutlinedString(g2d, name, 208 - g2d.getFontMetrics().stringWidth(name), 30, 3, deck.getFrame().getBackgroundColor());
+		Graph.drawOutlinedString(g2d, StringUtils.abbreviate(card.getName(), MAX_NAME_LENGTH), 12, 30, 2, deck.getFrame().getBackgroundColor());
 
 		if (!getCharms().isEmpty()) {
 			List<BufferedImage> icons = charms.stream()
