fd7c0996ac1c6588e0b01704b30074b4586ab163
ygimenez <yagogimenez1@hotmail.com>
2022-07-05 11:28:08 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Senshi.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Senshi.java
@@ -400,7 +400,7 @@ public class Senshi extends DAO<Senshi> implements Drawable<Senshi>, EffectHolde
 			}
 			g2d.drawString(tags.stream().map(locale::get).map(String::toUpperCase).toList().toString(), 7, 275);
 
-			g2d.setFont(Fonts.HAMMERSMITH_ONE.deriveFont(Font.PLAIN, 10));
+			g2d.setFont(Fonts.HAMMERSMITH_ONE.deriveFont(Font.PLAIN, 11));
 			Graph.drawMultilineString(g2d,
 					StringUtils.abbreviate(desc, MAX_DESC_LENGTH), 7, 287, 211, 3,
 					parseValues(stats).andThen(s -> {
