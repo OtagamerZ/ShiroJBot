2473c681b64d61240742d6d39cf378c7200a7d87
yagog <Yago1234!!khk>
2022-07-10 19:22:00 -0300

    Correções menores

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Evogear.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Evogear.java
@@ -337,7 +337,7 @@ public class Evogear extends DAO<Evogear> implements Drawable<Evogear>, EffectHo
 					.toList();
 
 			if (!icons.isEmpty()) {
-				int y = desc.isBlank() ? 215 : 161;
+				int y = desc.isBlank() ? 238 : 184;
 				Graph.applyTransformed(g2d, 200 - 64, y, g -> {
 					if (icons.size() == 1) {
 						g.drawImage(icons.get(0), 0, 0, null);
