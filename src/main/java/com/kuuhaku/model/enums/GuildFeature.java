1f9d4c105defb05fdfd7952950dcfa31afe15dd1
ygimenez <yagogimenez1@hotmail.com>
2022-07-10 19:20:59 -0300

    Correções menores

--- src/main/java/com/kuuhaku/model/enums/GuildFeature.java
+++ src/main/java/com/kuuhaku/model/enums/GuildFeature.java
@@ -19,11 +19,12 @@
 package com.kuuhaku.model.enums;
 
 public enum GuildFeature {
-	ANTI_RAID,  //0x1
-	ANTI_LINK,  //0x2
-	ANTI_SPAM,  //0x3
-	ANTI_HOIST, //0x4
-	ANTI_ZALGO, //0x5
+	ANTI_RAID,     //0x1
+	ANTI_LINK,     //0x2
+	ANTI_SPAM,     //0x3
+	ANTI_HOIST,    //0x4
+	ANTI_ZALGO,    //0x5
 
-	NQN_MODE,   //0x6
+	NQN_MODE,      //0x6
+	NOTIFICATIONS, //0x7
 }
