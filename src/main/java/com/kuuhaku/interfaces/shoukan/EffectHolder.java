85782fa3fe109d111d8e2da5c71c981a201aa5fa
ygimenez <yagogimenez1@hotmail.com>
2022-07-11 16:12:43 -0300

    Continuation

--- src/main/java/com/kuuhaku/interfaces/shoukan/EffectHolder.java
+++ src/main/java/com/kuuhaku/interfaces/shoukan/EffectHolder.java
@@ -71,10 +71,10 @@ public interface EffectHolder {
 						case "dfs" -> g2d.setColor(new Color(0x00C500));
 						case "ddg" -> g2d.setColor(new Color(0xFFC800));
 						case "blk" -> g2d.setColor(new Color(0xA9A9A9));
-						case "sp" -> g2d.setColor(new Color(0xC58FFF));
+						case "sp" -> g2d.setColor(new Color(0x010101));
 					}
 
-					return val;
+					return val.replaceAll("\\{.+}", "");
 				} catch (Exception e) {
 					return StringUtils.abbreviate(s, Drawable.MAX_DESC_LENGTH);
 				}
