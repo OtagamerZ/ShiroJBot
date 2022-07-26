46e69b0215e22d52f931cb3ea33779e0b440a1dc
Ygimenez <yagogimenez1@hotmail.com>
2022-07-26 20:50:17 -0300

    Correções menores

---
+++ src/main/resources/database/triggers/prevent_selling_deck.sql
@@ -0,0 +1,38 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2022  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+CREATE OR REPLACE FUNCTION t_prevent_selling_deck()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF (NEW.deck_id IS NOT NULL AND NEW.price > 0) THEN
+        RAISE EXCEPTION 'Cannot have a card in deck and market at the same time';
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS prevent_selling_deck ON stashed_card;
+CREATE TRIGGER prevent_selling_deck
+    BEFORE UPDATE
+    ON stashed_card
+    FOR EACH ROW
+EXECUTE PROCEDURE t_prevent_selling_deck();
