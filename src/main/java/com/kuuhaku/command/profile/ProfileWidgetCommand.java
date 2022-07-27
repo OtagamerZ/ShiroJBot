8d40c36e4e134d05da217150c117a6ee80393ed7
Ygimenez <yagogimenez1@hotmail.com>
2022-07-27 20:53:10 -0300

    Correções menores

--- src/main/java/com/kuuhaku/command/profile/ProfileWidgetCommand.java
+++ src/main/java/com/kuuhaku/command/profile/ProfileWidgetCommand.java
@@ -42,7 +42,8 @@ import java.util.Locale;
 		category = Category.MISC
 )
 @Signature({
-		"<action:text:r>[add,remove] <id:number> <text:text:r>",
+		"<action:text:r>[add,remove] <text:text:r>",
+		"<action:text:r>[add,remove] <id:number:r> <text:text:r>",
 		"<action:text:r>[add,remove] <id:number:r>"
 })
 public class ProfileWidgetCommand implements Executable {
