89d6cdf426119968c993a4e1e812a03497616f41
Ygimenez <yagogimenez1@hotmail.com>
2022-07-28 16:12:32 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/user/Profile.java
+++ src/main/java/com/kuuhaku/model/persistent/user/Profile.java
@@ -244,7 +244,7 @@ public class Profile extends DAO<Profile> implements Blacklistable {
 					Shape desc = new RoundRectangle2D.Double(0, 0, w, h, 20, 20);
 					Graph.drawOutlined(g2, desc, 1, Color.BLACK);
 
-					g1.setColor(Color.WHITE);
+					g2.setColor(Color.WHITE);
 					Graph.drawMultilineString(g2, bio, 10, (int) (bounds.getHeight() + 10), w - 20, 3,
 							(s, px, py) -> Graph.drawOutlinedString(g2, s, px, py, 2, Color.BLACK)
 					);
