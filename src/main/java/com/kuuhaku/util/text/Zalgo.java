23ea179bc858936b45c09c16e062e1ae16b909cc
Ygimenez <yagogimenez1@hotmail.com>
2022-07-29 00:42:17 -0300

    Correções menores

--- src/main/java/com/kuuhaku/util/text/Zalgo.java
+++ src/main/java/com/kuuhaku/util/text/Zalgo.java
@@ -21,21 +21,32 @@ package com.kuuhaku.util.text;
 import com.kuuhaku.util.Calc;
 import com.kuuhaku.util.Utils;
 
-import java.util.function.Function;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 public class Zalgo {
 	public static final Zalgo INSTANCE = new Zalgo();
 
-	private static final Integer[] diacratic = Stream.of(
-					IntStream.rangeClosed(0x0300, 0x036F),
-					IntStream.rangeClosed(0x20D0, 0x20DF),
-					IntStream.of(0x20E5, 0x20E6)
-			)
-			.flatMapToInt(Function.identity())
-			.boxed()
-			.toArray(Integer[]::new);
+	private static final char[] diacUp = {
+			'\u030d', '\u030e', '\u0304', '\u0305', '\u033f', '\u0311', '\u0306', '\u0310',
+			'\u0352', '\u0357', '\u0351', '\u0307', '\u0308', '\u030a', '\u0342', '\u0343',
+			'\u0344', '\u034a', '\u034b', '\u034c', '\u0303', '\u0302', '\u030c', '\u0350',
+			'\u0300', '\u0301', '\u030b', '\u030f', '\u0312', '\u0313', '\u0314', '\u033d',
+			'\u0309', '\u0363', '\u0364', '\u0365', '\u0366', '\u0367', '\u0368', '\u0369',
+			'\u036a', '\u036b', '\u036c', '\u036d', '\u036e', '\u036f', '\u033e', '\u035b',
+			'\u0346', '\u031a'
+	};
+
+	private static final char[] diacMiddle = {
+			'\u0315', '\u031b', '\u0340', '\u0341', '\u0358', '\u0321', '\u0322', '\u0327',
+			'\u0328', '\u0334', '\u0335', '\u0336', '\u034f', '\u035c', '\u035d', '\u035e',
+			'\u035f', '\u0360', '\u0362', '\u0338', '\u0337', '\u0361', '\u0489'
+	};
+
+	private static final char[] diacDown = {
+			'\u0316', '\u0317', '\u0318', '\u0319', '\u031c', '\u031d', '\u031e', '\u031f',
+			'\u0320', '\u0324', '\u0325', '\u0326', '\u0329', '\u032a', '\u032b', '\u032c',
+			'\u032d', '\u032e', '\u032f', '\u0330', '\u0331', '\u0332', '\u0333', '\u0339',
+			'\u033a', '\u033b', '\u033c', '\u0345', '\u0347', '\u0348', '\u0349', '\u034d',
+			'\u034e', '\u0353', '\u0354', '\u0355', '\u0356', '\u0359', '\u035a', '\u0323'
+	};
 
 	private final int power;
 	private final double variation;
@@ -57,9 +68,19 @@ public class Zalgo {
 		for (char c : chars) {
 			sb.append(c);
 
-			int zc = ((int) Calc.rng(power * variation, power));
-			for (int i = 0; i < zc; i++) {
-				sb.append((char) Utils.getRandomEntry(diacratic).intValue());
+			int zu = ((int) Calc.rng(power * variation, power));
+			for (int i = 0; i < zu; i++) {
+				sb.append(Utils.getRandomEntry(diacUp));
+			}
+
+			int zm = ((int) Calc.rng(power * variation, power));
+			for (int i = 0; i < zm; i++) {
+				sb.append(Utils.getRandomEntry(diacMiddle));
+			}
+
+			int zd = ((int) Calc.rng(power * variation, power));
+			for (int i = 0; i < zd; i++) {
+				sb.append(Utils.getRandomEntry(diacDown));
 			}
 		}
 
