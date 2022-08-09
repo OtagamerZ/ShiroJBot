50eeae12f1be9644a6db04efe5362256ec2fcd6f
Ygimenez <yagogimenez1@hotmail.com>
2022-08-09 13:05:29 -0300

    Continuation

---
+++ src/main/java/com/kuuhaku/command/misc/MoneyCommand.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2022  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.misc;
+
+import com.expression.parser.exception.CalculatorException;
+import com.expression.parser.function.FunctionX;
+import com.kuuhaku.interfaces.Executable;
+import com.kuuhaku.interfaces.annotations.Command;
+import com.kuuhaku.interfaces.annotations.Signature;
+import com.kuuhaku.model.enums.Category;
+import com.kuuhaku.model.enums.I18N;
+import com.kuuhaku.model.records.EventData;
+import com.kuuhaku.model.records.MessageData;
+import com.kuuhaku.util.Utils;
+import com.kuuhaku.util.json.JSONObject;
+import net.dv8tion.jda.api.JDA;
+
+@Command(
+		name = "money",
+		category = Category.MISC
+)
+public class MoneyCommand implements Executable {
+	@Override
+	public void execute(JDA bot, I18N locale, EventData data, MessageData.Guild event, JSONObject args) {
+		data.profile().getAccount().addCR(100_000, "Testing feature");
+	}
+}
