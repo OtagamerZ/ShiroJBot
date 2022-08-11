5b0bed9bbba99060c532892e8415e6b2be026632
Ygimenez <yagogimenez1@hotmail.com>
2022-08-10 23:57:53 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Deck.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Deck.java
@@ -421,9 +421,10 @@ public class Deck extends DAO<Deck> {
 						if (str != null) {
 							g.setColor(new Color(0xD72929));
 							return str;
+						} else if (s.startsWith("\"")) {
+							g.setColor(Color.WHITE);
 						}
 
-						g.setColor(Color.WHITE);
 						return s;
 					}
 			);
