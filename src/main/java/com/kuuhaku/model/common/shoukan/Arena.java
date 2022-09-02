efa774e89256942b2ced031ade3140be7f92fd2e
Ygimenez <yagogimenez1@hotmail.com>
2022-09-02 09:35:48 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/common/shoukan/Arena.java
+++ src/main/java/com/kuuhaku/model/common/shoukan/Arena.java
@@ -285,19 +285,16 @@ public class Arena implements Renderer {
 			Graph.applyTransformed(g1, 0, SIZE.height, g2 -> {
 				int y = -MARGIN.y;
 
+				int prev = 0;
 				for (HistoryLog log : history) {
 					int h = g2.getFontMetrics().getHeight() * (int) Math.ceil(g1.getFontMetrics().stringWidth(log.message()) / (double) w);
-					if (y == Integer.MAX_VALUE) {
-						y = (int) (SIZE.height - (h * 2.25 * history.size())) + MARGIN.y;
-					}
+					y -= h * 1.25;
 
 					g1.setColor(game.getHands().get(log.side()).getUserDeck().getFrame().getThemeColor());
 					Graph.drawMultilineString(g1, log.message(),
-							MARGIN.x, y - MARGIN.y, w - MARGIN.x,
+							MARGIN.x, y, w - MARGIN.x,
 							(str, px, py) -> Graph.drawOutlinedString(g1, str, px, py, 6, Color.BLACK)
 					);
-
-					y -= h * 2.25;
 				}
 			});
 		});
