4824d4c19224bc3a7283e4aba9d1be42c0ff2e3c
Ygimenez <yagogimenez1@hotmail.com>
2022-09-20 15:22:51 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Deck.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Deck.java
@@ -358,7 +358,6 @@ public class Deck extends DAO<Deck> {
 						+ "\n\n- " + locale.get("minor/pureblood")
 						+ (ori.demon() ? "\n\n&- " + Race.DEMON.getMinor(locale) : "");
 			} else if (ori.minor().length > 1) {
-				g.drawImage(icons.get(2), 0, 0, 150, 150, null);
 				g.setFont(Fonts.OPEN_SANS_EXTRABOLD.deriveFont(Font.BOLD, 60));
 				g.setColor(Color.GRAY);
 
@@ -367,12 +366,12 @@ public class Deck extends DAO<Deck> {
 
 				g.setFont(Fonts.OPEN_SANS.deriveFont(Font.PLAIN, 36));
 				g.setColor(Color.WHITE);
-				effects = Arrays.stream(ori.minor())
+				effects = "- " + locale.get("minor/mixed")
+						+ "\n\n- " + Arrays.stream(ori.minor())
 						.filter(r -> r != Race.DEMON)
 						.map(o -> "- " + o.getMinor(locale))
 						.collect(Collectors.joining("\n\n"))
-						+ "\n\n- " + syn.getSynergy(locale)
-						+ (ori.demon() ? "\n\n&- " + Race.DEMON.getMinor(locale) : "");
+						+(ori.demon() ? "\n\n&- " + Race.DEMON.getMinor(locale) : "");
 			} else {
 				g.drawImage(icons.get(2), 0, 0, 150, 150, null);
 				g.setFont(Fonts.OPEN_SANS_EXTRABOLD.deriveFont(Font.BOLD, 60));
