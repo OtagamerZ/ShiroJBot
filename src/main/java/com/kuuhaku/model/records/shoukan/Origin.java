7260cb1b31a11df423072f96a2d798bae452c2b2
Ygimenez <yagogimenez1@hotmail.com>
2022-09-20 15:08:11 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/records/shoukan/Origin.java
+++ src/main/java/com/kuuhaku/model/records/shoukan/Origin.java
@@ -28,7 +28,7 @@ import java.util.List;
 
 public record Origin(Race major, Race... minor) {
 	public Race synergy() {
-		if (major == Race.NONE) return Race.NONE;
+		if (major == Race.NONE || minor.length == 0) return Race.NONE;
 
 		return major.fuse(minor[0]);
 	}
