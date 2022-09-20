a7762dcc8449d8dd4bec6c12ac24c5abbca1523a
Ygimenez <yagogimenez1@hotmail.com>
2022-09-20 11:53:33 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/common/shoukan/Arena.java
+++ src/main/java/com/kuuhaku/model/common/shoukan/Arena.java
@@ -536,7 +536,7 @@ public class Arena implements Renderer {
 					}
 			);
 
-			Graph.applyTransformed(g, reversed ? 1526 : 26, reversed ? 2449 : 168,
+			Graph.applyTransformed(g, reversed ? 1526 : 26, reversed ? 1869 /*2449*/ : 168,
 					g1 -> {
 						Origin ori = hand.getOrigin();
 
