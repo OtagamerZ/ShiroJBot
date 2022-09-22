b4f488ad4f842f6f06532397de37e4d13b1f3c1b
Ygimenez <yagogimenez1@hotmail.com>
2022-09-22 17:02:55 -0300

    Continue

--- src/main/java/com/kuuhaku/Constants.java
+++ src/main/java/com/kuuhaku/Constants.java
@@ -58,9 +58,9 @@ public abstract class Constants {
 	public static final String SERVER_ROOT = System.getenv("SERVER_URL");
 	public static final String API_ROOT = "https://api." + SERVER_ROOT + "/v2/";
 	public static final String SOCKET_ROOT = "wss://socket." + SERVER_ROOT + "/v2/";
-	public static final String ORIGIN_RESOURCES = System.getProperty("GIT_ORIGIN")
+	public static final String ORIGIN_RESOURCES = System.getenv("GIT_ORIGIN")
 			.replace("https://github.com/", "https://raw.githubusercontent.com/") +
-			"/" + System.getProperty("GIT_BRANCH") + "/src/main/resources/";
+			"/" + System.getenv("GIT_BRANCH") + "/src/main/resources/";
 
 	public static final Function<Object, String> LOADING = o -> "<a:loading:697879726630502401> | " + o;
 
