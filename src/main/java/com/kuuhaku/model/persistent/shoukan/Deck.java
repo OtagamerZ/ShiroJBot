230e566684021dbd3e2a0db6b8f4677581f3846b
Ygimenez <yagogimenez1@hotmail.com>
2022-09-22 17:35:49 -0300

    Continue

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Deck.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Deck.java
@@ -357,7 +357,7 @@ public class Deck extends DAO<Deck> {
 				effects = "- " + ori.major().getMajor(locale)
 						+ "\n\n- " + locale.get("minor/pureblood")
 						+ (ori.demon() ? "\n\n&- " + Race.DEMON.getMinor(locale) : "");
-			} else if (ori.minor().length > 1) {
+			} else if (ori.major() == Race.MIXED) {
 				g.setFont(Fonts.OPEN_SANS_EXTRABOLD.deriveFont(Font.BOLD, 60));
 				g.setColor(Graph.mix(Arrays.stream(ori.minor()).map(Race::getColor).toArray(Color[]::new)));
 
