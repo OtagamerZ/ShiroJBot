09d6f3ad9e7a4ff48fd01acf3ad022c9ef6ad507
Ygimenez <yagogimenez1@hotmail.com>
2022-09-23 10:59:19 -0300

    Continue

--- src/main/java/com/kuuhaku/command/info/KawaiponEvogearCommand.java
+++ src/main/java/com/kuuhaku/command/info/KawaiponEvogearCommand.java
@@ -29,45 +29,41 @@ import com.kuuhaku.interfaces.annotations.Signature;
 import com.kuuhaku.model.common.ColorlessEmbedBuilder;
 import com.kuuhaku.model.enums.Category;
 import com.kuuhaku.model.enums.I18N;
-import com.kuuhaku.model.enums.shoukan.Race;
 import com.kuuhaku.model.records.EventData;
 import com.kuuhaku.model.records.MessageData;
 import com.kuuhaku.util.Utils;
 import com.kuuhaku.util.json.JSONObject;
-import kotlin.Pair;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Command(
 		name = "kawaipon",
-		subname = "senshi",
+		subname = "evogear",
 		category = Category.INFO
 )
-@Signature("<race:word>")
+@Signature("<tier:number>")
 @Requires({
 		Permission.MESSAGE_EMBED_LINKS,
 		Permission.MESSAGE_ATTACH_FILES
 })
-public class KawaiponSenshiCommand implements Executable {
+public class KawaiponEvogearCommand implements Executable {
 	@Override
 	public void execute(JDA bot, I18N locale, EventData data, MessageData.Guild event, JSONObject args) {
-		if (!args.has("race")) {
-			int total = DAO.queryNative(Integer.class, "SELECT COUNT(1) FROM senshi");
+		if (!args.has("tier")) {
+			int total = DAO.queryNative(Integer.class, "SELECT COUNT(1) FROM evogear");
 
 			EmbedBuilder eb = new ColorlessEmbedBuilder()
-					.setAuthor(locale.get("str/available_cards", locale.get("type/senshi")))
-					.setTitle(locale.get("type/senshi"));
+					.setAuthor(locale.get("str/available_cards", locale.get("type/evogear")))
+					.setTitle(locale.get("type/evogear"));
 
 			List<Page> pages = new ArrayList<>();
 			int max = (int) Math.ceil(total / 50d);
 			for (int i = 1; i <= max; i++) {
-				eb.setImage((Constants.API_ROOT + "shoukan/%s/senshi?uid=%s&v=%s&page=%s").formatted(
+				eb.setImage((Constants.API_ROOT + "shoukan/%s/evogear?uid=%s&v=%s&page=%s").formatted(
 						locale, event.user().getId(), System.currentTimeMillis(), i
 				));
 				pages.add(new InteractPage(eb.build()));
@@ -77,39 +73,22 @@ public class KawaiponSenshiCommand implements Executable {
 			return;
 		}
 
-		Race race = args.getEnum(Race.class, "race");
-		if (race == null) {
-			Pair<String, Double> sug = Utils.didYouMean(args.getString("race"), Arrays.stream(Race.values()).map(Race::name).toList());
-			event.channel().sendMessage(locale.get("error/unknown_race", sug.getFirst())).queue();
-			return;
-		}
-
-		int total = race.getCount();
+		int tier = args.getInt("tier");
+		int total = DAO.queryNative(Integer.class, "SELECT COUNT(1) FROM evogear WHERE tier = ?1", tier);
 		if (total == 0) {
-			event.channel().sendMessage(locale.get("error/empty_race")).queue();
+			event.channel().sendMessage(locale.get("error/empty_tier")).queue();
 			return;
 		}
 
 		EmbedBuilder eb = new ColorlessEmbedBuilder()
-				.setAuthor(locale.get("str/available_cards", locale.get("type/senshi")))
-				.setThumbnail(Constants.ORIGIN_RESOURCES + "shoukan/race/full/" + race + ".png")
-				.setTitle(race.getName(locale) + " (`" + race.name() + "`)")
-				.setDescription(race.getDescription(locale));
-
-		if (Integer.bitCount(race.getFlag()) == 1) {
-			eb.addField(locale.get("str/sub_races"), Utils.properlyJoin(locale.get("str/and")).apply(Arrays.stream(race.derivates()).map(r -> r.getName(locale)).toList()), false)
-					.addField(locale.get("str/major_effect"), race.getMajor(locale), false)
-					.addField(locale.get("str/minor_effect"), race.getMinor(locale), false);
-		} else {
-			eb.addField(locale.get("str/origins"), Arrays.stream(race.split()).map(r -> r.getName(locale)).collect(Collectors.joining(" + ")), false)
-					.addField(locale.get("str/synergy_effect"), race.getSynergy(locale), false);
-		}
+				.setAuthor(locale.get("str/available_cards", locale.get("type/evogear")))
+				.setThumbnail(Constants.ORIGIN_RESOURCES + "shoukan/icons/tier_" + tier + "_full.png");
 
 		List<Page> pages = new ArrayList<>();
 		int max = (int) Math.ceil(total / 50d);
 		for (int i = 1; i <= max; i++) {
-			eb.setImage((Constants.API_ROOT + "shoukan/%s/senshi?race=%s&uid=%s&v=%s&page=%s").formatted(
-					locale, race.getFlag(), event.user().getId(), System.currentTimeMillis(), i
+			eb.setImage((Constants.API_ROOT + "shoukan/%s/evogar?tier=%s&uid=%s&v=%s&page=%s").formatted(
+					locale, tier, event.user().getId(), System.currentTimeMillis(), i
 			));
 			pages.add(new InteractPage(eb.build()));
 		}
