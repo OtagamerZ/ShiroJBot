dd0920ab9cb7342648f2657013200d5084144c29
Ygimenez <yagogimenez1@hotmail.com>
2022-09-29 14:23:20 -0300

    Continue

--- src/main/java/com/kuuhaku/game/Shoukan.java
+++ src/main/java/com/kuuhaku/game/Shoukan.java
@@ -1385,7 +1385,7 @@ public class Shoukan extends GameInstance<Phase> {
 	}
 
 	public String getString(String key, Object... params) {
-		LocalizedString str = DAO.find(LocalizedString.class, new LocalizedId(key, locale));
+		LocalizedString str = DAO.find(LocalizedString.class, new LocalizedId(key.toLowerCase(Locale.ROOT), locale));
 		if (str != null) {
 			return str.getValue().formatted(params);
 		} else {
