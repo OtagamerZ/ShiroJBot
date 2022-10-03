e2213556cd0d990a0c8bfd7f9c8164663ddcbc54
Ygimenez <yagogimenez1@hotmail.com>
2022-10-03 13:31:51 -0300

    Continue

--- src/main/java/com/kuuhaku/command/misc/GachaCommand.java
+++ src/main/java/com/kuuhaku/command/misc/GachaCommand.java
@@ -162,9 +162,9 @@ public class GachaCommand implements Executable {
 				case KAWAIPON -> {
 					KawaiponCard kc = new KawaiponCard(c, Calc.chance(0.1 * (1 - Spawn.getRarityMult())));
 
-					g2d.drawImage(c.drawCard(kc.isChrome()), 15, 45, null);
+					g2d.drawImage(c.drawCard(kc.isChrome()), 15, 35, null);
 					if (kc.isChrome()) {
-						g2d.drawImage(IO.getResourceAsImage("kawaipon/frames/" + hPath + "/destiny.png"), 0, 30, null);
+						g2d.drawImage(IO.getResourceAsImage("kawaipon/frames/" + hPath + "/destiny.png"), 0, 20, null);
 					}
 
 					kc.setKawaipon(kp);
@@ -173,9 +173,9 @@ public class GachaCommand implements Executable {
 				case EVOGEAR -> {
 					Evogear e = DAO.find(Evogear.class, card);
 
-					g2d.drawImage(e.render(locale, deck), 15, 45, null);
+					g2d.drawImage(e.render(locale, deck), 15, 35, null);
 					if (e.getTier() == 4) {
-						g2d.drawImage(IO.getResourceAsImage("kawaipon/frames/" + hPath + "/hero.png"), 0, 30, null);
+						g2d.drawImage(IO.getResourceAsImage("kawaipon/frames/" + hPath + "/hero.png"), 0, 20, null);
 					}
 
 					return new StashedCard(kp, c, tp);
@@ -183,7 +183,7 @@ public class GachaCommand implements Executable {
 				case FIELD -> {
 					Field f = DAO.find(Field.class, card);
 
-					g2d.drawImage(f.render(locale, deck), 15, 45, null);
+					g2d.drawImage(f.render(locale, deck), 15, 35, null);
 					return new StashedCard(kp, c, tp);
 				}
 			}
