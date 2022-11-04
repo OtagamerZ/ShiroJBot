a08d1745f10028accf53f36bd27acd83f7600fb6
Ygimenez <yagogimenez1@hotmail.com>
2022-11-04 09:59:39 -0300

    Continue

--- src/main/java/com/kuuhaku/model/common/shoukan/Hand.java
+++ src/main/java/com/kuuhaku/model/common/shoukan/Hand.java
@@ -107,6 +107,7 @@ public class Hand {
 			}
 
 			if (s.getStats().popFlag(Flag.NO_DEATH)) {
+				System.out.println("Defended");
 				return false;
 			} else if (ward != null) {
 				int charges = ward.getStats().getData().getInt("ward", 0) + 1;
