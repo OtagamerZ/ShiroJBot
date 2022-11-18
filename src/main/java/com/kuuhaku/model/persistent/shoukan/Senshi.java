2adfe427928ad09d3bad35e752a74b80e0dabb77
Ygimenez <yagogimenez1@hotmail.com>
2022-11-18 10:53:04 -0300

    Continue

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Senshi.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Senshi.java
@@ -1048,7 +1048,7 @@ public class Senshi extends DAO<Senshi> implements EffectHolder<Senshi> {
 
 					String str = locale.get("str/turns", time);
 
-					g1.setColor(Graph.getColor(overlay));
+					g1.setColor(Graph.getColor(overlay).brighter());
 					g1.setFont(Drawable.FONT.deriveFont(Drawable.FONT.getSize() * 1.5f));
 					Graph.drawOutlinedString(g1, str,
 							225 / 2 - g1.getFontMetrics().stringWidth(str) / 2, 350 - g1.getFontMetrics().getHeight(),
