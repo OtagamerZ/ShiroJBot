93e29e68f422667bcfba5c6079f29cf7c89ed7d5
Ygimenez <yagogimenez1@hotmail.com>
2022-12-13 11:32:10 -0300

    Continue

--- src/main/java/com/kuuhaku/model/common/shoukan/Hand.java
+++ src/main/java/com/kuuhaku/model/common/shoukan/Hand.java
@@ -149,6 +149,9 @@ public class Hand {
 		if (d instanceof Senshi s) {
 			if (s.getLastInteraction() != null) {
 				getGame().trigger(Trigger.ON_KILL, s.getLastInteraction().asSource(Trigger.ON_KILL), s.asTarget(Trigger.NONE));
+				if (s.popFlag(Flag.NO_DEATH)) {
+					return false;
+				}
 			}
 
 			if (!s.getEquipments().isEmpty()) {
