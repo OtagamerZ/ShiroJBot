1eeeeb60fb85d3ace0a4e98e484cee66a6a0aef0
Ygimenez <yagogimenez1@hotmail.com>
2023-01-20 15:44:06 -0300

    Continue

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Senshi.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Senshi.java
@@ -1238,7 +1238,7 @@ public class Senshi extends DAO<Senshi> implements EffectHolder<Senshi> {
 
 					FontMetrics fm = g1.getFontMetrics();
 					Graph.drawOutlinedString(g1, str,
-							225 / 2 - fm.stringWidth(str) / 2, overlay.getHeight() / 2 + fm.getHeight() / 2,
+							225 / 2 - fm.stringWidth(str) / 2, overlay.getHeight() / 2,
 							6, Color.BLACK
 					);
 					break;
