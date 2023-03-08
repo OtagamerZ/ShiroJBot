e5598b0a7a1fe82f634b88a2097b9ad7dda95468
Ygimenez <yagogimenez1@hotmail.com>
2023-03-08 14:18:30 -0300

    Atualização para versão 5 do JDA

--- src/main/java/com/kuuhaku/game/Shoukan.java
+++ src/main/java/com/kuuhaku/game/Shoukan.java
@@ -1715,6 +1715,7 @@ public class Shoukan extends GameInstance<Phase> {
 			s.execute(true, new EffectParameters(trigger, side, s.asSource(trigger)));
 		}
 
+		System.out.println(getTurn() + ": " + trigger + " -> " + side);
 		for (EffectHolder<?> leech : hands.get(side).getLeeches()) {
 			leech.execute(new EffectParameters(ON_LEECH, side, leech.asSource(trigger)));
 		}
