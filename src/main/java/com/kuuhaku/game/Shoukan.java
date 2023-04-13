2790ecdaf016ee6381bf2c25cb3d7d06905f1ec0
Ygimenez <yagogimenez1@hotmail.com>
2023-04-13 14:02:22 -0300

    Continuation

--- src/main/java/com/kuuhaku/game/Shoukan.java
+++ src/main/java/com/kuuhaku/game/Shoukan.java
@@ -1886,6 +1886,7 @@ public class Shoukan extends GameInstance<Phase> {
 			for (SlotColumn slt : slts) {
 				Senshi s = slt.getTop();
 				if (s != null) {
+					s.unlock();
 					s.setLastInteraction(null);
 					s.getStats().removeExpired(AttrMod::isExpired);
 					for (Evogear e : s.getEquipments()) {
@@ -1895,6 +1896,7 @@ public class Shoukan extends GameInstance<Phase> {
 
 				s = slt.getBottom();
 				if (s != null) {
+					s.unlock();
 					s.setLastInteraction(null);
 					s.getStats().removeExpired(AttrMod::isExpired);
 				}
