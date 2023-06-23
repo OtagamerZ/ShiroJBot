ad4da10ff6e42a8e18384e86c7eb326f3732b673
Ygimenez <yagogimenez1@hotmail.com>
2023-06-23 16:09:09 -0300

    Continuation

--- src/main/java/com/kuuhaku/command/misc/SelectTitleCommand.java
+++ src/main/java/com/kuuhaku/command/misc/SelectTitleCommand.java
@@ -65,10 +65,7 @@ public class SelectTitleCommand implements Executable {
 					.collect(Collectors.groupingBy(t -> Utils.getOr(Utils.extract(t.getId(), ".+(?=_(?:I|II|III|IV|V))|.+"), "")))
 					.values().stream()
 					.map(ts -> ts.stream()
-							.sorted(Comparator
-									.<Title, Boolean>comparing(t -> acc.hasTitle(t.getId()), Boolean::compareTo)
-									.thenComparing(t -> t.getRarity().ordinal(), Comparator.reverseOrder())
-							)
+							.sorted(Comparator.comparing(t -> t.getRarity().ordinal(), Comparator.reverseOrder()))
 							.collect(ArrayList<Title>::new, (lst, t) -> {
 								if (acc.hasTitle(t.getId())) {
 									if (lst.isEmpty()) {
