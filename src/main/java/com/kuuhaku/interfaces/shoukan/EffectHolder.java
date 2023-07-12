e900c9d98efc2a9e7ffc5aacefea646ce6ed9307
Ygimenez <yagogimenez1@hotmail.com>
2023-07-12 15:10:28 -0300

    Continuation

--- src/main/java/com/kuuhaku/interfaces/shoukan/EffectHolder.java
+++ src/main/java/com/kuuhaku/interfaces/shoukan/EffectHolder.java
@@ -149,7 +149,7 @@ public interface EffectHolder<T extends Drawable<T>> extends Drawable<T> {
 							if (e.getSecond() != null) {
 								colors.add(e.getSecond());
 								if (!Utils.equalsAny(type, "data", "b", "n")) {
-									val += ":" + StringUtils.leftPad(String.valueOf(e.getFirst()), 2, '0') + ":";
+									val += "@" + StringUtils.leftPad(String.valueOf(e.getFirst()), 2, '0');
 								}
 							}
 						}
@@ -204,9 +204,9 @@ public interface EffectHolder<T extends Drawable<T>> extends Drawable<T> {
 				int offset = 0;
 				FontMetrics fm = g2d.getFontMetrics();
 
-				for (String s : str.split(" ")) {
+				for (String s : str.split("@")) {
 					if (s.startsWith(":") && s.endsWith(":")) {
-						String path = "shoukan/icons/" + ICONS[Integer.parseInt(s.substring(1, 3))];
+						String path = "shoukan/icons/" + ICONS[Integer.parseInt(s.substring(1))];
 
 						BufferedImage icon = IO.getResourceAsImage(path);
 						if (icon != null) {
