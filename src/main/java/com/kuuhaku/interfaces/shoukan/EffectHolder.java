c0f02b4a067b4263fdacd31f2186051290cea25e
Ygimenez <yagogimenez1@hotmail.com>
2023-07-12 15:00:46 -0300

    Continuation

--- src/main/java/com/kuuhaku/interfaces/shoukan/EffectHolder.java
+++ src/main/java/com/kuuhaku/interfaces/shoukan/EffectHolder.java
@@ -149,7 +149,7 @@ public interface EffectHolder<T extends Drawable<T>> extends Drawable<T> {
 							if (e.getSecond() != null) {
 								colors.add(e.getSecond());
 								if (!Utils.equalsAny(type, "data", "b", "n")) {
-									val += ":" + StringUtils.leftPad(String.valueOf(e.getFirst()), 2, '0') + ": ";
+									val += "§" + StringUtils.leftPad(String.valueOf(e.getFirst()), 2, '0') + " ";
 								}
 							}
 						}
@@ -201,10 +201,11 @@ public interface EffectHolder<T extends Drawable<T>> extends Drawable<T> {
 			} else if (str.endsWith(Constants.VOID)) {
 				Graph.drawOutlinedString(g2d, str, x, y, 0.125f, g2d.getColor());
 			} else {
-				if (str.startsWith(":") && str.endsWith(": ")) {
+				if (str.startsWith("§")) {
 					String path = "shoukan/icons/" + ICONS[Integer.parseInt(str.substring(1, 3))];
 
 					BufferedImage icon = IO.getResourceAsImage(path);
+					System.out.println(icon);
 					if (icon != null) {
 						int size = g2d.getFont().getSize();
 						g2d.drawImage(icon, x, y - size + 1, size, size, null);
