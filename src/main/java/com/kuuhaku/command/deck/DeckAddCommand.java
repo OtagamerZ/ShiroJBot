1e8d0e3725a9cd9c4d4e3d9c3fbe71e725eadfb8
Ygimenez <yagogimenez1@hotmail.com>
2023-08-04 09:27:20 -0300

    Continuation

--- src/main/java/com/kuuhaku/command/deck/DeckAddCommand.java
+++ src/main/java/com/kuuhaku/command/deck/DeckAddCommand.java
@@ -147,9 +147,6 @@ public class DeckAddCommand implements Executable {
 				} else if (d.getSenshiRaw().size() >= 36) {
 					event.channel().sendMessage(locale.get("error/deck_full")).queue();
 					return false;
-				} else if (d.countCard(s) >= d.getMaxSenshiCopies()) {
-					event.channel().sendMessage(locale.get("error/card_copy_limit")).queue();
-					return false;
 				}
 			}
 			case EVOGEAR -> {
@@ -160,9 +157,6 @@ public class DeckAddCommand implements Executable {
 					return false;
 				} else if (d.getEvogearRaw().size() >= 24) {
 					event.channel().sendMessage(locale.get("error/deck_full")).queue();
-					return false;
-				} else if (d.countCard(e) >= d.getMaxEvogearCopies(e.getTier())) {
-					event.channel().sendMessage(locale.get("error/card_copy_limit")).queue();
 					return false;
 				}
 			}
