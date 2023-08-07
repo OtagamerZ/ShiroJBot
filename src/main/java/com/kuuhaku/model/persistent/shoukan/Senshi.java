a8313642705f6cb33d3b0ffccd6c4e7d4672933f
Ygimenez <yagogimenez1@hotmail.com>
2023-08-07 10:36:49 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Senshi.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Senshi.java
@@ -1046,8 +1046,6 @@ public class Senshi extends DAO<Senshi> implements EffectHolder<Senshi> {
 			targeted = true;
 		}
 
-		System.out.println(trigger);
-		System.out.println(ep);
 		if (trigger == ON_ACTIVATE && (getCooldown() > 0 || isSupporting())) return false;
 
 		Shoukan game = getGame();
