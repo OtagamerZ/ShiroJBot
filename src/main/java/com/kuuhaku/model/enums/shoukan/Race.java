9d3fbc37da491f291f1ebd346334db9834bb93a6
Ygimenez <yagogimenez1@hotmail.com>
2023-08-07 14:04:22 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/enums/shoukan/Race.java
+++ src/main/java/com/kuuhaku/model/enums/shoukan/Race.java
@@ -83,7 +83,7 @@ public enum Race {
 //		ALIEN(BEAST.flag | SPIRIT.flag),
 //		SLIME(BEAST.flag | UNDEAD.flag),
 //		DRAGON(BEAST.flag | MYSTICAL.flag),
-//		HELLHOUND(BEAST.flag | DEMON.flag),
+//	//	FIEND(BEAST.flag | DEMON.flag),
 //	//	EX_MACHINA(MACHINE.flag | DIVINITY.flag),
 //		GEIST(MACHINE.flag | SPIRIT.flag),
 //	//	VIRUS(MACHINE.flag | UNDEAD.flag),
