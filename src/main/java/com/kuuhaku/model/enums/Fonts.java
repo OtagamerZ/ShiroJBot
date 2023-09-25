2705b024b1f23cd1452dcba4b94f6c0e1a4250bd
Ygimenez <yagogimenez1@hotmail.com>
2023-09-25 15:43:06 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/enums/Fonts.java
+++ src/main/java/com/kuuhaku/model/enums/Fonts.java
@@ -42,12 +42,12 @@ public enum Fonts {
 		Font temp;
 
 		if (path.isBlank()) {
-			temp = new Font(Font.SANS_SERIF, Font.PLAIN, 11);
+			temp = new Font(Font.SANS_SERIF, Font.BOLD, 11);
 		} else {
 			try (InputStream is = IO.getResourceAsStream(path)) {
 				temp = Font.createFont(Font.TRUETYPE_FONT, is);
 			} catch (FontFormatException | IOException e) {
-				temp = new Font(Font.SANS_SERIF, Font.PLAIN, 11);
+				temp = new Font(Font.SANS_SERIF, Font.BOLD, 11);
 			}
 		}
 
