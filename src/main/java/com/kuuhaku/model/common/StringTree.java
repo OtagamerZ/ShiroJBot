c5a96c850766c4ba48a807430806672cd025d18f
Ygimenez <yagogimenez1@hotmail.com>
2023-09-29 10:47:36 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/common/StringTree.java
+++ src/main/java/com/kuuhaku/model/common/StringTree.java
@@ -54,11 +54,11 @@ public class StringTree {
 
     public static class Root extends TreeNode {
         @Override
-        public void print(XStringBuilder buffer, int level, boolean hasNext) {
+        public void print(XStringBuilder buffer, int level, boolean parentHasNext, boolean hasNext) {
             Iterator<TreeNode> iterator = children.values().iterator();
             while (iterator.hasNext()) {
                 TreeNode child = iterator.next();
-                child.print(buffer, 0, iterator.hasNext());
+                child.print(buffer, 0, hasNext, iterator.hasNext());
             }
         }
     }
@@ -75,11 +75,11 @@ public class StringTree {
         }
 
         @Override
-        public void print(XStringBuilder buffer, int level, boolean hasNext) {
+        public void print(XStringBuilder buffer, int level, boolean parentHasNext, boolean hasNext) {
             buffer.nextLine();
             for (int i = 0; i < level; i++) {
                 if (i < level - 1) {
-                    buffer.append(hasNext ? "  │ " : "    ");
+                    buffer.append(parentHasNext ? "  │ " : "    ");
                 } else {
                     buffer.append(hasNext ? "  ├─" : "  └─");
                 }
@@ -89,7 +89,7 @@ public class StringTree {
             Iterator<TreeNode> iterator = children.values().iterator();
             while (iterator.hasNext()) {
                 TreeNode child = iterator.next();
-                child.print(buffer, level + 1, iterator.hasNext());
+                child.print(buffer, level + 1, hasNext, iterator.hasNext());
             }
         }
     }
