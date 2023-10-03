6dc384aa160b8c3056f8f7080a014709a3739e5a
Ygimenez <yagogimenez1@hotmail.com>
2023-10-03 15:27:21 -0300

    Continuation

--- src/main/java/com/kuuhaku/controller/Manager.java
+++ src/main/java/com/kuuhaku/controller/Manager.java
@@ -34,7 +34,7 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.Semaphore;
 import java.util.stream.Stream;
 
 public abstract class Manager {
@@ -42,46 +42,53 @@ public abstract class Manager {
 	private static final String DB_NAME = System.getenv("DB_NAME");
 	private static final String DB_LOGIN = System.getenv("DB_LOGIN");
 	private static final String DB_PASS = System.getenv("DB_PASS");
+	private static final Semaphore lock = new Semaphore(1);
 
-	private static AtomicReference<EntityManagerFactory> emf = new AtomicReference<>();
+	private static EntityManagerFactory emf;
 
 	public static EntityManagerFactory getEntityManagerFactory() {
-		if (emf.get() == null) synchronized (Manager.class) {
-			emf.set(Persistence.createEntityManagerFactory("main", Map.of(
-					"jakarta.persistence.jdbc.user", DB_LOGIN,
-					"jakarta.persistence.jdbc.password", DB_PASS,
-					"jakarta.persistence.jdbc.url", "jdbc:postgresql://%s/%s?sslmode=require&useEncoding=true&characterEncoding=UTF-8".formatted(
-							SERVER_IP, DB_NAME
-					)
-			)));
-			Constants.LOGGER.info("Connected to database successfully");
+		try {
+			lock.acquire();
 
-			File initDir = IO.getResourceAsFile("database");
-			if (initDir != null && initDir.isDirectory()) {
-				Set<String> scripts = new HashSet<>();
-				try (Stream<Path> ioStream = Files.walk(initDir.toPath())) {
-					ioStream.filter(p -> Files.isRegularFile(p) && p.toString().endsWith(".sql"))
-							.sorted(Comparator.comparing(Path::getNameCount))
-							.peek(s -> scripts.add(FilenameUtils.removeExtension(s.getFileName().toString())))
-							.map(IO::readString)
-							.forEach(DAO::applyNative);
-				} catch (IOException e) {
-					throw new RuntimeException(e);
+			if (emf == null) synchronized (Manager.class) {
+				emf = Persistence.createEntityManagerFactory("main", Map.of(
+						"jakarta.persistence.jdbc.user", DB_LOGIN,
+						"jakarta.persistence.jdbc.password", DB_PASS,
+						"jakarta.persistence.jdbc.url", "jdbc:postgresql://%s/%s?sslmode=require&useEncoding=true&characterEncoding=UTF-8".formatted(
+								SERVER_IP, DB_NAME
+						)
+				));
+				Constants.LOGGER.info("Connected to database successfully");
+
+				File initDir = IO.getResourceAsFile("database");
+				if (initDir != null && initDir.isDirectory()) {
+					Set<String> scripts = new HashSet<>();
+					try (Stream<Path> ioStream = Files.walk(initDir.toPath())) {
+						ioStream.filter(p -> Files.isRegularFile(p) && p.toString().endsWith(".sql"))
+								.sorted(Comparator.comparing(Path::getNameCount))
+								.peek(s -> scripts.add(FilenameUtils.removeExtension(s.getFileName().toString())))
+								.map(IO::readString)
+								.forEach(DAO::applyNative);
+					} catch (IOException e) {
+						throw new RuntimeException(e);
+					}
+
+					Constants.LOGGER.info("Applied " + scripts.size() + " scripts: " + scripts);
 				}
 
-				Constants.LOGGER.info("Applied " + scripts.size() + " scripts: " + scripts);
+				GlobalProperty ver = DAO.find(GlobalProperty.class, "build_number");
+				if (ver == null) {
+					ver = new GlobalProperty("build_number", "0");
+				}
+
+				ver.setValue(Integer.parseInt(ver.getValue()) + 1);
+				ver.save();
 			}
 
-			GlobalProperty ver = DAO.find(GlobalProperty.class, "build_number");
-			if (ver == null) {
-				ver = new GlobalProperty("build_number", "0");
-			}
-
-			ver.setValue(Integer.parseInt(ver.getValue()) + 1);
-			ver.save();
+			return emf;
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
 		}
-
-		return emf.get();
 	}
 
 	public static EntityManager getEntityManager() {
