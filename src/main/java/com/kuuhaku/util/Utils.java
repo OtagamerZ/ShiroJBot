10c4eff8fa86ecd81e5d1a1348280c5911448829
Ygimenez <yagogimenez1@hotmail.com>
2024-05-14 15:33:41 -0300

    Continuation

--- src/main/java/com/kuuhaku/util/Utils.java
+++ src/main/java/com/kuuhaku/util/Utils.java
@@ -804,7 +804,7 @@ public abstract class Utils {
 		return DAO.queryNative(String.class, """
 				SELECT x."value"
 				FROM (
-				     SELECT x."value", levenshtein_less_equal(id, '%1$s', 5) AS dist
+				     SELECT x."value", levenshtein_less_equal(x."value", '%1$s', 5) AS dist
 				     FROM (%2$s) x
 				     ) x
 				WHERE x.dist <= 5
