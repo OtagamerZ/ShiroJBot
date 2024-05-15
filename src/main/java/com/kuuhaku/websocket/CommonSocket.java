faa18a197d5053e88bee16a75644cc463d0f0cdd
Ygimenez <yagogimenez1@hotmail.com>
2024-05-15 11:03:52 -0300

    Continuation

--- src/main/java/com/kuuhaku/websocket/CommonSocket.java
+++ src/main/java/com/kuuhaku/websocket/CommonSocket.java
@@ -100,7 +100,7 @@ public class CommonSocket extends WebSocketClient {
 
 				send(JSONObject.of(
 						Map.entry("type", "ATTACH"),
-						Map.entry("channels", List.of("eval", "shoukan", "i18n"))
+						Map.entry("channels", List.of("shoukan", "i18n", "invite"))
 				).toString());
 				return;
 			}
