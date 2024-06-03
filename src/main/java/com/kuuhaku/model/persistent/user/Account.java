ce94ddb821168e789a2c3df10b70e6ee922b5f9d
Ygimenez <yagogimenez1@hotmail.com>
2024-06-03 18:42:40 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/user/Account.java
+++ src/main/java/com/kuuhaku/model/persistent/user/Account.java
@@ -306,7 +306,7 @@ public class Account extends DAO<Account> implements AutoMake<Account>, Blacklis
 	}
 
 	public Profile getProfile(Member member) {
-		return DAO.find(Profile.class, new ProfileId(member.getGuild().getId(), member.getId()));
+		return DAO.find(Profile.class, new ProfileId(member.getId(), member.getGuild().getId()));
 	}
 
 	public AccountSettings getSettings() {
