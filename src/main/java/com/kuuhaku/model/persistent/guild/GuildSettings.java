538266125840bb04648d7c3c1df7d7d71e165b7e
Ygimenez <yagogimenez1@hotmail.com>
2024-06-04 16:25:39 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/guild/GuildSettings.java
+++ src/main/java/com/kuuhaku/model/persistent/guild/GuildSettings.java
@@ -51,17 +51,17 @@ public class GuildSettings extends DAO<GuildSettings> {
 	@Column(name = "gid", nullable = false)
 	private String gid;
 
-	@ElementCollection
+	@ElementCollection(fetch = FetchType.EAGER)
 	@Column(name = "kawaipon_channels")
 	@CollectionTable(name = "guild_settings_kawaiponChannels", joinColumns = @JoinColumn(name = "gid"))
 	private Set<GuildMessageChannel> kawaiponChannels = new HashSet<>();
 
-	@ElementCollection
+	@ElementCollection(fetch = FetchType.EAGER)
 	@Column(name = "drop_channels")
 	@CollectionTable(name = "guild_settings_dropChannels", joinColumns = @JoinColumn(name = "gid"))
 	private Set<GuildMessageChannel> dropChannels = new HashSet<>();
 
-	@ElementCollection
+	@ElementCollection(fetch = FetchType.EAGER)
 	@Column(name = "denied_channels")
 	@CollectionTable(name = "guild_settings_deniedChannels", joinColumns = @JoinColumn(name = "gid"))
 	private Set<GuildMessageChannel> deniedChannels = new HashSet<>();
@@ -100,12 +100,12 @@ public class GuildSettings extends DAO<GuildSettings> {
 	@OrderBy("threshold")
 	private final Set<AutoRule> autoRules = new HashSet<>();
 
-	@ElementCollection
+	@ElementCollection(fetch = FetchType.EAGER)
 	@Column(name = "disabled_categories")
 	@CollectionTable(name = "guild_settings_disabledcategories", joinColumns = @JoinColumn(name = "gid"))
 	private Set<Category> disabledCategories = new HashSet<>();
 
-	@ElementCollection
+	@ElementCollection(fetch = FetchType.EAGER)
 	@Column(name = "disabled_commands")
 	@CollectionTable(name = "guild_settings_disabledcommands", joinColumns = @JoinColumn(name = "gid"))
 	private Set<String> disabledCommands = new HashSet<>();
@@ -127,7 +127,7 @@ public class GuildSettings extends DAO<GuildSettings> {
 	private JSONObject aliases = new JSONObject();
 
 	@Enumerated(EnumType.STRING)
-	@ElementCollection
+	@ElementCollection(fetch = FetchType.EAGER)
 	@CollectionTable(
 			name = "automod_entries",
 			joinColumns = @JoinColumn(name = "gid", referencedColumnName = "gid")
