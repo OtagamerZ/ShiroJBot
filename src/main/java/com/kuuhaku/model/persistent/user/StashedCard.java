bd4aa0b1048f70e9ccc837fb242bc4e6a3b14213
Ygimenez <yagogimenez1@hotmail.com>
2024-06-07 17:43:50 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/user/StashedCard.java
+++ src/main/java/com/kuuhaku/model/persistent/user/StashedCard.java
@@ -89,13 +89,13 @@ public class StashedCard extends DAO<StashedCard> {
 		this.card = card.getCard();
 		this.type = CardType.KAWAIPON;
 		this.kawaipon = kawaipon;
-		this.details = DAO.find(CardDetails.class, uuid);
+		this.details = Utils.getOr(DAO.find(CardDetails.class, uuid), new CardDetails());
 	}
 
 	public StashedCard(Kawaipon kawaipon, Drawable<?> card) {
 		this.card = card.getCard();
 		this.kawaipon = kawaipon;
-		this.details = DAO.find(CardDetails.class, uuid);
+		this.details = Utils.getOr(DAO.find(CardDetails.class, uuid), new CardDetails());
 
 		if (card instanceof Senshi) {
 			this.type = CardType.KAWAIPON;
