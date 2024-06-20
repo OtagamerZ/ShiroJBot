afeaeb7fc4a64c1906f1e655d92ba930fb8230ca
Ygimenez <yagogimenez1@hotmail.com>
2024-06-20 11:36:34 -0300

    Continuation

---
+++ src/main/java/com/kuuhaku/command/misc/TransferCrCommand.java
@@ -0,0 +1,79 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2023  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+package com.kuuhaku.command.misc;
+
+import com.kuuhaku.exceptions.PendingConfirmationException;
+import com.kuuhaku.interfaces.Executable;
+import com.kuuhaku.interfaces.annotations.Command;
+import com.kuuhaku.interfaces.annotations.Requires;
+import com.kuuhaku.interfaces.annotations.Signature;
+import com.kuuhaku.model.enums.Category;
+import com.kuuhaku.model.enums.Currency;
+import com.kuuhaku.model.enums.I18N;
+import com.kuuhaku.model.persistent.user.Account;
+import com.kuuhaku.model.records.EventData;
+import com.kuuhaku.model.records.MessageData;
+import com.kuuhaku.util.Utils;
+import com.ygimenez.json.JSONObject;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.User;
+
+@Command(
+		name = "transfer",
+		path = "cr",
+		category = Category.MISC
+)
+@Signature("<user:user:r> <value:number:r>")
+@Requires(Permission.MESSAGE_EMBED_LINKS)
+public class TransferCrCommand implements Executable {
+	@Override
+	public void execute(JDA bot, I18N locale, EventData data, MessageData.Guild event, JSONObject args) {
+		User target = event.users(0);
+		if (target == null) {
+			event.channel().sendMessage(locale.get("error/invalid_mention")).queue();
+			return;
+		} else if (target.equals(event.user())) {
+			event.channel().sendMessage(locale.get("error/self_not_allowed")).queue();
+			return;
+		}
+
+		Account acc = data.profile().getAccount();
+
+		int value = args.getInt("value");
+		if (value <= 0) {
+			event.channel().sendMessage(locale.get("error/invalid_value_low", 0)).queue();
+			return;
+		} else if (!acc.hasEnough(value, Currency.CR)) {
+			event.channel().sendMessage(locale.get("error/insufficient_cr")).queue();
+			return;
+		}
+
+		try {
+			Utils.confirm(locale.get("question/transfer", Utils.separate(value) + " ₵R", target.getName()), event.channel(), w -> {
+						acc.transfer(value, target.getId());
+						event.channel().sendMessage(locale.get("success/transfer")).queue();
+						return true;
+					}, event.user()
+			);
+		} catch (PendingConfirmationException e) {
+			event.channel().sendMessage(locale.get("error/pending_confirmation")).queue();
+		}
+	}
+}
