e406e12ef9e9ac255ca0d650cf1cf4659b104313
Ygimenez <yagogimenez1@hotmail.com>
2024-06-26 10:23:29 -0300

    Continuation

--- src/main/java/com/kuuhaku/command/misc/SelectTitleCommand.java
+++ src/main/java/com/kuuhaku/command/misc/SelectTitleCommand.java
@@ -68,14 +68,16 @@ public class SelectTitleCommand implements Executable {
 					.map(ts -> ts.stream()
 							.sorted(Comparator.comparing(t -> t.getRarity().ordinal()))
 							.collect(ArrayList<Title>::new, (lst, t) -> {
-								if (acc.hasTitle(t.getId())) {
-									if (lst.isEmpty()) {
-										lst.add(t);
-									} else {
-										lst.set(0, t);
+								boolean has = acc.hasTitle(t.getId());
+
+								if (!has || lst.isEmpty()) {
+									if (!has && Utils.equalsAny(t.getId(), "VETERAN", "TEST_DUMMY")) {
+										return;
 									}
-								} else {
+
 									lst.add(t);
+								} else {
+									lst.set(0, t);
 								}
 							}, ArrayList::addAll)
 					)
