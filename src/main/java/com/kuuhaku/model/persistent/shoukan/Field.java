b088401c5e95d5554354ac47d317c769cc955677
Ygimenez <yagogimenez1@hotmail.com>
2024-06-28 13:17:58 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/shoukan/Field.java
+++ src/main/java/com/kuuhaku/model/persistent/shoukan/Field.java
@@ -322,7 +322,7 @@ public class Field extends DAO<Field> implements Drawable<Field> {
 
 				List<Pair<Race, Double>> mods = modifiers.entrySet().stream()
 						.map(e -> new Pair<>(Race.valueOf(e.getKey()), ((Number) e.getValue()).doubleValue()))
-						.sorted(Comparator.comparing(p -> -p.getSecond()))
+						.sorted(Comparator.comparing(Pair::getSecond))
 						.toList();
 
 				int i = 0;
