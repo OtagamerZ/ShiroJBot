98c747d4ee0ca51e030fafc91a237d6b4a24b667
Ygimenez <yagogimenez1@hotmail.com>
2024-07-15 13:15:50 -0300

    Continuation

--- src/main/java/com/kuuhaku/util/SignatureParser.java
+++ src/main/java/com/kuuhaku/util/SignatureParser.java
@@ -33,264 +33,241 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class SignatureParser {
-    private static final Pattern ARGUMENT_PATTERN = Pattern.compile("^<(?<name>[a-z]\\w*):(?<type>[a-z]+)(?<required>:[r])?>(?:\\[(?<options>[^\\[\\]]+)+])?$", Pattern.CASE_INSENSITIVE);
+	private static final Pattern ARGUMENT_PATTERN = Pattern.compile("^<(?<name>[a-z]\\w*):(?<type>[a-z]+)(?<required>:[r])?>(?:\\[(?<options>[^\\[\\]]+)+])?$", Pattern.CASE_INSENSITIVE);
 
-    public static JSONObject parse(I18N locale, Executable command, String input) throws InvalidSignatureException {
-        Signature annot = command.getClass().getDeclaredAnnotation(Signature.class);
-        if (annot == null) return new JSONObject();
+	public static JSONObject parse(I18N locale, Executable command, String input) throws InvalidSignatureException {
+		Signature annot = command.getClass().getDeclaredAnnotation(Signature.class);
+		if (annot == null) return new JSONObject();
 
-        return parse(locale, annot.value(), annot.patterns(), annot.allowEmpty(), input);
-    }
+		return parse(locale, annot.value(), annot.patterns(), annot.allowEmpty(), input);
+	}
 
-    public static JSONObject parse(I18N locale, String[] signatures, SigPattern[] patterns, boolean allowEmpty, String input) throws InvalidSignatureException {
-        JSONObject out = new JSONObject();
-        List<FailedSignature> failed = new ArrayList<>();
+	public static JSONObject parse(I18N locale, String[] signatures, SigPattern[] patterns, boolean allowEmpty, String input) throws InvalidSignatureException {
+		JSONObject out = new JSONObject();
+		List<FailedSignature> failed = new ArrayList<>();
 
-        List<String> supplied = new ArrayList<>();
-        for (String sig : signatures) {
-            String str = input;
-            String[] args = sig.split(" +");
-            String[] failOpts = {};
+		List<String> supplied = new ArrayList<>();
+		for (String sig : signatures) {
+			List<String> args = new ArrayList<>(List.of(input.split(" +")));
+			String[] params = sig.split(" +");
+			String[] failOpts = {};
 
-            int i = 0;
-            int matches = 0;
-            boolean fail = false;
-            for (String arg : args) {
-                i++;
-                JSONObject groups = Utils.extractNamedGroups(arg, ARGUMENT_PATTERN);
-                if (groups.isEmpty()) {
-                    String s = str.split("\\s+")[0].trim();
-                    str = str.replaceFirst(Pattern.quote(s), "").trim();
-                    s = StringUtils.stripAccents(s);
+			int i = 0;
+			int matches = 0;
+			boolean fail = false;
+			for (String param : params) {
+				i++;
+				JSONObject groups = Utils.extractNamedGroups(param, ARGUMENT_PATTERN);
+				if (groups.isEmpty()) {
+					String s = args.removeFirst();
+					s = StringUtils.stripAccents(s);
 
-                    if (s.equalsIgnoreCase(arg)) {
-                        supplied.add(s);
-                        matches++;
-                    } else {
-                        fail = true;
-                        supplied.add(arg);
-                    }
-                    continue;
-                }
+					if (s.equalsIgnoreCase(param)) {
+						supplied.add(s);
+						matches++;
+					} else {
+						fail = true;
+						supplied.add(param);
+					}
+					continue;
+				}
 
-                String name = groups.getString("name");
-                boolean required = groups.has("required");
-                String wrap = required ? "[%s]" : "%s";
+				String name = groups.getString("name");
+				boolean required = groups.has("required");
+				String wrap = required ? "[%s]" : "%s";
 
-                if (str.isBlank() && required) {
-                    fail = true;
-                    supplied.add(wrap.formatted("> " + locale.get("signature/" + name) + " <"));
-                    continue;
-                }
+				if (args.isEmpty() && required) {
+					fail = true;
+					supplied.add(wrap.formatted("> " + locale.get("signature/" + name) + " <"));
+					continue;
+				}
 
-                Signature.Type type = groups.getEnum(Signature.Type.class, "type");
-                if (type == Signature.Type.TEXT) {
-                    if (i == args.length) {
-                        if (out.has(name)) {
-                            JSONArray arr;
-                            if (out.get(name) instanceof List<?> ls) {
-                                arr = new JSONArray(ls);
-                            } else {
-                                arr = new JSONArray();
-                                Object curr = out.get(name);
-                                arr.add(curr);
-                            }
-                            arr.add(str);
+				Signature.Type type = groups.getEnum(Signature.Type.class, "type");
+				if (type == Signature.Type.TEXT) {
+					if (out.has(name)) {
+						JSONArray arr;
+						if (out.get(name) instanceof List<?> ls) {
+							arr = new JSONArray(ls);
+						} else {
+							arr = new JSONArray();
+							Object curr = out.get(name);
+							arr.add(curr);
+						}
+						arr.add(String.join(" ", args));
 
-                            out.put(name, arr);
-                        } else {
-                            out.put(name, str);
-                        }
+						out.put(name, arr);
+					} else {
+						out.put(name, String.join(" ", args));
+					}
 
-                        str = "";
-                    } else {
-                        if (out.has(name)) {
-                            JSONArray arr;
-                            if (out.get(name) instanceof List<?> ls) {
-                                arr = new JSONArray(ls);
-                            } else {
-                                arr = new JSONArray();
-                                Object curr = out.get(name);
-                                arr.add(curr);
-                            }
-                            arr.add(Utils.extract(str, type.getPattern(), 1));
+					args.clear();
+					matches++;
+				} else {
+					String token = null;
+					List<String> opts = List.of();
 
-                            out.put(name, arr);
-                        } else {
-                            out.put(name, Utils.extract(str, type.getPattern(), 1));
-                        }
+					if (!fail) {
+						String arg = args.getFirst();
 
-                        str = str.replaceFirst(type.getRegex(), "").trim();
-                    }
+						if (type == Signature.Type.CUSTOM) {
+							@Language("RegExp") String opt = groups.getString("options", "");
+							@Language("RegExp") String pattern = opt;
+							if (patterns != null) {
+								pattern = "^" + Arrays.stream(patterns).parallel()
+										.filter(p -> p.id().equals(opt))
+										.map(SigPattern::value)
+										.findAny().orElse(opt);
+							}
 
-                    matches++;
-                } else {
-                    String token = null;
-                    List<String> opts = List.of();
+							if (Utils.match(arg, pattern)) {
+								token = arg;
+							}
+						} else {
+							opts = Arrays.stream(groups.getString("options", "").split(","))
+									.filter(s -> !s.isBlank())
+									.map(String::toLowerCase)
+									.toList();
 
-                    if (!fail) {
-                        if (type == Signature.Type.CUSTOM) {
-                            @Language("RegExp") String opt = groups.getString("options", "");
-                            @Language("RegExp") String pattern = opt;
-                            if (patterns != null) {
-                                pattern = "^" + Arrays.stream(patterns).parallel()
-                                        .filter(p -> p.id().equals(opt))
-                                        .map(SigPattern::value)
-                                        .findAny().orElse(opt);
-                            }
+							if (Utils.match(arg, type.getPattern())) {
+								token = arg;
+								if (!opts.isEmpty() && !opts.contains(token.toLowerCase())) {
+									token = null;
+								}
+							}
+						}
+					}
 
-                            Matcher match = Utils.regex(str, pattern);
-                            if (match.find()) {
-                                token = match.group();
-                            }
-                        } else {
-                            opts = Arrays.stream(groups.getString("options", "").split(","))
-                                    .filter(s -> !s.isBlank())
-                                    .map(String::toLowerCase)
-                                    .toList();
+					if (token != null) {
+						args.removeFirst();
+						token = StringUtils.stripAccents(token);
 
-                            Matcher match = Utils.regex(str, type.getPattern());
-                            if (match.find()) {
-                                token = match.group();
-                                if (!opts.isEmpty() && !opts.contains(token.toLowerCase())) {
-                                    token = null;
-                                }
-                            }
-                        }
-                    }
+						if (type.validate(token)) {
+							switch (type) {
+								case CHANNEL -> token = token.replaceAll("[<#>]", "");
+								case USER, ROLE -> token = token.replaceAll("[<@!>]", "");
+							}
 
-                    if (token != null) {
-                        str = str.replaceFirst(Pattern.quote(token), "").trim();
-                        token = StringUtils.stripAccents(token);
+							if (out.has(name)) {
+								JSONArray arr;
+								if (out.get(name) instanceof List<?> ls) {
+									arr = new JSONArray(ls);
+								} else {
+									arr = new JSONArray();
+									Object curr = out.get(name);
+									arr.add(curr);
+								}
+								arr.add(token);
 
-                        if (type.validate(token)) {
-                            switch (type) {
-                                case CHANNEL -> token = token.replaceAll("[<#>]", "");
-                                case USER, ROLE -> token = token.replaceAll("[<@!>]", "");
-                            }
+								out.put(name, arr);
+							} else {
+								out.put(name, token);
+							}
 
-                            if (out.has(name)) {
-                                JSONArray arr;
-                                if (out.get(name) instanceof List<?> ls) {
-                                    arr = new JSONArray(ls);
-                                } else {
-                                    arr = new JSONArray();
-                                    Object curr = out.get(name);
-                                    arr.add(curr);
-                                }
-                                arr.add(token);
+							supplied.add(token);
+							matches++;
+						}
+					}
 
-                                out.put(name, arr);
-                            } else {
-                                out.put(name, token);
-                            }
+					if (token == null && required) {
+						fail = true;
 
-                            supplied.add(token);
-                            matches++;
-                        }
-                    }
+						if (opts.isEmpty()) {
+							supplied.add(wrap.formatted("> " + locale.get("signature/" + name) + " <"));
+						} else {
+							supplied.add(wrap.formatted(String.join("|", opts)));
+							failOpts = opts.stream().map(o -> "`" + o + "`").toArray(String[]::new);
+						}
+					}
+				}
+			}
 
-                    if (token == null && required) {
-                        fail = true;
+			if (fail) {
+				out.clear();
+				failed.add(new FailedSignature(String.join(" ", supplied), failOpts, matches, params.length));
+				supplied.clear();
+			} else {
+				return out;
+			}
+		}
 
-                        if (opts.isEmpty()) {
-                            supplied.add(wrap.formatted("> " + locale.get("signature/" + name) + " <"));
-                        } else {
-                            supplied.add(wrap.formatted(String.join("|", opts)));
-                            failOpts = opts.stream().map(o -> "`" + o + "`").toArray(String[]::new);
-                        }
-                    }
-                }
-            }
+		if (allowEmpty) return new JSONObject();
+		else {
+			int argLength = input.split(" +").length;
+			FailedSignature first = failed.stream().max(
+					Comparator.comparingInt(FailedSignature::matches)
+							.thenComparing(fs -> fs.numArgs() - argLength, Comparator.reverseOrder())
+			).orElseThrow();
+			throw new InvalidSignatureException(first.line(), first.options());
+		}
+	}
 
-            if (fail) {
-                out.clear();
-                failed.add(new FailedSignature(String.join(" ", supplied), failOpts, matches, args.length));
-                supplied.clear();
-            } else {
-                return out;
-            }
-        }
+	public static List<String> extract(I18N locale, Executable command) {
+		Signature annot = command.getClass().getDeclaredAnnotation(Signature.class);
+		if (annot == null) return List.of();
 
-        if (allowEmpty) return new JSONObject();
-        else {
-            int argLength = input.split(" +").length;
-            FailedSignature first = failed.stream().max(
-                    Comparator.comparingInt(FailedSignature::matches)
-                            .thenComparing(fs -> fs.numArgs() - argLength, Comparator.reverseOrder())
-            ).orElseThrow();
-            throw new InvalidSignatureException(first.line(), first.options());
-        }
-    }
+		return extract(locale, annot.value(), annot.allowEmpty());
+	}
 
-    public static List<String> extract(I18N locale, Executable command) {
-        Signature annot = command.getClass().getDeclaredAnnotation(Signature.class);
-        if (annot == null) return List.of();
+	public static List<String> extract(I18N locale, String[] signatures, boolean allowEmpty) {
+		List<String> out = new ArrayList<>();
+		if (signatures == null) return List.of("%1$s%2$s");
 
-        return extract(locale, annot.value(), annot.allowEmpty());
-    }
+		if (allowEmpty) {
+			out.add("%1$s%2$s");
+		} else {
+			for (String sig : signatures) {
+				if (!Utils.regex(sig, ":r>").find()) {
+					out.add("%1$s%2$s");
+					break;
+				}
+			}
+		}
 
-    public static List<String> extract(I18N locale, String[] signatures, boolean allowEmpty) {
-        List<String> out = new ArrayList<>();
-        if (signatures == null) return List.of("%1$s%2$s");
+		List<String> supplied = new ArrayList<>();
+		for (String sig : signatures) {
+			String[] args = sig.split(" +");
 
-        if (allowEmpty) {
-            out.add("%1$s%2$s");
-        } else {
-            for (String sig : signatures) {
-                if (!Utils.regex(sig, ":r>").find()) {
-                    out.add("%1$s%2$s");
-                    break;
-                }
-            }
-        }
+			supplied.add("%1$s%2$s");
+			for (String arg : args) {
+				JSONObject groups = Utils.extractNamedGroups(arg, ARGUMENT_PATTERN);
+				if (groups.isEmpty()) {
+					supplied.add(arg);
+					continue;
+				}
 
-        List<String> supplied = new ArrayList<>();
-        for (String sig : signatures) {
-            String[] args = sig.split(" +");
+				String name = groups.getString("name");
+				String type = groups.getString("type");
+				String opt = groups.getString("options");
+				boolean required = groups.has("required");
+				String wrap = "%s";
 
-            supplied.add("%1$s%2$s");
-            for (String arg : args) {
-                JSONObject groups = Utils.extractNamedGroups(arg, ARGUMENT_PATTERN);
-                if (groups.isEmpty()) {
-                    supplied.add(arg);
-                    continue;
-                }
+				if (!opt.isBlank()) {
+					List<String> opts = Arrays.stream(groups.getString("options", "").split(","))
+							.filter(s -> !s.isBlank())
+							.map(String::toLowerCase)
+							.toList();
 
-                String name = groups.getString("name");
-                String type = groups.getString("type");
-                String opt = groups.getString("options");
-                boolean required = groups.has("required");
-                String wrap = "%s";
+					wrap = String.join("|", opts);
+				}
 
-                if (!opt.isBlank()) {
-                    List<String> opts = Arrays.stream(groups.getString("options", "").split(","))
-                            .filter(s -> !s.isBlank())
-                            .map(String::toLowerCase)
-                            .toList();
+				if (type.equalsIgnoreCase(Signature.Type.TEXT.name()) && supplied.size() < args.length - 1) {
+					wrap = "\"" + wrap + "\"";
+				}
 
-                    wrap = String.join("|", opts);
-                }
+				if (required) {
+					wrap = "[" + wrap + "]";
+				}
 
-                if (type.equalsIgnoreCase(Signature.Type.TEXT.name()) && supplied.size() < args.length - 1) {
-                    wrap = "\"" + wrap + "\"";
-                }
+				supplied.add(wrap.formatted(locale.get("signature/" + name)));
+			}
 
-                if (required) {
-                    wrap = "[" + wrap + "]";
-                }
+			out.add(String.join(" ", supplied));
+			supplied.clear();
+		}
 
-                supplied.add(wrap.formatted(locale.get("signature/" + name)));
-            }
-
-            out.add(String.join(" ", supplied));
-            supplied.clear();
-        }
-
-        return out;
-    }
+		return out;
+	}
 }
