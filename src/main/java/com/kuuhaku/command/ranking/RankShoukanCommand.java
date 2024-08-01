2ebb3fbce1cd01182daa4b317d7d3df58538de79
Ygimenez <yagogimenez1@hotmail.com>
2024-08-01 14:49:24 -0300

    Continuation

--- src/main/java/com/kuuhaku/command/ranking/RankShoukanCommand.java
+++ src/main/java/com/kuuhaku/command/ranking/RankShoukanCommand.java
@@ -72,7 +72,7 @@ public class RankShoukanCommand implements Executable {
 				case 1 -> "\uD83E\uDD48";
 				case 2 -> "\uD83E\uDD49";
 				default -> i + 1;
-			} + " - " + e.name() + "`〽 " + e.score() + "`");
+			} + " - " + e.name() + "`🔺" + e.score() + "`");
 
 			if (e.uid().equals(event.user().getId())) {
 				eb.appendDescription("__");
