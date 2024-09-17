f97d407fa835c772cd1bb2d1655d291088eedef8
Ygimenez <yagogimenez1@hotmail.com>
2024-09-17 09:54:20 -0300

    Continuation

---
+++ src/main/resources/database/triggers/editable_desc_i18n.sql
@@ -0,0 +1,93 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2023  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+CREATE OR REPLACE FUNCTION t_insert_desc_i18n()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF (NEW.desc_pt IS NOT NULL) THEN
+        INSERT INTO card_descriptions (id, locale, description) VALUES (NEW.id, 'PT', NEW.desc_pt);
+    END IF;
+
+    IF (NEW.desc_en IS NOT NULL) THEN
+        INSERT INTO card_descriptions (id, locale, description) VALUES (NEW.id, 'EN', NEW.desc_en);
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS insert_desc_i18n ON v_desc_i18n;
+CREATE TRIGGER insert_desc_i18n
+    INSTEAD OF INSERT
+    ON v_desc_i18n
+    FOR EACH ROW
+EXECUTE PROCEDURE t_insert_desc_i18n();
+
+CREATE OR REPLACE FUNCTION t_update_desc_i18n()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    IF (NEW.desc_pt IS NOT NULL) THEN
+        INSERT INTO card_descriptions (id, locale, description)
+        VALUES (NEW.id, 'PT', NEW.desc_pt)
+        ON CONFLICT (id, locale) DO UPDATE SET description = excluded.description;
+    ELSE
+        DELETE FROM card_descriptions WHERE id = NEW.id AND locale = 'PT';
+    END IF;
+
+    IF (NEW.desc_en IS NOT NULL) THEN
+        INSERT INTO card_descriptions (id, locale, description)
+        VALUES (NEW.id, 'EN', NEW.desc_en)
+        ON CONFLICT (id, locale) DO UPDATE SET description = excluded.description;
+    ELSE
+        DELETE FROM card_descriptions WHERE id = NEW.id AND locale = 'EN';
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS update_desc_i18n ON v_desc_i18n;
+CREATE TRIGGER update_desc_i18n
+    INSTEAD OF UPDATE
+    ON v_desc_i18n
+    FOR EACH ROW
+EXECUTE PROCEDURE t_update_desc_i18n();
+
+CREATE OR REPLACE FUNCTION t_delete_desc_i18n()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    DELETE FROM card_descriptions WHERE id = OLD.id;
+    RETURN NEW;
+END
+$$;
+
+DROP TRIGGER IF EXISTS delete_desc_i18n ON v_desc_i18n;
+CREATE TRIGGER delete_desc_i18n
+    INSTEAD OF DELETE
+    ON v_desc_i18n
+    FOR EACH ROW
+EXECUTE PROCEDURE t_delete_desc_i18n();
---
+++ src/main/resources/database/views/desc_i18n.sql
@@ -0,0 +1,27 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2023  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+-- DROP VIEW IF EXISTS v_desc_i18n;
+CREATE OR REPLACE VIEW v_desc_i18n AS
+SELECT coalesce(pt.id, en.id) AS id
+     , pt.description AS desc_pt
+     , en.description AS desc_en
+FROM card_descriptions pt
+         FULL JOIN card_descriptions en ON en.id = pt.id AND en.locale = 'EN'
+WHERE pt.locale = 'PT'
+ORDER BY id
