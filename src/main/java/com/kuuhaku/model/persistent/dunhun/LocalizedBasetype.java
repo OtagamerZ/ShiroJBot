d3bdb4d731a97bfb7a78f55990f76c2c5df47f68
Ygimenez <yagogimenez1@hotmail.com>
2024-09-18 14:41:25 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/dunhun/LocalizedBasetype.java
+++ src/main/java/com/kuuhaku/model/persistent/dunhun/LocalizedBasetype.java
@@ -32,7 +32,7 @@ import java.util.Objects;
 @Entity
 @Cacheable
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
-@Table(name = "gear_basetype_info")
+@Table(name = "basetype_info")
 public class LocalizedBasetype extends DAO<LocalizedBasetype> implements Serializable {
 	@EmbeddedId
 	private LocalizedId id;
