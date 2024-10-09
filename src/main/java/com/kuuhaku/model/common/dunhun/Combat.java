ebc527b0a01fb2336db2c8322f8de3d05f19177d
Ygimenez <yagogimenez1@hotmail.com>
2024-10-09 19:57:54 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/common/dunhun/Combat.java
+++ src/main/java/com/kuuhaku/model/common/dunhun/Combat.java
@@ -128,7 +128,7 @@ public class Combat implements Renderer<BufferedImage> {
 
 			if (!divided) {
 				BufferedImage cbIcon = IO.getResourceAsImage("dunhun/icons/combat.png");
-				g2d.drawImage(cbIcon, offset, 153, null);
+				g2d.drawImage(cbIcon, offset, bi.getHeight() / 2 - cbIcon.getHeight() / 2, null);
 				offset += 64;
 				divided = true;
 			}
