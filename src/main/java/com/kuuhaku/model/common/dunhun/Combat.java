edcd51a065687b4d557f6e8acf5626ad87635c6e
Ygimenez <yagogimenez1@hotmail.com>
2024-10-10 19:29:43 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/common/dunhun/Combat.java
+++ src/main/java/com/kuuhaku/model/common/dunhun/Combat.java
@@ -429,8 +429,6 @@ public class Combat implements Renderer<BufferedImage> {
 						boolean used = false;
 						if (!skills.isEmpty() && (forcing || Calc.chance(33) || !(canAttack || (canDefend && curr.getAp() == 1)))) {
 							Skill skill = Utils.getRandomEntry(skills);
-							System.out.println(skill.getId());
-
 							List<Actor> tgts = skill.getTargets(this, curr).stream()
 									.filter(a -> a != null && !a.isSkipped())
 									.toList();
