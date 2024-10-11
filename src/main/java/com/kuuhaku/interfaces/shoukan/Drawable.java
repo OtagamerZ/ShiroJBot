4348ddfd954433a065bc8c084a069f301b997d6a
Ygimenez <yagogimenez1@hotmail.com>
2024-10-11 19:29:48 -0300

    Continuation

--- src/main/java/com/kuuhaku/interfaces/shoukan/Drawable.java
+++ src/main/java/com/kuuhaku/interfaces/shoukan/Drawable.java
@@ -101,6 +101,7 @@ public interface Drawable<T extends Drawable<T>> {
 
 	default Shoukan getGame() {
 		if (getHand() == null) return null;
+
 		return getHand().getGame();
 	}
 
@@ -111,6 +112,8 @@ public interface Drawable<T extends Drawable<T>> {
 	}
 
 	default int getIndex() {
+		if (getHand() == null) return -1;
+
 		return getSlot().getIndex();
 	}
 
