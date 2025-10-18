eda33c387bc6aef883e74427a53a4933311f51fd
Ygimenez <yagogimenez1@hotmail.com>
2025-10-18 17:39:58 -0300

    Continuation

--- src/main/java/com/kuuhaku/command/misc/CollectCommand.java
+++ src/main/java/com/kuuhaku/command/misc/CollectCommand.java
@@ -49,9 +49,6 @@ public class CollectCommand implements Executable {
 			if (!card.isValid()) {
 				event.channel().sendMessage(locale.get("error/no_card")).queue();
 				return;
-			} else if (card.peekProperty(kp::hasCard)) {
-				event.channel().sendMessage(locale.get("error/owned")).queue();
-				return;
 			} else if (card.peekProperty(kc -> !acc.hasEnough(kc.getPrice(), Currency.CR))) {
 				event.channel().sendMessage(locale.get("error/insufficient_cr")).queue();
 				return;
