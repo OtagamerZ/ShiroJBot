26ea85b1641363200cdfba82d6cedf5850417cc5
Ygimenez <yagogimenez1@hotmail.com>
2025-10-27 11:25:15 -0300

    Continuation.

--- src/main/java/com/kuuhaku/model/records/dunhun/CombatContext.java
+++ src/main/java/com/kuuhaku/model/records/dunhun/CombatContext.java
@@ -6,4 +6,7 @@ import com.kuuhaku.model.enums.shoukan.Trigger;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public record CombatContext(Trigger trigger, Actor<?> source, Actor<?> target, AtomicInteger value) {
+	public boolean isEnemy() {
+		return source.getTeam() != target.getTeam();
+	}
 }
