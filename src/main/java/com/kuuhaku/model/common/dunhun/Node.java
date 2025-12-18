3a67a4d54d2bdf11efd78322880513eae9f9a6c4
Ygimenez <yagogimenez1@hotmail.com>
2025-12-18 11:57:25 -0300

    Continuation.

--- src/main/java/com/kuuhaku/model/common/dunhun/Node.java
+++ src/main/java/com/kuuhaku/model/common/dunhun/Node.java
@@ -123,9 +123,11 @@ public class Node {
 	public int travelDistance(Node node) {
 		if (node == null || equals(node)) return 0;
 
-		for (Node parent : parents) {
-			int dist = parent.travelDistance(node) + 1;
-			if (dist > 0) return dist;
+		if (sublevel.getSublevel() > node.getSublevel().getSublevel()) {
+			for (Node parent : parents) {
+				int dist = parent.travelDistance(node) + 1;
+				if (dist > 0) return dist;
+			}
 		}
 
 		return -1;
