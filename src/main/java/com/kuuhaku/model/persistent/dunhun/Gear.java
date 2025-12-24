c0b2388d6fcf8f15f6165c276dfc51b42e025cc1
Ygimenez <yagogimenez1@hotmail.com>
2025-12-24 20:46:45 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/dunhun/Gear.java
+++ src/main/java/com/kuuhaku/model/persistent/dunhun/Gear.java
@@ -117,9 +117,12 @@ public class Gear extends DAO<Gear> {
 	}
 
 	public int getReqLevel() {
-		int minAffix = affixes.parallelStream()
-				.mapToInt(ga -> ga.getAffix().getMinLevel())
-				.max().orElse(0);
+		int minAffix = 0;
+		if (getRarityClass() != RarityClass.UNIQUE) {
+			minAffix = affixes.parallelStream()
+					.mapToInt(ga -> ga.getAffix().getMinLevel())
+					.max().orElse(0);
+		}
 
 		return Math.max(minAffix, basetype.getStats().requirements().level());
 	}
