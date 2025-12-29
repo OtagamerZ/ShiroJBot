b63edbcaf44dc73b637e5dff64a4c5b4546574f9
Ygimenez <yagogimenez1@hotmail.com>
2025-12-29 14:40:58 -0300

    Continuation.

--- src/main/java/com/kuuhaku/model/common/dunhun/Node.java
+++ src/main/java/com/kuuhaku/model/common/dunhun/Node.java
@@ -292,7 +292,7 @@ public class Node {
 				}
 
 				g2d.setStroke(new BasicStroke(strokeWidth, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 1, new float[]{17}, 0));
-				g2d.drawPolyline(arrX, arrY, 3);
+				g2d.drawPolyline(arrX, arrY, arrX.length);
 			} else {
 				g2d.setColor(Color.BLACK);
 				g2d.setStroke(new BasicStroke(strokeWidth + 3, BasicStroke.CAP_ROUND, BasicStroke.JOIN_MITER, 1));
