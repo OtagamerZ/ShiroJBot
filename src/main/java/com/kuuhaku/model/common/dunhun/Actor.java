8d0d093a02628f7db8e714cf55b96e56c471b419
Ygimenez <yagogimenez1@hotmail.com>
2025-12-30 17:27:38 -0300

    Continuation.

--- src/main/java/com/kuuhaku/model/common/dunhun/Actor.java
+++ src/main/java/com/kuuhaku/model/common/dunhun/Actor.java
@@ -393,16 +393,11 @@ public abstract class Actor<T extends Actor<T>> extends DAO<T> {
 		for (EffectBase e : queue) {
 			if (e.isLocked()) continue;
 			else if (e instanceof TriggeredEffect te) {
-				System.out.println(te);
-				System.out.println(trigger);
-				System.out.println(Arrays.toString(te.getTriggers()));
-
 				if (!Utils.equalsAny(trigger, te.getTriggers())) continue;
 				te.decLimit();
 			}
 
 			try {
-				System.out.println("exec");
 				e.lock();
 				e.getEffect().accept(e, context);
 			} finally {
