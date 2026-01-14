586f4acd69bb0827b89c1ebad2e3e4fea47a83d5
Ygimenez <yagogimenez1@hotmail.com>
2026-01-14 13:35:40 -0300

    Continuation.

--- src/main/java/com/kuuhaku/command/misc/TitleLocaleCommand.java
+++ src/main/java/com/kuuhaku/command/misc/TitleLocaleCommand.java
@@ -32,7 +32,7 @@ import net.dv8tion.jda.api.JDA;
 @Command(
 		name = "title",
 		path = "locale",
-		category = Category.MODERATION
+		category = Category.MISC
 )
 @Syntax("<locale:word>")
 public class TitleLocaleCommand implements Executable {
