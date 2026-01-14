c202957b5c1bc8ebfe64c702acb7f1e625682c60
Ygimenez <yagogimenez1@hotmail.com>
2026-01-14 11:59:04 -0300

    Continuation.

--- src/main/java/com/kuuhaku/model/records/dunhun/Loot.java
+++ src/main/java/com/kuuhaku/model/records/dunhun/Loot.java
@@ -1,13 +1,10 @@
 package com.kuuhaku.model.records.dunhun;
 
-import com.kuuhaku.model.common.XStringBuilder;
 import com.kuuhaku.model.enums.I18N;
 import com.kuuhaku.model.persistent.dunhun.Gear;
 import com.kuuhaku.model.persistent.user.UserItem;
-import com.kuuhaku.util.Utils;
 import org.apache.commons.collections4.Bag;
 import org.apache.commons.collections4.bag.TreeBag;
-import org.jspecify.annotations.NonNull;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -29,13 +26,5 @@ public record Loot(I18N locale, List<Gear> gear, Bag<UserItem> items, AtomicInte
 		xp.addAndGet(lt.xp.get());
 	}
 
-	@Override
-	public @NonNull String toString() {
-		XStringBuilder sb = new XStringBuilder();
-		for (Gear g : gear) {
-			sb.appendNewLine("-# " + g.getName(ctx.game.locale))
-		}
 
-		Utils.properlyJoin(ctx.game.locale, names)
-	}
 }
