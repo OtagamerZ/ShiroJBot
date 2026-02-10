2460817f5c33b24e19e10dc5fdac19f1f53aba0f
Ygimenez <yagogimenez1@hotmail.com>
2026-02-10 11:06:49 -0300

    Continuation

---
+++ src/main/resources/database/triggers/remove_dungeon_player.sql
@@ -0,0 +1,36 @@
+/*
+ * This file is part of Shiro J Bot.
+ * Copyright (C) 2019-2023  Yago Gimenez (KuuHaKu)
+ *
+ * Shiro J Bot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shiro J Bot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Shiro J Bot.  If not, see <https://www.gnu.org/licenses/>
+ */
+
+CREATE OR REPLACE FUNCTION t_remove_dungeon_player()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    DELETE FROM dungeon_run_player WHERE hero_id = OLD.id;
+
+    RETURN NEW;
+END;
+$$;
+
+DROP TRIGGER IF EXISTS remove_dungeon_player ON card;
+CREATE TRIGGER remove_dungeon_player
+    BEFORE DELETE
+    ON hero
+    FOR EACH ROW
+EXECUTE PROCEDURE t_remove_dungeon_player();
