91db4863cb7934f4d3c5c2d59a4cfadfbb33bcf6
Ygimenez <yagogimenez1@hotmail.com>
2026-02-23 10:54:41 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/records/dunhun/Attributes.java
+++ src/main/java/com/kuuhaku/model/records/dunhun/Attributes.java
@@ -37,10 +37,10 @@ public record Attributes(@Column(name = "attributes", nullable = false) int attr
 	}
 
 	public Attributes(int str, int dex, int wis, int vit) {
-		this((Calc.clamp(str, Byte.MIN_VALUE, Byte.MAX_VALUE) & 0xFF)
-			 | (Calc.clamp(dex, Byte.MIN_VALUE, Byte.MAX_VALUE) & 0xFF) << 8
-			 | (Calc.clamp(wis, Byte.MIN_VALUE, Byte.MAX_VALUE) & 0xFF) << 16
-			 | (Calc.clamp(vit, Byte.MIN_VALUE, Byte.MAX_VALUE) & 0xFF) << 24
+		this((Calc.clamp(str, 0, 255) & 0xFF)
+				| (Calc.clamp(dex, 0, 255) & 0xFF) << 8
+				| (Calc.clamp(wis, 0, 255) & 0xFF) << 16
+				| (Calc.clamp(vit, 0, 255) & 0xFF) << 24
 		);
 	}
 	/*
@@ -51,23 +51,23 @@ public record Attributes(@Column(name = "attributes", nullable = false) int attr
 	   └ vitality
 	 */
 
-	public byte str() {
-		return (byte) Bit32.get(attributes, 0, 8);
+	public int str() {
+		return Bit32.get(attributes, 0, 8);
 	}
 
-	public byte dex() {
-		return (byte) Bit32.get(attributes, 1, 8);
+	public int dex() {
+		return Bit32.get(attributes, 1, 8);
 	}
 
-	public byte wis() {
-		return (byte) Bit32.get(attributes, 2, 8);
+	public int wis() {
+		return Bit32.get(attributes, 2, 8);
 	}
 
-	public byte vit() {
-		return (byte) Bit32.get(attributes, 3, 8);
+	public int vit() {
+		return Bit32.get(attributes, 3, 8);
 	}
 
-	public byte get(AttrType type) {
+	public int get(AttrType type) {
 		return switch (type) {
 			case STR -> str();
 			case DEX -> dex();
