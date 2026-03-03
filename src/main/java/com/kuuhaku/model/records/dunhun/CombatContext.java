c7f4c98157921be9fb4a6fd1b0a2985cde8f17bb
Ygimenez <yagogimenez1@hotmail.com>
2026-03-03 11:27:26 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/records/dunhun/CombatContext.java
+++ src/main/java/com/kuuhaku/model/records/dunhun/CombatContext.java
@@ -3,8 +3,12 @@ package com.kuuhaku.model.records.dunhun;
 import com.kuuhaku.interfaces.dunhun.Usable;
 import com.kuuhaku.model.common.dunhun.Actor;
 import com.kuuhaku.model.common.dunhun.Combat;
+import com.kuuhaku.model.common.dunhun.MonsterBase;
 import com.kuuhaku.model.enums.shoukan.Trigger;
+import com.kuuhaku.model.persistent.dunhun.Skill;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -14,7 +18,8 @@ public record CombatContext(
 		Actor<?> source,
 		AtomicReference<Actor<?>> target,
 		Usable usable,
-		AtomicInteger value
+		AtomicInteger value,
+		Set<String> tags
 ) {
 	public CombatContext(Combat combat, Trigger trigger) {
 		this(combat, trigger, null, (Actor<?>) null, null, null);
@@ -22,6 +27,22 @@ public record CombatContext(
 
 	public CombatContext(Combat combat, Trigger trigger, Actor<?> source, Actor<?> target, Usable usable, AtomicInteger value) {
 		this(combat, trigger, source, new AtomicReference<>(target), usable, value);
+	}
+
+	public CombatContext(Combat combat, Trigger trigger, Actor<?> source, AtomicReference<Actor<?>> target, Usable usable, AtomicInteger value) {
+		this(combat, trigger, source, target, usable, value, new HashSet<>());
+
+		if (usable instanceof Skill s) {
+			for (Object tag : s.getStats().getTags()) {
+				tags.add(tag.toString());
+			}
+		}
+
+		if (source instanceof MonsterBase<?> m) {
+			for (Object tag : m.getStats().getTags()) {
+				tags.add(tag.toString());
+			}
+		}
 	}
 
 	public boolean isEnemy() {
