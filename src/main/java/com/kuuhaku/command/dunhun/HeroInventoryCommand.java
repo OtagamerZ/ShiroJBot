491ec91afeb082c60de877fce543e386137f2632
Ygimenez <yagogimenez1@hotmail.com>
2026-09-16 14:14:54 -0300

    Continuation

--- src/main/java/com/kuuhaku/command/dunhun/HeroInventoryCommand.java
+++ src/main/java/com/kuuhaku/command/dunhun/HeroInventoryCommand.java
@@ -59,7 +59,7 @@ public class HeroInventoryCommand implements Executable {
 				.setAuthor(locale.get("str/hero_inventory", h.getName(), equips.size(), h.getInventoryCapacity()));
 
 		List<Page> pages = Utils.generatePages(eb, equips, 10, 5,
-				g -> "`%d` - %s %s\n-# %s (lv: $s)\n".formatted(
+				g -> "`%d` - %s %s\n-# %s (ilvl: $s)\n".formatted(
 						g.getId(), g.getBasetype().getIcon(), g.getName(locale),
 						g.getBasetype().getInfo(locale).getName(), g.getItemLevel()
 				),
