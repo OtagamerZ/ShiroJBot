fdf62f0ad2da6c033fb7ee15228e32877a5aff8b
Ygimenez <yagogimenez1@hotmail.com>
2026-09-23 17:39:03 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/dunhun/Hero.java
+++ src/main/java/com/kuuhaku/model/persistent/dunhun/Hero.java
@@ -166,7 +166,7 @@ public class Hero extends Actor<Hero> {
 			crit = 1 - (1 - crit) * (1 - flat);
 		}
 
-		double bonus = stats.getRaceBonus().critical();
+		double bonus = getModifiers().getCritical(stats.getRaceBonus().critical());
 		return Math.clamp(crit * 100 + bonus, 0, 100);
 	}
 
