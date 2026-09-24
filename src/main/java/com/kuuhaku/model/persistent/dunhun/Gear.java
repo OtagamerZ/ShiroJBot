aa576ab1810d0aead0593d4ae77411236e14af62
Ygimenez <yagogimenez1@hotmail.com>
2026-09-24 11:49:09 -0300

    Continuation

--- src/main/java/com/kuuhaku/model/persistent/dunhun/Gear.java
+++ src/main/java/com/kuuhaku/model/persistent/dunhun/Gear.java
@@ -381,7 +381,7 @@ public class Gear extends DAO<Gear> {
 	}
 
 	public int getMaxAmmo(Actor<?> owner) {
-		int flat = (int) Math.max(1, modifiers.getMaxAmmo().apply(5));
+		int flat = (int) Math.max(1, modifiers.getMaxAmmo().apply(6));
 		if (owner != null) {
 			return (int) Math.max(1, owner.getModifiers().getMaxAmmo(flat));
 		}
